b7692581a959be50bea7532655cdfc40a69a51e8
panini4552 <panini4552@gmail.com>
2025-11-19 20:46:47 +0900

    Add files via upload

---
+++ data/Classic/Gold.js
@@ -0,0 +1,13 @@
+// data.js — ZIP方式用カードデータ
+const cards = [
+  // === エルフ ===
+  {
+    id: "",
+    name: "",
+    reading: ["", ""],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/  /.zip"
+  },
+ ];

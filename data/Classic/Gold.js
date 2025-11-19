974df92186a23387d59616b4ea21b960127017cc
panini4552 <panini4552@gmail.com>
2025-11-19 23:30:17 +0900

    Update Gold.js

--- data/Classic/Gold.js
+++ data/Classic/Gold.js
@@ -1,16 +1,116 @@
 // data/Classic/Gold.js
 
 export const Classic_Gold = [
-  // === エルフ ===
+const cards = [
   {
-    id: "",
-    name: "",
-    reading: ["", ""],
-    class: "",
+    id: "Rhinoceroach",
+    name: "リノセウス",
+    reading: ["りのせうす", "リノセウス"],
+    class: "エルフ",
     rarity: "Gold",
     pack: "Classic",
-    zip: "voices/Classic/Gold//.zip"
+    zip: "voices/Classic/Gold/Rhinoceroach/Rhinoceroach.zip"
   },
+  {
+    id: "Dragonewt_Scholar",
+    name: "ドラゴニュートスカラー",
+    reading: ["どらごにゅーとすからー", "ドラゴニュートスカラー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Dragonewt_Scholar/Dragonewt_Scholar.zip"
+  },
+  {
+    id: "Shapeshifting_Mage",
+    name: "変化する魔術師",
+    reading: ["へんかするまじゅつし", "ヘンカスルマジュツシ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Shapeshifting_Mage/Shapeshifting_Mage.zip"
+  },
+  {
+    id: "Gemstaff_Commander",
+    name: "宝杖の司令官",
+    reading: ["ほうじょうのしれいかん", "ホウジョウノシレイカン"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Gemstaff_Commander/Gemstaff_Commander.zip"
+  },
+  {
+    id: "Madame_Lich",
+    name: "マダムリッチ",
+    reading: ["まだむりっち", "マダムリッチ"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Madame_Lich/Madame_Lich.zip"
+  },
+  {
+    id: "Demon_Commander_Laura",
+    name: "デモンコマンダー・ラウラ",
+    reading: ["でもんこまんだーらうら", "デモンコマンダーラウラ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Demon_Commander_Laura/Demon_Commander_Laura.zip"
+  },
+  {
+    id: "Urd",
+    name: "ウルズ",
+    reading: ["うるず", "ウルズ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Urd/Urd.zip"
+  },
+  {
+    id: "Petal_Fencer",
+    name: "ペタルフェンサー",
+    reading: ["ぺたるふぇんさー", "ペタルフェンサー"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Petal_Fencer/Petal_Fencer.zip"
+  },
+  {
+    id: "Shadowed_Assassin",
+    name: "闇を纏う暗殺者",
+    reading: ["やみをまとうあんさつしゃ", "ヤミヲマトウアンサツシャ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Shadowed_Assassin/Shadowed_Assassin.zip"
+  },
+  {
+    id: "Latham,_Vanguard_Captain",
+    name: "ヴァンガード・レイサム",
+    reading: ["ゔぁんがーどれいさむ", "ヴァンガードレイサム"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Latham,_Vanguard_Captain/Latham,_Vanguard_Captain.zip"
+  },
+  {
+    id: "Deathmist_Dragon",
+    name: "デスミストドラゴン",
+    reading: ["ですみすとどらごん", "デスミストドラゴン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Deathmist_Dragon/Deathmist_Dragon.zip"
+  },
+  {
+    id: "Wight_King",
+    name: "ワイトキング",
+    reading: ["わいときんぐ", "ワイトキング"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "Classic",
+    zip: "voices/Classic/Gold/Wight_King/Wight_King.zip"
+  }
 ];
+
 
 

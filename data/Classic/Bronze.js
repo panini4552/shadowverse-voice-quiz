7aa1dae7fe5ccf56e04f7c487b872c8dda6be658
panini4552 <panini4552@gmail.com>
2025-11-19 20:49:02 +0900

    Add files via upload

---
+++ data/Classic/Bronze.js
@@ -0,0 +1,13 @@
+// data.js — ZIP方式用カードデータ
+const cards = [
+  // === エルフ ===
+  {
+    id: "",
+    name: "",
+    reading: ["", ""],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/  /.zip"
+  },
+ ];
---
+++ data/Classic/Silver.js
@@ -0,0 +1,13 @@
+// data.js — ZIP方式用カードデータ
+const cards = [
+  // === エルフ ===
+  {
+    id: "",
+    name: "",
+    reading: ["", ""],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/  /.zip"
+  },
+ ];

ef308ceaa14c9719db6d9859ab35a8569a942461
panini4552 <panini4552@gmail.com>
2025-11-21 17:32:37 +0900

    Update Bronze.js

--- data/Classic/Bronze.js
+++ data/Classic/Bronze.js
@@ -3,12 +3,785 @@
 export const Classic_Bronze = [
   // === エルフ ===
   {
-    id: "",
-    name: "",
-    reading: ["", ""],
+    id: "Baby_Elf_May",
+    name: "ベビーエルフ・メイ",
+    reading: ["べびーえるふめい", "ベビーエルフメイ"],
     class: "エルフ",
     rarity: "Bronze",
     pack: "Classic",
-    zip: "voices/Classic/Bronze//.zip"
+    zip: "voices/Classic/Bronze/Baby_Elf_May.zip"
+  },
+  {
+    id: "Brave_Fairy",
+    name: "ブレイブフェアリー",
+    reading: ["ぶれいぶふぇありー", "ブレイブフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Brave_Fairy.zip"
+  },
+  {
+    id: "Dungeon_Fairy",
+    name: "ダンジョンフェアリー",
+    reading: ["だんじょんふぇありー", "ダンジョンフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Dungeon_Fairy.zip"
+  },
+  {
+    id: "Fairy_Bringer",
+    name: "フェアリーブリンガー",
+    reading: ["ふぇありーぶりんがー", "フェアリーブリンガー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Fairy_Bringer.zip"
+  },
+  {
+    id: "Wonder_Elf_Mage",
+    name: "ワンダーエルフメイジ",
+    reading: ["わんだーえるふめいじ", "ワンダーエルフメイジ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Wonder_Elf_Mage.zip"
+  },
+  {
+    id: "Wandering_Elf",
+    name: "放浪するエルフ",
+    reading: ["ほうろうするえるふ", "ホウロウスルエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Wandering_Elf.zip"
+  },
+  {
+    id: "Archer",
+    name: "アーチャー",
+    reading: ["あーちゃー", "アーチャー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Archer.zip"
+  },
+  {
+    id: "Swamp_Spirit",
+    name: "沼の精",
+    reading: ["ぬまのせい", "ヌマノセイ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Swamp_Spirit.zip"
+  },
+  {
+    id: "Fairy_Caster",
+    name: "フェアリーキャスター",
+    reading: ["ふぇありーきゃすたー", "フェアリーキャスター"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Fairy_Caster.zip"
+  },
+  // === ロイヤル ===
+  {
+    id: "Ninja_Egg",
+    name: "ニンジャエッグ",
+    reading: ["にんじゃえっぐ", "ニンジャエッグ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Ninja_Egg.zip"
+  },
+  {
+    id: "Vanguard",
+    name: "ヴァンガード",
+    reading: ["ゔぁんがーど", "ヴァンガード"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Vanguard.zip"
+  },
+  {
+    id: "Indomitable_Soldier",
+    name: "不屈の兵士",
+    reading: ["ふくつのへいし", "フクツノヘイシ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Indomitable_Soldier.zip"
+  },
+  {
+    id: "Veteran_Lancer",
+    name: "歴戦のランサー",
+    reading: ["れきせんのらんさー", "レキセンノランサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Veteran_Lancer.zip"
+  },
+  {
+    id: "Palace_Fencer",
+    name: "パレスフェンサー",
+    reading: ["ぱれすふぇんさー", "パレスフェンサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Palace_Fencer.zip"
+  },
+  {
+    id: "Cyclone_Soldier",
+    name: "サイクロンソルジャー",
+    reading: ["さいくろんそるじゃー", "サイクロンソルジャー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Cyclone_Soldier.zip"
+  },
+  {
+    id: "Centaur_Vanguard",
+    name: "ケンタウロスヴァンガード",
+    reading: ["けんたうろすゔぁんがーど", "ケンタウロスヴァンガード"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Centaur_Vanguard.zip"
+  },
+  {
+    id: "Fencer",
+    name: "フェンサー",
+    reading: ["ふぇんさー", "フェンサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Fencer.zip"
+  },
+  {
+    id: "Novice_Trooper",
+    name: "ノーヴィストルーパー",
+    reading: ["のーゔぃすとるーぱー", "ノーヴィストルーパー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Novice_Trooper.zip"
+  },
+  {
+    id: "Snippy_Gardener",
+    name: "スニッピーガーデナー",
+    reading: ["すにっぴーがーでなー", "スニッピーガーデナー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Snippy_Gardener.zip"
+  },
+  {
+    id: "Valiant_Knight",
+    name: "勇猛たる騎士",
+    reading: ["ゆうもうたるきし", "ユウモウタルキシ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Valiant_Knight.zip"
+  },
+  {
+    id: "Sentry_Knight",
+    name: "セントリーナイト",
+    reading: ["せんとりーないと", "セントリーナイト"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Sentry_Knight.zip"
+  },
+  {
+    id: "Battlefield_Cavalry",
+    name: "戦場の騎兵",
+    reading: ["せんじょうのきへい", "センジョウノキヘイ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Battlefield_Cavalry.zip"
+  },
+  {
+    id: "Admiral",
+    name: "アドミラル",
+    reading: ["あどみらる", "アドミラル"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Admiral.zip"
+  },
+  {
+    id: "Giant_Slayer",
+    name: "ジャイアントスレイヤー",
+    reading: ["じゃいあんとすれいやー", "ジャイアントスレイヤー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Giant_Slayer.zip"
+  },
+  // === ウィッチ ===
+  {
+    id: "Craft_Warlock",
+    name: "クラフトウォーロック",
+    reading: ["くらふとうぉーろっく", "クラフトウォーロック"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Craft_Warlock.zip"
+  },
+  {
+    id: "Rookie_Alchemist",
+    name: "ルーキーアルケミスト",
+    reading: ["るーきーあるけみすと", "ルーキーアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rookie_Alchemist.zip"
+  },
+  {
+    id: "Rune_Guardian",
+    name: "ルーンガーディアン",
+    reading: ["るーんがーでぃあん", "ルーンガーディアン"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rune_Guardian.zip"
+  },
+  {
+    id: "Gauntlet_Healer",
+    name: "ガントレットヒーラー",
+    reading: ["がんとれっとひーらー", "ガントレットヒーラー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Gauntlet_Healer.zip"
+  },
+  {
+    id: "Barrier_Mage",
+    name: "結界魔術師",
+    reading: ["けっかいまじゅつし", "ケッカイマジュツシ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Barrier_Mage.zip"
+  },
+  {
+    id: "Senior_Alchemist",
+    name: "上級アルケミスト",
+    reading: ["じょうきゅうあるけみすと", "ジョウキュウアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Senior_Alchemist.zip"
+  },
+  {
+    id: "Storm_Mage",
+    name: "旋風の魔術師",
+    reading: ["せんぷうのまじゅつし", "センプウノマジュツシ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Storm_Mage.zip"
+  },
+  {
+    id: "Master_Alchemist",
+    name: "マスターアルケミスト",
+    reading: ["ますたーあるけみすと", "マスターアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Master_Alchemist.zip"
+  },
+  {
+    id: "Grudge_Witch",
+    name: "怨恨の魔女",
+    reading: ["えんこんのまじょ", "エンコンノマジョ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Grudge_Witch.zip"
+  },
+    // === ドラゴン ===
+  {
+    id: "Storm_Dragon",
+    name: "大嵐のドラゴン",
+    reading: ["おおあらしのどらごん", "オオアラシノドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Storm_Dragon.zip"
+  },
+  {
+    id: "Sandstorm_Dragon",
+    name: "サンドストームドラゴン",
+    reading: ["さんどすとーむどらごん", "サンドストームドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Sandstorm_Dragon.zip"
+  },
+  {
+    id: "Fire_Lizard",
+    name: "ファイアーリザード",
+    reading: ["ふぁいあーりざーど", "ファイアーリザード"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Fire_Lizard.zip"
+  },
+  {
+    id: "Dragon_Knight_Ayla",
+    name: "ドラゴンナイト・アイラ",
+    reading: ["どらごんないとあいら", "ドラゴンナイトアイラ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Dragon_Knight_Ayla.zip"
+  },
+  {
+    id: "Twin_Head_Dragon",
+    name: "ツインヘッドドラゴン",
+    reading: ["ついんへっどどらごん", "ツインヘッドドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Twin_Head_Dragon.zip"
+  },
+  {
+    id: "Rukh_Bird",
+    name: "ルフ鳥",
+    reading: ["るふちょう", "ルフチョウ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rukh_Bird.zip"
+  },
+  {
+    id: "Trinity_Dragon",
+    name: "トリニティドラゴン",
+    reading: ["とりにてぃどらごん", "トリニティドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Trinity_Dragon.zip"
+  },
+  {
+    id: "Glint_Dragon",
+    name: "グリントドラゴン",
+    reading: ["ぐりんとどらごん", "グリントドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Glint_Dragon.zip"
+  },
+  {
+    id: "Sea_Sword_Dragon",
+    name: "海剣竜",
+    reading: ["かいけんりゅう", "カイケンリュウ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Sea_Sword_Dragon.zip"
+  },
+  {
+    id: "Skull_Dragon",
+    name: "スカルドラゴン",
+    reading: ["すかるどらごん", "スカルドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Skull_Dragon.zip"
+  },
+  {
+    id: "Frost_Dragon",
+    name: "氷結のドラゴン",
+    reading: ["ひょうけつのどらごん", "ヒョウケツノドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Frost_Dragon.zip"
+  },
+  {
+    id: "Inferno_Dragon",
+    name: "インフェルノドラゴン",
+    reading: ["いんふぇるのどらごん", "インフェルノドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Inferno_Dragon.zip"
+  },
+  // === ヴァンパイア ===
+  {
+    id: "Crawling_Dead",
+    name: "蠢く死霊",
+    reading: ["うごめくしりょう", "ウゴメクシリョウ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Crawling_Dead.zip"
+  },
+  {
+    id: "Lilim",
+    name: "リリム",
+    reading: ["りりむ", "リリム"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Lilim.zip"
+  },
+  {
+    id: "Venom_Cobra",
+    name: "ヴェノムコブラ",
+    reading: ["ヴぇのむこぶら", "ヴェノムコブラ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Venom_Cobra.zip"
+  },
+  {
+    id: "Rainy_Devil",
+    name: "レイニーデビル",
+    reading: ["れいにーでびる", "レイニーデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rainy_Devil.zip"
+  },
+  {
+    id: "Blood_Wolf",
+    name: "ブラッドウルフ",
+    reading: ["ぶらっどうるふ", "ブラッドウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Blood_Wolf.zip"
+  },
+  {
+    id: "Metal_Gargoyle",
+    name: "メタルガーゴイル",
+    reading: ["めたるがーごいる", "メタルガーゴイル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Metal_Gargoyle.zip"
+  },
+  {
+    id: "Killer_Devil",
+    name: "キラーデビル",
+    reading: ["きらーでびる", "キラーデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Killer_Devil.zip"
+  },
+  {
+    id: "Demon_Hand_Assassin",
+    name: "デモンハンドアサシン",
+    reading: ["でもんはんどあさしん", "デモンハンドアサシン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Demon_Hand_Assassin.zip"
+  },
+  {
+    id: "Demon_Sniper",
+    name: "デモンスナイパー",
+    reading: ["でもんすないぱー", "デモンスナイパー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Demon_Sniper.zip"
+  },
+  {
+    id: "Succubus",
+    name: "サキュバス",
+    reading: ["さきゅばす", "サキュバス"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Succubus.zip"
+  },
+  {
+    id: "Giant_of_Rage",
+    name: "憤怒の巨人",
+    reading: ["ふんどのきょじん", "フンドノキョジン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Giant_of_Rage.zip"
+  },
+  {
+    id: "Barbaric_Demon",
+    name: "バーバリックデーモン",
+    reading: ["ばーばりっくでーもん", "バーバリックデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Barbaric_Demon.zip"
+  },
+  {
+    id: "Greedy_Beast",
+    name: "強欲な魔獣",
+    reading: ["ごうよくなまじゅう", "ゴウヨクナマジュウ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Greedy_Beast.zip"
+  },
+  {
+    id: "Rampage_Giant",
+    name: "ランページジャイアント",
+    reading: ["らんぺーじじゃいあんと", "ランページジャイアント"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rampage_Giant.zip"
+  },
+  // === ネクロマンサー ===
+  {
+    id: "Skull_Beast",
+    name: "スカルビースト",
+    reading: ["すかるびーすと", "スカルビースト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Skull_Beast.zip"
+  },
+  {
+    id: "Skeleton_Viper",
+    name: "スケルトンヴァイパー",
+    reading: ["すけるとんゔぁいぱー", "スケルトンヴァイパー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Skeleton_Viper.zip"
+  },
+  {
+    id: "Skull_Widow",
+    name: "スカルウィドウ",
+    reading: ["すかるうぃどう", "スカルウィドウ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Skull_Widow.zip"
+  },
+  {
+    id: "Wight",
+    name: "ワイト",
+    reading: ["わいと", "ワイト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Wight.zip"
+  },
+  {
+    id: "Lesser_Mummy",
+    name: "レッサーマミー",
+    reading: ["れっさーまみー", "レッサーマミー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Lesser_Mummy.zip"
+  },
+  {
+    id: "Clinging_Ghost",
+    name: "纏わりつく亡霊",
+    reading: ["まとわりつくぼうれい", "マトワリツクボウレイ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Clinging_Ghost.zip"
+  },
+  {
+    id: "Rabbit_Necromancer",
+    name: "ラビットネクロマンサー",
+    reading: ["らびっとねくろまんさー", "ラビットネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rabbit_Necromancer.zip"
+  },
+  {
+    id: "Skeleton_Knight",
+    name: "スケルトンナイト",
+    reading: ["すけるとんないと", "スケルトンナイト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Skeleton_Knight.zip"
+  },
+  {
+    id: "Spartoi",
+    name: "スパルトイ",
+    reading: ["すぱるとい", "スパルトイ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Spartoi.zip"
+  },
+  {
+    id: "Guilty_Courier",
+    name: "ギルティクーリエ",
+    reading: ["ぎるてぃくーりえ", "ギルティクーリエ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Guilty_Courier.zip"
+  },
+  {
+    id: "Soul_Eater",
+    name: "ソウルイーター",
+    reading: ["そうるいーたー", "ソウルイーター"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Soul_Eater.zip"
+  },
+  {
+    id: "Cursed_Soldier",
+    name: "カースドソルジャー",
+    reading: ["かーすどそるじゃー", "カースドソルジャー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Cursed_Soldier.zip"
+  },
+  {
+    id: "Orcus",
+    name: "オルクス",
+    reading: ["おるくす", "オルクス"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Orcus.zip"
+  },
+  // === ビショップ ===
+  {
+    id: "Rabbit_Healer",
+    name: "ラビットヒーラー",
+    reading: ["らびっとひーらー", "ラビットヒーラー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Rabbit_Healer.zip"
+  },
+  {
+    id: "Ardent_Sister",
+    name: "アーデントシスター",
+    reading: ["あーでんとしすたー", "アーデントシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Ardent_Sister.zip"
+  },
+  {
+    id: "Mainyu",
+    name: "マイニュ",
+    reading: ["まいにゅ", "マイニュ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Mainyu.zip"
+  },
+  {
+    id: "Novice_Sister",
+    name: "見習いシスター",
+    reading: ["みならいしすたー", "ミナライシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Novice_Sister.zip"
+  },
+  {
+    id: "Dark_Priest",
+    name: "ダークプリースト",
+    reading: ["だーくぷりーすと", "ダークプリースト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Dark_Priest.zip"
+  },
+  {
+    id: "Guardian_Sister",
+    name: "ガーディアンシスター",
+    reading: ["がーでぃあんしすたー", "ガーディアンシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Guardian_Sister.zip"
+  },
+  {
+    id: "Pious_Nun",
+    name: "敬虔な修道女",
+    reading: ["けいけんなしゅうどうじょ", "ケイケンナシュウドウジョ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Pious_Nun.zip"
+  },
+  {
+    id: "Heal_Priest",
+    name: "ヒールプリースト",
+    reading: ["ひーるぷりーすと", "ヒールプリースト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Heal_Priest.zip"
+  },
+  {
+    id: "Divine_Armor",
+    name: "ディバインアーマー",
+    reading: ["でぃばいんあーまー", "ディバインアーマー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Divine_Armor.zip"
+  },
+  {
+    id: "Mask_Giant",
+    name: "マスクジャイアント",
+    reading: ["ますくじゃいあんと", "マスクジャイアント"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Mask_Giant.zip"
+  },
+  // === ニュートラル ===
+  {
+    id: "Minotaur",
+    name: "ミノタウロス",
+    reading: ["みのたうろす", "ミノタウロス"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Minotaur.zip"
+  },
+  {
+    id: "Shield_Angel",
+    name: "シールドエンジェル",
+    reading: ["しーるどえんじぇる", "シールドエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Shield_Angel.zip"
+  },
+  {
+    id: "Healing_Angel",
+    name: "ヒーリングエンジェル",
+    reading: ["ひーりんぐえんじぇる", "ヒーリングエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Healing_Angel.zip"
+  },
+  {
+    id: "Desert_Rider",
+    name: "デザートライダー",
+    reading: ["でざーとらいだー", "デザートライダー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Classic",
+    zip: "voices/Classic/Bronze/Desert_Rider.zip"
   },
 ];
+

ebd9884c633f996061666cf870ffcb2cb8d5ac8d
panini4552 <panini4552@gmail.com>
2025-11-21 20:00:41 +0900

    Update Bronze.js

--- data/Classic/Bronze.js
+++ data/Classic/Bronze.js
@@ -311,7 +311,7 @@ export const Classic_Bronze = [
   class: "ドラゴン",
   rarity: "Bronze",
   pack: "Classic",
-  tags: ["dragon_voice"]
+  tags: ["dragon_voice"],
   zip: "voices/Classic/Bronze/Sandstorm_Dragon.zip"
 },
 {
@@ -321,7 +321,7 @@ export const Classic_Bronze = [
   class: "ドラゴン",
   rarity: "Bronze",
   pack: "Classic",
-  tags: ["dragon_voice"]
+  tags: ["dragon_voice"],
   zip: "voices/Classic/Bronze/Fire_Lizard.zip"
 },
 {
@@ -340,7 +340,7 @@ export const Classic_Bronze = [
   class: "ドラゴン",
   rarity: "Bronze",
   pack: "Classic",
-  tags: ["dragon_voice"]
+  tags: ["dragon_voice"],
   zip: "voices/Classic/Bronze/Twin-Headed_Dragon.zip"
 },
 {
@@ -369,7 +369,7 @@ export const Classic_Bronze = [
   class: "ドラゴン",
   rarity: "Bronze",
   pack: "Classic",
-  tags: ["dragon_voice"]
+  tags: ["dragon_voice"],
   zip: "voices/Classic/Bronze/Glint_Dragon.zip"
 },
 {
@@ -389,7 +389,7 @@ export const Classic_Bronze = [
   class: "ドラゴン",
   rarity: "Bronze",
   pack: "Classic",
-  tags: ["dragon_voice"]
+  tags: ["dragon_voice"],
   zip: "voices/Classic/Bronze/Skull_Dragon.zip"
 },
 {
@@ -788,6 +788,7 @@ export const Classic_Bronze = [
   zip: "voices/Classic/Bronze/Desert_Rider.zip"
 },
 ];
+
 
 
 

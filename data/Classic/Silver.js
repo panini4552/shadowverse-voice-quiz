a7a028699a7e98274acd73ee498b559f99206852
panini4552 <panini4552@gmail.com>
2025-11-21 14:35:45 +0900

    Update Silver.js

--- data/Classic/Silver.js
+++ data/Classic/Silver.js
@@ -3,13 +3,104 @@
 export const Classic_Silver = [
   // === エルフ ===
   {
-    id: "",
-    name: "",
-    reading: ["", ""],
+    id: "Blessed_Fairy_Dancer",
+    name: "ブレスフェアリーダンサー",
+    reading: ["ぶれすふぇありーだんさー", "ブレスフェアリーダンサー"],
     class: "エルフ",
     rarity: "Silver",
     pack: "Classic",
-    zip: "voices/Classic/Silver//.zip"
+    zip: "voices/Classic/Silver/Blessed_Fairy_Dancer.zip"
+  },
+  {
+    id: "Waltzing_Fairy",
+    name: "ワルツフェアリー",
+    reading: ["わるつふぇありー", "ワルツフェアリー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Waltzing_Fairy.zip"
+  },
+  {
+    id: "Tam_Lin",
+    name: "タムリン",
+    reading: ["たむりん", "タムリン"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Tam_Lin.zip"
+  },
+  {
+    id: "Carnivorous_Flower",
+    name: "カニバルフラワー",
+    reading: ["かにばるふらわー", "カニバルフラワー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Carnivorous_Flower.zip"
+  },
+  {
+    id: "Elven_Princess_Mage",
+    name: "エルフプリンセスメイジ",
+    reading: ["えるふぷりんせすめいじ", "エルフプリンセスメイジ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Elven_Princess_Mage.zip"
+  },
+  {
+    id: "Mana_Elk",
+    name: "マナエルク",
+    reading: ["まなえるく", "マナエルク"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Mana_Elk.zip"
+  },
+  {
+    id: "Elf_Prophetess",
+    name: "エルフプロフェテス",
+    reading: ["えるふぷろふぇてす", "エルフプロフェテス"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Elf_Prophetess.zip"
+  },
+  {
+    id: "Elder_Treant",
+    name: "エルダートレント",
+    reading: ["えるだーとれんと", "エルダートレント"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Elder_Treant.zip"
+  },
+  {
+    id: "Fairy_Beast",
+    name: "フェアリービースト",
+    reading: ["ふぇありーびーすと", "フェアリービースト"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Fairy_Beast.zip"
+  },
+  {
+    id: "Forest_Archelon",
+    name: "フォレストアケロウ",
+    reading: ["ふぉれすとあけろう", "フォレストアケロウ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Forest_Archelon.zip"
+  },
+  {
+    id: "Forest_Giant",
+    name: "フォレストジャイアント",
+    reading: ["ふぉれすとじゃいあんと", "フォレストジャイアント"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Forest_Giant.zip"
   },
 ];
+
 

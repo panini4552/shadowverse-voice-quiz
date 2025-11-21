e12c096a161c704c8ce53f606ce811568b55e753
panini4552 <panini4552@gmail.com>
2025-11-21 14:53:07 +0900

    Update Silver.js

--- data/Classic/Silver.js
+++ data/Classic/Silver.js
@@ -201,4 +201,467 @@ export const Classic_Silver = [
     pack: "Classic",
     zip: "voices/Classic/Silver/Enraged_General.zip"
   },
+    {
+    id: "Scholarly_Witch",
+    name: "スカラーウィッチ",
+    reading: ["すからーうぃっち", "スカラーウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Scholarly_Witch.zip"
+  },
+  {
+    id: "Spectral_Wizard",
+    name: "スペクトラルウィザード",
+    reading: ["すぺくとらるうぃざーど", "スペクトラルウィザード"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Spectral_Wizard.zip"
+  },
+  {
+    id: "Queen_Medb",
+    name: "クイーン・メイヴ",
+    reading: ["くいーんめいゔ", "クイーンメイヴ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Queen_Medb.zip"
+  },
+  {
+    id: "Rune_Blade_Summoner",
+    name: "ルーンブレードサモナー",
+    reading: ["るーんぶれーどさもなー", "ルーンブレードサモナー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Rune_Blade_Summoner.zip"
+  },
+  {
+    id: "Flame_Witch",
+    name: "フレイムウィッチ",
+    reading: ["ふれいむうぃっち", "フレイムウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Flame_Witch.zip"
+  },
+  {
+    id: "Gem_Golem",
+    name: "ジュエルゴーレム",
+    reading: ["じゅえるごーれむ", "ジュエルゴーレム"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Gem_Golem.zip"
+  },
+  {
+    id: "Witchcrafter_Magisa",
+    name: "ウィッチクラフト・マギサ",
+    reading: ["うぃっちくらふとまぎさ", "ウィッチクラフトマギサ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Witchcrafter_Magisa.zip"
+  },
+    {
+    id: "Dragonewt_Princess",
+    name: "ドラゴニュートプリンセス",
+    reading: ["どらごにゅーとぷりんせす", "ドラゴニュートプリンセス"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Dragonewt_Princess.zip"
+  },
+  {
+    id: "Sky_Dragon_Ethica",
+    name: "スカイドラコ・エチカ",
+    reading: ["すかいどらこえちか", "スカイドラコエチカ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Sky_Dragon_Ethica.zip"
+  },
+  {
+    id: "Imprisoned_Dragon",
+    name: "プリズンドラゴン",
+    reading: ["ぷりずんどらごん", "プリズンドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Imprisoned_Dragon.zip"
+  },
+  {
+    id: "Ace_Dragoon",
+    name: "エースドラグーン",
+    reading: ["えーすどらぐーん", "エースドラグーン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Ace_Dragoon.zip"
+  },
+  {
+    id: "Grim_Reaper",
+    name: "グリムリーパー",
+    reading: ["ぐりむりーぱー", "グリムリーパー"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Grim_Reaper.zip"
+  },
+  {
+    id: "Leviathan",
+    name: "リヴァイアサン",
+    reading: ["りゔぁいあさん", "リヴァイアサン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Leviathan.zip"
+  },
+  {
+    id: "Lightning_Behemoth",
+    name: "ライトニングベヒモス",
+    reading: ["らいとにんぐべひもす", "ライトニングベヒモス"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Lightning_Behemoth.zip"
+  },
+  {
+    id: "Dragonrider_Devil",
+    name: "ドラゴンライダーデビル",
+    reading: ["どらごんらいだーでびる", "ドラゴンライダーデビル"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Dragonrider_Devil.zip"
+  },
+  {
+    id: "Mist_Dragon",
+    name: "ミストドラゴン",
+    reading: ["みすとどらごん", "ミストドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Mist_Dragon.zip"
+  },
+  {
+    id: "Shenlong",
+    name: "神龍",
+    reading: ["しぇんろん", "シェンロン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Shenlong.zip"
+  },
+    {
+    id: "Skeleton_Fighter",
+    name: "スケルトンファイター",
+    reading: ["すけるとんふぁいたー", "スケルトンファイター"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Skeleton_Fighter.zip"
+  },
+  {
+    id: "Deadly_Widow",
+    name: "デッドリーウィドウ",
+    reading: ["でっどりーうぃどう", "デッドリーウィドウ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Deadly_Widow.zip"
+  },
+  {
+    id: "Necroelementist",
+    name: "ネクロエレメンタラー",
+    reading: ["ねくろえれめんたらー", "ネクロエレメンタラー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Necroelementist.zip"
+  },
+  {
+    id: "Goetia_Mage",
+    name: "ゴエティアメイジ",
+    reading: ["ごえてぃあめいじ", "ゴエティアメイジ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Goetia_Mage.zip"
+  },
+  {
+    id: "Skull_Rider",
+    name: "スカルライダー",
+    reading: ["すかるらいだー", "スカルライダー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Skull_Rider.zip"
+  },
+  {
+    id: "Soul_Glutton",
+    name: "ソウルグラットン",
+    reading: ["そうるぐらっとん", "ソウルグラットン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Soul_Glutton.zip"
+  },
+  {
+    id: "Soulhungry_Wraith",
+    name: "魂飢の亡霊",
+    reading: ["たましいうえのぼうれい", "タマシイウエノボウレイ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Soulhungry_Wraith.zip"
+  },
+  {
+    id: "Dullahan",
+    name: "デュラハン",
+    reading: ["でゅらはん", "デュラハン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Dullahan.zip"
+  },
+  {
+    id: "Necropolis_Queen",
+    name: "死都の女王",
+    reading: ["しとのじょおう", "シトノジョオウ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Necropolis_Queen.zip"
+  },
+  {
+    id: "Soul_Grinder",
+    name: "ソウルグラインダー",
+    reading: ["そうるぐらいんだー", "ソウルグラインダー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Classic",
+    zip: "voices/Classic/Silver/Soul_Grinder.zip"
+  },
+  // === ヴァンパイア ===
+{
+  id: "AccursedSwordVampire",
+  name: "呪剣の吸血鬼",
+  reading: ["じゅけんのきゅうけつき", "ジュケンノキュウケツキ"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/AccursedSwordVampire.zip"
+},
+{
+  id: "Mormo",
+  name: "モルモ",
+  reading: ["もるも", "モルモ"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/Mormo.zip"
+},
+{
+  id: "DarkSummoner",
+  name: "ダークサモナー",
+  reading: ["だーくさもなー", "ダークサモナー"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/DarkSummoner.zip"
+},
+{
+  id: "RevengeDemon",
+  name: "復讐の悪魔",
+  reading: ["ふくしゅうのあくま", "フクシュウノアクマ"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/RevengeDemon.zip"
+},
+{
+  id: "MidnightVampire",
+  name: "ミッドナイトヴァンパイア",
+  reading: ["みっどないとゔぁんぱいあ", "ミッドナイトヴァンパイア"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/MidnightVampire.zip"
+},
+{
+  id: "VampireLycan",
+  name: "ヴァンパイアライカン",
+  reading: ["ゔぁんぱいあらいかん", "ヴァンパイアライカン"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/VampireLycan.zip"
+},
+{
+  id: "MaskedSlayer",
+  name: "仮面の殺戮者",
+  reading: ["かめんのさつりくしゃ", "カメンノサツリクシャ"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/MaskedSlayer.zip"
+},
+{
+  id: "Geryon",
+  name: "ゲリュオン",
+  reading: ["げりゅおん", "ゲリュオン"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/Geryon.zip"
+},
+{
+  id: "VampireBird",
+  name: "ヴァンパイアバード",
+  reading: ["ゔぁんぱいあばーど", "ヴァンパイアバード"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/VampireBird.zip"
+},
+{
+  id: "Alucard",
+  name: "アルカード",
+  reading: ["あるかーど", "アルカード"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/Alucard.zip"
+},
+// === ビショップ ===
+{
+  id: "SisterOfGrace",
+  name: "加護の修道女",
+  reading: ["かごのしゅうどうじょ", "カゴノシュウドウジョ"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/SisterOfGrace.zip"
+},
+{
+  id: "ChurchProtector",
+  name: "教会の護り手",
+  reading: ["きょうかいのまもりて", "キョウカイノマモリテ"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/ChurchProtector.zip"
+},
+{
+  id: "PrismPriest",
+  name: "プリズムプリースト",
+  reading: ["ぷりずむぷりーすと", "プリズムプリースト"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/PrismPriest.zip"
+},
+{
+  id: "RadiantShaman",
+  name: "レディアントシャーマン",
+  reading: ["れでぃあんとしゃーまん", "レディアントシャーマン"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/RadiantShaman.zip"
+},
+{
+  id: "HolyMage",
+  name: "ホーリーメイジ",
+  reading: ["ほーりーめいじ", "ホーリーメイジ"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/HolyMage.zip"
+},
+{
+  id: "SadisticNun",
+  name: "残忍な修道女",
+  reading: ["ざんにんなしゅうどうじょ", "ザンニンナシュウドウジョ"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/SadisticNun.zip"
+},
+{
+  id: "ClericLancer",
+  name: "クレリックランサー",
+  reading: ["くれりっくらんさー", "クレリックランサー"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/ClericLancer.zip"
+},
+{
+  id: "ShrineKnightMaiden",
+  name: "シュラインナイトメイデン",
+  reading: ["しゅらいんないとめいでん", "シュラインナイトメイデン"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/ShrineKnightMaiden.zip"
+},
+{
+  id: "OracleNoble",
+  name: "神託の貴族",
+  reading: ["しんたくのきぞく", "シンタクノキゾク"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/OracleNoble.zip"
+},
+{
+  id: "WhiteKnight",
+  name: "ホワイトナイト",
+  reading: ["ほわいとないと", "ホワイトナイト"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/WhiteKnight.zip"
+},
+// === ニュートラル ===
+{
+  id: "BellringerAngel",
+  name: "ベルエンジェル",
+  reading: ["べるえんじぇる", "ベルエンジェル"],
+  class: "ニュートラル",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/BellringerAngel.zip"
+},
+{
+  id: "Lizardman",
+  name: "リザードマン",
+  reading: ["りざーどまん", "リザードマン"],
+  class: "ニュートラル",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/Lizardman.zip"
+},
+{
+  id: "GoblinmountDemon",
+  name: "ゴブリンマウントデーモン",
+  reading: ["ごぶりんまうんとでーもん", "ゴブリンマウントデーモン"],
+  class: "ニュートラル",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/GoblinmountDemon.zip"
+},
+{
+  id: "Gilgamesh",
+  name: "ギルガメッシュ",
+  reading: ["ぎるがめっしゅ", "ギルガメッシュ"],
+  class: "ニュートラル",
+  rarity: "Silver",
+  pack: "Classic",
+  zip: "voices/Classic/Silver/Gilgamesh.zip"
+},
 ];
+

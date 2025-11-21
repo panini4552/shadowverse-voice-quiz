83f7447a457194e994e29c45038e9c529dc65284
panini4552 <panini4552@gmail.com>
2025-11-21 22:41:54 +0900

    Update Legendary.js

--- data/DRK/Legendary.js
+++ data/DRK/Legendary.js
@@ -9,10 +9,65 @@ export const DRK_Legendary = [
     pack: "DRK",
     zip: "voices/DRK/Legendary/アレキサンダー.zip"
   },
+{
+  id: "オーディン",
+  name: "オーディン",
+  reading: ["おーでぃん", "オーディン"],
+  class: "ニュートラル",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/オーディン.zip"
+},
+{
+  id: "クリスタリアプリンセス_ティア",
+  name: "クリスタリアプリンセス・ティア",
+  reading: ["くりすたりあぷりんせすてぃあ", "クリスタリアプリンセスティア"],
+  class: "エルフ",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/クリスタリアプリンセス・ティア.zip"
+},
+{
+  id: "ダークエンジェル_オリヴィエ",
+  name: "ダークエンジェル・オリヴィエ",
+  reading: ["だーくえんじぇるおりヴぃえ", "ダークエンジェルオリヴィエ"],
+  class: "ニュートラルエルフ",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/ダークエンジェル・オリヴィエ.zip"
+},
+{
+  id: "ブラッディ_メアリー",
+  name: "ブラッディ・メアリー",
+  reading: ["ぶらっでぃめありー", "ブラッディメアリー"],
+  class: "ヴァンパイア",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/ブラッディ・メアリー.zip"
+},
+{
+  id: "太陽の巫女_パメラ",
+  name: "太陽の巫女・パメラ",
+  reading: ["たいようのみこぱめら", "太陽の巫女パメラ"],
+  class: "ウィッチ",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/太陽の巫女・パメラ.zip"
+},
+{
+  id: "蝿の王",
+  name: "蝿の王",
+  reading: ["はえのおう", "ハエノオウ","蝿の王"],
+  class: "ネクロマンサー",
+  rarity: "Legendary",
+  pack: "DRK",
+  zip: "voices/DRK/Legendary/蝿の王.zip"
+},
 
 
 
 
 ];
+
 
 

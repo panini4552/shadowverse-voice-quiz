1362570c331e5eecd8a0edc79ce6d2e7ac491a0c
panini4552 <panini4552@gmail.com>
2025-11-23 00:47:44 +0900

    Update Silver.js

--- data/WLD/Silver.js
+++ data/WLD/Silver.js
@@ -1,15 +1,233 @@
-export const Classic_Legendary = [
+export const WLD_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "アックスガール・アビー",
+    name: "アックスガール・アビー",
+    reading: ["あっくすがーる・あびー", "アックスガール・アビー", "アックスガール・アビー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/アックスガール・アビー.zip"
+  },
+  {
+    id: "雪原の戦士",
+    name: "雪原の戦士",
+    reading: ["せつげんのせんし", "セツゲンノセンシ", "雪原の戦士"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/雪原の戦士.zip"
+  },
+  {
+    id: "長老の樹",
+    name: "長老の樹",
+    reading: ["ちょうろうのき", "チョウロウノキ", "長老の樹"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/長老の樹.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "刃ネズミ",
+    name: "刃ネズミ",
+    reading: ["はねずみ", "ハネズミ", "刃ネズミ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/刃ネズミ.zip"
+  },
+  {
+    id: "お爺さんとお婆さん",
+    name: "お爺さんとお婆さん",
+    reading: ["おじいさんとおばあさん", "オジイサントオバアサン", "お爺さんとお婆さん"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/お爺さんとお婆さん.zip"
+  },
+  {
+    id: "若き鬼狩人・モモ",
+    name: "若き鬼狩人・モモ",
+    reading: ["わかきおにがりゅうど・もも", "ワカキオニガリュウド・モモ", "若き鬼狩人・モモ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/若き鬼狩人・モモ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マナリアグリモワール",
+    name: "マナリアグリモワール",
+    reading: ["まなりあぐりもわーる", "マナリアグリモワール", "マナリアグリモワール"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/マナリアグリモワール.zip"
+  },
+  {
+    id: "幻影使い",
+    name: "幻影使い",
+    reading: ["げんえいつかい", "ゲンエイツカイ", "幻影使い"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/幻影使い.zip"
+  },
+  {
+    id: "闇魔法の教師・ハインライン",
+    name: "闇魔法の教師・ハインライン",
+    reading: ["やみまほうのきょうし・はいんらいん", "ヤミマホウノキョウシ・ハインライン", "闇魔法の教師・ハインライン"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/闇魔法の教師・ハインライン.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ヒッポグリフの乗り手",
+    name: "ヒッポグリフの乗り手",
+    reading: ["ひっぽぐりふののりて", "ヒッポグリフノノリテ", "ヒッポグリフの乗り手"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/ヒッポグリフの乗り手.zip"
+  },
+  {
+    id: "竜技の達人",
+    name: "竜技の達人",
+    reading: ["りゅうぎのたつじん", "リュウギノタツジン", "竜技の達人"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/竜技の達人.zip"
+  },
+  {
+    id: "フォートレスキャンサー",
+    name: "フォートレスキャンサー",
+    reading: ["ふぉーとれすきゃんさー", "フォートレスキャンサー", "フォートレスキャンサー"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/フォートレスキャンサー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥河の導き手",
+    name: "冥河の導き手",
+    reading: ["めいがのみちびきて", "メイガノミチビキテ", "冥河の導き手"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/冥河の導き手.zip"
+  },
+  {
+    id: "悪夢の処刑人",
+    name: "悪夢の処刑人",
+    reading: ["あくむのしょけいにん", "アクムノショケイニン", "悪夢の処刑人"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/悪夢の処刑人.zip"
+  },
+  {
+    id: "謎かけの芋虫",
+    name: "謎かけの芋虫",
+    reading: ["なぞかけのいもむし", "ナゾカケノイモムシ", "謎かけの芋虫"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/謎かけの芋虫.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "狼少年",
+    name: "狼少年",
+    reading: ["おおかみしょうねん", "オオカミショウネン", "狼少年"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/狼少年.zip"
+  },
+  {
+    id: "緋色の剣士",
+    name: "緋色の剣士",
+    reading: ["ひいろのけんし", "ヒイロノケンシ", "緋色の剣士"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/緋色の剣士.zip"
+  },
+  {
+    id: "バンダースナッチ",
+    name: "バンダースナッチ",
+    reading: ["ばんだーすなっち", "バンダースナッチ", "バンダースナッチ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/バンダースナッチ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "天狐",
+    name: "天狐",
+    reading: ["てんこ", "テンコ", "天狐"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/天狐.zip"
+  },
+  {
+    id: "ハティ",
+    name: "ハティ",
+    reading: ["はてぃ", "ハティ", "ハティ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/ハティ.zip"
+  },
+  {
+    id: "イーグルマン",
+    name: "イーグルマン",
+    reading: ["いーぐるまん", "イーグルマン", "イーグルマン"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/イーグルマン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "歌劇姫・フェリア",
+    name: "歌劇姫・フェリア",
+    reading: ["かげきひめ・ふぇりあ", "カゲキヒメ・フェリア", "歌劇姫・フェリア"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/歌劇姫・フェリア.zip"
+  },
+  {
+    id: "屈強な山岳隊長",
+    name: "屈強な山岳隊長",
+    reading: ["くっきょうなさんがくたいちょう", "クッキョウナサンガクタイチョウ", "屈強な山岳隊長"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/屈強な山岳隊長.zip"
+  },
+  {
+    id: "獅子の豪傑",
+    name: "獅子の豪傑",
+    reading: ["ししのごうけつ", "シシノゴウケツ", "獅子の豪傑"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "WLD",
+    zip: "voices/WLD/Silver/獅子の豪傑.zip"
+  }
 ];

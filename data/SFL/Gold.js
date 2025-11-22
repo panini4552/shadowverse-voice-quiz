d9779a6f20e32eb3724309287cc73807673e63d6
panini4552 <panini4552@gmail.com>
2025-11-23 01:00:23 +0900

    Update Gold.js

--- data/SFL/Gold.js
+++ data/SFL/Gold.js
@@ -1,15 +1,161 @@
-export const Classic_Legendary = [
+export const SFL_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Zain_Boy_of_Demons",
+    name: "魔族の少年・ザイン",
+    reading: ["まぞくのしょうねん・ざいん", "マゾクノショウネン・ザイン", "魔族の少年・ザイン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Zain_Boy_of_Demons.zip"
+  },
+  {
+    id: "Venus",
+    name: "ヴィーナス",
+    reading: ["ヴぃーなす", "ヴィーナス", "ヴィーナス"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Venus.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "Perseus",
+    name: "ペルセウス",
+    reading: ["ぺるせうす", "ペルセウス", "ペルセウス"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Perseus.zip"
+  },
+  {
+    id: "Lou_Spear_of_the_Sun",
+    name: "太陽の槍・ルー",
+    reading: ["たいようのやり・るー", "タイヨウノヤリ・ルー", "太陽の槍・ルー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Lou_Spear_of_the_Sun.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "Europe",
+    name: "エウロペ",
+    reading: ["えうろぺ", "エウロペ", "エウロペ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Europe.zip"
+  },
+  {
+    id: "Rampaging_Golem",
+    name: "暴威のゴーレム",
+    reading: ["ぼういのごーれむ", "ボウイノゴーレム", "暴威のゴーレム"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Rampaging_Golem.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "Star_Phoenix",
+    name: "スターフェニックス",
+    reading: ["すたーふぇにっくす", "スターフェニックス", "スターフェニックス"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Star_Phoenix.zip"
+  },
+  {
+    id: "Dragonic_Singer",
+    name: "ドラゴニックシンガー",
+    reading: ["どらごにっくしんがー", "ドラゴニックシンガー", "ドラゴニックシンガー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Dragonic_Singer.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "Andrealphus",
+    name: "アンドレアルフス",
+    reading: ["あんどれあるふす", "アンドレアルフス", "アンドレアルフス"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Andrealphus.zip"
+  },
+  {
+    id: "Ceres_Lady_of_the_Night",
+    name: "夜の令嬢・セレス",
+    reading: ["よるのれいじょう・せれす", "ヨルノレイジョウ・セレス", "夜の令嬢・セレス"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Ceres_Lady_of_the_Night.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "Thunder_Demon",
+    name: "轟雷の悪魔",
+    reading: ["ごうらいのあくま", "ゴウライノアクマ", "轟雷の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Thunder_Demon.zip"
+  },
+  {
+    id: "Stheno",
+    name: "ステンノ",
+    reading: ["すてんの", "ステンノ", "ステンノ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Stheno.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "Andromeda",
+    name: "アンドロメダ",
+    reading: ["あんどろめだ", "アンドロメダ", "アンドロメダ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Andromeda.zip"
+  },
+  {
+    id: "Great_Taurus",
+    name: "グレートタウラス",
+    reading: ["ぐれーとたうらす", "グレートタウラス", "グレートタウラス"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Great_Taurus.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "Brindy",
+    name: "ブリンディ",
+    reading: ["ぶりんでぃ", "ブリンディ", "ブリンディ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Brindy.zip"
+  },
+  {
+    id: "Kirin",
+    name: "麒麟",
+    reading: ["きりん", "キリン", "麒麟"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "SFL",
+    zip: "voices/SFL/Gold/Kirin.zip"
+  }
 ];

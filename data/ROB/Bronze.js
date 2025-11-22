4e0acfa6afd5f35ac77826586f1cb4b47c3dde42
panini4552 <panini4552@gmail.com>
2025-11-22 23:44:56 +0900

    Update Bronze.js

--- data/ROB/Bronze.js
+++ data/ROB/Bronze.js
@@ -1,15 +1,290 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const ROB_Bronze = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    id: "ジュエルデビル・モリアナ",
+    name: "ジュエルデビル・モリアナ",
+    reading: ["じゅえるでびる・もりあな", "ジュエルデビル・モリアナ", "ジュエルデビル・モリアナ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ジュエルデビル・モリアナ.zip"
   },
-
-
-
+  {
+    id: "セクトール",
+    name: "セクトール",
+    reading: ["せくとーる", "セクトール", "セクトール"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "セクトール.zip"
+  },
+  {
+    id: "ソウルコレクター",
+    name: "ソウルコレクター",
+    reading: ["そうるこれくたー", "ソウルコレクター", "ソウルコレクター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ソウルコレクター.zip"
+  },
+  {
+    id: "ソードウィップメイド",
+    name: "ソードウィップメイド",
+    reading: ["そーどうぃっぷめいど", "ソードウィップメイド", "ソードウィップメイド"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ソードウィップメイド.zip"
+  },
+  {
+    id: "タイムレスウィッチ",
+    name: "タイムレスウィッチ",
+    reading: ["たいむれすうぃっち", "タイムレスウィッチ", "タイムレスウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "タイムレスウィッチ.zip"
+  },
+  {
+    id: "ドラゴンテイマー",
+    name: "ドラゴンテイマー",
+    reading: ["どらごんていまー", "ドラゴンテイマー", "ドラゴンテイマー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ドラゴンテイマー.zip"
+  },
+  {
+    id: "ハンサ",
+    name: "ハンサ",
+    reading: ["はんさ", "ハンサ", "ハンサ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ハンサ.zip"
+  },
+  {
+    id: "フェアリーナイト",
+    name: "フェアリーナイト",
+    reading: ["ふぇありーないと", "フェアリーナイト", "フェアリーナイト"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "フェアリーナイト.zip"
+  },
+  {
+    id: "フォレストギガース",
+    name: "フォレストギガース",
+    reading: ["ふぉれすとぎがーす", "フォレストギガース", "フォレストギガース"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "フォレストギガース.zip"
+  },
+  {
+    id: "フォレストスピリット",
+    name: "フォレストスピリット",
+    reading: ["ふぉれすとすぴりっと", "フォレストスピリット", "フォレストスピリット"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "フォレストスピリット.zip"
+  },
+  {
+    id: "ブリッツランサー",
+    name: "ブリッツランサー",
+    reading: ["ぶりっつらんさー", "ブリッツランサー", "ブリッツランサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ブリッツランサー.zip"
+  },
+  {
+    id: "フロッグクレリック",
+    name: "フロッグクレリック",
+    reading: ["ふろっぐくれりっく", "フロッグクレリック", "フロッグクレリック"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "フロッグクレリック.zip"
+  },
+  {
+    id: "ベビーフェルパー",
+    name: "ベビーフェルパー",
+    reading: ["べびーふぇるぱー", "ベビーフェルパー", "ベビーフェルパー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ベビーフェルパー.zip"
+  },
+  {
+    id: "マスターレディセージ",
+    name: "マスターレディセージ",
+    reading: ["ますたーれでぃせーじ", "マスターレディセージ", "マスターレディセージ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "マスターレディセージ.zip"
+  },
+  {
+    id: "マナリアウィザード・クレイグ",
+    name: "マナリアウィザード・クレイグ",
+    reading: ["まなりあうぃざーど・くれいぐ", "マナリアウィザード・クレイグ", "マナリアウィザード・クレイグ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "マナリアウィザード・クレイグ.zip"
+  },
+  {
+    id: "ミニゴブリンメイジ",
+    name: "ミニゴブリンメイジ",
+    reading: ["みにごぶりんめいじ", "ミニゴブリンメイジ", "ミニゴブリンメイジ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ミニゴブリンメイジ.zip"
+  },
+  {
+    id: "レヴィオンデューク・ユリウス",
+    name: "レヴィオンデューク・ユリウス",
+    reading: ["れびおんでゅーく・ゆりうす", "レヴィオンデューク・ユリウス", "レヴィオンデューク・ユリウス"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "レヴィオンデューク・ユリウス.zip"
+  },
+  {
+    id: "ワイリーワイバーン",
+    name: "ワイリーワイバーン",
+    reading: ["わいりーわいばーん", "ワイリーワイバーン", "ワイリーワイバーン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ワイリーワイバーン.zip"
+  },
+  {
+    id: "闇の従者",
+    name: "闇の従者",
+    reading: ["やみのじゅうしゃ", "ヤミノジュウシャ", "闇の従者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "闇の従者.zip"
+  },
+  {
+    id: "怪犬の墓守",
+    name: "怪犬の墓守",
+    reading: ["かいけんのはかもり", "カイケンノハカモリ", "怪犬の墓守"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "怪犬の墓守.zip"
+  },
+  {
+    id: "海賊ゾンビ",
+    name: "海賊ゾンビ",
+    reading: ["かいぞくぞんび", "カイゾクゾンビ", "海賊ゾンビ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "海賊ゾンビ.zip"
+  },
+  {
+    id: "御言葉の天使",
+    name: "御言葉の天使",
+    reading: ["みことばのてんし", "ミコトバノテンシ", "御言葉の天使"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "御言葉の天使.zip"
+  },
+  {
+    id: "高慢なる悪魔",
+    name: "高慢なる悪魔",
+    reading: ["こうまんなるあくま", "コウマンナルアクマ", "高慢なる悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "高慢なる悪魔.zip"
+  },
+  {
+    id: "熟達の調教師",
+    name: "熟達の調教師",
+    reading: ["じゅくたつのちょうきょうし", "ジュクタツノチョウキョウシ", "熟達の調教師"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "熟達の調教師.zip"
+  },
+  {
+    id: "人狼の群れ長",
+    name: "人狼の群れ長",
+    reading: ["じんろうのむれおさ", "ジンロウノムレオサ", "人狼の群れ長"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "人狼の群れ長.zip"
+  },
+  {
+    id: "刃の魔術師",
+    name: "刃の魔術師",
+    reading: ["やいばのまじゅつし", "ヤイバノマジュツシ", "刃の魔術師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "刃の魔術師.zip"
+  },
+  {
+    id: "双翼の警護者",
+    name: "双翼の警護者",
+    reading: ["そうよくのけいごしゃ", "ソウヨクノケイゴシャ", "双翼の警護者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "双翼の警護者.zip"
+  },
+  {
+    id: "煌牙の戦士・キット",
+    name: "煌牙の戦士・キット",
+    reading: ["こうがのせんし・きっと", "コウガノセンシ・キット", "煌牙の戦士・キット"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "煌牙の戦士・キット.zip"
+  },
+  {
+    id: "グリフォンナイト",
+    name: "グリフォンナイト",
+    reading: ["ぐりふぉんないと", "グリフォンナイト", "グリフォンナイト"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "グリフォンナイト.zip"
+  },
+  {
+    id: "ゲイザー",
+    name: "ゲイザー",
+    reading: ["げいざー", "ゲイザー", "ゲイザー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "ゲイザー.zip"
+  },
+  {
+    id: "サムライ",
+    name: "サムライ",
+    reading: ["さむらい", "サムライ", "サムライ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "サムライ.zip"
+  },
+  {
+    id: "シーフ",
+    name: "シーフ",
+    reading: ["しーふ", "シーフ", "シーフ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROB",
+    zip: "シーフ.zip"
+  }
 ];

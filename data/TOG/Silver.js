e5d58c6dfc1278f1df5c7aae5fd298bbe0adb57f
panini4552 <panini4552@gmail.com>
2025-11-23 00:04:46 +0900

    Update Silver.js

--- data/TOG/Silver.js
+++ data/TOG/Silver.js
@@ -1,15 +1,233 @@
-export const Classic_Legendary = [
+export const TOG_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "小さな勇士・スクナ",
+    name: "小さな勇士・スクナ",
+    reading: ["ちいさなゆうし・すくな", "チイサナユウシ・スクナ", "小さな勇士・スクナ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/小さな勇士・スクナ.zip"
+  },
+  {
+    id: "エルフの執事・ヴィスト",
+    name: "エルフの執事・ヴィスト",
+    reading: ["えるふのしつじ・う゛ぃすと", "エルフノシツジ・ヴィスト", "エルフの執事・ヴィスト"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/エルフの執事・ヴィスト.zip"
+  },
+  {
+    id: "勇猛なるドワーフ",
+    name: "勇猛なるドワーフ",
+    reading: ["ゆうもうなるどわーふ", "ユウモウナルドワーフ", "勇猛なるドワーフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/勇猛なるドワーフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "シャドウアサシン",
+    name: "シャドウアサシン",
+    reading: ["しゃどうあさしん", "シャドウアサシン", "シャドウアサシン"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/シャドウアサシン.zip"
+  },
+  {
+    id: "ルミナスメイジ",
+    name: "ルミナスメイジ",
+    reading: ["るみなすめいじ", "ルミナスメイジ", "ルミナスメイジ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/ルミナスメイジ.zip"
+  },
+  {
+    id: "ジョリー・ロジャー",
+    name: "ジョリー・ロジャー",
+    reading: ["じょりー・ろじゃー", "ジョリー・ロジャー", "ジョリー・ロジャー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/ジョリー・ロジャー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "学院の新入生・ルゥ",
+    name: "学院の新入生・ルゥ",
+    reading: ["がくいんのしんにゅうせい・るぅ", "ガクインノシンニュウセイ・ルゥ", "学院の新入生・ルゥ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/学院の新入生・ルゥ.zip"
+  },
+  {
+    id: "幻惑の奇術師",
+    name: "幻惑の奇術師",
+    reading: ["げんわくのきじゅつし", "ゲンワクノキジュツシ", "幻惑の奇術師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/幻惑の奇術師.zip"
+  },
+  {
+    id: "キマイラ",
+    name: "キマイラ",
+    reading: ["きまいら", "キマイラ", "キマイラ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/キマイラ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "風読みの少年・ゼル",
+    name: "風読みの少年・ゼル",
+    reading: ["かぜよみのしょうねん・ぜる", "カゼヨミノショウネン・ゼル", "風読みの少年・ゼル"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/風読みの少年・ゼル.zip"
+  },
+  {
+    id: "ラースドレイク",
+    name: "ラースドレイク",
+    reading: ["らーすどれいく", "ラースドレイク", "ラースドレイク"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/ラースドレイク.zip"
+  },
+  {
+    id: "邪毒の紫竜",
+    name: "邪毒の紫竜",
+    reading: ["じゃどくのしりゅう", "ジャドクノシリュウ", "邪毒の紫竜"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/邪毒の紫竜.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "骨の貴公子",
+    name: "骨の貴公子",
+    reading: ["ほねのきこうし", "ホネノキコウシ", "骨の貴公子"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/骨の貴公子.zip"
+  },
+  {
+    id: "オルトロス",
+    name: "オルトロス",
+    reading: ["おるとろす", "オルトロス", "オルトロス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/オルトロス.zip"
+  },
+  {
+    id: "忌刃の戦士",
+    name: "忌刃の戦士",
+    reading: ["いみばのせんし", "イミバノセンシ", "忌刃の戦士"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/忌刃の戦士.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "バフォメット",
+    name: "バフォメット",
+    reading: ["ばふぉめっと", "バフォメット", "バフォメット"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/バフォメット.zip"
+  },
+  {
+    id: "カオスシップ",
+    name: "カオスシップ",
+    reading: ["かおすしっぷ", "カオスシップ", "カオスシップ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/カオスシップ.zip"
+  },
+  {
+    id: "ヴラド・ドラキュラ",
+    name: "ヴラド・ドラキュラ",
+    reading: ["う゛らど・どらきゅら", "ヴラド・ドラキュラ", "ヴラド・ドラキュラ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/ヴラド・ドラキュラ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖なる弓使い・クルト",
+    name: "聖なる弓使い・クルト",
+    reading: ["せいなるゆみつかい・くると", "セイナルユミツカイ・クルト", "聖なる弓使い・クルト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/聖なる弓使い・クルト.zip"
+  },
+  {
+    id: "カリュドーン",
+    name: "カリュドーン",
+    reading: ["かりゅどーん", "カリュドーン", "カリュドーン"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/カリュドーン.zip"
+  },
+  {
+    id: "黄金都市の墓守・ソニア",
+    name: "黄金都市の墓守・ソニア",
+    reading: ["おうごんとしのはかもり・そにあ", "オウゴントシノハカモリ・ソニア", "黄金都市の墓守・ソニア"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/黄金都市の墓守・ソニア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "彷徨の詩人・エルタ",
+    name: "彷徨の詩人・エルタ",
+    reading: ["さまよいのしじん・えるた", "サマヨイノシジン・エルタ", "彷徨の詩人・エルタ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/彷徨の詩人・エルタ.zip"
+  },
+  {
+    id: "均衡の大梟",
+    name: "均衡の大梟",
+    reading: ["きんこうのたいきょう", "キンコウノタイキョウ", "均衡の大梟"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/均衡の大梟.zip"
+  },
+  {
+    id: "ゴブリンプリンセス",
+    name: "ゴブリンプリンセス",
+    reading: ["ごぶりんぷりんせす", "ゴブリンプリンセス", "ゴブリンプリンセス"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "TOG",
+    zip: "voices/TOG/Silver/ゴブリンプリンセス.zip"
+  }
 ];

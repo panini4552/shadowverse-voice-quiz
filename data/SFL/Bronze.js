c33e3afc6f4849c24d29b77b2c206e4326cb7bc1
panini4552 <panini4552@gmail.com>
2025-11-23 01:20:25 +0900

    Update Bronze.js

--- data/SFL/Bronze.js
+++ data/SFL/Bronze.js
@@ -1,233 +1,233 @@
 export const SFL_Bronze = [
   // === エルフ ===
   {
-    id: "Fito_Gentle_Elf",
+    id: "心優しきエルフ・フィト",
     name: "心優しきエルフ・フィト",
     reading: ["こころやさしきえるふ・ふぃと", "ココロヤサシキエルフ・フィト", "心優しきエルフ・フィト"],
     class: "エルフ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Fito_Gentle_Elf.zip"
+    zip: "voices/SFL/Bronze/心優しきエルフ・フィト.zip"
   },
   {
-    id: "Starry_Elf",
+    id: "スターリーエルフ",
     name: "スターリーエルフ",
     reading: ["すたーりーえるふ", "スターリーエルフ", "スターリーエルフ"],
     class: "エルフ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Starry_Elf.zip"
+    zip: "voices/SFL/Bronze/スターリーエルフ.zip"
   },
   {
-    id: "Ipiria",
+    id: "イピリア",
     name: "イピリア",
     reading: ["いぴりあ", "イピリア", "イピリア"],
     class: "エルフ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Ipiria.zip"
+    zip: "voices/SFL/Bronze/イピリア.zip"
   },
 
   // === ロイヤル ===
   {
-    id: "Flail_Knight",
+    id: "フレイルナイト",
     name: "フレイルナイト",
     reading: ["ふれいるないと", "フレイルナイト", "フレイルナイト"],
     class: "ロイヤル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Flail_Knight.zip"
+    zip: "voices/SFL/Bronze/フレイルナイト.zip"
   },
   {
-    id: "Pollux",
+    id: "ポルックス",
     name: "ポルックス",
     reading: ["ぽるっくす", "ポルックス", "ポルックス"],
     class: "ロイヤル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Pollux.zip"
+    zip: "voices/SFL/Bronze/ポルックス.zip"
   },
   {
-    id: "Horned_King",
+    id: "角冠の王",
     name: "角冠の王",
     reading: ["つのかんむりのおう", "ツノカンムリノオウ", "角冠の王"],
     class: "ロイヤル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Horned_King.zip"
+    zip: "voices/SFL/Bronze/角冠の王.zip"
   },
 
   // === ウィッチ ===
   {
-    id: "Magic_Owl",
+    id: "マジックオウル",
     name: "マジックオウル",
     reading: ["まじっくおうる", "マジックオウル", "マジックオウル"],
     class: "ウィッチ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Magic_Owl.zip"
+    zip: "voices/SFL/Bronze/マジックオウル.zip"
   },
   {
-    id: "Lightning_Lancer",
+    id: "ライトニングランサー",
     name: "ライトニングランサー",
     reading: ["らいとにんぐらんさー", "ライトニングランサー", "ライトニングランサー"],
     class: "ウィッチ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Lightning_Lancer.zip"
+    zip: "voices/SFL/Bronze/ライトニングランサー.zip"
   },
   {
-    id: "Misty_Astrologist",
+    id: "ミスティアストロジスト",
     name: "ミスティアストロジスト",
     reading: ["みすてぃあすとろじすと", "ミスティアストロジスト", "ミスティアストロジスト"],
     class: "ウィッチ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Misty_Astrologist.zip"
+    zip: "voices/SFL/Bronze/ミスティアストロジスト.zip"
   },
 
   // === ドラゴン ===
   {
-    id: "Mathilda_Dragon_Nurturer",
+    id: "竜を育む者・マチルダ",
     name: "竜を育む者・マチルダ",
     reading: ["りゅうをはぐくむもの・まちるだ", "リュウヲハグクムモノ・マチルダ", "竜を育む者・マチルダ"],
     class: "ドラゴン",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Mathilda_Dragon_Nurturer.zip"
+    zip: "voices/SFL/Bronze/竜を育む者・マチルダ.zip"
   },
   {
-    id: "Estrea_Beast",
+    id: "エストレアビースト",
     name: "エストレアビースト",
     reading: ["えすとれあびーすと", "エストレアビースト", "エストレアビースト"],
     class: "ドラゴン",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Estrea_Beast.zip"
+    zip: "voices/SFL/Bronze/エストレアビースト.zip"
   },
   {
-    id: "Scaled_Berserker",
+    id: "鱗の狂戦士",
     name: "鱗の狂戦士",
     reading: ["うろこのきょうせんし", "ウロコノキョウセンシ", "鱗の狂戦士"],
     class: "ドラゴン",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Scaled_Berserker.zip"
+    zip: "voices/SFL/Bronze/鱗の狂戦士.zip"
   },
 
   // === ネクロマンサー ===
   {
-    id: "Belenus",
+    id: "ベレヌス",
     name: "ベレヌス",
     reading: ["べれぬす", "ベレヌス", "ベレヌス"],
     class: "ネクロマンサー",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Belenus.zip"
+    zip: "voices/SFL/Bronze/ベレヌス.zip"
   },
   {
-    id: "Castor",
+    id: "カストール",
     name: "カストール",
     reading: ["かすとーる", "カストール", "カストール"],
     class: "ネクロマンサー",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Castor.zip"
+    zip: "voices/SFL/Bronze/カストール.zip"
   },
   {
-    id: "Jet_Black_Sword_Demon",
+    id: "漆黒の剣鬼",
     name: "漆黒の剣鬼",
     reading: ["しっこくのけんき", "シッコクノケンキ", "漆黒の剣鬼"],
     class: "ネクロマンサー",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Jet_Black_Sword_Demon.zip"
+    zip: "voices/SFL/Bronze/漆黒の剣鬼.zip"
   },
 
   // === ヴァンパイア ===
   {
-    id: "Scorpio",
+    id: "スコルピオ",
     name: "スコルピオ",
     reading: ["すこるぴお", "スコルピオ", "スコルピオ"],
     class: "ヴァンパイア",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Scorpio.zip"
+    zip: "voices/SFL/Bronze/スコルピオ.zip"
   },
   {
-    id: "Gunlod",
+    id: "グンロズ",
     name: "グンロズ",
     reading: ["ぐんろず", "グンロズ", "グンロズ"],
     class: "ヴァンパイア",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Gunlod.zip"
+    zip: "voices/SFL/Bronze/グンロズ.zip"
   },
   {
-    id: "Fang_Serpent",
+    id: "ファングサーペント",
     name: "ファングサーペント",
     reading: ["ふぁんぐさーぺんと", "ファングサーペント", "ファングサーペント"],
     class: "ヴァンパイア",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Fang_Serpent.zip"
+    zip: "voices/SFL/Bronze/ファングサーペント.zip"
   },
 
   // === ビショップ ===
   {
-    id: "Star_Bishop",
+    id: "スタービショップ",
     name: "スタービショップ",
     reading: ["すたーびしょっぷ", "スタービショップ", "スタービショップ"],
     class: "ビショップ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Star_Bishop.zip"
+    zip: "voices/SFL/Bronze/スタービショップ.zip"
   },
   {
-    id: "Star_Chaser_Spirit",
+    id: "星追いの精霊",
     name: "星追いの精霊",
     reading: ["ほしおいのせいれい", "ホシオイノセイレイ", "星追いの精霊"],
     class: "ビショップ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Star_Chaser_Spirit.zip"
+    zip: "voices/SFL/Bronze/星追いの精霊.zip"
   },
   {
-    id: "Punish_Sister",
+    id: "パニッシュシスター",
     name: "パニッシュシスター",
     reading: ["ぱにっしゅしすたー", "パニッシュシスター", "パニッシュシスター"],
     class: "ビショップ",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Punish_Sister.zip"
+    zip: "voices/SFL/Bronze/パニッシュシスター.zip"
   },
 
   // === ニュートラル ===
   {
-    id: "Nyx",
+    id: "ニュクス",
     name: "ニュクス",
     reading: ["にゅくす", "ニュクス", "ニュクス"],
     class: "ニュートラル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Nyx.zip"
+    zip: "voices/SFL/Bronze/ニュクス.zip"
   },
   {
-    id: "Mr_Full_Moon",
+    id: "ミスターフルムーン",
     name: "ミスターフルムーン",
     reading: ["みすたーふるむーん", "ミスターフルムーン", "ミスターフルムーン"],
     class: "ニュートラル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Mr_Full_Moon.zip"
+    zip: "voices/SFL/Bronze/ミスターフルムーン.zip"
   },
   {
-    id: "Rampage_Mammoth",
+    id: "ランページマンモス",
     name: "ランページマンモス",
     reading: ["らんぺーじめんもす", "ランページマンモス", "ランページマンモス"],
     class: "ニュートラル",
     rarity: "Bronze",
     pack: "SFL",
-    zip: "voices/SFL/Bronze/Rampage_Mammoth.zip"
+    zip: "voices/SFL/Bronze/ランページマンモス.zip"
   }
 ];

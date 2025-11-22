74bbb2b6094476bc16f628ca6974453a9f8a1ea6
panini4552 <panini4552@gmail.com>
2025-11-22 11:46:16 +0900

    Update Bronze.js

--- data/DRK/Bronze.js
+++ data/DRK/Bronze.js
@@ -1,15 +1,308 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const DRK_Bronze = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    id: "Island_Whale",
+    name: "アイランドホエール",
+    reading: ["あいらんどほえーる", "アイランドホエール", "アイランドホエール"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/アイランドホエール.zip"
   },
-
-
-
+  {
+    id: "Assault_Commander",
+    name: "アサルトコマンダー",
+    reading: ["あさるとこまんだー", "アサルトコマンダー", "アサルトコマンダー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/アサルトコマンダー.zip"
+  },
+  {
+    id: "Advance_Braver",
+    name: "アドバンスブレーダー",
+    reading: ["あどばんすぶれーだー", "アドバンスブレーダー", "アドバンスブレーダー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/アドバンスブレーダー.zip"
+  },
+  {
+    id: "Imperial_Mammoth",
+    name: "インペリアルマンモス",
+    reading: ["いんぺりあるまんもす", "インペリアルマンモス", "インペリアルマンモス"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/インペリアルマンモス.zip"
+  },
+  {
+    id: "Elf_Bird",
+    name: "エルフバード",
+    reading: ["えるふばーど", "エルフバード", "エルフバード"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/エルフバード.zip"
+  },
+  {
+    id: "Queen_Serpent",
+    name: "クイーンサーペント",
+    reading: ["くいーんさーぺんと", "クイーンサーペント", "クイーンサーペント"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/クイーンサーペント.zip"
+  },
+  {
+    id: "Grand_Gargoyle",
+    name: "グランドガーゴイル",
+    reading: ["ぐらんどがーごいる", "グランドガーゴイル", "グランドガーゴイル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/グランドガーゴイル.zip"
+  },
+  {
+    id: "Serpent_Charm",
+    name: "サーペントチャーマー",
+    reading: ["さーぺんとちゃーまー", "サーペントチャーマー", "サーペントチャーマー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/サーペントチャーマー.zip"
+  },
+  {
+    id: "Swift_Penetrator",
+    name: "スウィフトペネトレーター",
+    reading: ["すうぃふとぺねとれーたー", "スウィフトペネトレーター", "スウィフトペネトレーター"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/スウィフトペネトレーター.zip"
+  },
+  {
+    id: "Sky_Spirit",
+    name: "スカイスピリット",
+    reading: ["すかいすぴりっと", "スカイスピリット", "スカイスピリット"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/スカイスピリット.zip"
+  },
+  {
+    id: "Spartoi_Soldier",
+    name: "スパルトイソルジャー",
+    reading: ["すぱるといそるじゃー", "スパルトイソルジャー", "スパルトイソルジャー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/スパルトイソルジャー.zip"
+  },
+  {
+    id: "Soul_Navigator",
+    name: "ソウルナビゲーター",
+    reading: ["そうるなびげーたー", "ソウルナビゲーター", "ソウルナビゲーター"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ソウルナビゲーター.zip"
+  },
+  {
+    id: "Soul_Mini_Devil",
+    name: "ソウルミニデビル",
+    reading: ["そうるみにでびる", "ソウルミニデビル", "ソウルミニデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ソウルミニデビル.zip"
+  },
+  {
+    id: "Dark_Conjurer",
+    name: "ダークコンジュラー",
+    reading: ["だーくこんじゅらー", "ダークコンジュラー", "ダークコンジュラー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ダークコンジュラー.zip"
+  },
+  {
+    id: "Destroyers_Commander",
+    name: "デストロイヤーズコマンダー",
+    reading: ["ですとろいやーずこまんだー", "デストロイヤーズコマンダー", "デストロイヤーズコマンダー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/デストロイヤーズコマンダー.zip"
+  },
+  {
+    id: "Demon_Thrall",
+    name: "デモンスラル",
+    reading: ["でもんすらる", "デモンスラル", "デモンスラル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/デモンスラル.zip"
+  },
+  {
+    id: "Perfume_Dwarf",
+    name: "パフュームドワーフ",
+    reading: ["ぱふゅーむどわーふ", "パフュームドワーフ", "パフュームドワーフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/パフュームドワーフ.zip"
+  },
+  {
+    id: "Baby_Witch_Emil",
+    name: "ベビーウィッチ・エミル",
+    reading: ["べびーうぃっちえみる", "ベビーウィッチ・エミル", "ベビーウィッチ・エミル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ベビーウィッチ・エミル.zip"
+  },
+  {
+    id: "Holy_Fox_Grimoire",
+    name: "ホーリーフォックス・グリモー",
+    reading: ["ほーりーふぉっくすぐりもー", "ホーリーフォックス・グリモー", "ホーリーフォックス・グリモー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ホーリーフォックス・グリモー.zip"
+  },
+  {
+    id: "Malice_Ghost",
+    name: "マリスゴースト",
+    reading: ["まりすごーすと", "マリスゴースト", "マリスゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/マリスゴースト.zip"
+  },
+  {
+    id: "Mirage_Defender",
+    name: "ミラージュディフェンサー",
+    reading: ["みらーじゅでぃふぇんさー", "ミラージュディフェンサー", "ミラージュディフェンサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ミラージュディフェンサー.zip"
+  },
+  {
+    id: "Mushhush",
+    name: "ムシュフシュ",
+    reading: ["むしゅふしゅ", "ムシュフシュ", "ムシュフシュ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/ムシュフシュ.zip"
+  },
+  {
+    id: "Raging_Ettin",
+    name: "レイジングエティン",
+    reading: ["れいじんぐえてぃん", "レイジングエティン", "レイジングエティン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/レイジングエティン.zip"
+  },
+  {
+    id: "Pleasure_Demon",
+    name: "享楽の悪魔",
+    reading: ["きょうらくのあくま", "キョウラクノアクマ", "享楽の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/享楽の悪魔.zip"
+  },
+  {
+    id: "Brave_Standard_Bearer",
+    name: "勇敢なる旗手",
+    reading: ["ゆうかんなるはたて", "ユウカンナルハタテ", "勇敢なる旗手"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/勇敢なる旗手.zip"
+  },
+  {
+    id: "Heavenly_Dog",
+    name: "天界の忠犬",
+    reading: ["てんかいのちゅうけん", "テンカイノチュウケン", "天界の忠犬"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/天界の忠犬.zip"
+  },
+  {
+    id: "Forest_Beastman",
+    name: "森隠れの獣人",
+    reading: ["もりがくれのじゅうじん", "モリガクレノジュウジン", "森隠れの獣人"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/森隠れの獣人.zip"
+  },
+  {
+    id: "Death_Chaser",
+    name: "死を追う者",
+    reading: ["しをおうもの", "シヲオウモノ", "死を追う者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/死を追う者.zip"
+  },
+  {
+    id: "Crystal_Gunner",
+    name: "水晶の魔撃手",
+    reading: ["すいしょうのまげきしゅ", "スイショウノマゲキシュ", "水晶の魔撃手"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/水晶の魔撃手.zip"
+  },
+  {
+    id: "Deep_Green_Guardian",
+    name: "深緑の守護者",
+    reading: ["しんりょくのしゅごしゃ", "シンリョクノシュゴシャ", "深緑の守護者"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/深緑の守護者.zip"
+  },
+  {
+    id: "Healing_Elf",
+    name: "癒しのエルフ",
+    reading: ["いやしのえるふ", "イヤシノエルフ", "癒しのエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/癒しのエルフ.zip"
+  },
+  {
+    id: "Prince_of_Wings",
+    name: "翼の王子",
+    reading: ["つばさのおうじ", "ツバサノオウジ", "翼の王子"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/翼の王子.zip"
+  },
+  {
+    id: "Feral_Dragon_Girl",
+    name: "荒牙の竜少女",
+    reading: ["あらがのりゅうしょうじょ", "アラガノリュウショウジョ", "荒牙の竜少女"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/荒牙の竜少女.zip"
+  },
+  {
+    id: "Iron_Scales_Dragonkin",
+    name: "鉄鱗の竜人",
+    reading: ["てつりんのりゅうじん", "テツリンノリュウジン", "鉄鱗の竜人"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DRK",
+    zip: "voices/DRK/Bronze/鉄鱗の竜人.zip"
+  }
 ];

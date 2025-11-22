842401c7bcad9b408d2b61e8394eedd45f732bf5
panini4552 <panini4552@gmail.com>
2025-11-22 23:30:59 +0900

    Update Legendary.js

--- data/ROB/Legendary.js
+++ data/ROB/Legendary.js
@@ -1,15 +1,83 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const ROB_Legendary = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Nephthys",
+    name: "ネフティス",
+    reading: ["ねふてぃす", "ネフティス", "ネフティス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/ネフティス.zip"
+  },
+  {
+    id: "Bahamut",
+    name: "バハムート",
+    reading: ["ばはむーと", "バハムート", "バハムート"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/バハムート.zip"
+  },
+  {
+    id: "Levin_Saber_Albert",
+    name: "レヴィオンセイバー・アルベール",
+    reading: ["れヴぃおんせいばーあるべーる", "レヴィオンセイバーアルベール", "レヴィオンセイバー・アルベール"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/レヴィオンセイバー・アルベール.zip"
+  },
+  {
+    id: "Idol_of_Crazed_Devotion",
+    name: "狂信の偶像",
+    reading: ["きょうしんのぐうぞう", "キョウシンノグウゾウ", "狂信の偶像"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/狂信の偶像.zip"
+  },
+  {
+    id: "Empress_of_Bloodlust",
+    name: "血餓の女帝",
+    reading: ["けつがのじょてい", "ケツガノジョテイ", "血餓の女帝"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/血餓の女帝.zip"
+  },
+  {
+    id: "White_Wolf_of_Elder_Forest",
+    name: "古き森の白狼",
+    reading: ["ふるきもりのはくろう", "フルキモリノハクロウ", "古き森の白狼"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/古き森の白狼.zip"
   },
-
-
-
+  {
+    id: "Dimension_Witch_Dorothy",
+    name: "次元の魔女・ドロシー",
+    reading: ["じげんのまじょどろしー", "ジゲンノマジョドロシー", "次元の魔女・ドロシー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/次元の魔女・ドロシー.zip"
+  },
+  {
+    id: "Imperial_Dragoon",
+    name: "インペリアルドラグーン",
+    reading: ["いんぺりあるどらぐーん", "インペリアルドラグーン", "インペリアルドラグーン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/インペリアルドラグーン.zip"
+  },
+  {
+    id: "Sahaquiel",
+    name: "サハクィエル",
+    reading: ["さはくぃえる", "サハクィエル", "サハクィエル"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ROB",
+    zip: "voices/ROB/Legendary/サハクィエル.zip"
+  }
 ];

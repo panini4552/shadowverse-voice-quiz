29c6257de2b5eef71dfdf2fb53f930f146d1df7e
panini4552 <panini4552@gmail.com>
2025-11-22 11:26:24 +0900

    Update Gold.js

--- data/DRK/Gold.js
+++ data/DRK/Gold.js
@@ -1,135 +1,128 @@
-export const Classic_Legendary = [
-  // === エルフ ===
-export const Classic_Gold = [
+export const DRK_Gold = [
   {
     id: "アザゼル",
     name: "アザゼル",
-    reading: ["あざぜる", "アザゼル"],
-    class: "ヴァンパイア",
+    reading: ["あざぜる", "アザゼル", "アザゼル"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/アザゼル.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/アザゼル.zip"
   },
   {
     id: "アークエンジェル・レイナ",
     name: "アークエンジェル・レイナ",
-    reading: ["あーくえんじぇるれいな", "アークエンジェルレイナ"],
-    class: "ニュートラル",
+    reading: ["あーくえんじぇるれいな", "アークエンジェルレイナ", "アークエンジェル・レイナ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/アークエンジェル・レイナ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/アークエンジェル・レイナ.zip"
   },
   {
     id: "エルフの少女・リザ",
     name: "エルフの少女・リザ",
-    reading: ["えるふのしょうじょりざ", "エルフノショウジョリザ"],
-    class: "エルフ",
+    reading: ["えるふのしょうじょりざ", "エルフノショウジョリザ", "エルフの少女・リザ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/エルフの少女・リザ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/エルフの少女・リザ.zip"
   },
   {
     id: "エルフナイト・シンシア",
     name: "エルフナイト・シンシア",
-    reading: ["えるふないとしんしあ", "エルフナイトシンシア"],
-    class: "エルフ",
+    reading: ["えるふないとしんしあ", "エルフナイトシンシア", "エルフナイト・シンシア"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/エルフナイト・シンシア.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/エルフナイト・シンシア.zip"
   },
   {
     id: "カグヤ",
     name: "カグヤ",
-    reading: ["かぐや", "カグヤ"],
-    class: "ビショップ",
+    reading: ["かぐや", "カグヤ", "カグヤ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/カグヤ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/カグヤ.zip"
   },
   {
     id: "グランドアーチャー・セルウィン",
     name: "グランドアーチャー・セルウィン",
-    reading: ["ぐらんどあーちゃーせるうぃん", "グランドアーチャーセルウィン"],
-    class: "エルフ",
+    reading: ["ぐらんどあーちゃーせるうぃん", "グランドアーチャーセルウィン", "グランドアーチャー・セルウィン"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/グランドアーチャー・セルウィン.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/グランドアーチャー・セルウィン.zip"
   },
   {
     id: "ケリドウェン",
     name: "ケリドウェン",
-    reading: ["けりどうぇん", "ケリドウェン"],
-    class: "ネクロマンサー",
+    reading: ["けりどうぇん", "ケリドウェン", "ケリドウェン"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/ケリドウェン.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/ケリドウェン.zip"
   },
   {
     id: "ネプチューン",
     name: "ネプチューン",
-    reading: ["ねぷちゅーん", "ネプチューン"],
-    class: "ドラゴン",
+    reading: ["ねぷちゅーん", "ネプチューン", "ネプチューン"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/ネプチューン.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/ネプチューン.zip"
   },
   {
     id: "ホワイトパラディン",
     name: "ホワイトパラディン",
-    reading: ["ほわいとぱらでぃん", "ホワイトパラディン"],
-    class: "ロイヤル",
+    reading: ["ほわいとぱらでぃん", "ホワイトパラディン", "ホワイトパラディン"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/ホワイトパラディン.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/ホワイトパラディン.zip"
   },
   {
     id: "レオニダス",
     name: "レオニダス",
-    reading: ["れおにだす", "レオニダス"],
-    class: "ロイヤル",
+    reading: ["れおにだす", "レオニダス", "レオニダス"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/レオニダス.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/レオニダス.zip"
   },
   {
     id: "古き魔術師・レヴィ",
     name: "古き魔術師・レヴィ",
-    reading: ["ふるきまじゅつしれゔぃ", "フルキマジュツシレヴィ"],
-    class: "ウィッチ",
+    reading: ["ふるきまじゅつしれゔぃ", "フルキマジュツシレヴィ", "古き魔術師・レヴィ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/古き魔術師・レヴィ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/古き魔術師・レヴィ.zip"
   },
   {
     id: "吸血姫・ヴァンピィ",
     name: "吸血姫・ヴァンピィ",
-    reading: ["きゅうけつきゔぁんぴぃ", "キュウケツキヴァンピィ"],
-    class: "ヴァンパイア",
+    reading: ["きゅうけつきゔぁんぴぃ", "キュウケツキヴァンピィ", "吸血姫・ヴァンピィ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/吸血姫・ヴァンピィ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/吸血姫・ヴァンピィ.zip"
   },
   {
     id: "天空の守護者・ガルラ",
     name: "天空の守護者・ガルラ",
-    reading: ["てんくうのしゅごしゃがるら", "テンクウノシュゴシャガルラ"],
-    class: "ビショップ",
+    reading: ["てんくうのしゅごしゃがるら", "テンクウノシュゴシャガルラ", "天空の守護者・ガルラ"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/天空の守護者・ガルラ.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/天空の守護者・ガルラ.zip"
   },
   {
     id: "忌むべき悪魔の像",
     name: "忌むべき悪魔の像",
-    reading: ["いむべきあくまのぞう", "イムベキアクマノゾウ"],
-    class: "ニュートラル",
+    reading: ["いむべきあくまのぞう", "イムベキアクマノゾウ", "忌むべき悪魔の像"],
+    class: "",
     rarity: "Gold",
-    pack: "Classic",
-    zip: "voices/Classic/Gold/忌むべき悪魔の像.zip"
+    pack: "DRK",
+    zip: "voices/DRK/Gold/忌むべき悪魔の像.zip"
   }
-];
-
-
-
-
 ];

89cb9423af10101ce6cb413a0f9720078209715c
panini4552 <panini4552@gmail.com>
2025-11-22 23:40:12 +0900

    Update Silver.js

--- data/ROB/Silver.js
+++ data/ROB/Silver.js
@@ -1,15 +1,233 @@
-export const Classic_Legendary = [
+export const ROB_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "クリスタリア・リリィ",
+    name: "クリスタリア・リリィ",
+    reading: ["くりすたりあ・りりぃ", "クリスタリア・リリィ", "クリスタリア・リリィ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/クリスタリア・リリィ.zip"
+  },
+  {
+    id: "天馬のエルフ",
+    name: "天馬のエルフ",
+    reading: ["てんまのえるふ", "テンマノエルフ", "天馬のエルフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/天馬のエルフ.zip"
+  },
+  {
+    id: "エルフの王子・レオネル",
+    name: "エルフの王子・レオネル",
+    reading: ["えるふのおうじ・れおねる", "エルフノオウジ・レオネル", "エルフの王子・レオネル"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/エルフの王子・レオネル.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "サブリーダー・ゲルト",
+    name: "サブリーダー・ゲルト",
+    reading: ["さぶりーだー・げると", "サブリーダー・ゲルト", "サブリーダー・ゲルト"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/サブリーダー・ゲルト.zip"
+  },
+  {
+    id: "レヴィオンヴァンガード・ジェノ",
+    name: "レヴィオンヴァンガード・ジェノ",
+    reading: ["れゔぃおんヴぁんがーど・じぇの", "レヴィオンヴァンガード・ジェノ", "レヴィオンヴァンガード・ジェノ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/レヴィオンヴァンガード・ジェノ.zip"
+  },
+  {
+    id: "蒼穹の提督・モニカ",
+    name: "蒼穹の提督・モニカ",
+    reading: ["そうきゅうのていとく・もにか", "ソウキュウノテイトク・モニカ", "蒼穹の提督・モニカ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/蒼穹の提督・モニカ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ドワーフアルケミスト",
+    name: "ドワーフアルケミスト",
+    reading: ["どわーふあるけみすと", "ドワーフアルケミスト", "ドワーフアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/ドワーフアルケミスト.zip"
+  },
+  {
+    id: "神秘の探求者・クラーク",
+    name: "神秘の探求者・クラーク",
+    reading: ["しんぴのたんきゅうしゃ・くらーく", "シンピノタンキュウシャ・クラーク", "神秘の探求者・クラーク"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/神秘の探求者・クラーク.zip"
+  },
+  {
+    id: "ディザスターウィッチ",
+    name: "ディザスターウィッチ",
+    reading: ["でぃざすたーうぃっち", "ディザスターウィッチ", "ディザスターウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/ディザスターウィッチ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ワイバーンライダー・エイファ",
+    name: "ワイバーンライダー・エイファ",
+    reading: ["わいばーんらいだー・えいふぁ", "ワイバーンライダー・エイファ", "ワイバーンライダー・エイファ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/ワイバーンライダー・エイファ.zip"
+  },
+  {
+    id: "輝石のドラゴン",
+    name: "輝石のドラゴン",
+    reading: ["きせきのどらごん", "キセキノドラゴン", "輝石のドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/輝石のドラゴン.zip"
+  },
+  {
+    id: "オウルガーディアン",
+    name: "オウルガーディアン",
+    reading: ["おうるがーでぃあん", "オウルガーディアン", "オウルガーディアン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/オウルガーディアン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "よろめく不死者",
+    name: "よろめく不死者",
+    reading: ["よろめくふししゃ", "ヨロメクフシシャ", "よろめく不死者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/よろめく不死者.zip"
+  },
+  {
+    id: "ネクロアサシン",
+    name: "ネクロアサシン",
+    reading: ["ねくろあさしん", "ネクロアサシン", "ネクロアサシン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/ネクロアサシン.zip"
+  },
+  {
+    id: "冥守の戦士・カムラ",
+    name: "冥守の戦士・カムラ",
+    reading: ["めいしゅのせんし・かむら", "メイシュノセンシ・カムラ", "冥守の戦士・カムラ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/冥守の戦士・カムラ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "吸血貴・ヴァイト",
+    name: "吸血貴・ヴァイト",
+    reading: ["きゅうけつき・ゔぁいと", "キュウケツキ・ヴァイト", "吸血貴・ヴァイト"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/吸血貴・ヴァイト.zip"
+  },
+  {
+    id: "千雨の槍使い",
+    name: "千雨の槍使い",
+    reading: ["ちさめのやりつかい", "チサメノヤリヅカイ", "千雨の槍使い"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/千雨の槍使い.zip"
+  },
+  {
+    id: "キャタラクトビースト",
+    name: "キャタラクトビースト",
+    reading: ["きゃたらくとびーすと", "キャタラクトビースト", "キャタラクトビースト"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/キャタラクトビースト.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "煌翼の戦士・リノ",
+    name: "煌翼の戦士・リノ",
+    reading: ["こうよくのせんし・りの", "コウヨクノセンシ・リノ", "煌翼の戦士・リノ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/煌翼の戦士・リノ.zip"
+  },
+  {
+    id: "邪悪なる予言者・ダムス",
+    name: "邪悪なる予言者・ダムス",
+    reading: ["じゃあくなるよげんしゃ・だむす", "ジャアクナルヨゲンシャ・ダムス", "邪悪なる予言者・ダムス"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/邪悪なる予言者・ダムス.zip"
+  },
+  {
+    id: "エンシェントレオスピリット",
+    name: "エンシェントレオスピリット",
+    reading: ["えんしぇんとれおすぴりっと", "エンシェントレオスピリット", "エンシェントレオスピリット"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/エンシェントレオスピリット.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "天弓の天使・リリエル",
+    name: "天弓の天使・リリエル",
+    reading: ["てんきゅうのてんし・りりえる", "テンキュウノテンシ・リリエル", "天弓の天使・リリエル"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/天弓の天使・リリエル.zip"
+  },
+  {
+    id: "覇食帝・カイザ",
+    name: "覇食帝・カイザ",
+    reading: ["はしょくてい・かいざ", "ハショクテイ・カイザ", "覇食帝・カイザ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/覇食帝・カイザ.zip"
+  },
+  {
+    id: "歴戦の傭兵・フィーナ",
+    name: "歴戦の傭兵・フィーナ",
+    reading: ["れきせんのようへい・ふぃーな", "レキセンノヨウヘイ・フィーナ", "歴戦の傭兵・フィーナ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ROB",
+    zip: "voices/ROB/Silver/歴戦の傭兵・フィーナ.zip"
+  }
 ];

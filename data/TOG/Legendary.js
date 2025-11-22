e5ae778a38d3e31e6179a3e1d4ed05904adb70a1
panini4552 <panini4552@gmail.com>
2025-11-22 23:50:33 +0900

    Update Legendary.js

--- data/TOG/Legendary.js
+++ data/TOG/Legendary.js
@@ -1,15 +1,146 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const TOG_Legendary = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "イスラーフィール",
+    name: "イスラーフィール",
+    reading: ["いすらーふぃーる", "イスラーフィール", "イスラーフィール"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "イスラーフィール.zip"
+  },
+  {
+    id: "ウロボロス",
+    name: "ウロボロス",
+    reading: ["うろぼろす", "ウロボロス", "ウロボロス"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "ウロボロス.zip"
+  },
+  {
+    id: "エルフクイーン",
+    name: "エルフクイーン",
+    reading: ["えるふくいーん", "エルフクイーン", "エルフクイーン"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "TOG",
+    zip: "エルフクイーン.zip"
   },
-
-
-
+  {
+    id: "ゼウス",
+    name: "ゼウス",
+    reading: ["ぜうす", "ゼウス", "ゼウス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "ゼウス.zip"
+  },
+  {
+    id: "ダークジャンヌ",
+    name: "ダークジャンヌ",
+    reading: ["だーくじゃんぬ", "ダークジャンヌ", "ダークジャンヌ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "ダークジャンヌ.zip"
+  },
+  {
+    id: "ヘヴンリーイージス",
+    name: "ヘヴンリーイージス",
+    reading: ["へヴんりーいーじす", "ヘヴンリーイージス", "ヘヴンリーイージス"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "ヘヴンリーイージス.zip"
+  },
+  {
+    id: "ベルフェゴール",
+    name: "ベルフェゴール",
+    reading: ["べるふぇごーる", "ベルフェゴール", "ベルフェゴール"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "ベルフェゴール.zip"
+  },
+  {
+    id: "メイルストロームサーペント",
+    name: "メイルストロームサーペント",
+    reading: ["めいるすとろーむさーぺんと", "メイルストロームサーペント", "メイルストロームサーペント"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "メイルストロームサーペント.zip"
+  },
+  {
+    id: "円卓の騎士・ガウェイン",
+    name: "円卓の騎士・ガウェイン",
+    reading: ["えんたくのきし・がうぇいん", "エンタクノキシ・ガウェイン", "円卓の騎士・ガウェイン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "円卓の騎士・ガウェイン.zip"
+  },
+  {
+    id: "言霊遣い・ジンジャー",
+    name: "言霊遣い・ジンジャー",
+    reading: ["ことだまづかい・じんじゃー", "コトダマヅカイ・ジンジャー", "言霊遣い・ジンジャー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "言霊遣い・ジンジャー.zip"
+  },
+  {
+    id: "魂の番人・ミント",
+    name: "魂の番人・ミント",
+    reading: ["たましいのばんにん・みんと", "タマシイノバンニン・ミント", "魂の番人・ミント"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "魂の番人・ミント.zip"
+  },
+  {
+    id: "深き森の異形",
+    name: "深き森の異形",
+    reading: ["ふかきもりのいぎょう", "フカキモリノイギョウ", "深き森の異形"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "深き森の異形.zip"
+  },
+  {
+    id: "水竜神の巫女",
+    name: "水竜神の巫女",
+    reading: ["すいりゅうしんのみこ", "スイリュウシンノミコ", "水竜神の巫女"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "水竜神の巫女.zip"
+  },
+  {
+    id: "不滅の英雄・ローラン",
+    name: "不滅の英雄・ローラン",
+    reading: ["ふめつのえいゆう・ろーらん", "フメツノエイユウ・ローラン", "不滅の英雄・ローラン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "不滅の英雄・ローラン.zip"
+  },
+  {
+    id: "魔将軍・ヘクター",
+    name: "魔将軍・ヘクター",
+    reading: ["ましょうぐん・へくたー", "マショウグン・ヘクター", "魔将軍・ヘクター"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "魔将軍・ヘクター.zip"
+  },
+  {
+    id: "魔導の巨兵",
+    name: "魔導の巨兵",
+    reading: ["まどうのきょへい", "マドウノキョヘイ", "魔導の巨兵"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "TOG",
+    zip: "魔導の巨兵.zip"
+  }
 ];

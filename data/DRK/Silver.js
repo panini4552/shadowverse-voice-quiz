6d00854b9f8c44cb277fed37de907ed49471cb7e
panini4552 <panini4552@gmail.com>
2025-11-22 11:37:24 +0900

    Update Silver.js

--- data/DRK/Silver.js
+++ data/DRK/Silver.js
@@ -1,15 +1,182 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const DRK_Silver = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    id: "アンドラス",
+    name: "アンドラス",
+    reading: ["あんどらす", "アンドラス", "アンドラス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/アンドラス.zip"
   },
-
-
-
+  {
+    id: "エルフキング・ヴァルト",
+    name: "エルフキング・ヴァルト",
+    reading: ["えるふきんぐゔぁると", "エルフキングヴァルト", "エルフキング・ヴァルト"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/エルフキング・ヴァルト.zip"
+  },
+  {
+    id: "カローン",
+    name: "カローン",
+    reading: ["かろーん", "カローン", "カローン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/カローン.zip"
+  },
+  {
+    id: "ガンナーメイド・セリエ",
+    name: "ガンナーメイド・セリエ",
+    reading: ["がんなーめいどせりえ", "ガンナーメイドセリエ", "ガンナーメイド・セリエ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ガンナーメイド・セリエ.zip"
+  },
+  {
+    id: "コウガクノイチ",
+    name: "コウガクノイチ",
+    reading: ["こうがくのいち", "コウガクノイチ", "コウガクノイチ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/コウガクノイチ.zip"
+  },
+  {
+    id: "シャドウウィッチ",
+    name: "シャドウウィッチ",
+    reading: ["しゃどううぃっち", "シャドウウィッチ", "シャドウウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/シャドウウィッチ.zip"
+  },
+  {
+    id: "ジャガーノート",
+    name: "ジャガーノート",
+    reading: ["じゃがーのーと", "ジャガーノート", "ジャガーノート"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ジャガーノート.zip"
+  },
+  {
+    id: "ジークフリート",
+    name: "ジークフリート",
+    reading: ["じーくふりーと", "ジークフリート", "ジークフリート"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ジークフリート.zip"
+  },
+  {
+    id: "スレッジエクソシスト",
+    name: "スレッジエクソシスト",
+    reading: ["すれっじえくそしすと", "スレッジエクソシスト", "スレッジエクソシスト"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/スレッジエクソシスト.zip"
+  },
+  {
+    id: "デュアルウィッチ・レミラミ",
+    name: "デュアルウィッチ・レミラミ",
+    reading: ["でゅあるうぃっちれみらみ", "デュアルウィッチレミラミ", "デュアルウィッチ・レミラミ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/デュアルウィッチ・レミラミ.zip"
+  },
+  {
+    id: "ノクシャスロアードラゴン",
+    name: "ノクシャスロアードラゴン",
+    reading: ["のくしゃすろあーどらごん", "ノクシャスロアードラゴン", "ノクシャスロアードラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ノクシャスロアードラゴン.zip"
+  },
+  {
+    id: "ハンプティダンプティ",
+    name: "ハンプティダンプティ",
+    reading: ["はんぷてぃだんぷてぃ", "ハンプティダンプティ", "ハンプティダンプティ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ハンプティダンプティ.zip"
+  },
+  {
+    id: "ボーンキマイラ",
+    name: "ボーンキマイラ",
+    reading: ["ぼーんきまいら", "ボーンキマイラ", "ボーンキマイラ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ボーンキマイラ.zip"
+  },
+  {
+    id: "ユニコーンの踊り手・ユニコ",
+    name: "ユニコーンの踊り手・ユニコ",
+    reading: ["ゆにこーんのおどりてゆにこ", "ユニコーンノオドリテユニコ", "ユニコーンの踊り手・ユニコ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ユニコーンの踊り手・ユニコ.zip"
+  },
+  {
+    id: "ラビリンスデビル",
+    name: "ラビリンスデビル",
+    reading: ["らびりんすでびる", "ラビリンスデビル", "ラビリンスデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/ラビリンスデビル.zip"
+  },
+  {
+    id: "レディアンスエンジェル",
+    name: "レディアンスエンジェル",
+    reading: ["れでぃあんすえんじぇる", "レディアンスエンジェル", "レディアンスエンジェル"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/レディアンスエンジェル.zip"
+  },
+  {
+    id: "冷酷なる暗殺者",
+    name: "冷酷なる暗殺者",
+    reading: ["れいこくなるあんさつしゃ", "レイコクナルアンサツシャ", "冷酷なる暗殺者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/冷酷なる暗殺者.zip"
+  },
+  {
+    id: "千年妖狐・ユエル",
+    name: "千年妖狐・ユエル",
+    reading: ["せんねんようこゆえる", "センネンヨウコユエル", "千年妖狐・ユエル"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/千年妖狐・ユエル.zip"
+  },
+  {
+    id: "変異の巨竜",
+    name: "変異の巨竜",
+    reading: ["へんいのきょりゅう", "ヘンイノキョリュウ", "変異の巨竜"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/変異の巨竜.zip"
+  },
+  {
+    id: "太古の森神",
+    name: "太古の森神",
+    reading: ["たいこのしんしん", "タイコノシンシン", "太古の森神"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "DRK",
+    zip: "voices/DRK/Silver/太古の森神.zip"
+  }
 ];

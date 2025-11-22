f648d545c92e73b990f4b3fe5add13ed6b09894d
panini4552 <panini4552@gmail.com>
2025-11-22 23:34:23 +0900

    Update Gold.js

--- data/ROB/Gold.js
+++ data/ROB/Gold.js
@@ -1,15 +1,155 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const ROB_Gold = [
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    id: "Valhalla_General",
+    name: "ヴァルハラジェネラル",
+    reading: ["ゔぁるはらじぇねらる", "ヴァルハラジェネラル", "ヴァルハラジェネラル"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/ヴァルハラジェネラル.zip"
   },
-
-
-
+  {
+    id: "Uriel",
+    name: "ウリエル",
+    reading: ["うりえる", "ウリエル", "ウリエル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/ウリエル.zip"
+  },
+  {
+    id: "Goblin_Slayer_Lucius",
+    name: "ゴブリンスレイヤー・ルシウス",
+    reading: ["ごぶりんすれいやーるしうす", "ゴブリンスレイヤールシウス", "ゴブリンスレイヤー・ルシウス"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/ゴブリンスレイヤー・ルシウス.zip"
+  },
+  {
+    id: "Goblin_Breaker_Tina",
+    name: "ゴブリンブレイカー・ティナ",
+    reading: ["ごぶりんぶれいかーてぃな", "ゴブリンブレイカーティナ", "ゴブリンブレイカー・ティナ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/ゴブリンブレイカー・ティナ.zip"
+  },
+  {
+    id: "Shadow_Reaper",
+    name: "シャドウリーパー",
+    reading: ["しゃどうりーぱー", "シャドウリーパー", "シャドウリーパー"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/シャドウリーパー.zip"
+  },
+  {
+    id: "Balor",
+    name: "バロール",
+    reading: ["ばろーる", "バロール", "バロール"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/バロール.zip"
+  },
+  {
+    id: "Fang_Slayer",
+    name: "ファングスレイヤー",
+    reading: ["ふぁんぐすれいやー", "ファングスレイヤー", "ファングスレイヤー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/ファングスレイヤー.zip"
+  },
+  {
+    id: "Princess_Manaria_Anne",
+    name: "プリンセスマナリア・アン",
+    reading: ["ぷりんせすまなりああん", "プリンセスマナリアアン", "プリンセスマナリア・アン"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/プリンセスマナリア・アン.zip"
+  },
+  {
+    id: "Mastema",
+    name: "マステマ",
+    reading: ["ますてま", "マステマ", "マステマ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/マステマ.zip"
+  },
+  {
+    id: "Manaria_Draco_Grea",
+    name: "マナリアドラコ・グレア",
+    reading: ["まなりあどらこぐれあ", "マナリアドラコグレア", "マナリアドラコ・グレア"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/マナリアドラコ・グレア.zip"
+  },
+  {
+    id: "Mahes",
+    name: "マヘス",
+    reading: ["まへす", "マヘス", "マヘス"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/マヘス.zip"
+  },
+  {
+    id: "Masamune_Date",
+    name: "伊達政宗",
+    reading: ["だてまさむね", "ダテマサムネ", "伊達政宗"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/伊達政宗.zip"
+  },
+  {
+    id: "Forbidden_Researcher",
+    name: "禁忌の研究者",
+    reading: ["きんきのけんきゅうしゃ", "キンキノケンキュウシャ", "禁忌の研究者"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/禁忌の研究者.zip"
+  },
+  {
+    id: "Silver_Knight_Emilia",
+    name: "白銀の騎士・エミリア",
+    reading: ["はくぎんのきしえみりあ", "ハクギンノキシエミリア", "白銀の騎士・エミリア"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/白銀の騎士・エミリア.zip"
+  },
+  {
+    id: "Iceblade_Demon",
+    name: "氷剣の戦鬼",
+    reading: ["ひょうけんのせんき", "ヒョウケンノセンキ", "氷剣の戦鬼"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/氷剣の戦鬼.zip"
+  },
+  {
+    id: "Gem_Elf",
+    name: "宝石のエルフ",
+    reading: ["ほうせきのえるふ", "ホウセキノエルフ", "宝石のエルフ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/宝石のエルフ.zip"
+  },
+  {
+    id: "Gleaming_Horn_Sarissa",
+    name: "煌角の戦士・サリッサ",
+    reading: ["こうかくのせんしさりっさ", "コウカクノセンシサリッサ", "煌角の戦士・サリッサ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ROB",
+    zip: "voices/ROB/Gold/煌角の戦士・サリッサ.zip"
+  }
 ];

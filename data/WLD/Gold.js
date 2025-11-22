b5ce0b60805e32dca6985952a1d436549c298d15
panini4552 <panini4552@gmail.com>
2025-11-23 00:43:27 +0900

    Update Gold.js

--- data/WLD/Gold.js
+++ data/WLD/Gold.js
@@ -1,15 +1,170 @@
-export const Classic_Legendary = [
+export const WLD_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "大魔法の妖精・リラ",
+    name: "大魔法の妖精・リラ",
+    reading: ["だいまほうのようせい・りら", "ダイマホウノヨウセイ・リラ", "大魔法の妖精・リラ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/大魔法の妖精・リラ.zip"
+  },
+  {
+    id: "妖精の使役者",
+    name: "妖精の使役者",
+    reading: ["ようせいのしえきしゃ", "ヨウセイノシエキシャ", "妖精の使役者"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/妖精の使役者.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "赤ずきん・メイジー",
+    name: "赤ずきん・メイジー",
+    reading: ["あかずきん・めいじー", "アカズキン・メイジー", "赤ずきん・メイジー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/赤ずきん・メイジー.zip"
+  },
+  {
+    id: "銀嶺の秘剣士",
+    name: "銀嶺の秘剣士",
+    reading: ["ぎんれいのひけんし", "ギンレイノヒケンシ", "銀嶺の秘剣士"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/銀嶺の秘剣士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ルナルの魔術師・プリス",
+    name: "ルナルの魔術師・プリス",
+    reading: ["るなるのまじゅつし・ぷりす", "ルナルノマジュツシ・プリス", "ルナルの魔術師・プリス"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ルナルの魔術師・プリス.zip"
+  },
+  {
+    id: "偉大なる魔術師・レヴィ",
+    name: "偉大なる魔術師・レヴィ",
+    reading: ["いだいなるまじゅつし・れび", "イダイナルマジュツシ・レヴィ", "偉大なる魔術師・レヴィ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/偉大なる魔術師・レヴィ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "戯曲の竜剣士",
+    name: "戯曲の竜剣士",
+    reading: ["ぎきょくのりゅうけんし", "ギキョクノリュウケンシ", "戯曲の竜剣士"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/戯曲の竜剣士.zip"
+  },
+  {
+    id: "赤き猛竜",
+    name: "赤き猛竜",
+    reading: ["あかきもうりゅう", "アカキモウリュウ", "赤き猛竜"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/赤き猛竜.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ブラックスワン・オディール",
+    name: "ブラックスワン・オディール",
+    reading: ["ぶらっくすわん・おでぃーる", "ブラックスワン・オディール", "ブラックスワン・オディール"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ブラックスワン・オディール.zip"
+  },
+  {
+    id: "ジェド・マロース",
+    name: "ジェド・マロース",
+    reading: ["じぇど・まろーす", "ジェド・マロース", "ジェド・マロース"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ジェド・マロース.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "デモンオフィサー・エメラダ",
+    name: "デモンオフィサー・エメラダ",
+    reading: ["でもんおふぃさー・えめらだ", "デモンオフィサー・エメラダ", "デモンオフィサー・エメラダ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/デモンオフィサー・エメラダ.zip"
+  },
+  {
+    id: "ファントムキャット",
+    name: "ファントムキャット",
+    reading: ["ふぁんとむきゃっと", "ファントムキャット", "ファントムキャット"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ファントムキャット.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "赤枝の聖騎士・ノイシュ",
+    name: "赤枝の聖騎士・ノイシュ",
+    reading: ["あかえだのせいきし・のいしゅ", "アカエダノセイキシ・ノイシュ", "赤枝の聖騎士・ノイシュ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/赤枝の聖騎士・ノイシュ.zip"
+  },
+  {
+    id: "ホワイトスワン・オデット",
+    name: "ホワイトスワン・オデット",
+    reading: ["ほわいとすわん・おでっと", "ホワイトスワン・オデット", "ホワイトスワン・オデット"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ホワイトスワン・オデット.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ヘクトル",
+    name: "ヘクトル",
+    reading: ["へくとる", "ヘクトル", "ヘクトル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ヘクトル.zip"
+  },
+  {
+    id: "ラプンツェル",
+    name: "ラプンツェル",
+    reading: ["らぷんつぇる", "ラプンツェル", "ラプンツェル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/ラプンツェル.zip"
+  },
+  {
+    id: "白猫の賢者・スノウ",
+    name: "白猫の賢者・スノウ",
+    reading: ["しろねこのけんじゃ・すのう", "シロネコノケンジャ・スノウ", "白猫の賢者・スノウ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "WLD",
+    zip: "voices/WLD/Gold/白猫の賢者・スノウ.zip"
+  }
 ];

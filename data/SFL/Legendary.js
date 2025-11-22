7c1a1e45e7496fe6f6cb28daf06522921191a785
panini4552 <panini4552@gmail.com>
2025-11-23 00:56:49 +0900

    Update Legendary.js

--- data/SFL/Legendary.js
+++ data/SFL/Legendary.js
@@ -1,15 +1,161 @@
-export const Classic_Legendary = [
+export const SFL_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "カシオペア",
+    name: "カシオペア",
+    reading: ["かしおぺあ", "カシオペア", "カシオペア"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/カシオペア.zip"
+  },
+  {
+    id: "キングエレファント",
+    name: "キングエレファント",
+    reading: ["きんぐえれふぁんと", "キングエレファント", "キングエレファント"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/キングエレファント.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "静かなる炎将・マーズ",
+    name: "静かなる炎将・マーズ",
+    reading: ["しずかなるえんしょうまーず", "シズカナルエンショウマーズ", "静かなる炎将・マーズ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/静かなる炎将・マーズ.zip"
+  },
+  {
+    id: "バルバロッサ",
+    name: "バルバロッサ",
+    reading: ["ばるばろっさ", "バルバロッサ", "バルバロッサ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/バルバロッサ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ギガントキマイラ",
+    name: "ギガントキマイラ",
+    reading: ["ぎがんときまいら", "ギガントキマイラ", "ギガントキマイラ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/ギガントキマイラ.zip"
+  },
+  {
+    id: "星読みの魔女・ステラ",
+    name: "星読みの魔女・ステラ",
+    reading: ["ほしよみのまじょすてら", "ホシヨミノマジョステラ", "星読みの魔女・ステラ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/星読みの魔女・ステラ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "原初の竜使い",
+    name: "原初の竜使い",
+    reading: ["げんしょのりゅうづかい", "ゲンショノリュウヅカイ", "原初の竜使い"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/原初の竜使い.zip"
+  },
+  {
+    id: "ピュートーン",
+    name: "ピュートーン",
+    reading: ["ぴゅーとーん", "ピュートーン", "ピュートーン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/ピュートーン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "滅殺の鎧",
+    name: "滅殺の鎧",
+    reading: ["めっさつのよろい", "メッサツノヨロイ", "滅殺の鎧"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/滅殺の鎧.zip"
+  },
+  {
+    id: "トワイライトクイーン",
+    name: "トワイライトクイーン",
+    reading: ["とわいらいとくいーん", "トワイライトクイーン", "トワイライトクイーン"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/トワイライトクイーン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "毒牙の姫・メドゥーサ",
+    name: "毒牙の姫・メドゥーサ",
+    reading: ["どくがのひめめどぅーさ", "ドクガノヒメメドゥーサ", "毒牙の姫・メドゥーサ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/毒牙の姫・メドゥーサ.zip"
+  },
+  {
+    id: "ヨルムンガンド",
+    name: "ヨルムンガンド",
+    reading: ["よるむんがんど", "ヨルムンガンド", "ヨルムンガンド"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/ヨルムンガンド.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "邪教の権化",
+    name: "邪教の権化",
+    reading: ["じゃきょうのごんげ", "ジャキョウノゴンゲ", "邪教の権化"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/邪教の権化.zip"
+  },
+  {
+    id: "白翼の守護神・アイテール",
+    name: "白翼の守護神・アイテール",
+    reading: ["はくよくのしゅごしんあいてーる", "ハクヨクノシュゴシンアイテール", "白翼の守護神・アイテール"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/白翼の守護神・アイテール.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゾディアックデーモン",
+    name: "ゾディアックデーモン",
+    reading: ["ぞでぃあっくでーもん", "ゾディアックデーモン", "ゾディアックデーモン"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/ゾディアックデーモン.zip"
+  },
+  {
+    id: "ロキ",
+    name: "ロキ",
+    reading: ["ろき", "ロキ", "ロキ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "SFL",
+    zip: "voices/SFL/Legendary/ロキ.zip"
+  }
 ];

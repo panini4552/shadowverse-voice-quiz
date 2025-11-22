a32b5d7a9128f3f827624fe8eacc75773f871ccf
panini4552 <panini4552@gmail.com>
2025-11-23 00:50:57 +0900

    Update Bronze.js

--- data/WLD/Bronze.js
+++ data/WLD/Bronze.js
@@ -1,15 +1,233 @@
-export const Classic_Legendary = [
+export const WLD_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "フラワープリンセス",
+    name: "フラワープリンセス",
+    reading: ["ふらわーぷりんせす", "フラワープリンセス", "フラワープリンセス"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/フラワープリンセス.zip"
+  },
+  {
+    id: "ティートル・ディ＆ダム",
+    name: "ティートル・ディ＆ダム",
+    reading: ["てぃーとるでぃあんどだむ", "ティートル・ディ＆ダム", "ティートル・ディ＆ダム"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ティートル・ディ＆ダム.zip"
+  },
+  {
+    id: "心優しき木人",
+    name: "心優しき木人",
+    reading: ["こころやさしきぼくじん", "ココロヤサシキボクジン", "心優しき木人"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/心優しき木人.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "兎耳の従者",
+    name: "兎耳の従者",
+    reading: ["とみみのじゅうしゃ", "トミミノジュウシャ", "兎耳の従者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/兎耳の従者.zip"
+  },
+  {
+    id: "マスタークノイチ",
+    name: "マスタークノイチ",
+    reading: ["ますたーくのいち", "マスタークノイチ", "マスタークノイチ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/マスタークノイチ.zip"
+  },
+  {
+    id: "アックスデストロイヤー",
+    name: "アックスデストロイヤー",
+    reading: ["あっくすですとろいやー", "アックスデストロイヤー", "アックスデストロイヤー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/アックスデストロイヤー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "スイーツウィッチ",
+    name: "スイーツウィッチ",
+    reading: ["すいーつうぃっち", "スイーツウィッチ", "スイーツウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/スイーツウィッチ.zip"
+  },
+  {
+    id: "マナリアカーバンクル",
+    name: "マナリアカーバンクル",
+    reading: ["まなりあかーばんくる", "マナリアカーバンクル", "マナリアカーバンクル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/マナリアカーバンクル.zip"
+  },
+  {
+    id: "見習い魔法剣士",
+    name: "見習い魔法剣士",
+    reading: ["みならいまほうけんし", "ミナライマホウケンシ", "見習い魔法剣士"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/見習い魔法剣士.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ドラゴサモナー",
+    name: "ドラゴサモナー",
+    reading: ["どらごさもなー", "ドラゴサモナー", "ドラゴサモナー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ドラゴサモナー.zip"
+  },
+  {
+    id: "大亀の長老",
+    name: "大亀の長老",
+    reading: ["おおがめのちょうろう", "オオガメノチョウロウ", "大亀の長老"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/大亀の長老.zip"
+  },
+  {
+    id: "漆黒の獣戦士",
+    name: "漆黒の獣戦士",
+    reading: ["しっこくのじゅうせんし", "シッコクノジュウセンシ", "漆黒の獣戦士"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/漆黒の獣戦士.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "デーモンイーター",
+    name: "デーモンイーター",
+    reading: ["でーもんいーたー", "デーモンイーター", "デーモンイーター"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/デーモンイーター.zip"
+  },
+  {
+    id: "スケルトンオーガ",
+    name: "スケルトンオーガ",
+    reading: ["すけるとんおーが", "スケルトンオーガ", "スケルトンオーガ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/スケルトンオーガ.zip"
+  },
+  {
+    id: "マッドハッター",
+    name: "マッドハッター",
+    reading: ["まっどはったー", "マッドハッター", "マッドハッター"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/マッドハッター.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "トーヴ",
+    name: "トーヴ",
+    reading: ["とーゔ", "トーヴ", "トーヴ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/トーヴ.zip"
+  },
+  {
+    id: "豪拳の用心棒",
+    name: "豪拳の用心棒",
+    reading: ["ごうけんのようじんぼう", "ゴウケンノヨウジンボウ", "豪拳の用心棒"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/豪拳の用心棒.zip"
+  },
+  {
+    id: "ひねくれ者の悪魔",
+    name: "ひねくれ者の悪魔",
+    reading: ["ひねくれもののあくま", "ヒネクレモノノアクマ", "ひねくれ者の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ひねくれ者の悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ホワイトタイガー",
+    name: "ホワイトタイガー",
+    reading: ["ほわいとたいがー", "ホワイトタイガー", "ホワイトタイガー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ホワイトタイガー.zip"
+  },
+  {
+    id: "鳥飼いの使徒",
+    name: "鳥飼いの使徒",
+    reading: ["とりかいのしと", "トリカイノシト", "鳥飼いの使徒"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/鳥飼いの使徒.zip"
+  },
+  {
+    id: "清浄の僧侶",
+    name: "清浄の僧侶",
+    reading: ["せいじょうのそうりょ", "セイジョウノソウリョ", "清浄の僧侶"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/清浄の僧侶.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリンリーダー",
+    name: "ゴブリンリーダー",
+    reading: ["ごぶりんりーだー", "ゴブリンリーダー", "ゴブリンリーダー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ゴブリンリーダー.zip"
+  },
+  {
+    id: "エンジェルナイト",
+    name: "エンジェルナイト",
+    reading: ["えんじぇるないと", "エンジェルナイト", "エンジェルナイト"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/エンジェルナイト.zip"
+  },
+  {
+    id: "ダラダラ天使・エフェメラ",
+    name: "ダラダラ天使・エフェメラ",
+    reading: ["だらだらてんしえふぇめら", "ダラダラテンシエフェメラ", "ダラダラ天使・エフェメラ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "WLD",
+    zip: "voices/WLD/Bronze/ダラダラ天使・エフェメラ.zip"
+  }
 ];

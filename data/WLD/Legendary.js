e58a2542a9a1ffc77c1be573d67993c2809152d4
panini4552 <panini4552@gmail.com>
2025-11-23 00:38:52 +0900

    Update Legendary.js

--- data/WLD/Legendary.js
+++ data/WLD/Legendary.js
@@ -1,15 +1,161 @@
-export const Classic_Legendary = [
+export const WLD_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ビューティ＆ビースト",
+    name: "ビューティ＆ビースト",
+    reading: ["びゅーてぃあんどびーすと", "ビューティアンドビースト"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/ビューティ＆ビースト.zip"
+  },
+  {
+    id: "フェアリードラゴン",
+    name: "フェアリードラゴン",
+    reading: ["ふぇありーどらごん", "フェアリードラゴン"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/フェアリードラゴン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "古の英雄",
+    name: "古の英雄",
+    reading: ["いにしえのえいゆう", "イニシエノエイユウ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/古の英雄.zip"
+  },
+  {
+    id: "シンデレラ",
+    name: "シンデレラ",
+    reading: ["しんでれら", "シンデレラ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/シンデレラ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "呼び覚まされし禁忌",
+    name: "呼び覚まされし禁忌",
+    reading: ["よびさまされしきんき", "ヨビサマサレシキンキ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/呼び覚まされし禁忌.zip"
+  },
+  {
+    id: "オズの大魔女",
+    name: "オズの大魔女",
+    reading: ["おずのだいまじょ", "オズノダイマジョ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/オズの大魔女.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ジャバウォック",
+    name: "ジャバウォック",
+    reading: ["じゃばうぉっく", "ジャバウォック"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/ジャバウォック.zip"
+  },
+  {
+    id: "天窮の竜神",
+    name: "天窮の竜神",
+    reading: ["てんきゅうのりゅうじん", "テンキュウノリュウジン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/天窮の竜神.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ダークアリス",
+    name: "ダークアリス",
+    reading: ["だーくありす", "ダークアリス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/ダークアリス.zip"
+  },
+  {
+    id: "災厄の屍王",
+    name: "災厄の屍王",
+    reading: ["さいやくのしおう", "サイヤクノシオウ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/災厄の屍王.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "昏き底より出でる者",
+    name: "昏き底より出でる者",
+    reading: ["くらきそこよりいでるもの", "クラキソコヨリイデルモノ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/昏き底より出でる者.zip"
+  },
+  {
+    id: "邪悪なる妖精・カラボス",
+    name: "邪悪なる妖精・カラボス",
+    reading: ["じゃあくなるようせい・からぼす", "ジャアクナルヨウセイ・カラボス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/邪悪なる妖精・カラボス.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "黄金郷の獅子",
+    name: "黄金郷の獅子",
+    reading: ["おうごんきょうのしし", "オウゴンキョウノシシ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/黄金郷の獅子.zip"
+  },
+  {
+    id: "スノーホワイトプリンセス",
+    name: "スノーホワイトプリンセス",
+    reading: ["すのーほわいとぷりんせす", "スノーホワイトプリンセス"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/スノーホワイトプリンセス.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "不思議の探求者・アリス",
+    name: "不思議の探求者・アリス",
+    reading: ["ふしぎのたんきゅうしゃ・ありす", "フシギノタンキュウシャ・アリス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/不思議の探求者・アリス.zip"
+  },
+  {
+    id: "魔海の女王",
+    name: "魔海の女王",
+    reading: ["まかいのじょおう", "マカイノジョオウ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "WLD",
+    zip: "voices/WLD/Legendary/魔海の女王.zip"
+  }
 ];

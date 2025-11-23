da2e927dd7dae716efefced70d14e728658667ac
panini4552 <panini4552@gmail.com>
2025-11-24 00:44:17 +0900

    Update Bronze.js

--- data/DBN/Bronze.js
+++ data/DBN/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const DBN_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "エルフジェネラル",
+    name: "エルフジェネラル",
+    reading: ["えるふじぇねらる", "エルフジェネラル"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/エルフジェネラル.zip"
+  },
+  {
+    id: "バッタの指揮者",
+    name: "バッタの指揮者",
+    reading: ["ばったのしきしゃ", "バッタの指揮者"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/バッタの指揮者.zip"
+  },
+  {
+    id: "魔界樹の下僕",
+    name: "魔界樹の下僕",
+    reading: ["まかいじゅのしもべ", "魔界樹の下僕"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/魔界樹の下僕.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "マスターサムライ",
+    name: "マスターサムライ",
+    reading: ["ますたーさむらい", "マスターサムライ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/マスターサムライ.zip"
+  },
+  {
+    id: "ホーリーナイトベア",
+    name: "ホーリーナイトベア",
+    reading: ["ほーりーないとべあ", "ホーリーナイトベア"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/ホーリーナイトベア.zip"
+  },
+  {
+    id: "アックスプリンセス",
+    name: "アックスプリンセス",
+    reading: ["あっくすぷりんせす", "アックスプリンセス"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/アックスプリンセス.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "月影の魔法使い",
+    name: "月影の魔法使い",
+    reading: ["つきかげのまほうつかい", "月影の魔法使い"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/月影の魔法使い.zip"
+  },
+  {
+    id: "未来視の魔女",
+    name: "未来視の魔女",
+    reading: ["みらいしのまじょ", "未来視の魔女"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/未来視の魔女.zip"
+  },
+  {
+    id: "獣面の魔術師",
+    name: "獣面の魔術師",
+    reading: ["じゅうめんのまじゅつし", "獣面の魔術師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/獣面の魔術師.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "シャドウドラグーン・コーダ",
+    name: "シャドウドラグーン・コーダ",
+    reading: ["しゃどうどらぐーんこーだ", "シャドウドラグーン・コーダ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/シャドウドラグーン・コーダ.zip"
+  },
+  {
+    id: "竜翼の暗殺者",
+    name: "竜翼の暗殺者",
+    reading: ["りゅうよくのあんさつしゃ", "竜翼の暗殺者"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/竜翼の暗殺者.zip"
+  },
+  {
+    id: "竜装の大剣士",
+    name: "竜装の大剣士",
+    reading: ["りゅうそうのだいけんし", "竜装の大剣士"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/竜装の大剣士.zip"
+  },
+  {
+    id: "まどろみの白竜",
+    name: "まどろみの白竜",
+    reading: ["まどろみのはくりゅう", "まどろみの白竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/まどろみの白竜.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "夜想の死神",
+    name: "夜想の死神",
+    reading: ["やそうのしにがみ", "夜想の死神"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/夜想の死神.zip"
+  },
+  {
+    id: "骸の代弁者",
+    name: "骸の代弁者",
+    reading: ["むくろのだいべんしゃ", "骸の代弁者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/骸の代弁者.zip"
+  },
+  {
+    id: "悪戯な霊魂",
+    name: "悪戯な霊魂",
+    reading: ["いたずらなれいこん", "悪戯な霊魂"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/悪戯な霊魂.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ナイトメアデビル",
+    name: "ナイトメアデビル",
+    reading: ["ないとめあでびる", "ナイトメアデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/ナイトメアデビル.zip"
+  },
+  {
+    id: "激情の悪魔",
+    name: "激情の悪魔",
+    reading: ["げきじょうのあくま", "激情の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/激情の悪魔.zip"
+  },
+  {
+    id: "災厄の魔神",
+    name: "災厄の魔神",
+    reading: ["さいやくのまじん", "災厄の魔神"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/災厄の魔神.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "リュカオン",
+    name: "リュカオン",
+    reading: ["りゅかおん", "リュカオン"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/リュカオン.zip"
+  },
+  {
+    id: "デスサイズシスター",
+    name: "デスサイズシスター",
+    reading: ["ですさいずしすたー", "デスサイズシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/デスサイズシスター.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "冷酷な人形遣い",
+    name: "冷酷な人形遣い",
+    reading: ["れいこくなにんぎょうつかい", "冷酷な人形遣い"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/冷酷な人形遣い.zip"
+  },
+  {
+    id: "ジャンク",
+    name: "ジャンク",
+    reading: ["じゃんく", "ジャンク"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/ジャンク.zip"
+  },
+  {
+    id: "歴史を知る者",
+    name: "歴史を知る者",
+    reading: ["れきしをしるもの", "歴史を知る者"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/歴史を知る者.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "混沌のヴァルキリー",
+    name: "混沌のヴァルキリー",
+    reading: ["こんとんのゔぁるきりー", "混沌のヴァルキリー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/混沌のヴァルキリー.zip"
+  },
+  {
+    id: "タルトマン",
+    name: "タルトマン",
+    reading: ["たるとまん", "タルトマン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DBN",
+    zip: "voices/DBN/Bronze/タルトマン.zip"
+  }
 ];

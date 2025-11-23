5ffbf69b9432429b940d560855235a349c18334c
panini4552 <panini4552@gmail.com>
2025-11-24 00:10:26 +0900

    Update Gold.js

--- data/CGS/Gold.js
+++ data/CGS/Gold.js
@@ -1,15 +1,199 @@
-export const Classic_Legendary = [
+export const CGS_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "エルフの鍛冶師・レーネ",
+    name: "エルフの鍛冶師・レーネ",
+    reading: ["えるふのかじし・れーね", "エルフノカジシ・レーネ", "エルフの鍛冶師・レーネ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/エルフの鍛冶師・レーネ.zip"
+  },
+  {
+    id: "フェアリーセイバー",
+    name: "フェアリーセイバー",
+    reading: ["ふぇありーせいばー", "フェアリーセイバー", "フェアリーセイバー"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/フェアリーセイバー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "高潔なる大臣",
+    name: "高潔なる大臣",
+    reading: ["こうけつなるだいじん", "コウケツナルダイジン", "高潔なる大臣"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/高潔なる大臣.zip"
+  },
+  {
+    id: "嵐の槍使い",
+    name: "嵐の槍使い",
+    reading: ["あらしのやりつかい", "アラシノヤリツカイ", "嵐の槍使い"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/嵐の槍使い.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マナリアの生徒会長・ハンナ",
+    name: "マナリアの生徒会長・ハンナ",
+    reading: ["まなりあのせいとかいちょう・はんな", "マナリアノセイトカイチョウ・ハンナ", "マナリアの生徒会長・ハンナ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/マナリアの生徒会長・ハンナ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "狂えるドレイク",
+    name: "狂えるドレイク",
+    reading: ["くるえるどれいく", "クルエルドレイク", "狂えるドレイク"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/狂えるドレイク.zip"
+  },
+  {
+    id: "竜剣の少女・アイラ",
+    name: "竜剣の少女・アイラ",
+    reading: ["りゅうけんのしょうじょ・あいら", "リュウケンノショウジョ・アイラ", "竜剣の少女・アイラ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/竜剣の少女・アイラ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "死竜の使い手",
+    name: "死竜の使い手",
+    reading: ["しりゅうのつかいて", "シリュウノツカイテ", "死竜の使い手"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/死竜の使い手.zip"
+  },
+  {
+    id: "スケルトンプリンス",
+    name: "スケルトンプリンス",
+    reading: ["すけるとんぷりんす", "スケルトンプリンス", "スケルトンプリンス"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/スケルトンプリンス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "テラーデーモン",
+    name: "テラーデーモン",
+    reading: ["てらーでーもん", "テラーデーモン", "テラーデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/テラーデーモン.zip"
+  },
+  {
+    id: "ラブリーサキュバス・リリム",
+    name: "ラブリーサキュバス・リリム",
+    reading: ["らぶりーさきゅばす・りりむ", "ラブリーサキュバス・リリム", "ラブリーサキュバス・リリム"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/ラブリーサキュバス・リリム.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ペガサスデュラハン",
+    name: "ペガサスデュラハン",
+    reading: ["ぺがさすでゅらはん", "ペガサスデュラハン", "ペガサスデュラハン"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/ペガサスデュラハン.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "天威の剣士",
+    name: "天威の剣士",
+    reading: ["てんいのけんし", "テンイノケンシ", "天威の剣士"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/天威の剣士.zip"
+  },
+  {
+    id: "ゴッドバレットゴーレム",
+    name: "ゴッドバレットゴーレム",
+    reading: ["ごっどばれっとごーれむ", "ゴッドバレットゴーレム", "ゴッドバレットゴーレム"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/ゴッドバレットゴーレム.zip"
+  },
+  {
+    id: "造られし獣人・サフィラ",
+    name: "造られし獣人・サフィラ",
+    reading: ["つくられしじゅうじん・さふぃら", "ツクラレシジュウジン・サフィラ", "造られし獣人・サフィラ"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/造られし獣人・サフィラ.zip"
+  },
+  {
+    id: "ハクラビ",
+    name: "ハクラビ",
+    reading: ["はくらび", "ハクラビ", "ハクラビ"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/ハクラビ.zip"
+  },
+  {
+    id: "マインドルーラー・モートン",
+    name: "マインドルーラー・モートン",
+    reading: ["まいんどるーらー・もーとん", "マインドルーラー・モートン", "マインドルーラー・モートン"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/マインドルーラー・モートン.zip"
+  },
+  {
+    id: "銀車の回し手",
+    name: "銀車の回し手",
+    reading: ["ぎんしゃのまわして", "ギンシャノマワシテ", "銀車の回し手"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/銀車の回し手.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリンエンペラー",
+    name: "ゴブリンエンペラー",
+    reading: ["ごぶりんえんぺらー", "ゴブリンエンペラー", "ゴブリンエンペラー"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/ゴブリンエンペラー.zip"
+  },
+  {
+    id: "輝く鐘・ベルエンジェル",
+    name: "輝く鐘・ベルエンジェル",
+    reading: ["かがやくかね・べるえんじぇる", "カガヤクカネ・ベルエンジェル", "輝く鐘・ベルエンジェル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "CGS",
+    zip: "voices/CGS/Gold/輝く鐘・ベルエンジェル.zip"
+  }
 ];

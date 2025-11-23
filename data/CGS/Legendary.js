4d441664f666199cbb254cf2d98012f537ba7b80
panini4552 <panini4552@gmail.com>
2025-11-24 00:01:05 +0900

    Update Legendary.js

--- data/CGS/Legendary.js
+++ data/CGS/Legendary.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
+export const CGS_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "インセクトキング",
+    name: "インセクトキング",
+    reading: ["いんせくときんぐ", "インセクトキング", "インセクトキング"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/インセクトキング.zip"
+  },
+  {
+    id: "導きの妖精姫・アリア",
+    name: "導きの妖精姫・アリア",
+    reading: ["みちびきのようせいひめ・ありあ", "ミチビキノヨウセイヒメ・アリア", "導きの妖精姫・アリア"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/導きの妖精姫・アリア.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ダークセイバー・メリッサ",
+    name: "ダークセイバー・メリッサ",
+    reading: ["だーくせいばー・めりっさ", "ダークセイバー・メリッサ", "ダークセイバー・メリッサ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/ダークセイバー・メリッサ.zip"
+  },
+  {
+    id: "騎士王・アーサー",
+    name: "騎士王・アーサー",
+    reading: ["きしおう・あーさー", "キシオウ・アーサー", "騎士王・アーサー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/騎士王・アーサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "白銀剣のゴーレム",
+    name: "白銀剣のゴーレム",
+    reading: ["はくぎんけんのごーれむ", "ハクギンケンノゴーレム", "白銀剣のゴーレム"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/白銀剣のゴーレム.zip"
+  },
+  {
+    id: "魔術の始祖・マナリア",
+    name: "魔術の始祖・マナリア",
+    reading: ["まじゅつのしそ・まなりあ", "マジュツノシソ・マナリア", "魔術の始祖・マナリア"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/魔術の始祖・マナリア.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "アジ・ダハーカ",
+    name: "アジ・ダハーカ",
+    reading: ["あじ・だはーか", "アジ・ダハーカ", "アジ・ダハーカ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/アジ・ダハーカ.zip"
+  },
+  {
+    id: "竜の炎・ジルニトラ",
+    name: "竜の炎・ジルニトラ",
+    reading: ["りゅうのほのお・じるにとら", "リュウノホノオ・ジルニトラ", "竜の炎・ジルニトラ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/竜の炎・ジルニトラ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "死の淵に立つ者",
+    name: "死の淵に立つ者",
+    reading: ["しのふちにたつもの", "シノフチニタツモノ", "死の淵に立つ者"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/死の淵に立つ者.zip"
+  },
+  {
+    id: "冥守の長・アイシャ",
+    name: "冥守の長・アイシャ",
+    reading: ["めいしゅのちょう・あいしゃ", "メイシュノチョウ・アイシャ", "冥守の長・アイシャ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/冥守の長・アイシャ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ディアボロス・プセマ",
+    name: "ディアボロス・プセマ",
+    reading: ["でぃあぼろす・ぷせま", "ディアボロス・プセマ", "ディアボロス・プセマ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/ディアボロス・プセマ.zip"
+  },
+  {
+    id: "ヴァルプルギスナハト",
+    name: "ヴァルプルギスナハト",
+    reading: ["ゔぁるぷるぎすなはと", "ヴァルプルギスナハト", "ヴァルプルギスナハト"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/ヴァルプルギスナハト.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ヘヴンリーナイト",
+    name: "ヘヴンリーナイト",
+    reading: ["へゔぇんりーないと", "ヘヴンリーナイト", "ヘヴンリーナイト"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/ヘヴンリーナイト.zip"
+  },
+  {
+    id: "希望導く聖乙女・ジャンヌ",
+    name: "希望導く聖乙女・ジャンヌ",
+    reading: ["きぼうみちびくせいおとめ・じゃんぬ", "キボウミチビクセイオトメ・ジャンヌ", "希望導く聖乙女・ジャンヌ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/希望導く聖乙女・ジャンヌ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "マグナレガシー",
+    name: "マグナレガシー",
+    reading: ["まぐなれがしー", "マグナレガシー", "マグナレガシー"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/マグナレガシー.zip"
+  },
+  {
+    id: "デウスエクスマキナ",
+    name: "デウスエクスマキナ",
+    reading: ["でうすえくすまきな", "デウスエクスマキナ", "デウスエクスマキナ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/デウスエクスマキナ.zip"
+  },
+  {
+    id: "神秘の番人・スピネ",
+    name: "神秘の番人・スピネ",
+    reading: ["しんぴのばんにん・すぴね", "シンピノバンニン・スピネ", "神秘の番人・スピネ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/神秘の番人・スピネ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "バイヴカハ",
+    name: "バイヴカハ",
+    reading: ["ばいゔかは", "バイヴカハ", "バイヴカハ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/バイヴカハ.zip"
+  },
+  {
+    id: "クロノス",
+    name: "クロノス",
+    reading: ["くろのす", "クロノス", "クロノス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "CGS",
+    zip: "voices/CGS/Legendary/クロノス.zip"
+  }
 ];

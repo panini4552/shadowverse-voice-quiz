ebe866c624929098777490e1e09e65493af7f378
panini4552 <panini4552@gmail.com>
2025-11-24 00:23:37 +0900

    Update Legendary.js

--- data/DBN/Legendary.js
+++ data/DBN/Legendary.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const DBN_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ユグドラシル",
+    name: "ユグドラシル",
+    reading: ["ゆぐどらしる", "ユグドラシル", "ユグドラシル"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/ユグドラシル.zip"
+  },
+  {
+    id: "ホワイトヴァナラ",
+    name: "ホワイトヴァナラ",
+    reading: ["ほわいとゔぁなら", "ホワイトヴァナラ", "ホワイトヴァナラ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/ホワイトヴァナラ.zip"
+  },
+  {
+    id: "翠嵐の斧使い",
+    name: "翠嵐の斧使い",
+    reading: ["すいらんのおのつかい", "スイランノオノツカイ", "翠嵐の斧使い"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/翠嵐の斧使い.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "スカイフォートレス",
+    name: "スカイフォートレス",
+    reading: ["すかいふぉーとれす", "スカイフォートレス", "スカイフォートレス"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/スカイフォートレス.zip"
+  },
+  {
+    id: "空の指揮官・セリア",
+    name: "空の指揮官・セリア",
+    reading: ["そらのしきかんせりあ", "ソラノシキカンセリア", "空の指揮官・セリア"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/空の指揮官・セリア.zip"
+  },
+  {
+    id: "スパルタクス",
+    name: "スパルタクス",
+    reading: ["すぱるたくす", "スパルタクス", "スパルタクス"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/スパルタクス.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "無貌の魔女",
+    name: "無貌の魔女",
+    reading: ["むぼうのまじょ", "ムボウノマジョ", "無貌の魔女"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/無貌の魔女.zip"
+  },
+  {
+    id: "宿命の語り部・ルーニィ",
+    name: "宿命の語り部・ルーニィ",
+    reading: ["しゅくめいのかたりべるーにぃ", "シュクメイノカタリベルーニィ", "宿命の語り部・ルーニィ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/宿命の語り部・ルーニィ.zip"
+  },
+  {
+    id: "真実の究明者・ファウスト",
+    name: "真実の究明者・ファウスト",
+    reading: ["しんじつのきゅうめいしゃふぁうすと", "シンジツノキュウメイシャファウスト", "真実の究明者・ファウスト"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/真実の究明者・ファウスト.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "リントヴルム",
+    name: "リントヴルム",
+    reading: ["りんとゔるむ", "リントヴルム", "リントヴルム"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/リントヴルム.zip"
+  },
+  {
+    id: "ドラゴニックメイル・ギルヴァ",
+    name: "ドラゴニックメイル・ギルヴァ",
+    reading: ["どらごにっくめいるぎるゔぁ", "ドラゴニックメイルギルヴァ", "ドラゴニックメイル・ギルヴァ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/ドラゴニックメイル・ギルヴァ.zip"
+  },
+  {
+    id: "銀氷のドラゴニュート・フィルレイン",
+    name: "銀氷のドラゴニュート・フィルレイン",
+    reading: ["ぎんぴょうのどらごにゅーとふぃるれいん", "ギンピョウノドラゴニュートフィルレイン", "銀氷のドラゴニュート・フィルレイン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/銀氷のドラゴニュート・フィルレイン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "最果ての骸",
+    name: "最果ての骸",
+    reading: ["さいはてのむくろ", "サイハテノムクロ", "最果ての骸"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/最果ての骸.zip"
+  },
+  {
+    id: "永劫を求む者・ケリドウェン",
+    name: "永劫を求む者・ケリドウェン",
+    reading: ["えいごうをもとむものけりどうぇん", "エイゴウヲモトムモノケリドウェン", "永劫を求む者・ケリドウェン"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/永劫を求む者・ケリドウェン.zip"
+  },
+  {
+    id: "幽霊支配人・アーカス",
+    name: "幽霊支配人・アーカス",
+    reading: ["ゆうれいしはいにんあーかす", "ユウレイシハイニンアーカス", "幽霊支配人・アーカス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/幽霊支配人・アーカス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "闇喰らいの蝙蝠",
+    name: "闇喰らいの蝙蝠",
+    reading: ["やみくらいのこうもり", "ヤミクライノコウモリ", "闇喰らいの蝙蝠"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/闇喰らいの蝙蝠.zip"
+  },
+  {
+    id: "闇夜の姫・ヴァンピィ",
+    name: "闇夜の姫・ヴァンピィ",
+    reading: ["やみよのひめゔぁんぴぃ", "ヤミヨノヒメヴァンピィ", "闇夜の姫・ヴァンピィ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/闇夜の姫・ヴァンピィ.zip"
+  },
+  {
+    id: "人狼の王・ヴァルツ",
+    name: "人狼の王・ヴァルツ",
+    reading: ["じんろうのおうゔぁるつ", "ジンロウノオウヴァルツ", "人狼の王・ヴァルツ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/人狼の王・ヴァルツ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "神の盾・ブローディア",
+    name: "神の盾・ブローディア",
+    reading: ["かみのたてぶろーでぃあ", "カミノタテブローディア", "神の盾・ブローディア"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/神の盾・ブローディア.zip"
+  },
+  {
+    id: "ケリュネイア",
+    name: "ケリュネイア",
+    reading: ["けりゅねいあ", "ケリュネイア", "ケリュネイア"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/ケリュネイア.zip"
+  },
+  {
+    id: "ツタンカーメン",
+    name: "ツタンカーメン",
+    reading: ["つたんかーめん", "ツタンカーメン", "ツタンカーメン"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/ツタンカーメン.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "人形の少女・オーキス",
+    name: "人形の少女・オーキス",
+    reading: ["にんぎょうのしょうじょおーきす", "ニンギョウノショウジョオーキス", "人形の少女・オーキス"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/人形の少女・オーキス.zip"
+  },
+  {
+    id: "消し去るもの",
+    name: "消し去るもの",
+    reading: ["けしさるもの", "ケシサルモノ", "消し去るもの"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/消し去るもの.zip"
+  },
+  {
+    id: "魔導電磁サイ",
+    name: "魔導電磁サイ",
+    reading: ["まどうでんじさい", "マドウデンジサイ", "魔導電磁サイ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/魔導電磁サイ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "マルドゥーク",
+    name: "マルドゥーク",
+    reading: ["まるどぅーく", "マルドゥーク", "マルドゥーク"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "DBN",
+    zip: "voices/DBN/Legendary/マルドゥーク.zip"
+  }
 ];

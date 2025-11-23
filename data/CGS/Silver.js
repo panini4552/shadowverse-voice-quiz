856d36f84ddc3830d80903a2b8ed0cb824c9f44c
panini4552 <panini4552@gmail.com>
2025-11-24 00:14:01 +0900

    Update Silver.js

--- data/CGS/Silver.js
+++ data/CGS/Silver.js
@@ -1,15 +1,235 @@
-export const Classic_Legendary = [
+export const CGS_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "憤怒のエルダーウィードマン",
+    name: "憤怒のエルダーウィードマン",
+    reading: ["ふんぬのえるだーうぃーどまん", "憤怒のエルダーウィードマン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "憤怒のエルダーウィードマン.zip"
+  },
+  {
+    id: "紅弓のエルフ",
+    name: "紅弓のエルフ",
+    reading: ["こうきゅうのえるふ", "紅弓のエルフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "紅弓のエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "傭兵の帰還者",
+    name: "傭兵の帰還者",
+    reading: ["ようへのきかんしゃ", "傭兵の帰還者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "傭兵の帰還者.zip"
+  },
+  {
+    id: "先陣の騎兵",
+    name: "先陣の騎兵",
+    reading: ["せんじんのきへい", "先陣の騎兵"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "先陣の騎兵.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "笑顔の奇術師・サミー",
+    name: "笑顔の奇術師・サミー",
+    reading: ["えがおのきじゅつしさみー", "笑顔の奇術師・サミー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "笑顔の奇術師・サミー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "塔の守護竜・アデール",
+    name: "塔の守護竜・アデール",
+    reading: ["とうのしゅごりゅうあでーる", "塔の守護竜・アデール"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "塔の守護竜・アデール.zip"
+  },
+  {
+    id: "美麗なる竜人・マリオン",
+    name: "美麗なる竜人・マリオン",
+    reading: ["びれいなるりゅうじんまりおん", "美麗なる竜人・マリオン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "美麗なる竜人・マリオン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "アヌビス",
+    name: "アヌビス",
+    reading: ["あぬびす", "アヌビス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "アヌビス.zip"
+  },
+  {
+    id: "霊呼びの少女",
+    name: "霊呼びの少女",
+    reading: ["たまよびのしょうじょ", "霊呼びの少女"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "霊呼びの少女.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ヴァッサゴ",
+    name: "ヴァッサゴ",
+    reading: ["う゛ぁっさご", "ヴァッサゴ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "ヴァッサゴ.zip"
+  },
+  {
+    id: "パズズ",
+    name: "パズズ",
+    reading: ["ぱずず", "パズズ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "パズズ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "アークビショップ・カトレア",
+    name: "アークビショップ・カトレア",
+    reading: ["あーくびしょっぷかとれあ", "アークビショップ・カトレア"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "アークビショップ・カトレア.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "アメジストの巨兵",
+    name: "アメジストの巨兵",
+    reading: ["あめじすとのきょへい", "アメジストの巨兵"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "アメジストの巨兵.zip"
+  },
+  {
+    id: "復讐の人形遣い・ノア",
+    name: "復讐の人形遣い・ノア",
+    reading: ["ふくしゅうのにんぎょうつかいのあ", "復讐の人形遣い・ノア"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "復讐の人形遣い・ノア.zip"
+  },
+  {
+    id: "造られし巨像",
+    name: "造られし巨像",
+    reading: ["つくられしきょぞう", "造られし巨像"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "造られし巨像.zip"
+  },
+  {
+    id: "不定の実験体",
+    name: "不定の実験体",
+    reading: ["ふていのじっけんたい", "不定の実験体"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "不定の実験体.zip"
+  },
+  {
+    id: "オートマタソルジャー",
+    name: "オートマタソルジャー",
+    reading: ["おーとまたそるじゃー", "オートマタソルジャー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "オートマタソルジャー.zip"
+  },
+  {
+    id: "機構翼の少女・ローザ",
+    name: "機構翼の少女・ローザ",
+    reading: ["きこうよくのしょうじょろーざ", "機構翼の少女・ローザ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "機構翼の少女・ローザ.zip"
+  },
+  {
+    id: "機構職人・ジノ",
+    name: "機構職人・ジノ",
+    reading: ["きこうしょくにんじの", "機構職人・ジノ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "機構職人・ジノ.zip"
+  },
+  {
+    id: "イカロス",
+    name: "イカロス",
+    reading: ["いかろす", "イカロス"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "イカロス.zip"
+  },
+  {
+    id: "フラワードール",
+    name: "フラワードール",
+    reading: ["ふらわーどーる", "フラワードール"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "フラワードール.zip"
+  },
+  {
+    id: "熱狂の機兵士",
+    name: "熱狂の機兵士",
+    reading: ["ねっきょうのきへいし", "熱狂の機兵士"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "熱狂の機兵士.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリンマウントオーガ",
+    name: "ゴブリンマウントオーガ",
+    reading: ["ごぶりんまうんとおーが", "ゴブリンマウントオーガ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "ゴブリンマウントオーガ.zip"
+  },
+  {
+    id: "ホーリーエンジェル・アルテア",
+    name: "ホーリーエンジェル・アルテア",
+    reading: ["ほーりーえんじぇるあるてあ", "ホーリーエンジェル・アルテア"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "CGS",
+    zip: "ホーリーエンジェル・アルテア.zip"
+  }
 ];

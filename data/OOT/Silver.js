c0f197a3c7369e7aebbbc770dfee03fe31e17b88
panini4552 <panini4552@gmail.com>
2025-11-24 01:22:34 +0900

    Update Silver.js

--- data/OOT/Silver.js
+++ data/OOT/Silver.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const OOT_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "不殺の信者",
+    name: "不殺の信者",
+    reading: ["ふさつのしんじゃ", "フサツノシンジャ", "不殺の信者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/不殺の信者.zip"
+  },
+  {
+    id: "ホーネットソルジャー",
+    name: "ホーネットソルジャー",
+    reading: ["ほーねっとそるじゃー", "ホーネットソルジャー", "ホーネットソルジャー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/ホーネットソルジャー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "簒奪の信者",
+    name: "簒奪の信者",
+    reading: ["さんだつのしんじゃ", "サンダツノシンジャ", "簒奪の信者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/簒奪の信者.zip"
+  },
+  {
+    id: "アロアダイ",
+    name: "アロアダイ",
+    reading: ["あろあだい", "アロアダイ", "アロアダイ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/アロアダイ.zip"
+  },
+  {
+    id: "豪快な海賊",
+    name: "豪快な海賊",
+    reading: ["ごうかいなかいぞく", "ゴウカイナカイゾク", "豪快な海賊"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/豪快な海賊.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "真実の信者",
+    name: "真実の信者",
+    reading: ["しんじつのしんじゃ", "シンジツノシンジャ", "真実の信者"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/真実の信者.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "侮蔑の信者",
+    name: "侮蔑の信者",
+    reading: ["ぶべつのしんじゃ", "ブベツノシンジャ", "侮蔑の信者"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/侮蔑の信者.zip"
+  },
+  {
+    id: "グリフォンの守衛",
+    name: "グリフォンの守衛",
+    reading: ["ぐりふぉんのしゅえい", "グリフォンノシュエイ", "グリフォンの守衛"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/グリフォンの守衛.zip"
+  },
+  {
+    id: "キュアマーメイド・ジゼル",
+    name: "キュアマーメイド・ジゼル",
+    reading: ["きゅあまーめいどじぜる", "キュアマーメイドジゼル", "キュアマーメイド・ジゼル"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/キュアマーメイド・ジゼル.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "沈黙の信者",
+    name: "沈黙の信者",
+    reading: ["ちんもくのしんじゃ", "チンモクノシンジャ", "沈黙の信者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/沈黙の信者.zip"
+  },
+  {
+    id: "冥界の闘犬・オルトロス",
+    name: "冥界の闘犬・オルトロス",
+    reading: ["めいかいのとうけんおるとろす", "メイカイノトウケンオルトロス", "冥界の闘犬・オルトロス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/冥界の闘犬・オルトロス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "姦淫の信者",
+    name: "姦淫の信者",
+    reading: ["かんいんのしんじゃ", "カンインノシンジャ", "姦淫の信者"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/姦淫の信者.zip"
+  },
+  {
+    id: "ダークエレメンタル",
+    name: "ダークエレメンタル",
+    reading: ["だーくえれめんたる", "ダークエレメンタル", "ダークエレメンタル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/ダークエレメンタル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "安息の信者",
+    name: "安息の信者",
+    reading: ["あんそくのしんじゃ", "アンソクノシンジャ", "安息の信者"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/安息の信者.zip"
+  },
+  {
+    id: "ユニコーンナイト",
+    name: "ユニコーンナイト",
+    reading: ["ゆにこーんないと", "ユニコーンナイト", "ユニコーンナイト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/ユニコーンナイト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "破壊の信者",
+    name: "破壊の信者",
+    reading: ["はかいのしんじゃ", "ハカイノシンジャ", "破壊の信者"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/破壊の信者.zip"
+  },
+  {
+    id: "光明齎す発明家",
+    name: "光明齎す発明家",
+    reading: ["こうみょうもたらすはつめいか", "コウミョウモタラスハツメイカ", "光明齎す発明家"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/光明齎す発明家.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "重装のトロール",
+    name: "重装のトロール",
+    reading: ["じゅうそうのとろーる", "ジュウソウノトロール", "重装のトロール"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "OOT",
+    zip: "voices/OOT/Silver/重装のトロール.zip"
+  }
 ];

e4c4f50760c90fd6d6a5682de9e7443eb97d3c65
panini4552 <panini4552@gmail.com>
2025-11-24 00:50:00 +0900

    Update Legendary.js

--- data/BOS/Legendary.js
+++ data/BOS/Legendary.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const BOS_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "絢爛の紡ぎ手・コルワ",
+    name: "絢爛の紡ぎ手・コルワ",
+    reading: ["けんらんのつむぎて・こるわ", "ケンランノツムギテ・コルワ", "絢爛の紡ぎ手・コルワ"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/絢爛の紡ぎ手・コルワ.zip"
+  },
+  {
+    id: "神殺しの狩人・セルウィン",
+    name: "神殺しの狩人・セルウィン",
+    reading: ["かみごろしのかりゅうど・せるうぃん", "カミゴロシノカリュウド・セルウィン", "神殺しの狩人・セルウィン"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/神殺しの狩人・セルウィン.zip"
+  },
+  {
+    id: "天喰らう異形",
+    name: "天喰らう異形",
+    reading: ["あまぐらういぎょう", "アマグラウイギョウ", "天喰らう異形"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/天喰らう異形.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "高潔なる騎士・レイサム",
+    name: "高潔なる騎士・レイサム",
+    reading: ["こうけつなるきし・れいさむ", "コウケツナルキシ・レイサム", "高潔なる騎士・レイサム"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/高潔なる騎士・レイサム.zip"
+  },
+  {
+    id: "真紅の穿光・ゼタ",
+    name: "真紅の穿光・ゼタ",
+    reading: ["しんくのせんこう・ぜた", "シンクノセンコウ・ゼタ", "真紅の穿光・ゼタ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/真紅の穿光・ゼタ.zip"
+  },
+  {
+    id: "鉄拳の獣戦士",
+    name: "鉄拳の獣戦士",
+    reading: ["てっけんのじゅうせんし", "テッケンノジュウセンシ", "鉄拳の獣戦士"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/鉄拳の獣戦士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "神託の妖童・アルルメイヤ",
+    name: "神託の妖童・アルルメイヤ",
+    reading: ["しんたくのようどう・あるるめいや", "シンタクノヨウドウ・アルルメイヤ", "神託の妖童・アルルメイヤ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/神託の妖童・アルルメイヤ.zip"
+  },
+  {
+    id: "絶望のキマイラ",
+    name: "絶望のキマイラ",
+    reading: ["ぜつぼうのきまいら", "ゼツボウノキマイラ", "絶望のキマイラ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/絶望のキマイラ.zip"
+  },
+  {
+    id: "殺戮の魔女・ヴェスパー",
+    name: "殺戮の魔女・ヴェスパー",
+    reading: ["さつりくのまじょ・ゔぇすぱー", "サツリクノマジョ・ヴェスパー", "殺戮の魔女・ヴェスパー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/殺戮の魔女・ヴェスパー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "大いなる調停者・ゾーイ",
+    name: "大いなる調停者・ゾーイ",
+    reading: ["おおいなるちょうていしゃ・ぞーい", "オオイナルチョウテイシャ・ゾーイ", "大いなる調停者・ゾーイ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/大いなる調停者・ゾーイ.zip"
+  },
+  {
+    id: "アドラメレク",
+    name: "アドラメレク",
+    reading: ["あどらめれく", "アドラメレク", "アドラメレク"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/アドラメレク.zip"
+  },
+  {
+    id: "終戦のドラグーン",
+    name: "終戦のドラグーン",
+    reading: ["しゅうせんのどらぐーん", "シュウセンノドラグーン", "終戦のドラグーン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/終戦のドラグーン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "幽想の少女・フェリ",
+    name: "幽想の少女・フェリ",
+    reading: ["ゆうそうのしょうじょ・ふぇり", "ユウソウノショウジョ・フェリ", "幽想の少女・フェリ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/幽想の少女・フェリ.zip"
+  },
+  {
+    id: "永遠の決闘者・モルディカイ",
+    name: "永遠の決闘者・モルディカイ",
+    reading: ["えいえんのけっとうしゃ・もるでぃかい", "エイエンノケットウシャ・モルディカイ", "永遠の決闘者・モルディカイ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/永遠の決闘者・モルディカイ.zip"
+  },
+  {
+    id: "グレモリー",
+    name: "グレモリー",
+    reading: ["ぐれもりー", "グレモリー", "グレモリー"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/グレモリー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "狂恋の華鎧・ヴィーラ",
+    name: "狂恋の華鎧・ヴィーラ",
+    reading: ["きょうれんのかがい・ヴぃーら", "キョウレンノカガイ・ヴィーラ", "狂恋の華鎧・ヴィーラ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/狂恋の華鎧・ヴィーラ.zip"
+  },
+  {
+    id: "プルソン",
+    name: "プルソン",
+    reading: ["ぷるそん", "プルソン", "プルソン"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/プルソン.zip"
+  },
+  {
+    id: "轟雷のベヒーモス",
+    name: "轟雷のベヒーモス",
+    reading: ["ごうらいのべひーもす", "ゴウライノベヒーモス", "轟雷のベヒーモス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/轟雷のベヒーモス.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "七宝石の姫・レ・フィーエ",
+    name: "七宝石の姫・レ・フィーエ",
+    reading: ["しっぽうせきのひめ・れ・ふぃーえ", "シッポウセキノヒメ・レ・フィーエ", "七宝石の姫・レ・フィーエ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/七宝石の姫・レ・フィーエ.zip"
+  },
+  {
+    id: "烈風の翼神・ガルラ",
+    name: "烈風の翼神・ガルラ",
+    reading: ["れっぷうのよくしん・がるら", "レップウノヨクシン・ガルラ", "烈風の翼神・ガルラ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/烈風の翼神・ガルラ.zip"
+  },
+  {
+    id: "壊れたる神・チェキババ",
+    name: "壊れたる神・チェキババ",
+    reading: ["こわれたるかみ・ちぇきばば", "コワレタルカミ・チェキババ", "壊れたる神・チェキババ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/壊れたる神・チェキババ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "奮励の儁秀・シルヴァ",
+    name: "奮励の儁秀・シルヴァ",
+    reading: ["ふんれいのしゅんしゅう・しるう゛ぁ", "フンレイノシュンシュウ・シルヴァ", "奮励の儁秀・シルヴァ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/奮励の儁秀・シルヴァ.zip"
+  },
+  {
+    id: "リトルパペッター・ロココ",
+    name: "リトルパペッター・ロココ",
+    reading: ["りとるぱぺったー・ろここ", "リトルパペッター・ロココ", "リトルパペッター・ロココ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/リトルパペッター・ロココ.zip"
+  },
+  {
+    id: "マル・ドッキオ",
+    name: "マル・ドッキオ",
+    reading: ["まる・どっきお", "マル・ドッキオ", "マル・ドッキオ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/マル・ドッキオ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "プロトバハムート",
+    name: "プロトバハムート",
+    reading: ["ぷろとははむーと", "プロトバハムート", "プロトバハムート"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/プロトバハムート.zip"
+  },
+  {
+    id: "レジェンダリーファイター",
+    name: "レジェンダリーファイター",
+    reading: ["れじぇんだりーふぁいたー", "レジェンダリーファイター", "レジェンダリーファイター"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "BOS",
+    zip: "voices/BOS/Legendary/レジェンダリーファイター.zip"
+  }
 ];

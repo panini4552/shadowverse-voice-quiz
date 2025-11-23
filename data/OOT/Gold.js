21c0ab16602cc70f77005732ee4e4ce00dc1bbfb
panini4552 <panini4552@gmail.com>
2025-11-24 01:15:50 +0900

    Update Gold.js

--- data/OOT/Gold.js
+++ data/OOT/Gold.js
@@ -1,15 +1,237 @@
-export const Classic_Legendary = [
+// data/OOT/OOT_Gold.js
+
+export const OOT_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "不殺の使徒",
+    name: "不殺の使徒",
+    reading: ["ふさつのしと", "フサツノシト", "不殺の使徒"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/不殺の使徒.zip"
+  },
+  {
+    id: "煌刃の戦士・ベイル",
+    name: "煌刃の戦士・ベイル",
+    reading: ["こうじんのせんしべいる", "コウジンノセンシベイル", "煌刃の戦士・ベイル"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/煌刃の戦士・ベイル.zip"
+  },
+  {
+    id: "水晶の指揮者・リリィ",
+    name: "水晶の指揮者・リリィ",
+    reading: ["すいしょうのしきしゃりりぃ", "スイショウノシキシャリリィ", "水晶の指揮者・リリィ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/水晶の指揮者・リリィ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "簒奪の使徒",
+    name: "簒奪の使徒",
+    reading: ["さんだつのしと", "サンダツノシト", "簒奪の使徒"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/簒奪の使徒.zip"
+  },
+  {
+    id: "レイピアマスター",
+    name: "レイピアマスター",
+    reading: ["れいぴあますたー", "レイピアマスター", "レイピアマスター"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/レイピアマスター.zip"
+  },
+  {
+    id: "巨人殺しの剣士",
+    name: "巨人殺しの剣士",
+    reading: ["きょじんごろしのけんし", "キョジンゴロシノケンシ", "巨人殺しの剣士"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/巨人殺しの剣士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "真実の使徒",
+    name: "真実の使徒",
+    reading: ["しんじつのしと", "シンジツノシト", "真実の使徒"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/真実の使徒.zip"
+  },
+  {
+    id: "マナリアの魔導姫・アン",
+    name: "マナリアの魔導姫・アン",
+    reading: ["まなりあのまどうひめあん", "マナリアノマドウヒメアン", "マナリアの魔導姫・アン"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/マナリアの魔導姫・アン.zip"
+  },
+  {
+    id: "マナリアの竜姫・グレア",
+    name: "マナリアの竜姫・グレア",
+    reading: ["まなりあのりゅうきぐれあ", "マナリアノリュウキグレア", "マナリアの竜姫・グレア"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/マナリアの竜姫・グレア.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "侮蔑の使徒",
+    name: "侮蔑の使徒",
+    reading: ["ぶべつのしと", "ブベツノシト", "侮蔑の使徒"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/侮蔑の使徒.zip"
+  },
+  {
+    id: "白亜の竜騎士",
+    name: "白亜の竜騎士",
+    reading: ["はくあのりゅうきし", "ハクアノリュウキシ", "白亜の竜騎士"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/白亜の竜騎士.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "沈黙の使徒",
+    name: "沈黙の使徒",
+    reading: ["ちんもくのしと", "チンモクノシト", "沈黙の使徒"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/沈黙の使徒.zip"
+  },
+  {
+    id: "冥河の渡し守・カローン",
+    name: "冥河の渡し守・カローン",
+    reading: ["めいがのわたしもりかろーん", "メイガノワタシモリカローン", "冥河の渡し守・カローン"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/冥河の渡し守・カローン.zip"
+  },
+  {
+    id: "死と闇のレイヴン",
+    name: "死と闇のレイヴン",
+    reading: ["しとやみのれいゔん", "シトヤミノレイヴン", "死と闇のレイヴン"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/死と闇のレイヴン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "姦淫の使徒",
+    name: "姦淫の使徒",
+    reading: ["かんいんのしと", "カンインノシト", "姦淫の使徒"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/姦淫の使徒.zip"
+  },
+  {
+    id: "フラウロス",
+    name: "フラウロス",
+    reading: ["ふらうろす", "フラウロス", "フラウロス"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/フラウロス.zip"
+  },
+  {
+    id: "デビルアイドル・トリル",
+    name: "デビルアイドル・トリル",
+    reading: ["でびるあいどるとりる", "デビルアイドルトリル", "デビルアイドル・トリル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/デビルアイドル・トリル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "安息の使徒",
+    name: "安息の使徒",
+    reading: ["あんそくのしと", "アンソクノシト", "安息の使徒"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/安息の使徒.zip"
+  },
+  {
+    id: "アサルトプリースト",
+    name: "アサルトプリースト",
+    reading: ["あさるとぷりーすと", "アサルトプリースト", "アサルトプリースト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/アサルトプリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "破壊の使徒",
+    name: "破壊の使徒",
+    reading: ["はかいのしと", "ハカイノシト", "破壊の使徒"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/破壊の使徒.zip"
+  },
+  {
+    id: "レジスタンスの指導者・ルキナ",
+    name: "レジスタンスの指導者・ルキナ",
+    reading: ["れじすたんすのしどうしゃるきな", "レジスタンスノシドウシャルキナ", "レジスタンスの指導者・ルキナ"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/レジスタンスの指導者・ルキナ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "飢餓の使徒",
+    name: "飢餓の使徒",
+    reading: ["きがのしと", "キガノシト", "飢餓の使徒"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/飢餓の使徒.zip"
+  },
+  {
+    id: "爆砕の傭兵・フィーナ",
+    name: "爆砕の傭兵・フィーナ",
+    reading: ["ばくさいのようへいふぃーな", "バクサイノヨウヘイフィーナ", "爆砕の傭兵・フィーナ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/爆砕の傭兵・フィーナ.zip"
+  },
+  {
+    id: "神弓の座天使・リリエル",
+    name: "神弓の座天使・リリエル",
+    reading: ["しんきゅうのざてんしりりえる", "シンキュウノザテンシリリエル", "神弓の座天使・リリエル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "OOT",
+    zip: "voices/OOT/Gold/神弓の座天使・リリエル.zip"
+  }
 ];

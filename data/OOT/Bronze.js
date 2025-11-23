e1cd25a50f0630b82519fe8d4c27e515e5a09ddb
panini4552 <panini4552@gmail.com>
2025-11-24 01:26:26 +0900

    Update Bronze.js

--- data/OOT/Bronze.js
+++ data/OOT/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const OOT_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "不殺の従者",
+    name: "不殺の従者",
+    reading: ["ふさつのじゅうしゃ", "フサツノジュウシャ", "不殺の従者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/不殺の従者.zip"
+  },
+  {
+    id: "怒れる山神",
+    name: "怒れる山神",
+    reading: ["いかれるやまがみ", "イカレルヤマガミ", "怒れる山神"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/怒れる山神.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "簒奪の従者",
+    name: "簒奪の従者",
+    reading: ["さんだつのじゅうしゃ", "サンダツノジュウシャ", "簒奪の従者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/簒奪の従者.zip"
+  },
+  {
+    id: "老いたる先導者",
+    name: "老いたる先導者",
+    reading: ["おいたるせんどうしゃ", "オイタルセンドウシャ", "老いたる先導者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/老いたる先導者.zip"
+  },
+  {
+    id: "荒々しい女戦士",
+    name: "荒々しい女戦士",
+    reading: ["あらあらしいおんなせんし", "アラアラシイオンナセンシ", "荒々しい女戦士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/荒々しい女戦士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "真実の従者",
+    name: "真実の従者",
+    reading: ["しんじつのじゅうしゃ", "シンジツノジュウシャ", "真実の従者"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/真実の従者.zip"
+  },
+  {
+    id: "マナリアナイト・オーウェン",
+    name: "マナリアナイト・オーウェン",
+    reading: ["まなりあないとおーうぇん", "マナリアナイトオーウェン", "マナリアナイト・オーウェン"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/マナリアナイト・オーウェン.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "侮蔑の従者",
+    name: "侮蔑の従者",
+    reading: ["ぶべつのじゅうしゃ", "ブベツノジュウシャ", "侮蔑の従者"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/侮蔑の従者.zip"
+  },
+  {
+    id: "ドラゴニックシューター",
+    name: "ドラゴニックシューター",
+    reading: ["どらごにっくしゅーたー", "ドラゴニックシューター", "ドラゴニックシューター"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/ドラゴニックシューター.zip"
+  },
+  {
+    id: "ドラゴスネーク",
+    name: "ドラゴスネーク",
+    reading: ["どらごすねーく", "ドラゴスネーク", "ドラゴスネーク"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/ドラゴスネーク.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "沈黙の従者",
+    name: "沈黙の従者",
+    reading: ["ちんもくのじゅうしゃ", "チンモクノジュウシャ", "沈黙の従者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/沈黙の従者.zip"
+  },
+  {
+    id: "イビルコフィン",
+    name: "イビルコフィン",
+    reading: ["いびるこふぃん", "イビルコフィン", "イビルコフィン"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/イビルコフィン.zip"
+  },
+  {
+    id: "冥界の騎兵",
+    name: "冥界の騎兵",
+    reading: ["めいかいのきへい", "メイカイノキヘイ", "冥界の騎兵"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/冥界の騎兵.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "姦淫の従者",
+    name: "姦淫の従者",
+    reading: ["かんいんのじゅうしゃ", "カンインノジュウシャ", "姦淫の従者"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/姦淫の従者.zip"
+  },
+  {
+    id: "血に飢えたハムスター",
+    name: "血に飢えたハムスター",
+    reading: ["ちにうえたはむすたー", "チニウエタハムスター", "血に飢えたハムスター"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/血に飢えたハムスター.zip"
+  },
+  {
+    id: "炎蛇の剣士",
+    name: "炎蛇の剣士",
+    reading: ["えんじゃのけんし", "エンジャノケンシ", "炎蛇の剣士"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/炎蛇の剣士.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "安息の従者",
+    name: "安息の従者",
+    reading: ["あんそくのじゅうしゃ", "アンソクノジュウシャ", "安息の従者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/安息の従者.zip"
+  },
+  {
+    id: "神鳥の修道女",
+    name: "神鳥の修道女",
+    reading: ["しんちょうのしゅうどうじょ", "シンチョウノシュウドウジョ", "神鳥の修道女"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/神鳥の修道女.zip"
+  },
+  {
+    id: "聖域の執行者",
+    name: "聖域の執行者",
+    reading: ["せいいきのしっこうしゃ", "セイイキノシッコウシャ", "聖域の執行者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/聖域の執行者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "破壊の従者",
+    name: "破壊の従者",
+    reading: ["はかいのじゅうしゃ", "ハカイノジュウシャ", "破壊の従者"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/破壊の従者.zip"
+  },
+  {
+    id: "流体使い",
+    name: "流体使い",
+    reading: ["りゅうたいづかい", "リュウタイヅカイ", "流体使い"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/流体使い.zip"
+  },
+  {
+    id: "銀の機械像",
+    name: "銀の機械像",
+    reading: ["ぎんのきかいぞう", "ギンノキカイゾウ", "銀の機械像"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/銀の機械像.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリンファイター",
+    name: "ゴブリンファイター",
+    reading: ["ごぶりんふぁいたー", "ゴブリンファイター", "ゴブリンファイター"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/ゴブリンファイター.zip"
+  },
+  {
+    id: "スカアハ",
+    name: "スカアハ",
+    reading: ["すかあは", "スカアハ", "スカアハ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/スカアハ.zip"
+  },
+  {
+    id: "堅牢なる天使",
+    name: "堅牢なる天使",
+    reading: ["けんろうなるてんし", "ケンロウナルテンシ", "堅牢なる天使"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "OOT",
+    zip: "voices/OOT/Bronze/堅牢なる天使.zip"
+  }
 ];

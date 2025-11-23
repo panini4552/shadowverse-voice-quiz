3613684c55611e2639d7b0c671fc203fa1e2a559
panini4552 <panini4552@gmail.com>
2025-11-24 00:33:17 +0900

    Update Gold.js

--- data/DBN/Gold.js
+++ data/DBN/Gold.js
@@ -1,15 +1,199 @@
-export const Classic_Legendary = [
+export const DBN_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "瞬速の大狼",
+    name: "瞬速の大狼",
+    reading: ["しゅんそくのおおかみ", "瞬速の大狼"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/瞬速の大狼.zip"
+  },
+  {
+    id: "愛の妖精・ポーラ",
+    name: "愛の妖精・ポーラ",
+    reading: ["あいのようせいぽーら", "愛の妖精・ポーラ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/愛の妖精・ポーラ.zip"
+  },
+  {
+    id: "歴戦の鷹匠",
+    name: "歴戦の鷹匠",
+    reading: ["れきせんのたかじょう", "歴戦の鷹匠"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/歴戦の鷹匠.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "イノセントプリンセス・プリム",
+    name: "イノセントプリンセス・プリム",
+    reading: ["いのせんとぷりんせすぷりむ", "イノセントプリンセス・プリム"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/イノセントプリンセス・プリム.zip"
+  },
+  {
+    id: "悪魔伯爵・ダリオ",
+    name: "悪魔伯爵・ダリオ",
+    reading: ["あくまはくしゃくだりお", "悪魔伯爵・ダリオ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/悪魔伯爵・ダリオ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "キングスノーマン",
+    name: "キングスノーマン",
+    reading: ["きんぐすのーまん", "キングスノーマン"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/キングスノーマン.zip"
+  },
+  {
+    id: "オニオン軍団の長・バーゼント",
+    name: "オニオン軍団の長・バーゼント",
+    reading: ["おにおんぐんだんのちょうばーぜんと", "オニオン軍団の長・バーゼント"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/オニオン軍団の長・バーゼント.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "斬竜剣士・ロイ",
+    name: "斬竜剣士・ロイ",
+    reading: ["ざんりゅうけんしろい", "斬竜剣士・ロイ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/斬竜剣士・ロイ.zip"
+  },
+  {
+    id: "竜鎧の戦士",
+    name: "竜鎧の戦士",
+    reading: ["りゅうがいのせんし", "竜鎧の戦士"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/竜鎧の戦士.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "気紛れな蘇生者",
+    name: "気紛れな蘇生者",
+    reading: ["きまぐれなそせいしゃ", "気紛れな蘇生者"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/気紛れな蘇生者.zip"
+  },
+  {
+    id: "常闇の梟",
+    name: "常闇の梟",
+    reading: ["とこやみのふくろう", "常闇の梟"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/常闇の梟.zip"
+  },
+  {
+    id: "スカルデッドロード",
+    name: "スカルデッドロード",
+    reading: ["すかるでっどろーど", "スカルデッドロード"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/スカルデッドロード.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "血統の王",
+    name: "血統の王",
+    reading: ["けっとうのおう", "血統の王"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/血統の王.zip"
+  },
+  {
+    id: "宵の吸血鬼・ヴァーニア",
+    name: "宵の吸血鬼・ヴァーニア",
+    reading: ["よいのきゅうけつきヴぁーにあ", "宵の吸血鬼・ヴァーニア"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/宵の吸血鬼・ヴァーニア.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "宝石の巫女",
+    name: "宝石の巫女",
+    reading: ["ほうせきのみこ", "宝石の巫女"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/宝石の巫女.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "殺戮の人形・ツヴァイ",
+    name: "殺戮の人形・ツヴァイ",
+    reading: ["さつりくのにんぎょうつゔぁい", "殺戮の人形・ツヴァイ"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/殺戮の人形・ツヴァイ.zip"
+  },
+  {
+    id: "狭間の悪魔",
+    name: "狭間の悪魔",
+    reading: ["はざまのあくま", "狭間の悪魔"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/狭間の悪魔.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "スットゥング",
+    name: "スットゥング",
+    reading: ["すっとぅんぐ", "スットゥング"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/スットゥング.zip"
+  },
+  {
+    id: "天界の尖兵",
+    name: "天界の尖兵",
+    reading: ["てんかいのせんぺい", "天界の尖兵"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/天界の尖兵.zip"
+  },
+  {
+    id: "オケアノス",
+    name: "オケアノス",
+    reading: ["おけあのす", "オケアノス"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "DBN",
+    zip: "voices/DBN/Gold/オケアノス.zip"
+  }
 ];

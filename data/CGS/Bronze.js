5821587bb8b729c1eb3fc2b713e9be95f8f1f5d3
panini4552 <panini4552@gmail.com>
2025-11-24 00:18:02 +0900

    Update Bronze.js

--- data/CGS/Bronze.js
+++ data/CGS/Bronze.js
@@ -1,15 +1,343 @@
-export const Classic_Legendary = [
+export const CGS_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "リーフマン",
+    name: "リーフマン",
+    reading: ["りーふまん", "リーフマン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "リーフマン.zip"
+  },
+  {
+    id: "百錬のアーチャー",
+    name: "百錬のアーチャー",
+    reading: ["ひゃくれんのあーちゃー", "百錬のアーチャー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "百錬のアーチャー.zip"
+  },
+  {
+    id: "森の哲学者",
+    name: "森の哲学者",
+    reading: ["もりのてつがくしゃ", "森の哲学者"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "森の哲学者.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "王城の守衛",
+    name: "王城の守衛",
+    reading: ["おうじょうのしゅえい", "王城の守衛"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "王城の守衛.zip"
+  },
+  {
+    id: "マーチャントガール",
+    name: "マーチャントガール",
+    reading: ["まーちゃんとがーる", "マーチャントガール"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "マーチャントガール.zip"
+  },
+  {
+    id: "狼牙の剣士",
+    name: "狼牙の剣士",
+    reading: ["ろうがのけんし", "狼牙の剣士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "狼牙の剣士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マナリアの召喚士・ベリル",
+    name: "マナリアの召喚士・ベリル",
+    reading: ["まなりあのしょうかんしべりる", "マナリアの召喚士・ベリル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "マナリアの召喚士・ベリル.zip"
+  },
+  {
+    id: "術式の教師・ジル",
+    name: "術式の教師・ジル",
+    reading: ["じゅつしきのきょうしじる", "術式の教師・ジル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "術式の教師・ジル.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "氷炎竜",
+    name: "氷炎竜",
+    reading: ["ひょうえんりゅう", "氷炎竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "氷炎竜.zip"
+  },
+  {
+    id: "竜友の少女",
+    name: "竜友の少女",
+    reading: ["りゅうゆうのしょうじょ", "竜友の少女"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "竜友の少女.zip"
+  },
+  {
+    id: "バジリスクライダー",
+    name: "バジリスクライダー",
+    reading: ["ばじりすくらいだー", "バジリスクライダー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "バジリスクライダー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "マミー",
+    name: "マミー",
+    reading: ["まみー", "マミー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "マミー.zip"
+  },
+  {
+    id: "骸骨虫",
+    name: "骸骨虫",
+    reading: ["がいこつむし", "骸骨虫"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "骸骨虫.zip"
+  },
+  {
+    id: "陰鬱な死霊術師",
+    name: "陰鬱な死霊術師",
+    reading: ["いんうつなしりょうじゅつし", "陰鬱な死霊術師"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "陰鬱な死霊術師.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "サーベイジウルフ",
+    name: "サーベイジウルフ",
+    reading: ["さーべいじうるふ", "サーベイジウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "サーベイジウルフ.zip"
+  },
+  {
+    id: "呪いのカカシ",
+    name: "呪いのカカシ",
+    reading: ["のろいのかかし", "呪いのカカシ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "呪いのカカシ.zip"
+  },
+  {
+    id: "硝子刃の悪魔",
+    name: "硝子刃の悪魔",
+    reading: ["がらすばのあくま", "硝子刃の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "硝子刃の悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "誓いの翼戦士",
+    name: "誓いの翼戦士",
+    reading: ["ちかいのつばさせんし", "誓いの翼戦士"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "誓いの翼戦士.zip"
+  },
+  {
+    id: "ソードシスター",
+    name: "ソードシスター",
+    reading: ["そーどしすたー", "ソードシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "ソードシスター.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エアシップホエール",
+    name: "エアシップホエール",
+    reading: ["えあしっぷほえーる", "エアシップホエール"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "エアシップホエール.zip"
+  },
+  {
+    id: "キャプテン・メテオ",
+    name: "キャプテン・メテオ",
+    reading: ["きゃぷてんめてお", "キャプテン・メテオ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "キャプテン・メテオ.zip"
+  },
+  {
+    id: "魔銃の傭兵",
+    name: "魔銃の傭兵",
+    reading: ["まじゅうのようへい", "魔銃の傭兵"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "魔銃の傭兵.zip"
+  },
+  {
+    id: "仮面人形",
+    name: "仮面人形",
+    reading: ["かめんにんぎょう", "仮面人形"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "仮面人形.zip"
+  },
+  {
+    id: "重力戦士",
+    name: "重力戦士",
+    reading: ["じゅうりょくせんし", "重力戦士"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "重力戦士.zip"
+  },
+  {
+    id: "機構の弓兵",
+    name: "機構の弓兵",
+    reading: ["きこうのきゅうへい", "機構の弓兵"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "機構の弓兵.zip"
+  },
+  {
+    id: "鉄杖の機構士",
+    name: "鉄杖の機構士",
+    reading: ["てつじょうのきこうし", "鉄杖の機構士"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "鉄杖の機構士.zip"
+  },
+  {
+    id: "機構翼の剣士",
+    name: "機構翼の剣士",
+    reading: ["きこうよくのけんし", "機構翼の剣士"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "機構翼の剣士.zip"
+  },
+  {
+    id: "鋼鉄の壊し屋",
+    name: "鋼鉄の壊し屋",
+    reading: ["こうてつのこわしや", "鋼鉄の壊し屋"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "鋼鉄の壊し屋.zip"
+  },
+  {
+    id: "肉球砲の撃ち手",
+    name: "肉球砲の撃ち手",
+    reading: ["にくきゅうほうのうちて", "肉球砲の撃ち手"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "肉球砲の撃ち手.zip"
+  },
+  {
+    id: "カースドストーン",
+    name: "カースドストーン",
+    reading: ["かーすどすとーん", "カースドストーン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "カースドストーン.zip"
+  },
+  {
+    id: "熊人形の少女",
+    name: "熊人形の少女",
+    reading: ["くまにんぎょうのしょうじょ", "熊人形の少女"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "熊人形の少女.zip"
+  },
+  {
+    id: "ハーメルン",
+    name: "ハーメルン",
+    reading: ["はーめるん", "ハーメルン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "ハーメルン.zip"
+  },
+  {
+    id: "悪魔のエピタフ",
+    name: "悪魔のエピタフ",
+    reading: ["あくまのえぴたふ", "悪魔のエピタフ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "悪魔のエピタフ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ハッピーピッグ",
+    name: "ハッピーピッグ",
+    reading: ["はっぴーぴっぐ", "ハッピーピッグ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "ハッピーピッグ.zip"
+  },
+  {
+    id: "豪快な斧使い",
+    name: "豪快な斧使い",
+    reading: ["ごうかいなおのつかい", "豪快な斧使い"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "豪快な斧使い.zip"
+  },
+  {
+    id: "ダンジョンエクスプローラー・クロエ",
+    name: "ダンジョンエクスプローラー・クロエ",
+    reading: ["だんじょんえくすぷろーらーくろえ", "ダンジョンエクスプローラー・クロエ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "CGS",
+    zip: "ダンジョンエクスプローラー・クロエ.zip"
+  }
 ];

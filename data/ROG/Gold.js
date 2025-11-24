f7be6e5672c709835aaf881238b6ee209b847eac
panini4552 <panini4552@gmail.com>
2025-11-24 17:40:52 +0900

    Update Gold.js

--- data/ROG/Gold.js
+++ data/ROG/Gold.js
@@ -1,15 +1,208 @@
-export const Classic_Legendary = [
+export const ROG_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "豪風のリノセウス",
+    name: "豪風のリノセウス",
+    reading: ["ごうふうのりのせうす", "豪風のリノセウス"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/豪風のリノセウス.zip"
+  },
+  {
+    id: "機械樹の番人",
+    name: "機械樹の番人",
+    reading: ["きかいじゅのばんにん", "機械樹の番人"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/機械樹の番人.zip"
+  },
+  {
+    id: "アクティブエルフ・メイ",
+    name: "アクティブエルフ・メイ",
+    reading: ["あくてぃぶえるふめい", "アクティブエルフ・メイ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/アクティブエルフ・メイ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レヴィオンの猛者・ジェノ",
+    name: "レヴィオンの猛者・ジェノ",
+    reading: ["れびおんのもさじぇの", "レヴィオンの猛者・ジェノ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/レヴィオンの猛者・ジェノ.zip"
+  },
+  {
+    id: "麗しきフェンサー",
+    name: "麗しきフェンサー",
+    reading: ["うるわしきふぇんさー", "麗しきフェンサー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/麗しきフェンサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "プリンセスウィッチ・メルヴィ",
+    name: "プリンセスウィッチ・メルヴィ",
+    reading: ["ぷりんせすうぃっちめるゔぃ", "プリンセスウィッチ・メルヴィ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/プリンセスウィッチ・メルヴィ.zip"
+  },
+  {
+    id: "叡智の魔術師・レヴィ",
+    name: "叡智の魔術師・レヴィ",
+    reading: ["えいちのまじゅつしれゔぃ", "叡智の魔術師・レヴィ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/叡智の魔術師・レヴィ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "聡明のドラゴニュートスカラー",
+    name: "聡明のドラゴニュートスカラー",
+    reading: ["そうめいのどらごにゅーとすからー", "聡明のドラゴニュートスカラー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/聡明のドラゴニュートスカラー.zip"
+  },
+  {
+    id: "ドラゴンバスター・イアン",
+    name: "ドラゴンバスター・イアン",
+    reading: ["どらごんばすたーいあん", "ドラゴンバスター・イアン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/ドラゴンバスター・イアン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥守の継承者・カムラ",
+    name: "冥守の継承者・カムラ",
+    reading: ["めいしゅのけいしょうしゃかむら", "冥守の継承者・カムラ"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/冥守の継承者・カムラ.zip"
+  },
+  {
+    id: "魔拳法・ソーラ",
+    name: "魔拳法・ソーラ",
+    reading: ["まけんほうそーら", "魔拳法・ソーラ"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/魔拳法・ソーラ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "哀切の悪鬼",
+    name: "哀切の悪鬼",
+    reading: ["あいせつのあっき", "哀切の悪鬼"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/哀切の悪鬼.zip"
+  },
+  {
+    id: "堕落の漆黒・アザゼル",
+    name: "堕落の漆黒・アザゼル",
+    reading: ["だらくのしっこくあざぜる", "堕落の漆黒・アザゼル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/堕落の漆黒・アザゼル.zip"
+  },
+  {
+    id: "シェイドデビル・モリアナ",
+    name: "シェイドデビル・モリアナ",
+    reading: ["しぇいどでびるもりあな", "シェイドデビル・モリアナ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/シェイドデビル・モリアナ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖弓の使い手・クルト",
+    name: "聖弓の使い手・クルト",
+    reading: ["せいきゅうのつかいてくると", "聖弓の使い手・クルト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/聖弓の使い手・クルト.zip"
+  },
+  {
+    id: "調和の聖女・レリア",
+    name: "調和の聖女・レリア",
+    reading: ["ちょうわのせいじょれりあ", "調和の聖女・レリア"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/調和の聖女・レリア.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エアリアルクラフト",
+    name: "エアリアルクラフト",
+    reading: ["えありあるくらふと", "エアリアルクラフト"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/エアリアルクラフト.zip"
+  },
+  {
+    id: "アーティファクトリメイカー",
+    name: "アーティファクトリメイカー",
+    reading: ["あーてぃふぁくとりめいかー", "アーティファクトリメイカー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/アーティファクトリメイカー.zip"
+  },
+  {
+    id: "ドールスローター・フィア",
+    name: "ドールスローター・フィア",
+    reading: ["どーるすろーたーふぃあ", "ドールスローター・フィア"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/ドールスローター・フィア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "再編の女神・ウルズ",
+    name: "再編の女神・ウルズ",
+    reading: ["さいへんのめがみうるず", "再編の女神・ウルズ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/再編の女神・ウルズ.zip"
+  },
+  {
+    id: "聖鳥・ハンサ",
+    name: "聖鳥・ハンサ",
+    reading: ["せいちょうはんさ", "聖鳥・ハンサ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ROG",
+    zip: "voices/ROG/Gold/聖鳥・ハンサ.zip"
+  }
 ];

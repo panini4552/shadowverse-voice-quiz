a733a1d3027329996bf3fb5b6d509cfaa40f6f57
panini4552 <panini4552@gmail.com>
2025-11-24 16:57:04 +0900

    Update Gold.js

--- data/ALT/Gold.js
+++ data/ALT/Gold.js
@@ -1,15 +1,201 @@
-export const Classic_Legendary = [
+// data/ALT/Gold.js
+
+export const ALT_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "獣戦士・セタス",
+    name: "獣戦士・セタス",
+    reading: ["じゅうせんしせたす", "ジュウセンシセタス", "獣戦士・セタス"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/獣戦士・セタス.zip"
+  },
+  {
+    id: "猛毒の姫・リコリス",
+    name: "猛毒の姫・リコリス",
+    reading: ["もうどくのひめりこりす", "モウドクノヒメリコリス", "猛毒の姫・リコリス"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/猛毒の姫・リコリス.zip"
+  },
+  {
+    id: "ルナールツインズ・リナ＆レナ",
+    name: "ルナールツインズ・リナ＆レナ",
+    reading: ["るなーるついんずりなれな", "ルナールツインズリナレナ", "ルナールツインズ・リナ＆レナ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/ルナールツインズ・リナ＆レナ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "月の刃・リオード",
+    name: "月の刃・リオード",
+    reading: ["つきのやいばりおーど", "ツキノヤイバリオード", "月の刃・リオード"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/月の刃・リオード.zip"
+  },
+  {
+    id: "マスターディーラー・アルヤスカ",
+    name: "マスターディーラー・アルヤスカ",
+    reading: ["ますたーでぃーらーあるやすか", "マスターディーラーアルヤスカ", "マスターディーラー・アルヤスカ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/マスターディーラー・アルヤスカ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "天外の華・エレノア",
+    name: "天外の華・エレノア",
+    reading: ["てんがいのはなえれのあ", "テンガイノハナエレノア", "天外の華・エレノア"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/天外の華・エレノア.zip"
+  },
+  {
+    id: "ラブリー★モニカ",
+    name: "ラブリー★モニカ",
+    reading: ["らぶりーもにか", "ラブリーモニカ", "ラブリー★モニカ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/ラブリー★モニカ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "怒れる豪炎のドラゴン",
+    name: "怒れる豪炎のドラゴン",
+    reading: ["いかれるごうえんのどらごん", "イカルルゴウエンノドラゴン", "怒れる豪炎のドラゴン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/怒れる豪炎のドラゴン.zip"
+  },
+  {
+    id: "アンネローゼ",
+    name: "アンネローゼ",
+    reading: ["あんねろーぜ", "アンネローゼ", "アンネローゼ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/アンネローゼ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "禁絶の腕・ニコラ",
+    name: "禁絶の腕・ニコラ",
+    reading: ["きんぜつのうでにこら", "キンゼツノウデニコラ", "禁絶の腕・ニコラ"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/禁絶の腕・ニコラ.zip"
+  },
+  {
+    id: "暴食の女帝",
+    name: "暴食の女帝",
+    reading: ["ぼうしょくのじょてい", "ボウショクノジョテイ", "暴食の女帝"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/暴食の女帝.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "片翼の悪魔・ピユラ",
+    name: "片翼の悪魔・ピユラ",
+    reading: ["かたよくのあくまぴゆら", "カタヨクノアクマピユラ", "片翼の悪魔・ピユラ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/片翼の悪魔・ピユラ.zip"
+  },
+  {
+    id: "ライカンベルセルク",
+    name: "ライカンベルセルク",
+    reading: ["らいかんべるせるく", "ライカンベルセルク", "ライカンベルセルク"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/ライカンベルセルク.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "均衡の光・マーロン",
+    name: "均衡の光・マーロン",
+    reading: ["きんこうのひかりまーろん", "キンコウノヒカリマーロン", "均衡の光・マーロン"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/均衡の光・マーロン.zip"
+  },
+  {
+    id: "セクメト",
+    name: "セクメト",
+    reading: ["せくめと", "セクメト", "セクメト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/セクメト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "人形の支配者・リーアム",
+    name: "人形の支配者・リーアム",
+    reading: ["にんぎょうのしはいしゃりーあむ", "ニンギョウノシハイシャリーアム", "人形の支配者・リーアム"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/人形の支配者・リーアム.zip"
+  },
+  {
+    id: "世界の扉・ティル",
+    name: "世界の扉・ティル",
+    reading: ["せかいのとびらてぃる", "セカイノトビラティル", "世界の扉・ティル"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/世界の扉・ティル.zip"
+  },
+  {
+    id: "粛清の英雄・メイシア",
+    name: "粛清の英雄・メイシア",
+    reading: ["しゅくせいのえいゆうめいしあ", "シュクセイノエイユウメイシア", "粛清の英雄・メイシア"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/粛清の英雄・メイシア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ソードエンジェル・エフェメラ",
+    name: "ソードエンジェル・エフェメラ",
+    reading: ["そーどえんじぇるえふぇめら", "ソードエンジェルエフェメラ", "ソードエンジェル・エフェメラ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/ソードエンジェル・エフェメラ.zip"
+  },
+  {
+    id: "大鎧のミノタウロス",
+    name: "大鎧のミノタウロス",
+    reading: ["おおよろいのみのたうろす", "オオヨロイノミノタウロス", "大鎧のミノタウロス"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ALT",
+    zip: "voices/ALT/Gold/大鎧のミノタウロス.zip"
+  }
 ];

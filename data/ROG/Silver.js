9f36a2364c7ad0916181bed60705fe60aa97e369
panini4552 <panini4552@gmail.com>
2025-11-24 17:41:34 +0900

    Update Silver.js

--- data/ROG/Silver.js
+++ data/ROG/Silver.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
+export const ROG_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "翅を裂く者・メリラ",
+    name: "翅を裂く者・メリラ",
+    reading: ["はねをさくものめりら", "翅を裂く者・メリラ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/翅を裂く者・メリラ.zip"
+  },
+  {
+    id: "清き泉のエルフプリンセスメイジ",
+    name: "清き泉のエルフプリンセスメイジ",
+    reading: ["きよきいずみのえるふぷりんせすめいじ", "清き泉のエルフプリンセスメイジ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/清き泉のエルフプリンセスメイジ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レヴィオンの猛者・ジェノ",
+    name: "レヴィオンの猛者・ジェノ",
+    reading: ["れびおんのもさじぇの", "レヴィオンの猛者・ジェノ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/レヴィオンの猛者・ジェノ.zip"
+  },
+  {
+    id: "麗しきフェンサー",
+    name: "麗しきフェンサー",
+    reading: ["うるわしきふぇんさー", "麗しきフェンサー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/麗しきフェンサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "笑顔の魔女・エミル",
+    name: "笑顔の魔女・エミル",
+    reading: ["えがおのまじょえみる", "笑顔の魔女・エミル"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/笑顔の魔女・エミル.zip"
+  },
+  {
+    id: "マシンブックソーサラー",
+    name: "マシンブックソーサラー",
+    reading: ["ましんぶっくそーさらー", "マシンブックソーサラー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/マシンブックソーサラー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "鋼爪の竜戦士",
+    name: "鋼爪の竜戦士",
+    reading: ["こうそうのりゅうせんし", "鋼爪の竜戦士"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/鋼爪の竜戦士.zip"
+  },
+  {
+    id: "風に乗る者・エイファ",
+    name: "風に乗る者・エイファ",
+    reading: ["かぜにのるものえいふぁ", "風に乗る者・エイファ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/風に乗る者・エイファ.zip"
+  },
+  {
+    id: "螺旋のムシュフシュ",
+    name: "螺旋のムシュフシュ",
+    reading: ["らせんのむしゅふしゅ", "螺旋のムシュフシュ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/螺旋のムシュフシュ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "アイアンツールドクター",
+    name: "アイアンツールドクター",
+    reading: ["あいあんつーるどくたー", "アイアンツールドクター"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/アイアンツールドクター.zip"
+  },
+  {
+    id: "冥守の祈祷師・ティティ",
+    name: "冥守の祈祷師・ティティ",
+    reading: ["めいしゅのきとうしてぃてぃ", "冥守の祈祷師・ティティ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/冥守の祈祷師・ティティ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "マシンファームデビル",
+    name: "マシンファームデビル",
+    reading: ["ましんふぁーむでびる", "マシンファームデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/マシンファームデビル.zip"
+  },
+  {
+    id: "ルインドリーム・ナイトメア",
+    name: "ルインドリーム・ナイトメア",
+    reading: ["るいんどりーむないとめあ", "ルインドリーム・ナイトメア"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/ルインドリーム・ナイトメア.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "機械腕の祈り手",
+    name: "機械腕の祈り手",
+    reading: ["きかいうでのいのりて", "機械腕の祈り手"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/機械腕の祈り手.zip"
+  },
+  {
+    id: "邪教の神父",
+    name: "邪教の神父",
+    reading: ["じゃきょうのしんぷ", "邪教の神父"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/邪教の神父.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "天翔のイカロス",
+    name: "天翔のイカロス",
+    reading: ["てんしょうのいかろす", "天翔のイカロス"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/天翔のイカロス.zip"
+  },
+  {
+    id: "歯車の廻し手・リヒト",
+    name: "歯車の廻し手・リヒト",
+    reading: ["はぐるまのまわしてりひと", "歯車の廻し手・リヒト"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/歯車の廻し手・リヒト.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "アポロン",
+    name: "アポロン",
+    reading: ["あぽろん", "アポロン"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/アポロン.zip"
+  },
+  {
+    id: "一角獣の巫女・ユニコ",
+    name: "一角獣の巫女・ユニコ",
+    reading: ["いっかくじゅうのみこゆにこ", "一角獣の巫女・ユニコ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ROG",
+    zip: "voices/ROG/Silver/一角獣の巫女・ユニコ.zip"
+  }
 ];

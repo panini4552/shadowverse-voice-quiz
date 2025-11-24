8b84fbe4f69f83250da0492fecaeb5181b12f600
panini4552 <panini4552@gmail.com>
2025-11-24 17:01:29 +0900

    Update Silver.js

--- data/ALT/Silver.js
+++ data/ALT/Silver.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
-  // === エルフ ===
+export const ALT_Silver = [
+  // エルフ
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "心読む少女・ミツェル",
+    name: "心読む少女・ミツェル",
+    reading: ["こころよむしょうじょみつぇる", "心読む少女・ミツェル"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "心読む少女・ミツェル.zip"
+  },
+  {
+    id: "森の戦士",
+    name: "森の戦士",
+    reading: ["もりのせんし", "森の戦士"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "森の戦士.zip"
   },
 
+  // ロイヤル
+  {
+    id: "ウェポンディーラー・イルミスナ",
+    name: "ウェポンディーラー・イルミスナ",
+    reading: ["うぇぽんでぃーらーいるみすな", "ウェポンディーラー・イルミスナ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "ウェポンディーラー・イルミスナ.zip"
+  },
+  {
+    id: "狂槍のケンタウロス",
+    name: "狂槍のケンタウロス",
+    reading: ["きょうそうのけんたうろす", "狂槍のケンタウロス"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "狂槍のケンタウロス.zip"
+  },
 
+  // ウィッチ
+  {
+    id: "マナリアの見習い教師・パスカル",
+    name: "マナリアの見習い教師・パスカル",
+    reading: ["まなりあのみならいきょうしぱすかる", "マナリアの見習い教師・パスカル"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "マナリアの見習い教師・パスカル.zip"
+  },
+  {
+    id: "ケンタウロスシャーマン",
+    name: "ケンタウロスシャーマン",
+    reading: ["けんたうろすしゃーまん", "ケンタウロスシャーマン"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "ケンタウロスシャーマン.zip"
+  },
 
+  // ドラゴン
+  {
+    id: "侮蔑の狂信者",
+    name: "侮蔑の狂信者",
+    reading: ["ぶべつのきょうしんしゃ", "侮蔑の狂信者"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "侮蔑の狂信者.zip"
+  },
+  {
+    id: "正義のドラグーン",
+    name: "正義のドラグーン",
+    reading: ["せいぎのどらぐーん", "正義のドラグーン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "正義のドラグーン.zip"
+  },
+  {
+    id: "岩食い竜",
+    name: "岩食い竜",
+    reading: ["いわぐいりゅう", "岩食い竜"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "岩食い竜.zip"
+  },
+
+  // ネクロマンサー
+  {
+    id: "ワイトナイト",
+    name: "ワイトナイト",
+    reading: ["わいとないと", "ワイトナイト"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "ワイトナイト.zip"
+  },
+  {
+    id: "パンプキンヘッド・クリス",
+    name: "パンプキンヘッド・クリス",
+    reading: ["ぱんぷきんへっどくりす", "パンプキンヘッド・クリス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "パンプキンヘッド・クリス.zip"
+  },
+  {
+    id: "死を知る者",
+    name: "死を知る者",
+    reading: ["しをしるもの", "死を知る者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "死を知る者.zip"
+  },
+
+  // ヴァンパイア
+  {
+    id: "盟約の魔人",
+    name: "盟約の魔人",
+    reading: ["めいやくのまじん", "盟約の魔人"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "盟約の魔人.zip"
+  },
+  {
+    id: "サロメ",
+    name: "サロメ",
+    reading: ["さろめ", "サロメ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "サロメ.zip"
+  },
+
+  // ビショップ
+  {
+    id: "セイントガンナー・コレット",
+    name: "セイントガンナー・コレット",
+    reading: ["せいんとがんなーこれっと", "セイントガンナー・コレット"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "セイントガンナー・コレット.zip"
+  },
+  {
+    id: "戦場の審問官",
+    name: "戦場の審問官",
+    reading: ["せんじょうのしんもんかん", "戦場の審問官"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "戦場の審問官.zip"
+  },
+
+  // ネメシス
+  {
+    id: "マリオネットボーイ",
+    name: "マリオネットボーイ",
+    reading: ["まりおねっとぼーい", "マリオネットボーイ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "マリオネットボーイ.zip"
+  },
+  {
+    id: "機構二輪の天使",
+    name: "機構二輪の天使",
+    reading: ["きこうにりんのてんし", "機構二輪の天使"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "機構二輪の天使.zip"
+  },
+
+  // ニュートラル
+  {
+    id: "進化の導き手・レイナ",
+    name: "進化の導き手・レイナ",
+    reading: ["しんかのみちびきてれいな", "進化の導き手・レイナ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ALT",
+    zip: "進化の導き手・レイナ.zip"
+  }
 ];

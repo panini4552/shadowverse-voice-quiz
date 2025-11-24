f61ea2fa89e0a22849d30cd74baf217c184c0707
panini4552 <panini4552@gmail.com>
2025-11-24 17:36:04 +0900

    Update Legendary.js

--- data/ROG/Legendary.js
+++ data/ROG/Legendary.js
@@ -1,15 +1,226 @@
-export const Classic_Legendary = [
+export const ROG_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "始祖の大狼・オムニス",
+    name: "始祖の大狼・オムニス",
+    reading: ["しそのたいろうおむにす", "シソノタイロウオムニス", "始祖の大狼・オムニス"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/始祖の大狼・オムニス.zip"
+  },
+  {
+    id: "慈愛の翅・ティスタ",
+    name: "慈愛の翅・ティスタ",
+    reading: ["じあいのつばさてぃすた", "ジアイノツバサティスタ", "慈愛の翅・ティスタ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/慈愛の翅・ティスタ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レヴィオンの英雄・アルベール",
+    name: "レヴィオンの英雄・アルベール",
+    reading: ["れヴぃおんのえいゆうあるべーる", "レヴィオンノエイユウアルベール", "レヴィオンの英雄・アルベール"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/レヴィオンの英雄・アルベール.zip"
+  },
+  {
+    id: "海賊女王・アルビダ",
+    name: "海賊女王・アルビダ",
+    reading: ["かいぞくじょおうあるびだ", "カイゾクジョオウアルビダ", "海賊女王・アルビダ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/海賊女王・アルビダ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "老練の大魔術師・エラスムス",
+    name: "老練の大魔術師・エラスムス",
+    reading: ["ろうれんのだいまじゅつしえらすむす", "ロウレンノダイマジュツシエラスムス", "老練の大魔術師・エラスムス"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/老練の大魔術師・エラスムス.zip"
+  },
+  {
+    id: "呪いの言霊・ジンジャー",
+    name: "呪いの言霊・ジンジャー",
+    reading: ["のろいのことだまじんじゃー", "ノロイノコトダマジンジャー", "呪いの言霊・ジンジャー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/呪いの言霊・ジンジャー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "天災のジェネシスドラゴン",
+    name: "天災のジェネシスドラゴン",
+    reading: ["てんさいのじぇねしすどらごん", "テンサイノジェネシスドラゴン", "天災のジェネシスドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/天災のジェネシスドラゴン.zip"
+  },
+  {
+    id: "ハルクドラゴニュート",
+    name: "ハルクドラゴニュート",
+    reading: ["はるくどらごにゅーと", "ハルクドラゴニュート", "ハルクドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/ハルクドラゴニュート.zip"
+  },
+  {
+    id: "絆の竜剣士・アイラ",
+    name: "絆の竜剣士・アイラ",
+    reading: ["きずなのりゅうけんしあいら", "キズナノリュウケンシアイラ", "絆の竜剣士・アイラ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/絆の竜剣士・アイラ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥界神・ハデス",
+    name: "冥界神・ハデス",
+    reading: ["めいかいしんはです", "メイカイシンハデス", "冥界神・ハデス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/冥界神・ハデス.zip"
+  },
+  {
+    id: "冥守の頂点・アイシャ",
+    name: "冥守の頂点・アイシャ",
+    reading: ["めいしゅのちょうてんあいしゃ", "メイシュノチョウテンアイシャ", "冥守の頂点・アイシャ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/冥守の頂点・アイシャ.zip"
+  },
+  {
+    id: "ソウルテイカー・ララ",
+    name: "ソウルテイカー・ララ",
+    reading: ["そうるていからら", "ソウルテイカララ", "ソウルテイカー・ララ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/ソウルテイカー・ララ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "悪逆の公爵・ユリウス",
+    name: "悪逆の公爵・ユリウス",
+    reading: ["あくぎゃくのこうしゃくゆりうす", "アクギャクノコウシャクユリウス", "悪逆の公爵・ユリウス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/悪逆の公爵・ユリウス.zip"
+  },
+  {
+    id: "絢爛のセクシーヴァンパイア",
+    name: "絢爛のセクシーヴァンパイア",
+    reading: ["けんらんのせくしーヴぁんぱいあ", "ケンランノセクシーヴァンパイア", "絢爛のセクシーヴァンパイア"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/絢爛のセクシーヴァンパイア.zip"
+  },
+  {
+    id: "天閉ざす昏き者",
+    name: "天閉ざす昏き者",
+    reading: ["てんとざすくらきもの", "テントザスクラキモノ", "天閉ざす昏き者"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/天閉ざす昏き者.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "清純なる祈り・エイラ",
+    name: "清純なる祈り・エイラ",
+    reading: ["せいじゅんなるいのりえいら", "セイジュンナルイノリエイラ", "清純なる祈り・エイラ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/清純なる祈り・エイラ.zip"
+  },
+  {
+    id: "賛美の狂信像・イミナ",
+    name: "賛美の狂信像・イミナ",
+    reading: ["さんびのきょうしんぞういみな", "サンビノキョウシンゾウイミナ", "賛美の狂信像・イミナ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/賛美の狂信像・イミナ.zip"
+  },
+  {
+    id: "デスティニーウィングナイト",
+    name: "デスティニーウィングナイト",
+    reading: ["ですてぃにーうぃんぐないと", "デスティニーウィングナイト", "デスティニーウィングナイト"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/デスティニーウィングナイト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "レゾナンスハート・ツヴァイ",
+    name: "レゾナンスハート・ツヴァイ",
+    reading: ["れぞなんすはーとつヴぁい", "レゾナンスハートツヴァイ", "レゾナンスハート・ツヴァイ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/レゾナンスハート・ツヴァイ.zip"
+  },
+  {
+    id: "マグナジャイアント",
+    name: "マグナジャイアント",
+    reading: ["まぐなじゃいあんと", "マグナジャイアント", "マグナジャイアント"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/マグナジャイアント.zip"
+  },
+  {
+    id: "リンクハート・オーキス",
+    name: "リンクハート・オーキス",
+    reading: ["りんくはーとおーきす", "リンクハートオーキス", "リンクハート・オーキス"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/リンクハート・オーキス.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "至高神・ゼウス",
+    name: "至高神・ゼウス",
+    reading: ["しこうしんぜうす", "シコウシンゼウス", "至高神・ゼウス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/至高神・ゼウス.zip"
+  },
+  {
+    id: "闇の翼・オリヴィエ",
+    name: "闇の翼・オリヴィエ",
+    reading: ["やみのつばさおりヴぃえ", "ヤミノツバサオリヴィエ", "闇の翼・オリヴィエ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ROG",
+    zip: "voices/ROG/Legendary/闇の翼・オリヴィエ.zip"
+  }
 ];

f4a8d9845762810a8859ae95e7eeb80470011a72
panini4552 <panini4552@gmail.com>
2025-11-24 18:12:52 +0900

    Update Bronze.js

--- data/VEC/Bronze.js
+++ data/VEC/Bronze.js
@@ -1,15 +1,271 @@
-export const Classic_Legendary = [
+export const VEC_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "スケアリートレント",
+    name: "スケアリートレント",
+    reading: ["すけありーとれんと", "スケアリートレント", "スケアリートレント"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/スケアリートレント.zip"
+  },
+  {
+    id: "悪戯の精霊",
+    name: "悪戯の精霊",
+    reading: ["いたずらのせいれい", "イタズラノセイレイ", "悪戯の精霊"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/悪戯の精霊.zip"
+  },
+  {
+    id: "ブリッツエルフ",
+    name: "ブリッツエルフ",
+    reading: ["ぶりっつえるふ", "ブリッツエルフ", "ブリッツエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ブリッツエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ハルバードウルフ",
+    name: "ハルバードウルフ",
+    reading: ["はるばーどうるふ", "ハルバードウルフ", "ハルバードウルフ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ハルバードウルフ.zip"
+  },
+  {
+    id: "シーフガール",
+    name: "シーフガール",
+    reading: ["しーふがーる", "シーフガール", "シーフガール"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/シーフガール.zip"
+  },
+  {
+    id: "レヴィオンの探索者",
+    name: "レヴィオンの探索者",
+    reading: ["れゔぃおんのたんさくしゃ", "レヴィオンの探索者", "レヴィオンの探索者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/レヴィオンの探索者.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アースエレメンタラー",
+    name: "アースエレメンタラー",
+    reading: ["あーすえれめんたらー", "アースエレメンタラー", "アースエレメンタラー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/アースエレメンタラー.zip"
+  },
+  {
+    id: "豪腕のゴーレム",
+    name: "豪腕のゴーレム",
+    reading: ["ごうわんのごーれむ", "ゴウワンノゴーレム", "豪腕のゴーレム"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/豪腕のゴーレム.zip"
+  },
+  {
+    id: "インパルスアルケミスト",
+    name: "インパルスアルケミスト",
+    reading: ["いんぱるすあるけみすと", "インパルスアルケミスト", "インパルスアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/インパルスアルケミスト.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "雷電のヴェロキラプトル",
+    name: "雷電のヴェロキラプトル",
+    reading: ["らいでんのゔぇろきらぷとる", "雷電のヴェロキラプトル", "雷電のヴェロキラプトル"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/雷電のヴェロキラプトル.zip"
+  },
+  {
+    id: "ドラゴンシェフ",
+    name: "ドラゴンシェフ",
+    reading: ["どらごんしぇふ", "ドラゴンシェフ", "ドラゴンシェフ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ドラゴンシェフ.zip"
+  },
+  {
+    id: "ニードルドラゴニュート",
+    name: "ニードルドラゴニュート",
+    reading: ["にーどるどらごにゅーと", "ニードルドラゴニュート", "ニードルドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ニードルドラゴニュート.zip"
+  },
+  {
+    id: "ドラゴンブレイダー",
+    name: "ドラゴンブレイダー",
+    reading: ["どらごんぶれいだー", "ドラゴンブレイダー", "ドラゴンブレイダー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ドラゴンブレイダー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "シープスピリット",
+    name: "シープスピリット",
+    reading: ["しーぷすぴりっと", "シープスピリット", "シープスピリット"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/シープスピリット.zip"
+  },
+  {
+    id: "取り憑く怨霊",
+    name: "取り憑く怨霊",
+    reading: ["とりつくおんりょう", "トリツクオンリョウ", "取り憑く怨霊"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/取り憑く怨霊.zip"
+  },
+  {
+    id: "ロンリネスゴースト",
+    name: "ロンリネスゴースト",
+    reading: ["ろんりねすごーすと", "ロンリネスゴースト", "ロンリネスゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ロンリネスゴースト.zip"
+  },
+  {
+    id: "ソウルスナッチャー",
+    name: "ソウルスナッチャー",
+    reading: ["そうるすなっちゃー", "ソウルスナッチャー", "ソウルスナッチャー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ソウルスナッチャー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "歪みし蝙蝠",
+    name: "歪みし蝙蝠",
+    reading: ["ゆがみしこうもり", "ユガミシコウモリ", "歪みし蝙蝠"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/歪みし蝙蝠.zip"
+  },
+  {
+    id: "レラジュ",
+    name: "レラジュ",
+    reading: ["れらじゅ", "レラジュ", "レラジュ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/レラジュ.zip"
+  },
+  {
+    id: "スマッシュオーガ",
+    name: "スマッシュオーガ",
+    reading: ["すまっしゅおーが", "スマッシュオーガ", "スマッシュオーガ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/スマッシュオーガ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "エンジェルラット",
+    name: "エンジェルラット",
+    reading: ["えんじぇるらっと", "エンジェルラット", "エンジェルラット"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/エンジェルラット.zip"
+  },
+  {
+    id: "タリスマンシスター",
+    name: "タリスマンシスター",
+    reading: ["たりすまんしすたー", "タリスマンシスター", "タリスマンシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/タリスマンシスター.zip"
+  },
+  {
+    id: "エクセスプリースト",
+    name: "エクセスプリースト",
+    reading: ["えくせすぷりーすと", "エクセスプリースト", "エクセスプリースト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/エクセスプリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ドリルヘッジホッグ",
+    name: "ドリルヘッジホッグ",
+    reading: ["どりるへっじほっぐ", "ドリルヘッジホッグ", "ドリルヘッジホッグ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ドリルヘッジホッグ.zip"
+  },
+  {
+    id: "ドールマイスター",
+    name: "ドールマイスター",
+    reading: ["どーるまいすたー", "ドールマイスター", "ドールマイスター"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ドールマイスター.zip"
+  },
+  {
+    id: "ブライトシューター",
+    name: "ブライトシューター",
+    reading: ["ぶらいとしゅーたー", "ブライトシューター", "ブライトシューター"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ブライトシューター.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "スウィートディテクティブ",
+    name: "スウィートディテクティブ",
+    reading: ["すうぃーとでぃてくてぃぶ", "スウィートディテクティブ", "スウィートディテクティブ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/スウィートディテクティブ.zip"
+  },
+  {
+    id: "ワンダーコック",
+    name: "ワンダーコック",
+    reading: ["わんだーこっく", "ワンダーコック", "ワンダーコック"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "VEC",
+    zip: "voices/VEC/Bronze/ワンダーコック.zip"
+  }
 ];

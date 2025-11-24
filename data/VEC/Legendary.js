d588dce1ee1ecf8d6fffca0140fc1561aa579840
panini4552 <panini4552@gmail.com>
2025-11-24 17:55:54 +0900

    Update Legendary.js

--- data/VEC/Legendary.js
+++ data/VEC/Legendary.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const VEC_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "回帰する抱擁・ラティカ",
+    name: "回帰する抱擁・ラティカ",
+    reading: ["かいきするほうようらてぃか", "カイキスルホウヨウラティカ", "回帰する抱擁・ラティカ"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "VEC",
+    zip: "回帰する抱擁・ラティカ.zip"
+  },
+  {
+    id: "フェアリーブレイダー・アマツ",
+    name: "フェアリーブレイダー・アマツ",
+    reading: ["ふぇありーぶれいだーあまつ", "フェアリーブレイダーアマツ", "フェアリーブレイダー・アマツ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "フェアリーブレイダー・アマツ.zip"
+  },
+  {
+    id: "プライマルギガント",
+    name: "プライマルギガント",
+    reading: ["ぷらいまるぎがんと", "プライマルギガント", "プライマルギガント"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "プライマルギガント.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "王たる光・ベイリオン",
+    name: "王たる光・ベイリオン",
+    reading: ["おうたるひかりべいりおん", "オウタルヒカリベイリオン", "王たる光・ベイリオン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "王たる光・ベイリオン.zip"
+  },
+  {
+    id: "君臨する猛虎",
+    name: "君臨する猛虎",
+    reading: ["くんりんするもうこ", "クンリンスルモウコ", "君臨する猛虎"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "君臨する猛虎.zip"
+  },
+  {
+    id: "妖刀の鬼・ツバキ",
+    name: "妖刀の鬼・ツバキ",
+    reading: ["ようとうのおにつばき", "ヨウトウノオニツバキ", "妖刀の鬼・ツバキ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "妖刀の鬼・ツバキ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "極点のエレメンタル",
+    name: "極点のエレメンタル",
+    reading: ["きょくてんのえれめんたる", "キョクテンノエレメンタル", "極点のエレメンタル"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "極点のエレメンタル.zip"
+  },
+  {
+    id: "魔神の使役者",
+    name: "魔神の使役者",
+    reading: ["ましんのしえきしゃ", "マシンノシエキシャ", "魔神の使役者"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "魔神の使役者.zip"
+  },
+  {
+    id: "秘蹟の魔女・メディア",
+    name: "秘蹟の魔女・メディア",
+    reading: ["ひせきのまじょめでぃあ", "ヒセキノマジョメディア", "秘蹟の魔女・メディア"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "秘蹟の魔女・メディア.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "呪われし影・ヴァイディ",
+    name: "呪われし影・ヴァイディ",
+    reading: ["のろわれしかげゔぁいでぃ", "ノロワレシカゲヴァイディ", "呪われし影・ヴァイディ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "呪われし影・ヴァイディ.zip"
+  },
+  {
+    id: "蒼海の主・ネプチューン",
+    name: "蒼海の主・ネプチューン",
+    reading: ["そうかいのぬしねぷちゅーん", "ソウカイノヌシネプチューン", "蒼海の主・ネプチューン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "蒼海の主・ネプチューン.zip"
+  },
+  {
+    id: "猛火のティラノサウルス",
+    name: "猛火のティラノサウルス",
+    reading: ["もうかのてぃらのさうるす", "モウカノティラノサウルス", "猛火のティラノサウルス"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "猛火のティラノサウルス.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "トート",
+    name: "トート",
+    reading: ["とーと", "トート", "トート"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "トート.zip"
+  },
+  {
+    id: "ネクログループ・ルベル",
+    name: "ネクログループ・ルベル",
+    reading: ["ねくろぐるーぷるべる", "ネクログループルベル", "ネクログループ・ルベル"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "ネクログループ・ルベル.zip"
+  },
+  {
+    id: "レジェンダリースケルトン",
+    name: "レジェンダリースケルトン",
+    reading: ["れじぇんだりーすけるとん", "レジェンダリースケルトン", "レジェンダリースケルトン"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "レジェンダリースケルトン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "仁義の悪魔・ユヅキ",
+    name: "仁義の悪魔・ユヅキ",
+    reading: ["じんぎのあくまゆづき", "ジンギノアクマユヅキ", "仁義の悪魔・ユヅキ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "仁義の悪魔・ユヅキ.zip"
+  },
+  {
+    id: "胎動の魔神",
+    name: "胎動の魔神",
+    reading: ["たいどうのましん", "タイドウノマシン", "胎動の魔神"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "胎動の魔神.zip"
+  },
+  {
+    id: "クドラク",
+    name: "クドラク",
+    reading: ["くどらく", "クドラク", "クドラク"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "クドラク.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ミュースプリンセス・ミルフィ",
+    name: "ミュースプリンセス・ミルフィ",
+    reading: ["みゅーすぷりんせすみるふぃ", "ミュースプリンセスミルフィ", "ミュースプリンセス・ミルフィ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "ミュースプリンセス・ミルフィ.zip"
+  },
+  {
+    id: "聖波動のスフィンクス",
+    name: "聖波動のスフィンクス",
+    reading: ["せいはどうのすふぃんくす", "セイハドウノスフィンクス", "聖波動のスフィンクス"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "聖波動のスフィンクス.zip"
+  },
+  {
+    id: "炎氷の司祭・カラト",
+    name: "炎氷の司祭・カラト",
+    reading: ["えんぴのしさいからと", "エンピノシサイカラト", "炎氷の司祭・カラト"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "炎氷の司祭・カラト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "マキシマムメタルゴリラ",
+    name: "マキシマムメタルゴリラ",
+    reading: ["まきしまむめたるごりら", "マキシマムメタルゴリラ", "マキシマムメタルゴリラ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "マキシマムメタルゴリラ.zip"
+  },
+  {
+    id: "ラグナアウェイク",
+    name: "ラグナアウェイク",
+    reading: ["らぐなあうぇいく", "ラグナアウェイク", "ラグナアウェイク"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "ラグナアウェイク.zip"
+  },
+  {
+    id: "アブソリュート・モデスト",
+    name: "アブソリュート・モデスト",
+    reading: ["あぶそりゅーと もですと", "アブソリュートモデスト", "アブソリュート・モデスト"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "アブソリュート・モデスト.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "母なる君",
+    name: "母なる君",
+    reading: ["ははなるきみ", "ハハナルキミ", "母なる君"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "母なる君.zip"
+  },
+  {
+    id: "シヴァ",
+    name: "シヴァ",
+    reading: ["しゔぁ", "シヴァ", "シヴァ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "VEC",
+    zip: "シヴァ.zip"
+  }
 ];

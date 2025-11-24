eeeebd2226d72356947fe162101c064d46721276
panini4552 <panini4552@gmail.com>
2025-11-24 17:44:47 +0900

    Update Bronze.js

--- data/ROG/Bronze.js
+++ data/ROG/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const ROG_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "マシンクローエルフ",
+    name: "マシンクローエルフ",
+    reading: ["ましんくろーえるふ", "マシンクローエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/マシンクローエルフ.zip"
+  },
+  {
+    id: "不屈のブレイブフェアリー",
+    name: "不屈のブレイブフェアリー",
+    reading: ["ふくつのぶれいぶふぇありー", "不屈のブレイブフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/不屈のブレイブフェアリー.zip"
+  },
+  {
+    id: "木立の精霊・ドリアード",
+    name: "木立の精霊・ドリアード",
+    reading: ["こだちのせいれいどりあーど", "木立の精霊・ドリアード"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/木立の精霊・ドリアード.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レヴィオンの見習い・ルネス",
+    name: "レヴィオンの見習い・ルネス",
+    reading: ["れびおんのみならいるねす", "レヴィオンの見習い・ルネス"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/レヴィオンの見習い・ルネス.zip"
+  },
+  {
+    id: "レヴィオンの弓使い",
+    name: "レヴィオンの弓使い",
+    reading: ["れびおんのゆみつかい", "レヴィオンの弓使い"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/レヴィオンの弓使い.zip"
+  },
+  {
+    id: "歴戦の旅ガエル",
+    name: "歴戦の旅ガエル",
+    reading: ["れきせんのたびがえる", "歴戦の旅ガエル"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/歴戦の旅ガエル.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ライトニングキャット",
+    name: "ライトニングキャット",
+    reading: ["らいとにんぐきゃっと", "ライトニングキャット"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/ライトニングキャット.zip"
+  },
+  {
+    id: "ジェットウィッチ",
+    name: "ジェットウィッチ",
+    reading: ["じぇっとうぃっち", "ジェットウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/ジェットウィッチ.zip"
+  },
+  {
+    id: "双刃の魔剣士",
+    name: "双刃の魔剣士",
+    reading: ["そうじんのまけんし", "双刃の魔剣士"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/双刃の魔剣士.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ガジェットマーメイド",
+    name: "ガジェットマーメイド",
+    reading: ["がじぇっとまーめいど", "ガジェットマーメイド"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/ガジェットマーメイド.zip"
+  },
+  {
+    id: "風を知る者・ゼル",
+    name: "風を知る者・ゼル",
+    reading: ["かぜをしるものぜる", "風を知る者・ゼル"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/風を知る者・ゼル.zip"
+  },
+  {
+    id: "雄大なるルフ鳥",
+    name: "雄大なるルフ鳥",
+    reading: ["ゆうだいなるるふどり", "雄大なるルフ鳥"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/雄大なるルフ鳥.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "秀才の死神・ミーノ",
+    name: "秀才の死神・ミーノ",
+    reading: ["しゅうさいのしにがみみーの", "秀才の死神・ミーノ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/秀才の死神・ミーノ.zip"
+  },
+  {
+    id: "グレートレッサーマミー",
+    name: "グレートレッサーマミー",
+    reading: ["ぐれーとれっさーまみー", "グレートレッサーマミー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/グレートレッサーマミー.zip"
+  },
+  {
+    id: "機械鋸の死運び",
+    name: "機械鋸の死運び",
+    reading: ["きかいのこのしにんはこび", "機械鋸の死運び"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/機械鋸の死運び.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ギアスネイクテイマー",
+    name: "ギアスネイクテイマー",
+    reading: ["ぎあすねいくていまー", "ギアスネイクテイマー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/ギアスネイクテイマー.zip"
+  },
+  {
+    id: "慟哭の魔人・ネフィリオ",
+    name: "慟哭の魔人・ネフィリオ",
+    reading: ["どうこくのまじんねふぃりお", "慟哭の魔人・ネフィリオ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/慟哭の魔人・ネフィリオ.zip"
+  },
+  {
+    id: "犇めく悪鬼",
+    name: "犇めく悪鬼",
+    reading: ["ひしめくあっき", "犇めく悪鬼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/犇めく悪鬼.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "温情のラビットヒーラー",
+    name: "温情のラビットヒーラー",
+    reading: ["おんじょうのらびっとひーらー", "温情のラビットヒーラー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/温情のラビットヒーラー.zip"
+  },
+  {
+    id: "怒れる天界の忠犬",
+    name: "怒れる天界の忠犬",
+    reading: ["いかれるてんかいのちゅうけん", "怒れる天界の忠犬"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/怒れる天界の忠犬.zip"
+  },
+  {
+    id: "鉄鋼の翼人",
+    name: "鉄鋼の翼人",
+    reading: ["てっこうのよくじん", "鉄鋼の翼人"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/鉄鋼の翼人.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "キャットガンナー",
+    name: "キャットガンナー",
+    reading: ["きゃっとがんなー", "キャットガンナー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/キャットガンナー.zip"
+  },
+  {
+    id: "魔笛の奏者・ハーメルン",
+    name: "魔笛の奏者・ハーメルン",
+    reading: ["まてきのそうしゃはーめるん", "魔笛の奏者・ハーメルン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/魔笛の奏者・ハーメルン.zip"
+  },
+  {
+    id: "メカニカルマンモス",
+    name: "メカニカルマンモス",
+    reading: ["めかにかるまんもす", "メカニカルマンモス"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/メカニカルマンモス.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "フロートボードマーセナリー",
+    name: "フロートボードマーセナリー",
+    reading: ["ふろーとぼーどまーせなりー", "フロートボードマーセナリー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/フロートボードマーセナリー.zip"
+  },
+  {
+    id: "重装のゴリアテ",
+    name: "重装のゴリアテ",
+    reading: ["じゅうそうのごりあて", "重装のゴリアテ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ROG",
+    zip: "voices/ROG/Bronze/重装のゴリアテ.zip"
+  }
 ];

c9341cce9ba4b3fbbbd04e5d184d9cd8d3ded7f5
panini4552 <panini4552@gmail.com>
2025-11-24 16:53:24 +0900

    Update Legendary.js

--- data/ALT/Legendary.js
+++ data/ALT/Legendary.js
@@ -1,15 +1,246 @@
-export const Classic_Legendary = [
+// data/ALT/Legendary.js
+
+export const ALT_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "森の巫女・パメラ",
+    name: "森の巫女・パメラ",
+    reading: ["もりのみこぱめら", "モリノミコパメラ", "森の巫女・パメラ"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/森の巫女・パメラ.zip"
+  },
+  {
+    id: "無窮の輝石・カーバンクル",
+    name: "無窮の輝石・カーバンクル",
+    reading: ["むきゅうのきせきかーばんくる", "ムキュウノキセキカーバンクル", "無窮の輝石・カーバンクル"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/無窮の輝石・カーバンクル.zip"
+  },
+  {
+    id: "ホーネットクイーン・バビー",
+    name: "ホーネットクイーン・バビー",
+    reading: ["ほーねっとくいーんばびー", "ホーネットクイーンバビー", "ホーネットクイーン・バビー"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/ホーネットクイーン・バビー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "白翼の戦神・アイテール",
+    name: "白翼の戦神・アイテール",
+    reading: ["はくよくのせんしんあいてーる", "ハクヨクノセンシンアイテール", "白翼の戦神・アイテール"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/白翼の戦神・アイテール.zip"
+  },
+  {
+    id: "炎獅子の大将軍",
+    name: "炎獅子の大将軍",
+    reading: ["えんじしのだいしょうぐん", "エンジシノダイショウグン", "炎獅子の大将軍"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/炎獅子の大将軍.zip"
+  },
+  {
+    id: "舞い踊る刃・ディオネ",
+    name: "舞い踊る刃・ディオネ",
+    reading: ["まいおどるやいばでぃおね", "マイオドルヤイバディオネ", "舞い踊る刃・ディオネ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/舞い踊る刃・ディオネ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "スウィート★メドゥーサ",
+    name: "スウィート★メドゥーサ",
+    reading: ["すうぃーとめどぅーさ", "スウィートメドゥーサ", "スウィート★メドゥーサ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/スウィート★メドゥーサ.zip"
+  },
+  {
+    id: "眠れる災厄獣",
+    name: "眠れる災厄獣",
+    reading: ["ねむれるさいやくじゅう", "ネムレルサイヤクジュウ", "眠れる災厄獣"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/眠れる災厄獣.zip"
+  },
+  {
+    id: "ハンマーソーサラー・ジオス",
+    name: "ハンマーソーサラー・ジオス",
+    reading: ["はんまーそーさらーじおす", "ハンマーソーサラージオス", "ハンマーソーサラー・ジオス"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/ハンマーソーサラー・ジオス.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "憤怒の竜帝・乙姫",
+    name: "憤怒の竜帝・乙姫",
+    reading: ["ふんどのりゅうていおとひめ", "フンドノリュウテイオトヒメ", "憤怒の竜帝・乙姫"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/憤怒の竜帝・乙姫.zip"
+  },
+  {
+    id: "天翔けるドラゴニュート",
+    name: "天翔けるドラゴニュート",
+    reading: ["あまかけるどらごにゅーと", "アマカケルドラゴニュート", "天翔けるドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/天翔けるドラゴニュート.zip"
+  },
+  {
+    id: "トラロック",
+    name: "トラロック",
+    reading: ["とらろっく", "トラロック", "トラロック"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/トラロック.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "屍竜・ファフニール",
+    name: "屍竜・ファフニール",
+    reading: ["しりゅうふぁふにーる", "シリュウファフニール", "屍竜・ファフニール"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/屍竜・ファフニール.zip"
+  },
+  {
+    id: "地獄の君主・ゼベット",
+    name: "地獄の君主・ゼベット",
+    reading: ["じごくのくんしゅぜべっと", "ジゴクノクンシュゼベット", "地獄の君主・ゼベット"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/地獄の君主・ゼベット.zip"
+  },
+  {
+    id: "昏く淀む者・タルタロス",
+    name: "昏く淀む者・タルタロス",
+    reading: ["くらくよどむものたるたろす", "クラクヨドムモノタルタロス", "昏く淀む者・タルタロス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/昏く淀む者・タルタロス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "真紅のローズクイーン",
+    name: "真紅のローズクイーン",
+    reading: ["しんくのろーずくいーん", "シンクノローズクイーン", "真紅のローズクイーン"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/真紅のローズクイーン.zip"
+  },
+  {
+    id: "ディアボロス・アギト",
+    name: "ディアボロス・アギト",
+    reading: ["でぃあぼろすあぎと", "ディアボロスアギト", "ディアボロス・アギト"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/ディアボロス・アギト.zip"
+  },
+  {
+    id: "レイジコマンダー・ラウラ",
+    name: "レイジコマンダー・ラウラ",
+    reading: ["れいじこまんだーらうら", "レイジコマンダーラウラ", "レイジコマンダー・ラウラ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/レイジコマンダー・ラウラ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖騎士・ヘクター",
+    name: "聖騎士・ヘクター",
+    reading: ["せいきしへくたー", "セイキシヘクター", "聖騎士・ヘクター"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/聖騎士・ヘクター.zip"
+  },
+  {
+    id: "審判の聖女・ライナ",
+    name: "審判の聖女・ライナ",
+    reading: ["しんぱんのせいじょらいな", "シンパンノセイジョライナ", "審判の聖女・ライナ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/審判の聖女・ライナ.zip"
+  },
+  {
+    id: "楽園の聖獣",
+    name: "楽園の聖獣",
+    reading: ["らくえんのせいじゅう", "ラクエンノセイジュウ", "楽園の聖獣"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/楽園の聖獣.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "断罪者・シルヴィア",
+    name: "断罪者・シルヴィア",
+    reading: ["だんざいしゃしるヴぃあ", "ダンザイシャシルヴィア", "断罪者・シルヴィア"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/断罪者・シルヴィア.zip"
+  },
+  {
+    id: "エンシェントブレイダー・ダイン",
+    name: "エンシェントブレイダー・ダイン",
+    reading: ["えんしぇんとぶれいだーだいん", "エンシェントブレイダーダイン", "エンシェントブレイダー・ダイン"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/エンシェントブレイダー・ダイン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "異界を統べる者",
+    name: "異界を統べる者",
+    reading: ["いかいをすべるもの", "イカイヲスベルモノ", "異界を統べる者"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/異界を統べる者.zip"
+  },
+  {
+    id: "氷獄の王・サタン",
+    name: "氷獄の王・サタン",
+    reading: ["ひょうごくのおうさたん", "ヒョウゴクノオウサタン", "氷獄の王・サタン"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ALT",
+    zip: "voices/ALT/Legendary/氷獄の王・サタン.zip"
+  }
 ];

5fb53c6ccd6cd42d086eb216ef8595a381ce52f5
panini4552 <panini4552@gmail.com>
2025-11-24 17:08:31 +0900

    Update Bronze.js

--- data/ALT/Bronze.js
+++ data/ALT/Bronze.js
@@ -1,15 +1,262 @@
-export const Classic_Legendary = [
+export const ALT_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "アーボリスト・ライラ",
+    name: "アーボリスト・ライラ",
+    reading: ["あーぼりすとらいら", "アーボリストライラ", "アーボリスト・ライラ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/アーボリスト・ライラ.zip"
+  },
+  {
+    id: "不殺の狂信者",
+    name: "不殺の狂信者",
+    reading: ["ふさつのきょうしんしゃ", "フサツノキョウシンシャ", "不殺の狂信者"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/不殺の狂信者.zip"
+  },
+  {
+    id: "狂気のダークエルフ",
+    name: "狂気のダークエルフ",
+    reading: ["きょうきのだーくえるふ", "キョウキノダークエルフ", "狂気のダークエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/狂気のダークエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "マジックディーラー・エルネスタ",
+    name: "マジックディーラー・エルネスタ",
+    reading: ["まじっくでぃーらーえるねすた", "マジックディーラーエルネスタ", "マジックディーラー・エルネスタ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/マジックディーラー・エルネスタ.zip"
+  },
+  {
+    id: "ウォードッグ",
+    name: "ウォードッグ",
+    reading: ["うぉーどっぐ", "ウォードッグ", "ウォードッグ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/ウォードッグ.zip"
+  },
+  {
+    id: "簒奪の狂信者",
+    name: "簒奪の狂信者",
+    reading: ["さんだつのきょうしんしゃ", "サンダツノキョウシンシャ", "簒奪の狂信者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/簒奪の狂信者.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ラビットメイジ",
+    name: "ラビットメイジ",
+    reading: ["らびっとめいじ", "ラビットメイジ", "ラビットメイジ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/ラビットメイジ.zip"
+  },
+  {
+    id: "マナリアライター・ティコ",
+    name: "マナリアライター・ティコ",
+    reading: ["まなりあらいたーてぃこ", "マナリアライターティコ", "マナリアライター・ティコ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/マナリアライター・ティコ.zip"
+  },
+  {
+    id: "真実の狂信者",
+    name: "真実の狂信者",
+    reading: ["しんじつのきょうしんしゃ", "シンジツノキョウシンシャ", "真実の狂信者"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/真実の狂信者.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "侮蔑の狂信者",
+    name: "侮蔑の狂信者",
+    reading: ["ぶべつのきょうしんしゃ", "ブベツノキョウシンシャ", "侮蔑の狂信者"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/侮蔑の狂信者.zip"
+  },
+  {
+    id: "正義のドラグーン",
+    name: "正義のドラグーン",
+    reading: ["せいぎのどらぐーん", "セイギノドラグーン", "正義のドラグーン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/正義のドラグーン.zip"
+  },
+  {
+    id: "岩食い竜",
+    name: "岩食い竜",
+    reading: ["いわくいりゅう", "イワクイリュウ", "岩食い竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/岩食い竜.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ボーンバッファロー",
+    name: "ボーンバッファロー",
+    reading: ["ぼーんばっふぁろー", "ボーンバッファロー", "ボーンバッファロー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/ボーンバッファロー.zip"
+  },
+  {
+    id: "沈黙の狂信者",
+    name: "沈黙の狂信者",
+    reading: ["ちんもくのきょうしんしゃ", "チンモクノキョウシンシャ", "沈黙の狂信者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/沈黙の狂信者.zip"
+  },
+  {
+    id: "マミー・メリエン",
+    name: "マミー・メリエン",
+    reading: ["まみーめりえん", "マミーメリエン", "マミー・メリエン"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/マミー・メリエン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "デビルレイダー",
+    name: "デビルレイダー",
+    reading: ["でびるれいだー", "デビルレイダー", "デビルレイダー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/デビルレイダー.zip"
+  },
+  {
+    id: "フルフル",
+    name: "フルフル",
+    reading: ["ふるふる", "フルフル", "フルフル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/フルフル.zip"
+  },
+  {
+    id: "姦淫の狂信者",
+    name: "姦淫の狂信者",
+    reading: ["かんいんのきょうしんしゃ", "カンインノキョウシンシャ", "姦淫の狂信者"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/姦淫の狂信者.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "神殿の風熊",
+    name: "神殿の風熊",
+    reading: ["しんでんのかぜぐま", "シンデンノカゼグマ", "神殿の風熊"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/神殿の風熊.zip"
+  },
+  {
+    id: "イビルアルミラージ",
+    name: "イビルアルミラージ",
+    reading: ["いびるあるみらーじ", "イビルアルミラージ", "イビルアルミラージ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/イビルアルミラージ.zip"
+  },
+  {
+    id: "安息の狂信者",
+    name: "安息の狂信者",
+    reading: ["あんそくのきょうしんしゃ", "アンソクノキョウシンシャ", "安息の狂信者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/安息の狂信者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "破壊の狂信者",
+    name: "破壊の狂信者",
+    reading: ["はかいのきょうしんしゃ", "ハカイノキョウシンシャ", "破壊の狂信者"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/破壊の狂信者.zip"
+  },
+  {
+    id: "ドールロード・ヴィオラ",
+    name: "ドールロード・ヴィオラ",
+    reading: ["どーるろーどゔぃおら", "ドールロードヴィオラ", "ドールロード・ヴィオラ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/ドールロード・ヴィオラ.zip"
+  },
+  {
+    id: "マリオネットドラゴン",
+    name: "マリオネットドラゴン",
+    reading: ["まりおねっとどらごん", "マリオネットドラゴン", "マリオネットドラゴン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/マリオネットドラゴン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "香味の天使・レイズ",
+    name: "香味の天使・レイズ",
+    reading: ["こうみのてんしれいず", "コミノテンシレイズ", "香味の天使・レイズ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/香味の天使・レイズ.zip"
+  },
+  {
+    id: "パブマスター",
+    name: "パブマスター",
+    reading: ["ぱぶますたー", "パブマスター", "パブマスター"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/パブマスター.zip"
+  },
+  {
+    id: "ハイエンチャンター",
+    name: "ハイエンチャンター",
+    reading: ["はいえんちゃんたー", "ハイエンチャンター", "ハイエンチャンター"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ALT",
+    zip: "voices/ALT/Bronze/ハイエンチャンター.zip"
+  }
 ];

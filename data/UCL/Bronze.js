8fd01a63053b3efa004542e111658d49584900ef
panini4552 <panini4552@gmail.com>
2025-11-24 18:34:32 +0900

    Update Bronze.js

--- data/UCL/Bronze.js
+++ data/UCL/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const UCL_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "密林の守人",
+    name: "密林の守人",
+    reading: ["みつりんのもりびと", "密林の守人"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/密林の守人.zip"
+  },
+  {
+    id: "クロスボウスナイパー",
+    name: "クロスボウスナイパー",
+    reading: ["くろすぼうすないぱー", "クロスボウスナイパー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/クロスボウスナイパー.zip"
+  },
+  {
+    id: "ハンマーモンキー",
+    name: "ハンマーモンキー",
+    reading: ["はんまーもんきー", "ハンマーモンキー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ハンマーモンキー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ブルームキャバリエ",
+    name: "ブルームキャバリエ",
+    reading: ["ぶるーむきゃばりえ", "ブルームキャバリエ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ブルームキャバリエ.zip"
+  },
+  {
+    id: "無頼の野武士",
+    name: "無頼の野武士",
+    reading: ["ぶらいののぶし", "無頼の野武士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/無頼の野武士.zip"
+  },
+  {
+    id: "フォックスランサー",
+    name: "フォックスランサー",
+    reading: ["ふぉっくすらんさー", "フォックスランサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/フォックスランサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "符術の門弟",
+    name: "符術の門弟",
+    reading: ["ふじゅつのもんてい", "符術の門弟"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/符術の門弟.zip"
+  },
+  {
+    id: "ゲイルエレメンタラー",
+    name: "ゲイルエレメンタラー",
+    reading: ["げいるえれめんたらー", "ゲイルエレメンタラー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ゲイルエレメンタラー.zip"
+  },
+  {
+    id: "マジカルラット",
+    name: "マジカルラット",
+    reading: ["まじかるらっと", "マジカルラット"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/マジカルラット.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ソードウィップドラグーン",
+    name: "ソードウィップドラグーン",
+    reading: ["そーどうぃっぷどらぐーん", "ソードウィップドラグーン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ソードウィップドラグーン.zip"
+  },
+  {
+    id: "トライデントマーマン",
+    name: "トライデントマーマン",
+    reading: ["とらいでんとまーまん", "トライデントマーマン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/トライデントマーマン.zip"
+  },
+  {
+    id: "猛毒のディロフォサウルス",
+    name: "猛毒のディロフォサウルス",
+    reading: ["もうどくのでぃろふぉさうるす", "猛毒のディロフォサウルス"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/猛毒のディロフォサウルス.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "座敷童",
+    name: "座敷童",
+    reading: ["ざしきわらし", "座敷童"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/座敷童.zip"
+  },
+  {
+    id: "スカルクレイン",
+    name: "スカルクレイン",
+    reading: ["すかるくれいん", "スカルクレイン"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/スカルクレイン.zip"
+  },
+  {
+    id: "マミーコック",
+    name: "マミーコック",
+    reading: ["まみーこっく", "マミーコック"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/マミーコック.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "夢魔の新参",
+    name: "夢魔の新参",
+    reading: ["むまのしんざん", "夢魔の新参"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/夢魔の新参.zip"
+  },
+  {
+    id: "アンテロープウォーリアー",
+    name: "アンテロープウォーリアー",
+    reading: ["あんてろーぷうぉーりあー", "アンテロープウォーリアー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/アンテロープウォーリアー.zip"
+  },
+  {
+    id: "ジャイアントコブラ",
+    name: "ジャイアントコブラ",
+    reading: ["じゃいあんとこぶら", "ジャイアントコブラ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ジャイアントコブラ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖杖の司祭",
+    name: "聖杖の司祭",
+    reading: ["せいじょうのしさい", "聖杖の司祭"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/聖杖の司祭.zip"
+  },
+  {
+    id: "カームフェザーフォルク",
+    name: "カームフェザーフォルク",
+    reading: ["かーむふぇざーふぉるく", "カームフェザーフォルク"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/カームフェザーフォルク.zip"
+  },
+  {
+    id: "セイントグリフォン",
+    name: "セイントグリフォン",
+    reading: ["せいんとぐりふぉん", "セイントグリフォン"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/セイントグリフォン.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ペネトレイトランサー",
+    name: "ペネトレイトランサー",
+    reading: ["ぺねとれいとらんさー", "ペネトレイトランサー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ペネトレイトランサー.zip"
+  },
+  {
+    id: "重力の柔術士",
+    name: "重力の柔術士",
+    reading: ["じゅうりょくのじゅうじゅつし", "重力の柔術士"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/重力の柔術士.zip"
+  },
+  {
+    id: "ゴーグルモール",
+    name: "ゴーグルモール",
+    reading: ["ごーぐるもーる", "ゴーグルモール"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/ゴーグルモール.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "シープライダー",
+    name: "シープライダー",
+    reading: ["しーぷらいだー", "シープライダー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/シープライダー.zip"
+  },
+  {
+    id: "バズーカゴブリンズ",
+    name: "バズーカゴブリンズ",
+    reading: ["ばずーかごぶりんず", "バズーカゴブリンズ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "UCL",
+    zip: "voices/UCL/Bronze/バズーカゴブリンズ.zip"
+  }
 ];

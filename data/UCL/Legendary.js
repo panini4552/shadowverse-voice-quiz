182887dd3540fdfed2cc2a62def790cfc1f16acc
panini4552 <panini4552@gmail.com>
2025-11-24 18:30:12 +0900

    Update Legendary.js

--- data/UCL/Legendary.js
+++ data/UCL/Legendary.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const UCL_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "フォレストエース・リマーガ",
+    name: "フォレストエース・リマーガ",
+    reading: ["ふぉれすとえーすりまーが", "フォレストエースリマーガ", "フォレストエース・リマーガ"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/フォレストエース・リマーガ.zip"
+  },
+  {
+    id: "導きの巫女・コッコロ",
+    name: "導きの巫女・コッコロ",
+    reading: ["みちびきのみここっころ", "ミチビキノミココッコロ", "導きの巫女・コッコロ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/導きの巫女・コッコロ.zip"
+  },
+  {
+    id: "幻想の妖精竜",
+    name: "幻想の妖精竜",
+    reading: ["げんそうのようせいりゅう", "ゲンソウノヨウセイリュウ", "幻想の妖精竜"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/幻想の妖精竜.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "無敗の剣聖・カゲミツ",
+    name: "無敗の剣聖・カゲミツ",
+    reading: ["むはいのけんせいかげみつ", "ムハイノケンセイカゲミツ", "無敗の剣聖・カゲミツ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/無敗の剣聖・カゲミツ.zip"
+  },
+  {
+    id: "空腹の姫君・ペコリーヌ",
+    name: "空腹の姫君・ペコリーヌ",
+    reading: ["くうふくのひめぎみぺこりーぬ", "クウフクノヒメギミペコリーヌ", "空腹の姫君・ペコリーヌ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/空腹の姫君・ペコリーヌ.zip"
+  },
+  {
+    id: "煌剣の戦士・アンリエット",
+    name: "煌剣の戦士・アンリエット",
+    reading: ["こうけんのせんしあんりえっと", "コウケンノセンシアンリエット", "煌剣の戦士・アンリエット"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/煌剣の戦士・アンリエット.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "陰陽の開祖・クオン",
+    name: "陰陽の開祖・クオン",
+    reading: ["おんみょうのかいそくおん", "オンミョウノカイソクオン", "陰陽の開祖・クオン"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/陰陽の開祖・クオン.zip"
+  },
+  {
+    id: "猫耳の魔法使い・キャル",
+    name: "猫耳の魔法使い・キャル",
+    reading: ["ねこみみのまほうつかいきゃる", "ネコミミノマホウツカイキャル", "猫耳の魔法使い・キャル"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/猫耳の魔法使い・キャル.zip"
+  },
+  {
+    id: "禁約の黒魔術師",
+    name: "禁約の黒魔術師",
+    reading: ["きんやくのくろまじゅつし", "キンヤクノクロマジュツシ", "禁約の黒魔術師"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/禁約の黒魔術師.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "覇道の龍人・ガリュウ",
+    name: "覇道の龍人・ガリュウ",
+    reading: ["はどうのりゅうじんがりゅう", "ハドウノリュウジンガリュウ", "覇道の龍人・ガリュウ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/覇道の龍人・ガリュウ.zip"
+  },
+  {
+    id: "喧嘩屋の少女・カヤ",
+    name: "喧嘩屋の少女・カヤ",
+    reading: ["けんかやのしょうじょかや", "ケンカヤノショウジョカヤ", "喧嘩屋の少女・カヤ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/喧嘩屋の少女・カヤ.zip"
+  },
+  {
+    id: "ダークジェイルドラゴン",
+    name: "ダークジェイルドラゴン",
+    reading: ["だーくじぇいるどらごん", "ダークジェイルドラゴン", "ダークジェイルドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/ダークジェイルドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "大妖狐・ギンセツ",
+    name: "大妖狐・ギンセツ",
+    reading: ["だいようこぎんせつ", "ダイヨウコギンセツ", "大妖狐・ギンセツ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/大妖狐・ギンセツ.zip"
+  },
+  {
+    id: "ドクロの霊能者・シノブ",
+    name: "ドクロの霊能者・シノブ",
+    reading: ["どくろのれいのうしゃしのぶ", "ドクロノレイノウシャシノブ", "ドクロの霊能者・シノブ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/ドクロの霊能者・シノブ.zip"
+  },
+  {
+    id: "レヴナントソウル・アーカス",
+    name: "レヴナントソウル・アーカス",
+    reading: ["れう゛なんとそうるあーかす", "レヴナントソウルアーカス", "レヴナントソウル・アーカス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/レヴナントソウル・アーカス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ウールヴヘジン・アラガヴィ",
+    name: "ウールヴヘジン・アラガヴィ",
+    reading: ["うーるう゛へじんあらがゔぃ", "ウールヴヘジンアラガヴィ", "ウールヴヘジン・アラガヴィ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/ウールヴヘジン・アラガヴィ.zip"
+  },
+  {
+    id: "夜を統べる者・イリヤ",
+    name: "夜を統べる者・イリヤ",
+    reading: ["よるをすべるものいりや", "ヨルヲスベルモノイリヤ", "夜を統べる者・イリヤ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/夜を統べる者・イリヤ.zip"
+  },
+  {
+    id: "永久凍土のベヒーモス",
+    name: "永久凍土のベヒーモス",
+    reading: ["えいきゅうとうどのべひーもす", "エイキュウトウドノベヒーモス", "永久凍土のベヒーモス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/永久凍土のベヒーモス.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "グランドナイト・ウィルバート",
+    name: "グランドナイト・ウィルバート",
+    reading: ["ぐらんどないとうぃるばーと", "グランドナイトウィルバート", "グランドナイト・ウィルバート"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/グランドナイト・ウィルバート.zip"
+  },
+  {
+    id: "救護院の主・サレン",
+    name: "救護院の主・サレン",
+    reading: ["きゅうごいんのぬしされん", "キュウゴインノヌシサレン", "救護院の主・サレン"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/救護院の主・サレン.zip"
+  },
+  {
+    id: "豊穣の西風神",
+    name: "豊穣の西風神",
+    reading: ["ほうじょうのせいふうしん", "ホウジョウノセイフウシン", "豊穣の西風神"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/豊穣の西風神.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "アーツマスター・カルラ",
+    name: "アーツマスター・カルラ",
+    reading: ["あーつますたーかるら", "アーツマスターカルラ", "アーツマスター・カルラ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/アーツマスター・カルラ.zip"
+  },
+  {
+    id: "カイザーインサイト",
+    name: "カイザーインサイト",
+    reading: ["かいざーいんさいと", "カイザーインサイト", "カイザーインサイト"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/カイザーインサイト.zip"
+  },
+  {
+    id: "ヴァーテクスコロニー",
+    name: "ヴァーテクスコロニー",
+    reading: ["ゔぁーてくすころにー", "ヴァーテクスコロニー", "ヴァーテクスコロニー"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/ヴァーテクスコロニー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "アイギスシールド・アテナ",
+    name: "アイギスシールド・アテナ",
+    reading: ["あいぎすしーるどあてな", "アイギスシールドアテナ", "アイギスシールド・アテナ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/アイギスシールド・アテナ.zip"
+  },
+  {
+    id: "ゴッドコロシアムマンモス",
+    name: "ゴッドコロシアムマンモス",
+    reading: ["ごっどころしあむまんもす", "ゴッドコロシアムマンモス", "ゴッドコロシアムマンモス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "UCL",
+    zip: "voices/UCL/Legendary/ゴッドコロシアムマンモス.zip"
+  }
 ];

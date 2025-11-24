f454a8f574b13b205f2ade83e80206d2fd145670
panini4552 <panini4552@gmail.com>
2025-11-24 18:33:45 +0900

    Update Silver.js

--- data/UCL/Silver.js
+++ data/UCL/Silver.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
+export const UCL_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "森林の撃砕者",
+    name: "森林の撃砕者",
+    reading: ["しんりんのげきさいしゃ", "森林の撃砕者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/森林の撃砕者.zip"
+  },
+  {
+    id: "アサルトジャガー",
+    name: "アサルトジャガー",
+    reading: ["あさるとじゃがー", "アサルトジャガー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/アサルトジャガー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "秘伝の抜刀者",
+    name: "秘伝の抜刀者",
+    reading: ["ひでんのばっとうしゃ", "秘伝の抜刀者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/秘伝の抜刀者.zip"
+  },
+  {
+    id: "デュアルブレイダー",
+    name: "デュアルブレイダー",
+    reading: ["でゅあるぶれいだー", "デュアルブレイダー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/デュアルブレイダー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "クラシカルソーサラー",
+    name: "クラシカルソーサラー",
+    reading: ["くらしかるそーさらー", "クラシカルソーサラー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/クラシカルソーサラー.zip"
+  },
+  {
+    id: "鬼呼びの導師",
+    name: "鬼呼びの導師",
+    reading: ["おによびのどうし", "鬼呼びの導師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/鬼呼びの導師.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "深緑龍の軍師",
+    name: "深緑龍の軍師",
+    reading: ["しんりょくりゅうのぐんし", "深緑龍の軍師"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/深緑龍の軍師.zip"
+  },
+  {
+    id: "水鱗の猛者",
+    name: "水鱗の猛者",
+    reading: ["すいりんのもうじゃ", "水鱗の猛者"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/水鱗の猛者.zip"
+  },
+  {
+    id: "氷上の竜戦士",
+    name: "氷上の竜戦士",
+    reading: ["ひょうじょうのりゅうせんし", "氷上の竜戦士"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/氷上の竜戦士.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "雪女",
+    name: "雪女",
+    reading: ["ゆきおんな", "雪女"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/雪女.zip"
+  },
+  {
+    id: "火車",
+    name: "火車",
+    reading: ["かしゃ", "火車"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/火車.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "クーガークローガール",
+    name: "クーガークローガール",
+    reading: ["くーがーくろーがーる", "クーガークローガール"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/クーガークローガール.zip"
+  },
+  {
+    id: "猪突する狂戦士",
+    name: "猪突する狂戦士",
+    reading: ["ちょとつするきょうせんし", "猪突する狂戦士"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/猪突する狂戦士.zip"
+  },
+  {
+    id: "ヴァンパイアスレイヤー・ルシウス",
+    name: "ヴァンパイアスレイヤー・ルシウス",
+    reading: ["ゔぁんぱいあすれいやー・るしうす", "ヴァンパイアスレイヤー・ルシウス"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/ヴァンパイアスレイヤー・ルシウス.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖なる槍使い",
+    name: "聖なる槍使い",
+    reading: ["せいなるやりつかい", "聖なる槍使い"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/聖なる槍使い.zip"
+  },
+  {
+    id: "ゴールデンイーグル",
+    name: "ゴールデンイーグル",
+    reading: ["ごーるでんいーぐる", "ゴールデンイーグル"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/ゴールデンイーグル.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "バレッジスマッシャー",
+    name: "バレッジスマッシャー",
+    reading: ["ばれっじすまっしゃー", "バレッジスマッシャー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/バレッジスマッシャー.zip"
+  },
+  {
+    id: "ブーストキッカー",
+    name: "ブーストキッカー",
+    reading: ["ぶーすときっかー", "ブーストキッカー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/ブーストキッカー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "闘技場の司会者・チャット",
+    name: "闘技場の司会者・チャット",
+    reading: ["とうぎじょうのしかいしゃ・ちゃっと", "闘技場の司会者・チャット"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "UCL",
+    zip: "voices/UCL/Silver/闘技場の司会者・チャット.zip"
+  }
 ];

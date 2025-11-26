9f787ff1a49a76f8534ea1e7c4613e932c20392f
panini4552 <panini4552@gmail.com>
2025-11-26 22:19:44 +0900

    Update Legendary.js

--- data/FOH/Legendary.js
+++ data/FOH/Legendary.js
@@ -1,15 +1,255 @@
-export const Classic_Legendary = [
+// data/FOH/Legendary.js
+
+export const FOH_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "《吊るされた男》・ローフラッド",
+    name: "《吊るされた男》・ローフラッド",
+    reading: ["つるされたおとこ・ろーふらっど", "ローフラッド", "《吊るされた男》・ローフラッド"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《吊るされた男》・ローフラッド.zip"
+  },
+  {
+    id: "地を裂く異形",
+    name: "地を裂く異形",
+    reading: ["ちをさくいぎょう", "地を裂く異形"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/地を裂く異形.zip"
+  },
+  {
+    id: "永久なる輝き・エリン",
+    name: "永久なる輝き・エリン",
+    reading: ["えいきゅうなるかがやき・えりん", "エリン", "永久なる輝き・エリン"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/永久なる輝き・エリン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "《戦車》・オルオーン",
+    name: "《戦車》・オルオーン",
+    reading: ["せんしゃ・おるおーん", "オルオーン", "《戦車》・オルオーン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《戦車》・オルオーン.zip"
+  },
+  {
+    id: "ダイヤモンドパラディン",
+    name: "ダイヤモンドパラディン",
+    reading: ["だいやもんどぱらでぃん", "ダイヤモンドパラディン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/ダイヤモンドパラディン.zip"
+  },
+  {
+    id: "戦争商人・アルヤスカ",
+    name: "戦争商人・アルヤスカ",
+    reading: ["せんそうしょうにん・あるやすか", "アルヤスカ", "戦争商人・アルヤスカ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/戦争商人・アルヤスカ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "《愚者》・リンクル",
+    name: "《愚者》・リンクル",
+    reading: ["ぐしゃ・りんくる", "リンクル", "《愚者》・リンクル"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《愚者》・リンクル.zip"
+  },
+  {
+    id: "アダマンタイトゴーレム",
+    name: "アダマンタイトゴーレム",
+    reading: ["あだまんたいとごーれむ", "アダマンタイトゴーレム"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/アダマンタイトゴーレム.zip"
+  },
+  {
+    id: "決意の予言者・ルーニィ",
+    name: "決意の予言者・ルーニィ",
+    reading: ["けついのよげんしゃ・るーにぃ", "ルーニィ", "決意の予言者・ルーニィ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/決意の予言者・ルーニィ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "《正義》・イランツァ",
+    name: "《正義》・イランツァ",
+    reading: ["せいぎ・いらんつぁ", "イランツァ", "《正義》・イランツァ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《正義》・イランツァ.zip"
+  },
+  {
+    id: "大逆のドラゴサモナー",
+    name: "大逆のドラゴサモナー",
+    reading: ["たいぎゃくのどらごさもなー", "大逆のドラゴサモナー"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/大逆のドラゴサモナー.zip"
+  },
+  {
+    id: "絶対零度・フィルレイン",
+    name: "絶対零度・フィルレイン",
+    reading: ["ぜったいれいど・ふぃるれいん", "フィルレイン", "絶対零度・フィルレイン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/絶対零度・フィルレイン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "《恋人》・ミルティオ",
+    name: "《恋人》・ミルティオ",
+    reading: ["こいびと・みるてぃお", "ミルティオ", "《恋人》・ミルティオ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《恋人》・ミルティオ.zip"
+  },
+  {
+    id: "征伐の死帝",
+    name: "征伐の死帝",
+    reading: ["せいばつのしてい", "征伐の死帝"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/征伐の死帝.zip"
+  },
+  {
+    id: "冥府の女主人・ネフティス",
+    name: "冥府の女主人・ネフティス",
+    reading: ["めいふのしゅじん・ねふてぃす", "ネフティス", "冥府の女主人・ネフティス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/冥府の女主人・ネフティス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "《節制》・ルーゼン",
+    name: "《節制》・ルーゼン",
+    reading: ["せっせい・るーぜん", "ルーゼン", "《節制》・ルーゼン"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《節制》・ルーゼン.zip"
+  },
+  {
+    id: "深淵の約定・ダルホード",
+    name: "深淵の約定・ダルホード",
+    reading: ["しんえんのやくじょう・だるほーど", "ダルホード", "深淵の約定・ダルホード"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/深淵の約定・ダルホード.zip"
+  },
+  {
+    id: "バアル",
+    name: "バアル",
+    reading: ["ばある", "バアル"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/バアル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "《力》・ソフィーナ",
+    name: "《力》・ソフィーナ",
+    reading: ["ちから・そふぃーな", "ソフィーナ", "《力》・ソフィーナ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《力》・ソフィーナ.zip"
+  },
+  {
+    id: "闇穿つ煌き・サリッサ",
+    name: "闇穿つ煌き・サリッサ",
+    reading: ["やみうがつきらめき・さりっさ", "サリッサ", "闇穿つ煌き・サリッサ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/闇穿つ煌き・サリッサ.zip"
+  },
+  {
+    id: "光輝の顕現・ラー",
+    name: "光輝の顕現・ラー",
+    reading: ["こうきのけんげん・らー", "ラー", "光輝の顕現・ラー"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/光輝の顕現・ラー.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "《運命の輪》・スロース",
+    name: "《運命の輪》・スロース",
+    reading: ["うんめいのわ・すろーす", "スロース", "《運命の輪》・スロース"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《運命の輪》・スロース.zip"
+  },
+  {
+    id: "ディメンションドミネーター",
+    name: "ディメンションドミネーター",
+    reading: ["でぃめんしょんどみねーたー", "ディメンションドミネーター"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/ディメンションドミネーター.zip"
+  },
+  {
+    id: "遺物の番人・ルチル",
+    name: "遺物の番人・ルチル",
+    reading: ["いぶつのばんにん・るちる", "ルチル", "遺物の番人・ルチル"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/遺物の番人・ルチル.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "《世界》・ゼルガネイア",
+    name: "《世界》・ゼルガネイア",
+    reading: ["せかい・ぜるがねいあ", "ゼルガネイア", "《世界》・ゼルガネイア"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/《世界》・ゼルガネイア.zip"
+  },
+  {
+    id: "天覇風神・フェイラン",
+    name: "天覇風神・フェイラン",
+    reading: ["てんぱふうじん・ふぇいらん", "フェイラン", "天覇風神・フェイラン"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "FOH",
+    zip: "voices/FOH/Legendary/天覇風神・フェイラン.zip"
+  }
 ];

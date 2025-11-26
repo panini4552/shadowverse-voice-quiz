2960cb355ffc8c58f6b176879183dc7302b79442
panini4552 <panini4552@gmail.com>
2025-11-26 22:29:25 +0900

    Update Silver.js

--- data/FOH/Silver.js
+++ data/FOH/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+export const FOH_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "クラッシュインセクト",
+    name: "クラッシュインセクト",
+    reading: ["くらっしゅいんせくと", "クラッシュインセクト"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/クラッシュインセクト.zip"
+  },
+  {
+    id: "喝采の獣使い",
+    name: "喝采の獣使い",
+    reading: ["かっさいのけものつかい", "喝采の獣使い"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/喝采の獣使い.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "武器商人・エルネスタ",
+    name: "武器商人・エルネスタ",
+    reading: ["ぶきしょうにんえるねすた", "武器商人・エルネスタ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/武器商人・エルネスタ.zip"
+  },
+  {
+    id: "安らぎの女王",
+    name: "安らぎの女王",
+    reading: ["やすらぎのじょおう", "安らぎの女王"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/安らぎの女王.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "猫の奇術師",
+    name: "猫の奇術師",
+    reading: ["ねこのきじゅつし", "猫の奇術師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/猫の奇術師.zip"
+  },
+  {
+    id: "スウィートマジシャン",
+    name: "スウィートマジシャン",
+    reading: ["すうぃーとまじしゃん", "スウィートマジシャン"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/スウィートマジシャン.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "オーシャンアングラー",
+    name: "オーシャンアングラー",
+    reading: ["おーしゃんあんぐらー", "オーシャンアングラー"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/オーシャンアングラー.zip"
+  },
+  {
+    id: "日輪のドラゴン",
+    name: "日輪のドラゴン",
+    reading: ["にちりんのどらごん", "日輪のドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/日輪のドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ゴーストメイド",
+    name: "ゴーストメイド",
+    reading: ["ごーすとめいど", "ゴーストメイド"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/ゴーストメイド.zip"
+  },
+  {
+    id: "幽暗の墓守",
+    name: "幽暗の墓守",
+    reading: ["ゆうあんのはかもり", "幽暗の墓守"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/幽暗の墓守.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ラッシュインプ",
+    name: "ラッシュインプ",
+    reading: ["らっしゅいんぷ", "ラッシュインプ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/ラッシュインプ.zip"
+  },
+  {
+    id: "暴威の悪魔",
+    name: "暴威の悪魔",
+    reading: ["ぼういのあくま", "暴威の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/暴威の悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ピラータートル",
+    name: "ピラータートル",
+    reading: ["ぴらーたーとる", "ピラータートル"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/ピラータートル.zip"
+  },
+  {
+    id: "光明のプリースト",
+    name: "光明のプリースト",
+    reading: ["こうみょうのぷりーすと", "光明のプリースト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/光明のプリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "キャットチューナー",
+    name: "キャットチューナー",
+    reading: ["きゃっとちゅーなー", "キャットチューナー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/キャットチューナー.zip"
+  },
+  {
+    id: "パワフルマリオネッター",
+    name: "パワフルマリオネッター",
+    reading: ["ぱわふるまりおねったー", "パワフルマリオネッター"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/パワフルマリオネッター.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "クラウドギガース",
+    name: "クラウドギガース",
+    reading: ["くらうどぎがーす", "クラウドギガース"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "FOH",
+    zip: "voices/FOH/Silver/クラウドギガース.zip"
+  }
 ];

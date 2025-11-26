ab3366e41550da48c68407c44c9a3246150e4567
panini4552 <panini4552@gmail.com>
2025-11-26 22:37:36 +0900

    Update Bronze.js

--- data/FOH/Bronze.js
+++ data/FOH/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const FOH_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ブロッサムアーチャー",
+    name: "ブロッサムアーチャー",
+    reading: ["ぶろっさむあーちゃー", "ブロッサムアーチャー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ブロッサムアーチャー.zip"
+  },
+  {
+    id: "深謀の獣人",
+    name: "深謀の獣人",
+    reading: ["しんぼうのじゅうじん", "深謀の獣人"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/深謀の獣人.zip"
+  },
+  {
+    id: "森林の狼",
+    name: "森林の狼",
+    reading: ["しんりんのおおかみ", "森林の狼"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/森林の狼.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "オネストシーフ",
+    name: "オネストシーフ",
+    reading: ["おねすとしーふ", "オネストシーフ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/オネストシーフ.zip"
+  },
+  {
+    id: "ストライクナイト",
+    name: "ストライクナイト",
+    reading: ["すとらいくないと", "ストライクナイト"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ストライクナイト.zip"
+  },
+  {
+    id: "無私の貴族",
+    name: "無私の貴族",
+    reading: ["むしのきぞく", "無私の貴族"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/無私の貴族.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "形成の魔術師",
+    name: "形成の魔術師",
+    reading: ["けいせいのまじゅつし", "形成の魔術師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/形成の魔術師.zip"
+  },
+  {
+    id: "魔導書の書き手",
+    name: "魔導書の書き手",
+    reading: ["まどうしょのかきて", "魔導書の書き手"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/魔導書の書き手.zip"
+  },
+  {
+    id: "ポーションウィザード",
+    name: "ポーションウィザード",
+    reading: ["ぽーしょんうぃざーど", "ポーションウィザード"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ポーションウィザード.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "竜装の槍術士",
+    name: "竜装の槍術士",
+    reading: ["りゅうそうのそうじゅつし", "竜装の槍術士"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/竜装の槍術士.zip"
+  },
+  {
+    id: "水呼びの竜使い",
+    name: "水呼びの竜使い",
+    reading: ["みずよびのりゅうつかい", "水呼びの竜使い"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/水呼びの竜使い.zip"
+  },
+  {
+    id: "トロピカルフィッシュ",
+    name: "トロピカルフィッシュ",
+    reading: ["とろぴかるふぃっしゅ", "トロピカルフィッシュ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/トロピカルフィッシュ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "スピリットキュレーター",
+    name: "スピリットキュレーター",
+    reading: ["すぴりっときゅれーたー", "スピリットキュレーター"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/スピリットキュレーター.zip"
+  },
+  {
+    id: "死の魔鳥",
+    name: "死の魔鳥",
+    reading: ["しのまちょう", "死の魔鳥"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/死の魔鳥.zip"
+  },
+  {
+    id: "ソウルボックス",
+    name: "ソウルボックス",
+    reading: ["そうるぼっくす", "ソウルボックス"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ソウルボックス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "銀矢の狩人",
+    name: "銀矢の狩人",
+    reading: ["ぎんやのかりゅうど", "銀矢の狩人"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/銀矢の狩人.zip"
+  },
+  {
+    id: "月下の人狼",
+    name: "月下の人狼",
+    reading: ["げっかのじんろう", "月下の人狼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/月下の人狼.zip"
+  },
+  {
+    id: "蛮声のオーガ",
+    name: "蛮声のオーガ",
+    reading: ["ばんせいのおーが", "蛮声のオーガ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/蛮声のオーガ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "先見の神官",
+    name: "先見の神官",
+    reading: ["せんけんのしんかん", "先見の神官"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/先見の神官.zip"
+  },
+  {
+    id: "剛健の翼人",
+    name: "剛健の翼人",
+    reading: ["ごうけんのよくじん", "剛健の翼人"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/剛健の翼人.zip"
+  },
+  {
+    id: "エレガントバード",
+    name: "エレガントバード",
+    reading: ["えれがんとばーど", "エレガントバード"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/エレガントバード.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ドールズオーナー",
+    name: "ドールズオーナー",
+    reading: ["どーるずおーなー", "ドールズオーナー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ドールズオーナー.zip"
+  },
+  {
+    id: "機構の技師",
+    name: "機構の技師",
+    reading: ["きこうのぎし", "機構の技師"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/機構の技師.zip"
+  },
+  {
+    id: "鋼の猛虎",
+    name: "鋼の猛虎",
+    reading: ["はがねのもうこ", "鋼の猛虎"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/鋼の猛虎.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ピアッシングエンジェル",
+    name: "ピアッシングエンジェル",
+    reading: ["ぴあっしんぐえんじぇる", "ピアッシングエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ピアッシングエンジェル.zip"
+  },
+  {
+    id: "ウィングメッセンジャー",
+    name: "ウィングメッセンジャー",
+    reading: ["うぃんぐめっせんじゃー", "ウィングメッセンジャー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "FOH",
+    zip: "voices/FOH/Bronze/ウィングメッセンジャー.zip"
+  }
 ];

6a5d0009d4ce81ea150b554f50248df1ababe9ce
panini4552 <panini4552@gmail.com>
2025-11-26 22:09:20 +0900

    Update Silver.js

--- data/WUP/Silver.js
+++ data/WUP/Silver.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const WUP_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "マシンブーメランエルフ",
+    name: "マシンブーメランエルフ",
+    reading: ["ましんぶーめらんえるふ", "マシンブーメランエルフ", "マシンブーメランエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/マシンブーメランエルフ.zip"
+  },
+  {
+    id: "森の手斧使い",
+    name: "森の手斧使い",
+    reading: ["もりのおのづかい", "モリノオノヅカイ", "森の手斧使い"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/森の手斧使い.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "黒豹の偵察者",
+    name: "黒豹の偵察者",
+    reading: ["くろひょうのていさつしゃ", "クロヒョウノテイサツシャ", "黒豹の偵察者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/黒豹の偵察者.zip"
+  },
+  {
+    id: "猛然たる騎士・ゲルト",
+    name: "猛然たる騎士・ゲルト",
+    reading: ["もうぜんたるきしげると", "モウゼンタルキシゲルト", "猛然たる騎士・ゲルト"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/猛然たる騎士・ゲルト.zip"
+  },
+  {
+    id: "レヴィオンの魔獣使い",
+    name: "レヴィオンの魔獣使い",
+    reading: ["れヴぃおんのまじゅうつかい", "レヴィオンノマジュウツカイ", "レヴィオンの魔獣使い"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/レヴィオンの魔獣使い.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "大地の魔女",
+    name: "大地の魔女",
+    reading: ["だいちのまじょ", "ダイチノマジョ", "大地の魔女"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/大地の魔女.zip"
+  },
+  {
+    id: "境界の魔道士",
+    name: "境界の魔道士",
+    reading: ["きょうかいのまどうし", "キョウカイノマドウシ", "境界の魔道士"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/境界の魔道士.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "アマルガムドラゴン",
+    name: "アマルガムドラゴン",
+    reading: ["あまるがむどらごん", "アマルガムドラゴン", "アマルガムドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/アマルガムドラゴン.zip"
+  },
+  {
+    id: "繁茂のステゴサウルス",
+    name: "繁茂のステゴサウルス",
+    reading: ["はんものすてごさうるす", "ハンモノステゴサウルス", "繁茂のステゴサウルス"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/繁茂のステゴサウルス.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "トライサイズリーパー",
+    name: "トライサイズリーパー",
+    reading: ["とらいさいずりーぱー", "トライサイズリーパー", "トライサイズリーパー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/トライサイズリーパー.zip"
+  },
+  {
+    id: "獣の霊媒師",
+    name: "獣の霊媒師",
+    reading: ["けもののれいばいし", "ケモノノレイバイシ", "獣の霊媒師"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/獣の霊媒師.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "凶槍の戦士",
+    name: "凶槍の戦士",
+    reading: ["きょうそうのせんし", "キョウソウノセンシ", "凶槍の戦士"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/凶槍の戦士.zip"
+  },
+  {
+    id: "ノクターンヴァンパイア",
+    name: "ノクターンヴァンパイア",
+    reading: ["のくたーんゔぁんぱいあ", "ノクターンヴァンパイア", "ノクターンヴァンパイア"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/ノクターンヴァンパイア.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "トゥインクルミュース",
+    name: "トゥインクルミュース",
+    reading: ["とぅいんくるみゅーす", "トゥインクルミュース", "トゥインクルミュース"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/トゥインクルミュース.zip"
+  },
+  {
+    id: "光の信仰者",
+    name: "光の信仰者",
+    reading: ["ひかりのしんこうしゃ", "ヒカリノシンコウシャ", "光の信仰者"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/光の信仰者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "デバイスチューナー",
+    name: "デバイスチューナー",
+    reading: ["でばいすちゅーなー", "デバイスチューナー", "デバイスチューナー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/デバイスチューナー.zip"
+  },
+  {
+    id: "チャクラムオフィサー",
+    name: "チャクラムオフィサー",
+    reading: ["ちゃくらむおふぃさー", "チャクラムオフィサー", "チャクラムオフィサー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/チャクラムオフィサー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ラブソングシンガー",
+    name: "ラブソングシンガー",
+    reading: ["らぶそんぐしんがー", "ラブソングシンガー", "ラブソングシンガー"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "WUP",
+    zip: "voices/WUP/Silver/ラブソングシンガー.zip"
+  }
 ];

fbdf8d0aa319929aace4585c6d23b63a31fc5c64
panini4552 <panini4552@gmail.com>
2025-11-26 22:13:33 +0900

    Update Bronze.js

--- data/WUP/Bronze.js
+++ data/WUP/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const WUP_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "鉄甲のカブトムシ",
+    name: "鉄甲のカブトムシ",
+    reading: ["てっこうのかぶとむし", "テッコウノカブトムシ", "鉄甲のカブトムシ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/鉄甲のカブトムシ.zip"
+  },
+  {
+    id: "若葉の精霊",
+    name: "若葉の精霊",
+    reading: ["わかばのせいれい", "ワカバノセイレイ", "若葉の精霊"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/若葉の精霊.zip"
+  },
+  {
+    id: "妖精の参謀",
+    name: "妖精の参謀",
+    reading: ["ようせいのさんぼう", "ヨウセイノサンボウ", "妖精の参謀"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/妖精の参謀.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "機械の輸送者",
+    name: "機械の輸送者",
+    reading: ["きかいのゆそうしゃ", "キカイノユソウシャ", "機械の輸送者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/機械の輸送者.zip"
+  },
+  {
+    id: "スラッシュアサシン",
+    name: "スラッシュアサシン",
+    reading: ["すらっしゅあさしん", "スラッシュアサシン", "スラッシュアサシン"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/スラッシュアサシン.zip"
+  },
+  {
+    id: "インパクトソルジャー",
+    name: "インパクトソルジャー",
+    reading: ["いんぱくとそるじゃー", "インパクトソルジャー", "インパクトソルジャー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/インパクトソルジャー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マシンフォーチュンテラー",
+    name: "マシンフォーチュンテラー",
+    reading: ["ましんふぉーちゅんてらー", "マシンフォーチュンテラー", "マシンフォーチュンテラー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/マシンフォーチュンテラー.zip"
+  },
+  {
+    id: "氷雪の魔術師",
+    name: "氷雪の魔術師",
+    reading: ["ひょうせつのまじゅつし", "ヒョウセツノマジュツシ", "氷雪の魔術師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/氷雪の魔術師.zip"
+  },
+  {
+    id: "エーテルゴーレム",
+    name: "エーテルゴーレム",
+    reading: ["えーてるごーれむ", "エーテルゴーレム", "エーテルゴーレム"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/エーテルゴーレム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "岩石のアンキロサウルス",
+    name: "岩石のアンキロサウルス",
+    reading: ["がんせきのあんきろさうるす", "ガンセキノアンキロサウルス", "岩石のアンキロサウルス"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/岩石のアンキロサウルス.zip"
+  },
+  {
+    id: "治癒のドラグーン",
+    name: "治癒のドラグーン",
+    reading: ["ちゆのどらぐーん", "チユノドラグーン", "治癒のドラグーン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/治癒のドラグーン.zip"
+  },
+  {
+    id: "トンファードラゴニュート",
+    name: "トンファードラゴニュート",
+    reading: ["とんふぁーどらごにゅーと", "トンファードラゴニュート", "トンファードラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/トンファードラゴニュート.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "亡霊の使役者",
+    name: "亡霊の使役者",
+    reading: ["ぼうれいのしえきしゃ", "ボウレイノシエキシャ", "亡霊の使役者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/亡霊の使役者.zip"
+  },
+  {
+    id: "ダンシングゴースト",
+    name: "ダンシングゴースト",
+    reading: ["だんしんぐごーすと", "ダンシングゴースト", "ダンシングゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/ダンシングゴースト.zip"
+  },
+  {
+    id: "リバースネクロマンサー",
+    name: "リバースネクロマンサー",
+    reading: ["りばーすねくろまんさー", "リバースネクロマンサー", "リバースネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/リバースネクロマンサー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "密約の吸血鬼",
+    name: "密約の吸血鬼",
+    reading: ["みつやくのきゅうけつき", "ミツヤクノキュウケツキ", "密約の吸血鬼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/密約の吸血鬼.zip"
+  },
+  {
+    id: "ブラッドベルセルク",
+    name: "ブラッドベルセルク",
+    reading: ["ぶらっどべるせるく", "ブラッドベルセルク", "ブラッドベルセルク"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/ブラッドベルセルク.zip"
+  },
+  {
+    id: "孤高の悪鬼",
+    name: "孤高の悪鬼",
+    reading: ["ここうのあっき", "ココウノアッキ", "孤高の悪鬼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/孤高の悪鬼.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "サンライトシスター",
+    name: "サンライトシスター",
+    reading: ["さんらいとしすたー", "サンライトシスター", "サンライトシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/サンライトシスター.zip"
+  },
+  {
+    id: "ブレッシングバード",
+    name: "ブレッシングバード",
+    reading: ["ぶれっしんぐばーど", "ブレッシングバード", "ブレッシングバード"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/ブレッシングバード.zip"
+  },
+  {
+    id: "フラッグフェザーフォルク",
+    name: "フラッグフェザーフォルク",
+    reading: ["ふらっぐふぇざーふぉるく", "フラッグフェザーフォルク", "フラッグフェザーフォルク"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/フラッグフェザーフォルク.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ロボティクスユーザー",
+    name: "ロボティクスユーザー",
+    reading: ["ろぼてぃくすゆーざー", "ロボティクスユーザー", "ロボティクスユーザー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/ロボティクスユーザー.zip"
+  },
+  {
+    id: "アーマードアルマジロ",
+    name: "アーマードアルマジロ",
+    reading: ["あーまーどあるまじろ", "アーマードアルマジロ", "アーマードアルマジロ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/アーマードアルマジロ.zip"
+  },
+  {
+    id: "マジックエンジニア",
+    name: "マジックエンジニア",
+    reading: ["まじっくえんじにあ", "マジックエンジニア", "マジックエンジニア"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/マジックエンジニア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "風来の絵描き",
+    name: "風来の絵描き",
+    reading: ["ふうらいのえかき", "フウライノエカキ", "風来の絵描き"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/風来の絵描き.zip"
+  },
+  {
+    id: "ロマントレジャーハンター",
+    name: "ロマントレジャーハンター",
+    reading: ["ろまんとれじゃーはんたー", "ロマントレジャーハンター", "ロマントレジャーハンター"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "WUP",
+    zip: "voices/WUP/Bronze/ロマントレジャーハンター.zip"
+  }
 ];

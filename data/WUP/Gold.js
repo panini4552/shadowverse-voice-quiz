198a91825eedabf3fb949e7b80ea2691aa020b98
panini4552 <panini4552@gmail.com>
2025-11-26 22:05:23 +0900

    Update Gold.js

--- data/WUP/Gold.js
+++ data/WUP/Gold.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const WUP_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "鉄扇のエルフ",
+    name: "鉄扇のエルフ",
+    reading: ["てっせんのえるふ", "テッセンノエルフ", "鉄扇のエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/鉄扇のエルフ.zip"
+  },
+  {
+    id: "追い風の妖精",
+    name: "追い風の妖精",
+    reading: ["おいかぜのようせい", "オイカゼノヨウセイ", "追い風の妖精"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/追い風の妖精.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "栄華のプリンス",
+    name: "栄華のプリンス",
+    reading: ["えいがのぷりんす", "エイガノプリンス", "栄華のプリンス"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/栄華のプリンス.zip"
+  },
+  {
+    id: "流麗なる槍術士",
+    name: "流麗なる槍術士",
+    reading: ["りゅうれいなるそうじゅつし", "リュウレイナルソウジュツシ", "流麗なる槍術士"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/流麗なる槍術士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ストームエレメンタラー",
+    name: "ストームエレメンタラー",
+    reading: ["すとーむえれめんたらー", "ストームエレメンタラー", "ストームエレメンタラー"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/ストームエレメンタラー.zip"
+  },
+  {
+    id: "ペルソナウィザード",
+    name: "ペルソナウィザード",
+    reading: ["ぺるそなうぃざーど", "ペルソナウィザード", "ペルソナウィザード"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/ペルソナウィザード.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "嵐鉄の竜人",
+    name: "嵐鉄の竜人",
+    reading: ["らんてつのりゅうじん", "ランテツノリュウジン", "嵐鉄の竜人"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/嵐鉄の竜人.zip"
+  },
+  {
+    id: "金剛のパキケファロサウルス",
+    name: "金剛のパキケファロサウルス",
+    reading: ["こんごうのぱきけふぁろさうるす", "コンゴウノパキケファロサウルス", "金剛のパキケファロサウルス"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/金剛のパキケファロサウルス.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "モーターグレイブディガー",
+    name: "モーターグレイブディガー",
+    reading: ["もーたーぐれいぶでぃがー", "モーターグレイブディガー", "モーターグレイブディガー"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/モーターグレイブディガー.zip"
+  },
+  {
+    id: "王墓の骸",
+    name: "王墓の骸",
+    reading: ["おうぼのむくろ", "オウボノムクロ", "王墓の骸"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/王墓の骸.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "マシンナースデビル",
+    name: "マシンナースデビル",
+    reading: ["ましんなーすでびる", "マシンナースデビル", "マシンナースデビル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/マシンナースデビル.zip"
+  },
+  {
+    id: "背徳の狂獣",
+    name: "背徳の狂獣",
+    reading: ["はいとくのきょうじゅう", "ハイトクノキョウジュウ", "背徳の狂獣"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/背徳の狂獣.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "機械鞭の僧侶",
+    name: "機械鞭の僧侶",
+    reading: ["きかいむちのそうりょ", "キカイムチノソウリョ", "機械鞭の僧侶"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/機械鞭の僧侶.zip"
+  },
+  {
+    id: "博愛の翼人",
+    name: "博愛の翼人",
+    reading: ["はくあいのよくじん", "ハクアイノヨクジン", "博愛の翼人"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/博愛の翼人.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "アイアンスティンガー",
+    name: "アイアンスティンガー",
+    reading: ["あいあんすてぃんがー", "アイアンスティンガー", "アイアンスティンガー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/アイアンスティンガー.zip"
+  },
+  {
+    id: "シューティングタートル",
+    name: "シューティングタートル",
+    reading: ["しゅーてぃんぐたーとる", "シューティングタートル", "シューティングタートル"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/シューティングタートル.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "デュアルエンジェル",
+    name: "デュアルエンジェル",
+    reading: ["でゅあるえんじぇる", "デュアルエンジェル", "デュアルエンジェル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/デュアルエンジェル.zip"
+  },
+  {
+    id: "爆炎の魔神",
+    name: "爆炎の魔神",
+    reading: ["ばくえんのまじん", "バクエンノマジン", "爆炎の魔神"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "WUP",
+    zip: "voices/WUP/Gold/爆炎の魔神.zip"
+  }
 ];

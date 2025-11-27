19a4dea008157592aad88d3cd560b07a2044c0a9
panini4552 <panini4552@gmail.com>
2025-11-27 21:19:30 +0900

    Update Legendary.js

--- data/ETA/Legendary.js
+++ data/ETA/Legendary.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+[
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Thorn_Hunter",
+    name: "魔眼の狩人・ソーン",
+    reading: ["まがんのかりゅうどそーん", "魔眼の狩人・ソーン"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/魔眼の狩人・ソーン.zip"
+  },
+  {
+    id: "Zeno_Sagittarius",
+    name: "ゼノ・サジタリウス",
+    reading: ["ぜのさじたりうす", "ゼノ・サジタリウス"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/ゼノ・サジタリウス.zip"
+  },
+  {
+    id: "Alberta_Elf",
+    name: "閃光のエルフ・アルバータ",
+    reading: ["せんこうのえるふあるばーた", "閃光のエルフ・アルバータ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/閃光のエルフ・アルバータ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "Siete_Starblade",
+    name: "天星剣王・シエテ",
+    reading: ["てんせいけんおうしえて", "天星剣王・シエテ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/天星剣王・シエテ.zip"
+  },
+  {
+    id: "Octo_Swordmaster",
+    name: "刀神・オクトー",
+    reading: ["とうしんおくとー", "刀神・オクトー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/刀神・オクトー.zip"
+  },
+  {
+    id: "Chevalier_Magna",
+    name: "シュヴァリエ・マグナ",
+    reading: ["しゅヴぁりえまぐな", "シュヴァリエ・マグナ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/シュヴァリエ・マグナ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "Fuenf_Prodigy",
+    name: "魔導の申し子・フュンフ",
+    reading: ["まどうのもうしごふゅんふ", "魔導の申し子・フュンフ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/魔導の申し子・フュンフ.zip"
+  },
+  {
+    id: "Zeno_Ifrit",
+    name: "ゼノ・イフリート",
+    reading: ["ぜのいふりーと", "ゼノ・イフリート"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/ゼノ・イフリート.zip"
+  },
+  {
+    id: "Alestor_SilverAsh",
+    name: "銀灰の星・アレイスター",
+    reading: ["ぎんかいのほしあれいすたー", "銀灰の星・アレイスター"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/銀灰の星・アレイスター.zip"
+  },
 
-];
+  // === ドラゴン ===
+  {
+    id: "Sarasa_GiantForce",
+    name: "怪力乱神・サラーサ",
+    reading: ["かいりょくらんしんさらーさ", "怪力乱神・サラーサ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/怪力乱神・サラーサ.zip"
+  },
+  {
+    id: "Dizlestan_SeaGod",
+    name: "神魚・ディズレスタン",
+    reading: ["しんぎょでぃずれすたん", "神魚・ディズレスタン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/神魚・ディズレスタン.zip"
+  },
+  {
+    id: "Tiamat_Magna",
+    name: "ティアマト・マグナ",
+    reading: ["てぃあまとまぐな", "ティアマト・マグナ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/ティアマト・マグナ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "Nio_Musician",
+    name: "繊細の魔奏者・ニオ",
+    reading: ["せんさいのまそうしゃにお", "繊細の魔奏者・ニオ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/繊細の魔奏者・ニオ.zip"
+  },
+  {
+    id: "Death_Emperor",
+    name: "死滅の剣皇",
+    reading: ["しめつのけんおう", "死滅の剣皇"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/死滅の剣皇.zip"
+  },
+  {
+    id: "Celeste_Magna",
+    name: "セレスト・マグナ",
+    reading: ["せれすとまぐな", "セレスト・マグナ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/セレスト・マグナ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "Shis_WolfGod",
+    name: "神狼・シス",
+    reading: ["しんろうしす", "神狼・シス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/神狼・シス.zip"
+  },
+  {
+    id: "Zeno_Diablos",
+    name: "ゼノ・ディアボロス",
+    reading: ["ぜのでぃあぼろす", "ゼノ・ディアボロス"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/ゼノ・ディアボロス.zip"
+  },
+  {
+    id: "Shemhaza",
+    name: "シェムハザ",
+    reading: ["しぇむはざ", "シェムハザ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/シェムハザ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "Uno_Enlightened",
+    name: "開眼者・ウーノ",
+    reading: ["かいがんしゃうーの", "開眼者・ウーノ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/開眼者・ウーノ.zip"
+  },
+  {
+    id: "Zeno_Wof",
+    name: "ゼノ・ウォフマナフ",
+    reading: ["ぜのうぉふまなふ", "ゼノ・ウォフマナフ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/ゼノ・ウォフマナフ.zip"
+  },
+  {
+    id: "Frigg",
+    name: "フリッグ",
+    reading: ["ふりっぐ", "フリッグ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/フリッグ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "Qatol_Twinblades",
+    name: "閃耀の双剣・カトル",
+    reading: ["せんようのそうけんかとる", "閃耀の双剣・カトル"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/閃耀の双剣・カトル.zip"
+  },
+  {
+    id: "Essel_Gunner",
+    name: "魔弾の射手・エッセル",
+    reading: ["まだんのしゃしゅえっせる", "魔弾の射手・エッセル"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/魔弾の射手・エッセル.zip"
+  },
+  {
+    id: "Colossus_Magna",
+    name: "コロッサス・マグナ",
+    reading: ["ころっさすまぐな", "コロッサス・マグナ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/コロッサス・マグナ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "Bahamut_Prime",
+    name: "始原の竜・バハムート",
+    reading: ["しげんのりゅうばはむーと", "始原の竜・バハムート"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ETA",
+    zip: "voices/ETA/Legendary/始原の竜・バハムート.zip"
+  }
+]

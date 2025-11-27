7fb3906c0a16422380ca8c3651ebec39ea303596
panini4552 <panini4552@gmail.com>
2025-11-27 21:15:15 +0900

    Add files via upload

---
+++ data/ETA/Gold.js
@@ -0,0 +1,15 @@
+export const Classic_Legendary = [
+  // === エルフ ===
+  {
+    id: "Ancient_Elf",
+    name: "エンシェントエルフ",
+    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "Classic",
+    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+  },
+
+
+
+];

21fdf16c89ddb6a92cfe1c67a4fc24208911f208
panini4552 <panini4552@gmail.com>
2025-11-27 20:53:04 +0900

    Update Silver.js

--- data/SOR/Silver.js
+++ data/SOR/Silver.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const SOR_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "笑顔の破壊者・カーマイン",
+    name: "笑顔の破壊者・カーマイン",
+    reading: ["えがおのはかいしゃ・かーまいん", "笑顔の破壊者・カーマイン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/笑顔の破壊者・カーマイン.zip"
+  },
+  {
+    id: "ヴァーミンハンター",
+    name: "ヴァーミンハンター",
+    reading: ["ゔぁーみんはんたー", "ヴァーミンハンター"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/ヴァーミンハンター.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "アウトローガンナー",
+    name: "アウトローガンナー",
+    reading: ["あうとろーがんなー", "アウトローガンナー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/アウトローガンナー.zip"
+  },
+  {
+    id: "フリングソルジャー",
+    name: "フリングソルジャー",
+    reading: ["ふりんぐそるじゃー", "フリングソルジャー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/フリングソルジャー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アーティストメイジ",
+    name: "アーティストメイジ",
+    reading: ["あーてぃすとめいじ", "アーティストメイジ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/アーティストメイジ.zip"
+  },
+  {
+    id: "マーシャルゴーレム",
+    name: "マーシャルゴーレム",
+    reading: ["まーしゃるごーれむ", "マーシャルゴーレム"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/マーシャルゴーレム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "蒼炎のドラゴニュート",
+    name: "蒼炎のドラゴニュート",
+    reading: ["そうえんのどらごにゅーと", "蒼炎のドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/蒼炎のドラゴニュート.zip"
+  },
+  {
+    id: "竜人の工匠",
+    name: "竜人の工匠",
+    reading: ["りゅうじんのこうしょう", "竜人の工匠"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/竜人の工匠.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "金鉱のネクロマンサー",
+    name: "金鉱のネクロマンサー",
+    reading: ["きんこうのねくろまんさー", "金鉱のネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/金鉱のネクロマンサー.zip"
+  },
+  {
+    id: "暗がりの悪霊",
+    name: "暗がりの悪霊",
+    reading: ["くらがりのあくりょう", "暗がりの悪霊"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/暗がりの悪霊.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "クラップスデビル",
+    name: "クラップスデビル",
+    reading: ["くらっぷすでびる", "クラップスデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/クラップスデビル.zip"
+  },
+  {
+    id: "レッドキャップ",
+    name: "レッドキャップ",
+    reading: ["れっどきゃっぷ", "レッドキャップ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/レッドキャップ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ダーティプリースト",
+    name: "ダーティプリースト",
+    reading: ["だーてぃぷりーすと", "ダーティプリースト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/ダーティプリースト.zip"
+  },
+  {
+    id: "セト",
+    name: "セト",
+    reading: ["せと", "セト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/セト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "カプセルホムンクルス",
+    name: "カプセルホムンクルス",
+    reading: ["かぷせるほむんくるす", "カプセルホムンクルス"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/カプセルホムンクルス.zip"
+  },
+  {
+    id: "保線機能のコヨーテ",
+    name: "保線機能のコヨーテ",
+    reading: ["ほせんきのうのこよーて", "保線機能のコヨーテ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/保線機能のコヨーテ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ワイルドリザード",
+    name: "ワイルドリザード",
+    reading: ["わいるどりざーど", "ワイルドリザード"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/ワイルドリザード.zip"
+  },
+  {
+    id: "雷鳴のギタリスト",
+    name: "雷鳴のギタリスト",
+    reading: ["らいめいのぎたりすと", "雷鳴のギタリスト"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "SOR",
+    zip: "voices/SOR/Silver/雷鳴のギタリスト.zip"
+  }
 ];

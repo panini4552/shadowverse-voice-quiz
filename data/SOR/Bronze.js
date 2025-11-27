e4be4d7aa9a9bf4cc99321ef8f1792a3d6debb5c
panini4552 <panini4552@gmail.com>
2025-11-27 20:58:56 +0900

    Update Bronze.js

--- data/SOR/Bronze.js
+++ data/SOR/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+[
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Monomi_no_Elf",
+    name: "物見のエルフ",
+    reading: ["ものみのえるふ", "物見のエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/物見のエルフ.zip"
+  },
+  {
+    id: "Forest_Patrol",
+    name: "フォレストパトロール",
+    reading: ["ふぉれすとぱとろーる", "フォレストパトロール"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/フォレストパトロール.zip"
+  },
+  {
+    id: "Saboten_Cowboy",
+    name: "サボテンカウボーイ",
+    reading: ["さぼてんかうぼーい", "サボテンカウボーイ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/サボテンカウボーイ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "Frontline_Instructor",
+    name: "前線の教官",
+    reading: ["ぜんせんのきょうかん", "前線の教官"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/前線の教官.zip"
+  },
+  {
+    id: "Nacht_Private",
+    name: "ナハトの私兵",
+    reading: ["なはとのしへい", "ナハトの私兵"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ナハトの私兵.zip"
+  },
+  {
+    id: "Shinobi_Tanuki",
+    name: "シノビタヌキ",
+    reading: ["しのびたぬき", "シノビタヌキ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/シノビタヌキ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "Reveals_Deputy",
+    name: "レヴィールの保安官補",
+    reading: ["れヴぃーるのほあんかんほ", "レヴィールの保安官補"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/レヴィールの保安官補.zip"
+  },
+  {
+    id: "Crystal_Sword_Mage",
+    name: "水晶の魔剣士",
+    reading: ["すいしょうのまけんし", "水晶の魔剣士"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/水晶の魔剣士.zip"
+  },
+  {
+    id: "Mirror_Witch",
+    name: "ミラーウィッチ",
+    reading: ["みらーうぃっち", "ミラーウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ミラーウィッチ.zip"
+  },
 
-];
+  // === ドラゴン ===
+  {
+    id: "Conductor_Mermaid",
+    name: "コンダクターマーメイド",
+    reading: ["こんだくたーまーめいど", "コンダクターマーメイド"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/コンダクターマーメイド.zip"
+  },
+  {
+    id: "Dragon_Fire_Artisan",
+    name: "竜炎の設計士",
+    reading: ["りゅうえんのせっけいし", "竜炎の設計士"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/竜炎の設計士.zip"
+  },
+  {
+    id: "Pumpkin_Dragon",
+    name: "パンプキンドラゴン",
+    reading: ["ぱんぷきんどらごん", "パンプキンドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/パンプキンドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "Urami_Storyteller",
+    name: "恨みの語り部",
+    reading: ["うらみのかたりべ", "恨みの語り部"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/恨みの語り部.zip"
+  },
+  {
+    id: "Bone_Freak",
+    name: "ボーンフリーク",
+    reading: ["ぼーんふりーく", "ボーンフリーク"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ボーンフリーク.zip"
+  },
+  {
+    id: "Skull_Dreamer",
+    name: "スカルドリーマー",
+    reading: ["すかるどりーまー", "スカルドリーマー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/スカルドリーマー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "Will_Werewolf",
+    name: "闘志の人狼",
+    reading: ["とうしのじんろう", "闘志の人狼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/闘志の人狼.zip"
+  },
+  {
+    id: "Berserk_Rager",
+    name: "激昂のバーサーカー",
+    reading: ["げきこうのばーさーかー", "激昂のバーサーカー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/激昂のバーサーカー.zip"
+  },
+  {
+    id: "Roulette_Vampire",
+    name: "ルーレットヴァンパイア",
+    reading: ["るーれっとヴぁんぱいあ", "ルーレットヴァンパイア"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ルーレットヴァンパイア.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "Holy_Stone_Acolyte",
+    name: "聖石の使徒",
+    reading: ["せいせきのしと", "聖石の使徒"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/聖石の使徒.zip"
+  },
+  {
+    id: "Holy_Enchanter",
+    name: "ホーリーエンチャンター",
+    reading: ["ほーりーえんちゃんたー", "ホーリーエンチャンター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ホーリーエンチャンター.zip"
+  },
+  {
+    id: "Revolver_Eagle",
+    name: "リボルバーイーグル",
+    reading: ["りぼるばーいーぐる", "リボルバーイーグル"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/リボルバーイーグル.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "Beam_YoYo_Girl",
+    name: "ビームヨーヨーガール",
+    reading: ["びーむよーよーがーる", "ビームヨーヨーガール"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ビームヨーヨーガール.zip"
+  },
+  {
+    id: "String_Meister",
+    name: "ストリングマイスター",
+    reading: ["すとりんぐまいすたー", "ストリングマイスター"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/ストリングマイスター.zip"
+  },
+  {
+    id: "Igyou",
+    name: "異形",
+    reading: ["いぎょう", "異形"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/異形.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "Reveals_Outlaw",
+    name: "レヴィールの無法者",
+    reading: ["れヴぃーるのむほうもの", "レヴィールの無法者"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/レヴィールの無法者.zip"
+  },
+  {
+    id: "Free_Adventurer",
+    name: "自由なる冒険者",
+    reading: ["じゆうなるぼうけんしゃ", "自由なる冒険者"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "SOR",
+    zip: "voices/SOR/Bronze/自由なる冒険者.zip"
+  }
+]

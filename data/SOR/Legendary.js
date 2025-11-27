b940e61533341e3862c2bfeb2cbe46e051e007e9
panini4552 <panini4552@gmail.com>
2025-11-27 20:45:03 +0900

    Update Legendary.js

--- data/SOR/Legendary.js
+++ data/SOR/Legendary.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const SOR_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "開拓のロデオガイ・ロキサス",
+    name: "開拓のロデオガイ・ロキサス",
+    reading: ["かいたくのろでおがい・ろきさす", "開拓のロデオガイ・ロキサス"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/開拓のロデオガイ・ロキサス.zip"
+  },
+  {
+    id: "優美な猫姉妹・シャム＆シャマ",
+    name: "優美な猫姉妹・シャム＆シャマ",
+    reading: ["ゆうびなねこしまい・しゃむ&しゃま", "優美な猫姉妹・シャム＆シャマ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/優美な猫姉妹・シャム＆シャマ.zip"
+  },
+  {
+    id: "天香の剣士・ルヴァン",
+    name: "天香の剣士・ルヴァン",
+    reading: ["てんこうのけんし・るゔぁん", "天香の剣士・ルヴァン"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/天香の剣士・ルヴァン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ファントムデュオ・バニー＆バロン",
+    name: "ファントムデュオ・バニー＆バロン",
+    reading: ["ふぁんとむでゅお・ばにー&ばろん", "ファントムデュオ・バニー＆バロン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ファントムデュオ・バニー＆バロン.zip"
+  },
+  {
+    id: "カースドクイーン・ナハト・ナハト",
+    name: "カースドクイーン・ナハト・ナハト",
+    reading: ["かーすどくいーん・なはと・なはと", "カースドクイーン・ナハト・ナハト"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/カースドクイーン・ナハト・ナハト.zip"
+  },
+  {
+    id: "スカイキャプテン・ヴァルフリート",
+    name: "スカイキャプテン・ヴァルフリート",
+    reading: ["すかいきゃぷてん・ゔぁるふりーと", "スカイキャプテン・ヴァルフリート"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/スカイキャプテン・ヴァルフリート.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アクセルヒーロー・マイザー",
+    name: "アクセルヒーロー・マイザー",
+    reading: ["あくせるひーろー・まいざー", "アクセルヒーロー・マイザー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/アクセルヒーロー・マイザー.zip"
+  },
+  {
+    id: "ピースキーパー・ヴィンセント",
+    name: "ピースキーパー・ヴィンセント",
+    reading: ["ぴーすきーぱー・ゔぃんせんと", "ピースキーパー・ヴィンセント"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ピースキーパー・ヴィンセント.zip"
+  },
+  {
+    id: "無尽の魔像",
+    name: "無尽の魔像",
+    reading: ["むじんのまぞう", "無尽の魔像"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/無尽の魔像.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "竜喰らう禁忌",
+    name: "竜喰らう禁忌",
+    reading: ["りゅうくらうきんき", "竜喰らう禁忌"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/竜喰らう禁忌.zip"
+  },
+  {
+    id: "烈覇のアルチザン・レジー",
+    name: "烈覇のアルチザン・レジー",
+    reading: ["れっぱのあるちざん・れじー", "烈覇のアルチザン・レジー"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/烈覇のアルチザン・レジー.zip"
+  },
+  {
+    id: "ゲオルギウス",
+    name: "ゲオルギウス",
+    reading: ["げおるぎうす", "ゲオルギウス"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ゲオルギウス.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ゴールドラッシュゴースト",
+    name: "ゴールドラッシュゴースト",
+    reading: ["ごーるどらっしゅごーすと", "ゴールドラッシュゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ゴールドラッシュゴースト.zip"
+  },
+  {
+    id: "トリニティモンスターズ",
+    name: "トリニティモンスターズ",
+    reading: ["とりにてぃもんすたーず", "トリニティモンスターズ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/トリニティモンスターズ.zip"
+  },
+  {
+    id: "イシス",
+    name: "イシス",
+    reading: ["いしす", "イシス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/イシス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "傷無き双翼・ピユラ",
+    name: "傷無き双翼・ピユラ",
+    reading: ["きずなきそうよく・ぴゆら", "傷無き双翼・ピユラ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/傷無き双翼・ピユラ.zip"
+  },
+  {
+    id: "享楽の支配人・ボルテオ",
+    name: "享楽の支配人・ボルテオ",
+    reading: ["きょうらくのしはいにん・ぼるてお", "享楽の支配人・ボルテオ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/享楽の支配人・ボルテオ.zip"
+  },
+  {
+    id: "鏖殺の大悪魔",
+    name: "鏖殺の大悪魔",
+    reading: ["おうさつのだいあくま", "鏖殺の大悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/鏖殺の大悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "メルティングシュガー・セリーナ",
+    name: "メルティングシュガー・セリーナ",
+    reading: ["めるてぃんぐしゅがー・せりーな", "メルティングシュガー・セリーナ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/メルティングシュガー・セリーナ.zip"
+  },
+  {
+    id: "双砲の神罰・アンヴェルト",
+    name: "双砲の神罰・アンヴェルト",
+    reading: ["そうほうのしんばつ・あんヴぇると", "双砲の神罰・アンヴェルト"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/双砲の神罰・アンヴェルト.zip"
+  },
+  {
+    id: "殲滅のアルミラージ",
+    name: "殲滅のアルミラージ",
+    reading: ["せんめつのあるみらーじ", "殲滅のアルミラージ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/殲滅のアルミラージ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ストレイホロウ・イルガンノ",
+    name: "ストレイホロウ・イルガンノ",
+    reading: ["すとれいほろう・いるがんの", "ストレイホロウ・イルガンノ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ストレイホロウ・イルガンノ.zip"
+  },
+  {
+    id: "カオスルーラー・アイシィレンドリング",
+    name: "カオスルーラー・アイシィレンドリング",
+    reading: ["かおするーらー・あいしぃれんどりんぐ", "カオスルーラー・アイシィレンドリング"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/カオスルーラー・アイシィレンドリング.zip"
+  },
+  {
+    id: "均衡の刃・シルヴィア",
+    name: "均衡の刃・シルヴィア",
+    reading: ["きんこうのやいば・しるゔぃあ", "均衡の刃・シルヴィア"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/均衡の刃・シルヴィア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "虚無の堕天使・ルシフェル",
+    name: "虚無の堕天使・ルシフェル",
+    reading: ["きょむのだてんし・るしふぇる", "虚無の堕天使・ルシフェル"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/虚無の堕天使・ルシフェル.zip"
+  },
+  {
+    id: "ラミエル",
+    name: "ラミエル",
+    reading: ["らみえる", "ラミエル"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "SOR",
+    zip: "voices/SOR/Legendary/ラミエル.zip"
+  }
 ];

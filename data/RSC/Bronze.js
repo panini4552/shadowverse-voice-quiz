f0c542007ff5ccb14b331f1993ec9988b51f2758
panini4552 <panini4552@gmail.com>
2025-12-01 21:13:15 +0900

    Update Bronze.js

--- data/RSC/Bronze.js
+++ data/RSC/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const RSC_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "スコルの探索者",
+    name: "スコルの探索者",
+    reading: ["すこるのたんさくしゃ", "スコルノタンサクシャ", "スコルの探索者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/スコルの探索者.zip"
+  },
+  {
+    id: "ヒーリングフェアリー",
+    name: "ヒーリングフェアリー",
+    reading: ["ひーりんぐふぇありー", "ヒーリングフェアリー", "ヒーリングフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ヒーリングフェアリー.zip"
+  },
+  {
+    id: "大樹のエルク",
+    name: "大樹のエルク",
+    reading: ["たいじゅのえるく", "タイジュノエルク", "大樹のエルク"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/大樹のエルク.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "荒波のパイレーツ",
+    name: "荒波のパイレーツ",
+    reading: ["あらなみのぱいれーつ", "アラナミノパイレーツ", "荒波のパイレーツ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/荒波のパイレーツ.zip"
+  },
+  {
+    id: "無暁の残党",
+    name: "無暁の残党",
+    reading: ["むぎょうのざんとう", "ムギョウノザントウ", "無暁の残党"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/無暁の残党.zip"
+  },
+  {
+    id: "ムササビの伝令兵",
+    name: "ムササビの伝令兵",
+    reading: ["むささびのでんれいへい", "ムササビノデンレイヘイ", "ムササビの伝令兵"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ムササビの伝令兵.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "心持つ機械",
+    name: "心持つ機械",
+    reading: ["こころもつきかい", "ココロモツキカイ", "心持つ機械"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/心持つ機械.zip"
+  },
+  {
+    id: "霊薬のアルケミスト",
+    name: "霊薬のアルケミスト",
+    reading: ["れいやくのあるけみすと", "レイヤクノアルケミスト", "霊薬のアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/霊薬のアルケミスト.zip"
+  },
+  {
+    id: "モンスターブック",
+    name: "モンスターブック",
+    reading: ["もんすたーぶっく", "モンスターブック", "モンスターブック"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/モンスターブック.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "マーナガルムの斥候",
+    name: "マーナガルムの斥候",
+    reading: ["まーながるむのせっこう", "マーナガルムノセッコウ", "マーナガルムの斥候"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/マーナガルムの斥候.zip"
+  },
+  {
+    id: "メルヘンマーメイド",
+    name: "メルヘンマーメイド",
+    reading: ["めるへんまーめいど", "メルヘンマーメイド", "メルヘンマーメイド"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/メルヘンマーメイド.zip"
+  },
+  {
+    id: "フロストリザード",
+    name: "フロストリザード",
+    reading: ["ふろすとりざーど", "フロストリザード", "フロストリザード"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/フロストリザード.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "鎖杖のネクロマンサー",
+    name: "鎖杖のネクロマンサー",
+    reading: ["くさりづえのねくろまんさー", "クサリヅエノネクロマンサー", "鎖杖のネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/鎖杖のネクロマンサー.zip"
+  },
+  {
+    id: "ソウルコンダクター",
+    name: "ソウルコンダクター",
+    reading: ["そうるこんだくたー", "ソウルコンダクター", "ソウルコンダクター"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ソウルコンダクター.zip"
+  },
+  {
+    id: "スカルチーフ",
+    name: "スカルチーフ",
+    reading: ["すかるちーふ", "スカルチーフ", "スカルチーフ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/スカルチーフ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ブラッディピクシー",
+    name: "ブラッディピクシー",
+    reading: ["ぶらっでぃぴくしー", "ブラッディピクシー", "ブラッディピクシー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ブラッディピクシー.zip"
+  },
+  {
+    id: "狂爪の悪魔",
+    name: "狂爪の悪魔",
+    reading: ["きょうそうのあくま", "キョウソウノアクマ", "狂爪の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/狂爪の悪魔.zip"
+  },
+  {
+    id: "蹂躙のデーモン",
+    name: "蹂躙のデーモン",
+    reading: ["じゅうりんのでーもん", "ジュウリンノデーモン", "蹂躙のデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/蹂躙のデーモン.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "パラディンパンダ",
+    name: "パラディンパンダ",
+    reading: ["ぱらでぃんぱんだ", "パラディンパンダ", "パラディンパンダ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/パラディンパンダ.zip"
+  },
+  {
+    id: "エスカマリの助言者",
+    name: "エスカマリの助言者",
+    reading: ["えすかまりのじょげんしゃ", "エスカマリノジョゲンシャ", "エスカマリの助言者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/エスカマリの助言者.zip"
+  },
+  {
+    id: "コンス",
+    name: "コンス",
+    reading: ["こんす", "コンス", "コンス"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/コンス.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "メカニックヒーロー",
+    name: "メカニックヒーロー",
+    reading: ["めかにっくひーろー", "メカニックヒーロー", "メカニックヒーロー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/メカニックヒーロー.zip"
+  },
+  {
+    id: "コイルデバーサー",
+    name: "コイルデバーサー",
+    reading: ["こいるでばーさー", "コイルデバーサー", "コイルデバーサー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/コイルデバーサー.zip"
+  },
+  {
+    id: "ミニマムタンク",
+    name: "ミニマムタンク",
+    reading: ["みにまむたんく", "ミニマムタンク", "ミニマムタンク"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ミニマムタンク.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴスペルエンジェル",
+    name: "ゴスペルエンジェル",
+    reading: ["ごすぺるえんじぇる", "ゴスペルエンジェル", "ゴスペルエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ゴスペルエンジェル.zip"
+  },
+  {
+    id: "ダンジョンミノタウロス",
+    name: "ダンジョンミノタウロス",
+    reading: ["だんじょんみのたうろす", "ダンジョンミノタウロス", "ダンジョンミノタウロス"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RSC",
+    zip: "voices/RSC/Bronze/ダンジョンミノタウロス.zip"
+  }
 ];

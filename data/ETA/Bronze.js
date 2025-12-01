f45cf7ceb19f1e4ac0fce1eb3fe658cbd1b62559
panini4552 <panini4552@gmail.com>
2025-12-01 20:31:27 +0900

    Update Bronze.js

--- data/ETA/Bronze.js
+++ data/ETA/Bronze.js
@@ -1,15 +1,307 @@
-export const Classic_Legendary = [
+export const ETA_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "エルフの呪術者",
+    name: "エルフの呪術者",
+    reading: ["えるふのじゅじゅつしゃ", "エルフの呪術者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/エルフの呪術者.zip"
+  },
+  {
+    id: "ブロッサムトレント",
+    name: "ブロッサムトレント",
+    reading: ["ぶろっさむとれんと", "ブロッサムトレント"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ブロッサムトレント.zip"
+  },
+  {
+    id: "深緑のレインディア",
+    name: "深緑のレインディア",
+    reading: ["しんりょくのれいんでぃあ", "深緑のレインディア"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/深緑のレインディア.zip"
+  },
+  {
+    id: "ウィンドフェアリー",
+    name: "ウィンドフェアリー",
+    reading: ["うぃんどふぇありー", "ウィンドフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ウィンドフェアリー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ブレードバンデッド",
+    name: "ブレードバンデッド",
+    reading: ["ぶれーどばんでっど", "ブレードバンデッド"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ブレードバンデッド.zip"
+  },
+  {
+    id: "ストーンマーチャント",
+    name: "ストーンマーチャント",
+    reading: ["すとーんまーちゃんと", "ストーンマーチャント"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ストーンマーチャント.zip"
+  },
+  {
+    id: "マーシャルファイター",
+    name: "マーシャルファイター",
+    reading: ["まーしゃるふぁいたー", "マーシャルファイター"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/マーシャルファイター.zip"
+  },
+  {
+    id: "クラッシュライノ",
+    name: "クラッシュライノ",
+    reading: ["くらっしゅらいの", "クラッシュライノ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/クラッシュライノ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "博学の魔導士",
+    name: "博学の魔導士",
+    reading: ["はくがくのまどうし", "博学の魔導士"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/博学の魔導士.zip"
+  },
+  {
+    id: "石英の魔女",
+    name: "石英の魔女",
+    reading: ["せきえいのまじょ", "石英の魔女"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/石英の魔女.zip"
+  },
+  {
+    id: "バイオレンスゴーレム",
+    name: "バイオレンスゴーレム",
+    reading: ["ばいおれんすごーれむ", "バイオレンスゴーレム"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/バイオレンスゴーレム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "残影のドラゴニュート",
+    name: "残影のドラゴニュート",
+    reading: ["ざんえいのどらごにゅーと", "残影のドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/残影のドラゴニュート.zip"
+  },
+  {
+    id: "ジャイアントバジリスク",
+    name: "ジャイアントバジリスク",
+    reading: ["じゃいあんとばじりすく", "ジャイアントバジリスク"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ジャイアントバジリスク.zip"
+  },
+  {
+    id: "シャークウォーリアー",
+    name: "シャークウォーリアー",
+    reading: ["しゃーくうぉーりあー", "シャークウォーリアー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/シャークウォーリアー.zip"
+  },
+  {
+    id: "マーメイドアーチャー",
+    name: "マーメイドアーチャー",
+    reading: ["まーめいどあーちゃー", "マーメイドアーチャー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/マーメイドアーチャー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "フレンドリーネクロマンサー",
+    name: "フレンドリーネクロマンサー",
+    reading: ["ふれんどりーねくろまんさー", "フレンドリーネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/フレンドリーネクロマンサー.zip"
+  },
+  {
+    id: "スカルフィッシュ",
+    name: "スカルフィッシュ",
+    reading: ["すかるふぃっしゅ", "スカルフィッシュ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/スカルフィッシュ.zip"
+  },
+  {
+    id: "ウィングゾンビ",
+    name: "ウィングゾンビ",
+    reading: ["うぃんぐぞんび", "ウィングゾンビ"],
+    class: "ネ クロマンサー",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ウィングゾンビ.zip"
+  },
+  {
+    id: "ソウルガイド",
+    name: "ソウルガイド",
+    reading: ["そうるがいど", "ソウルガイド"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ソウルガイド.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ルージュヴァンパイア",
+    name: "ルージュヴァンパイア",
+    reading: ["るーじゅゔぁんぱいあ", "ルージュヴァンパイア"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ルージュヴァンパイア.zip"
+  },
+  {
+    id: "悪魔の狂戦士",
+    name: "悪魔の狂戦士",
+    reading: ["あくまのきょうせんし", "悪魔の狂戦士"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/悪魔の狂戦士.zip"
+  },
+  {
+    id: "イビルウルフ",
+    name: "イビルウルフ",
+    reading: ["いびるうるふ", "イビルウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/イビルウルフ.zip"
+  },
+  {
+    id: "インサイトデビル",
+    name: "インサイトデビル",
+    reading: ["いんさいとでびる", "インサイトデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/インサイトデビル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "鐘楼のシスター",
+    name: "鐘楼のシスター",
+    reading: ["しょうろうのしすたー", "鐘楼のシスター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/鐘楼のシスター.zip"
+  },
+  {
+    id: "セイントタイガー",
+    name: "セイントタイガー",
+    reading: ["せいんとたいがー", "セイントタイガー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/セイントタイガー.zip"
+  },
+  {
+    id: "ソードアルミラージ",
+    name: "ソードアルミラージ",
+    reading: ["そーどあるみらーじ", "ソードアルミラージ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/ソードアルミラージ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "機械化歩兵",
+    name: "機械化歩兵",
+    reading: ["きかいかほへい", "機械化歩兵"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/機械化歩兵.zip"
+  },
+  {
+    id: "スパイデバイス",
+    name: "スパイデバイス",
+    reading: ["すぱいでばいす", "スパイデバイス"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/スパイデバイス.zip"
+  },
+  {
+    id: "トイリペアラー",
+    name: "トイリペアラー",
+    reading: ["といりぺあらー", "トイリペアラー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/トイリペアラー.zip"
+  },
+  {
+    id: "突破のマリオネッター",
+    name: "突破のマリオネッター",
+    reading: ["とっぱのまりおねったー", "突破のマリオネッター"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/突破のマリオネッター.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "フラッフィーエンジェル",
+    name: "フラッフィーエンジェル",
+    reading: ["ふらっふぃーえんじぇる", "フラッフィーエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/フラッフィーエンジェル.zip"
+  },
+  {
+    id: "熟練の探偵",
+    name: "熟練の探偵",
+    reading: ["じゅくれんのたんてい", "熟練の探偵"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ETA",
+    zip: "voices/ETA/Bronze/熟練の探偵.zip"
+  }
 ];

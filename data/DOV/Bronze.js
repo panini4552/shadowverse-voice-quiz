f965651e0483695ec7072e74572013b3f0314e62
panini4552 <panini4552@gmail.com>
2025-12-01 20:45:12 +0900

    Update Bronze.js

--- data/DOV/Bronze.js
+++ data/DOV/Bronze.js
@@ -1,15 +1,262 @@
-export const Classic_Legendary = [
+export const DOV_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ブルームドワーフ",
+    name: "ブルームドワーフ",
+    reading: ["ぶるーむどわーふ", "ブルームドワーフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ブルームドワーフ.zip"
+  },
+  {
+    id: "ブレードエルフ",
+    name: "ブレードエルフ",
+    reading: ["ぶれーどえるふ", "ブレードエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ブレードエルフ.zip"
+  },
+  {
+    id: "大木の巨人",
+    name: "大木の巨人",
+    reading: ["たいぼくのきょじん", "大木の巨人"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/大木の巨人.zip"
+  },
+  {
+    id: "フォレストフェアリー",
+    name: "フォレストフェアリー",
+    reading: ["ふぉれすとふぇありー", "フォレストフェアリー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/フォレストフェアリー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ドレスアサシン",
+    name: "ドレスアサシン",
+    reading: ["どれすあさしん", "ドレスアサシン"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ドレスアサシン.zip"
+  },
+  {
+    id: "ストライクガード",
+    name: "ストライクガード",
+    reading: ["すとらいくがーど", "ストライクガード"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ストライクガード.zip"
+  },
+  {
+    id: "ウォーボア",
+    name: "ウォーボア",
+    reading: ["うぉーぼあ", "ウォーボア"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ウォーボア.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "猫使いの魔女",
+    name: "猫使いの魔女",
+    reading: ["ねこつかいのまじょ", "猫使いの魔女"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/猫使いの魔女.zip"
+  },
+  {
+    id: "サークルウィザード",
+    name: "サークルウィザード",
+    reading: ["さーくるうぃざーど", "サークルウィザード"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/サークルウィザード.zip"
+  },
+  {
+    id: "マジカルリス",
+    name: "マジカルリス",
+    reading: ["まじかるりす", "マジカルリス"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/マジカルリス.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "マーガライトマーメイド",
+    name: "マーガライトマーメイド",
+    reading: ["まーがらいとまーめいど", "マーガライトマーメイド"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/マーガライトマーメイド.zip"
+  },
+  {
+    id: "大地のドラゴニュート",
+    name: "大地のドラゴニュート",
+    reading: ["だいちのどらごにゅーと", "大地のドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/大地のドラゴニュート.zip"
+  },
+  {
+    id: "ドラゴンヒーラー",
+    name: "ドラゴンヒーラー",
+    reading: ["どらごんひーらー", "ドラゴンヒーラー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ドラゴンヒーラー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ゴシックリーパー",
+    name: "ゴシックリーパー",
+    reading: ["ごしっくりーぱー", "ゴシックリーパー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/ゴシックリーパー.zip"
+  },
+  {
+    id: "スマッシュミイラ",
+    name: "スマッシュミイラ",
+    reading: ["すまっしゅみいら", "スマッシュミイラ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/スマッシュミイラ.zip"
+  },
+  {
+    id: "四腕の死兵",
+    name: "四腕の死兵",
+    reading: ["よんわんのしへい", "四腕の死兵"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/四腕の死兵.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "シルヴァリーブレイダー",
+    name: "シルヴァリーブレイダー",
+    reading: ["しるゔぁりーぶれいだー", "シルヴァリーブレイダー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/シルヴァリーブレイダー.zip"
+  },
+  {
+    id: "双頭のスコーピオン",
+    name: "双頭のスコーピオン",
+    reading: ["そうとうのすこーぴおん", "双頭のスコーピオン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/双頭のスコーピオン.zip"
+  },
+  {
+    id: "魔界の処刑人",
+    name: "魔界の処刑人",
+    reading: ["まかいのしょけいにん", "魔界の処刑人"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/魔界の処刑人.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "極彩色の翼人",
+    name: "極彩色の翼人",
+    reading: ["ごくさいしょくのよくじん", "極彩色の翼人"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/極彩色の翼人.zip"
+  },
+  {
+    id: "フレイルプリースト",
+    name: "フレイルプリースト",
+    reading: ["ふれいるぷりーすと", "フレイルプリースト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/フレイルプリースト.zip"
+  },
+  {
+    id: "聖地の守護獣",
+    name: "聖地の守護獣",
+    reading: ["せいちのしゅごじゅう", "聖地の守護獣"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/聖地の守護獣.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エナジーサプライヤー",
+    name: "エナジーサプライヤー",
+    reading: ["えなじーさぷらいやー", "エナジーサプライヤー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/エナジーサプライヤー.zip"
+  },
+  {
+    id: "アームメカニック",
+    name: "アームメカニック",
+    reading: ["あーむめかにっく", "アームメカニック"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/アームメカニック.zip"
+  },
+  {
+    id: "強襲の小型機",
+    name: "強襲の小型機",
+    reading: ["きょうしゅうのこがたき", "強襲の小型機"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/強襲の小型機.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "天使の伴奏者",
+    name: "天使の伴奏者",
+    reading: ["てんしのばんそうしゃ", "天使の伴奏者"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/天使の伴奏者.zip"
+  },
+  {
+    id: "アーマーゴブリン",
+    name: "アーマーゴブリン",
+    reading: ["あーまーごぶりん", "アーマーゴブリン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "DOV",
+    zip: "voices/DOV/Bronze/アーマーゴブリン.zip"
+  }
 ];

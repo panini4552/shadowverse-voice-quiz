ed6de0ab258ba4e2457b1040e0e51811ea562eb5
panini4552 <panini4552@gmail.com>
2025-12-01 20:43:40 +0900

    Update Silver.js

--- data/DOV/Silver.js
+++ data/DOV/Silver.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const DOV_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "陽光のエルフ",
+    name: "陽光のエルフ",
+    reading: ["ようこうのえるふ", "陽光のエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/陽光のエルフ.zip"
+  },
+  {
+    id: "妖精の弓使い",
+    name: "妖精の弓使い",
+    reading: ["ようせいのゆみつかい", "妖精の弓使い"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/妖精の弓使い.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "王城の司令官",
+    name: "王城の司令官",
+    reading: ["おうじょうのしれいかん", "王城の司令官"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/王城の司令官.zip"
+  },
+  {
+    id: "生還の突撃兵",
+    name: "生還の突撃兵",
+    reading: ["せいかんのとつげきへい", "生還の突撃兵"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/生還の突撃兵.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "グリモアソーサラー",
+    name: "グリモアソーサラー",
+    reading: ["ぐりもあそーさらー", "グリモアソーサラー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/グリモアソーサラー.zip"
+  },
+  {
+    id: "ハリケーンゴーレム",
+    name: "ハリケーンゴーレム",
+    reading: ["はりけーんごーれむ", "ハリケーンゴーレム"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ハリケーンゴーレム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "炎柱の竜人",
+    name: "炎柱の竜人",
+    reading: ["えんちゅうのりゅうじん", "炎柱の竜人"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/炎柱の竜人.zip"
+  },
+  {
+    id: "コーラルシャーク",
+    name: "コーラルシャーク",
+    reading: ["こーらるしゃーく", "コーラルシャーク"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/コーラルシャーク.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "求魂の死霊術師",
+    name: "求魂の死霊術師",
+    reading: ["きゅうこんのしりょうじゅつし", "求魂の死霊術師"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/求魂の死霊術師.zip"
+  },
+  {
+    id: "ノーブルファントム",
+    name: "ノーブルファントム",
+    reading: ["のーぶるふぁんとむ", "ノーブルファントム"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ノーブルファントム.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "デコレーションデビル",
+    name: "デコレーションデビル",
+    reading: ["でこれーしょんでびる", "デコレーションデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/デコレーションデビル.zip"
+  },
+  {
+    id: "霹靂の悪魔",
+    name: "霹靂の悪魔",
+    reading: ["へきれきのあくま", "霹靂の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/霹靂の悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ステッキアルミラージ",
+    name: "ステッキアルミラージ",
+    reading: ["すてっきあるみらーじ", "ステッキアルミラージ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ステッキアルミラージ.zip"
+  },
+  {
+    id: "ルナールの聖騎士",
+    name: "ルナールの聖騎士",
+    reading: ["るなーるのせいきし", "ルナールの聖騎士"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ルナールの聖騎士.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "デュオパペッター",
+    name: "デュオパペッター",
+    reading: ["でゅおぱぺったー", "デュオパペッター"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/デュオパペッター.zip"
+  },
+  {
+    id: "ガジェットユーザー",
+    name: "ガジェットユーザー",
+    reading: ["がじぇっとゆーざー", "ガジェットユーザー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ガジェットユーザー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "マジカルグロッサー",
+    name: "マジカルグロッサー",
+    reading: ["まじかるぐろっさー", "マジカルグロッサー"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/マジカルグロッサー.zip"
+  },
+  {
+    id: "ディテクティブガール",
+    name: "ディテクティブガール",
+    reading: ["でぃてくてぃぶがーる", "ディテクティブガール"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "DOV",
+    zip: "voices/DOV/Silver/ディテクティブガール.zip"
+  }
 ];

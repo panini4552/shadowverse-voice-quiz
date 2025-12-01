b5aebc720c5ad06c09244cacb1449167aca483df
panini4552 <panini4552@gmail.com>
2025-12-01 21:07:41 +0900

    Update Silver.js

--- data/RSC/Silver.js
+++ data/RSC/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+export const RSC_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "採集の獣人",
+    name: "採集の獣人",
+    reading: ["さいしゅうのじゅうじん", "サイシュウノジュウジン", "採集の獣人"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/採集の獣人.zip"
+  },
+  {
+    id: "幻獣の遣い",
+    name: "幻獣の遣い",
+    reading: ["げんじゅうのつかい", "ゲンジュウノツカイ", "幻獣の遣い"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/幻獣の遣い.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ブレイドライツの小隊長",
+    name: "ブレイドライツの小隊長",
+    reading: ["ぶれいどらいつのしょうたいちょう", "ブレイドライツノショウタイチョウ", "ブレイドライツの小隊長"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/ブレイドライツの小隊長.zip"
+  },
+  {
+    id: "ヴァローナディーラー",
+    name: "ヴァローナディーラー",
+    reading: ["ゔぁろーなでぃーらー", "ヴァローナディーラー", "ヴァローナディーラー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/ヴァローナディーラー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マギスの魔導師",
+    name: "マギスの魔導師",
+    reading: ["まぎすのまどうし", "マギスノマドウシ", "マギスの魔導師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/マギスの魔導師.zip"
+  },
+  {
+    id: "フラワリングマジシャン・マリー",
+    name: "フラワリングマジシャン・マリー",
+    reading: ["ふらわりんぐまじしゃんまりー", "フラワリングマジシャンマリー", "フラワリングマジシャン・マリー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/フラワリングマジシャン・マリー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "爪牙の戦闘員",
+    name: "爪牙の戦闘員",
+    reading: ["そうがのせんとういん", "ソウガノセントウイン", "爪牙の戦闘員"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/爪牙の戦闘員.zip"
+  },
+  {
+    id: "ストーンホエール",
+    name: "ストーンホエール",
+    reading: ["すとーんほえーる", "ストーンホエール", "ストーンホエール"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/ストーンホエール.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ファイアゾンビ",
+    name: "ファイアゾンビ",
+    reading: ["ふぁいあぞんび", "ファイアゾンビ", "ファイアゾンビ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/ファイアゾンビ.zip"
+  },
+  {
+    id: "怨念の呪殺者",
+    name: "怨念の呪殺者",
+    reading: ["おんねんのじゅさつしゃ", "オンネンノジュサツシャ", "怨念の呪殺者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/怨念の呪殺者.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ハーベストデビル",
+    name: "ハーベストデビル",
+    reading: ["はーべすとでびる", "ハーベストデビル", "ハーベストデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/ハーベストデビル.zip"
+  },
+  {
+    id: "デモニックハンター",
+    name: "デモニックハンター",
+    reading: ["でもにっくはんたー", "デモニックハンター", "デモニックハンター"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/デモニックハンター.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "明光の翼人",
+    name: "明光の翼人",
+    reading: ["めいこうのよくじん", "メイコウノヨクジン", "明光の翼人"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/明光の翼人.zip"
+  },
+  {
+    id: "エスカマリの巡回者",
+    name: "エスカマリの巡回者",
+    reading: ["えすかまりのじゅんかいしゃ", "エスカマリノジュンカイシャ", "エスカマリの巡回者"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/エスカマリの巡回者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "糸切り人形",
+    name: "糸切り人形",
+    reading: ["いときりにんぎょう", "イトキリニンギョウ", "糸切り人形"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/糸切り人形.zip"
+  },
+  {
+    id: "機械兵",
+    name: "機械兵",
+    reading: ["きかいへい", "キカイヘイ", "機械兵"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/機械兵.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "怒れるワイズマーマン",
+    name: "怒れるワイズマーマン",
+    reading: ["いかれるわいずまーまん", "イカレルワイズマーマン", "怒れるワイズマーマン"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "RSC",
+    zip: "voices/RSC/Silver/怒れるワイズマーマン.zip"
+  }
 ];

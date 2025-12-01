e69870e28473e45458c61fa95ca22de31c196145
panini4552 <panini4552@gmail.com>
2025-12-01 20:28:32 +0900

    Update Silver.js

--- data/ETA/Silver.js
+++ data/ETA/Silver.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const ETA_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "フォレストレンジャー・ウェルダー",
+    name: "フォレストレンジャー・ウェルダー",
+    reading: ["ふぉれすとれんじゃーうぇるだー", "フォレストレンジャー・ウェルダー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/フォレストレンジャー・ウェルダー.zip"
+  },
+  {
+    id: "気ままな笛奏者・ミムルメモル",
+    name: "気ままな笛奏者・ミムルメモル",
+    reading: ["きままなてきそうしゃみむるめもる", "気ままな笛奏者・ミムルメモル"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/気ままな笛奏者・ミムルメモル.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "眠れる輝竜・アーサー",
+    name: "眠れる輝竜・アーサー",
+    reading: ["ねむれるきりゅうあーさー", "眠れる輝竜・アーサー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/眠れる輝竜・アーサー.zip"
+  },
+  {
+    id: "眠れる獅子・モルドレッド",
+    name: "眠れる獅子・モルドレッド",
+    reading: ["ねむれるししもるどれっど", "眠れる獅子・モルドレッド"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/眠れる獅子・モルドレッド.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "炎獄の葬送者・エルモート",
+    name: "炎獄の葬送者・エルモート",
+    reading: ["えんごくのそうそうしゃえるもーと", "炎獄の葬送者・エルモート"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/炎獄の葬送者・エルモート.zip"
+  },
+  {
+    id: "リトルメイガス・アリステラ",
+    name: "リトルメイガス・アリステラ",
+    reading: ["りとるめいがすありすてら", "リトルメイガス・アリステラ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/リトルメイガス・アリステラ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "愛の大殿堂・ファスティバ",
+    name: "愛の大殿堂・ファスティバ",
+    reading: ["あいのだいでんどうふぁすてぃば", "愛の大殿堂・ファスティバ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/愛の大殿堂・ファスティバ.zip"
+  },
+  {
+    id: "古今独歩の大拳豪・ガンダゴウザ",
+    name: "古今独歩の大拳豪・ガンダゴウザ",
+    reading: ["ここんどっぽのだいけんごうがんだごうざ", "古今独歩の大拳豪・ガンダゴウザ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/古今独歩の大拳豪・ガンダゴウザ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "妖しき薬師・シャオ",
+    name: "妖しき薬師・シャオ",
+    reading: ["あやしきくすししゃお", "妖しき薬師・シャオ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/妖しき薬師・シャオ.zip"
+  },
+  {
+    id: "壊妄の狂戦士・ザザ",
+    name: "壊妄の狂戦士・ザザ",
+    reading: ["かいもうのきょうせんしざざ", "壊妄の狂戦士・ザザ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/壊妄の狂戦士・ザザ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "壊天災・ハレゼナ",
+    name: "壊天災・ハレゼナ",
+    reading: ["かいてんさいはれぜな", "壊天災・ハレゼナ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/壊天災・ハレゼナ.zip"
+  },
+  {
+    id: "自由を駆ける漢・スカル",
+    name: "自由を駆ける漢・スカル",
+    reading: ["じゆうをかけるおとこすかる", "自由を駆ける漢・スカル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/自由を駆ける漢・スカル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "魔物を愛でる闇の聖職者・ウィル",
+    name: "魔物を愛でる闇の聖職者・ウィル",
+    reading: ["まものをめでるやみのせいしょくしゃうぃる", "魔物を愛でる闇の聖職者・ウィル"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/魔物を愛でる闇の聖職者・ウィル.zip"
+  },
+  {
+    id: "星界の艇人・ノア",
+    name: "星界の艇人・ノア",
+    reading: ["せいかいのていじんのあ", "星界の艇人・ノア"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/星界の艇人・ノア.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "夜失の桃煙・キャサリン",
+    name: "夜失の桃煙・キャサリン",
+    reading: ["やしつのとうえんきゃさりん", "夜失の桃煙・キャサリン"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/夜失の桃煙・キャサリン.zip"
+  },
+  {
+    id: "回転芸術家・スピナー",
+    name: "回転芸術家・スピナー",
+    reading: ["かいてんげいじゅつかすぴなー", "回転芸術家・スピナー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/回転芸術家・スピナー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "蒼烈の志士・オイゲン",
+    name: "蒼烈の志士・オイゲン",
+    reading: ["そうれつのししおいげん", "蒼烈の志士・オイゲン"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/蒼烈の志士・オイゲン.zip"
+  },
+  {
+    id: "蒼心の少女・イオ",
+    name: "蒼心の少女・イオ",
+    reading: ["そうしんのしょうじょいお", "蒼心の少女・イオ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ETA",
+    zip: "voices/ETA/Silver/蒼心の少女・イオ.zip"
+  }
 ];

f9a6b5eb2625a2b0c9c2e01f4741a237ba6aa362
panini4552 <panini4552@gmail.com>
2025-12-01 20:26:11 +0900

    Update Gold.js

--- data/ETA/Gold.js
+++ data/ETA/Gold.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const ETA_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "アシュトレト",
+    name: "アシュトレト",
+    reading: ["あしゅとれと", "アシュトレト"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/アシュトレト.zip"
+  },
+  {
+    id: "原始の悪神",
+    name: "原始の悪神",
+    reading: ["げんしのあくがみ", "原始の悪神"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/原始の悪神.zip"
+  },
+  {
+    id: "爪嵐の猫娘・セン",
+    name: "爪嵐の猫娘・セン",
+    reading: ["つめあらしのねこむすめせん", "爪嵐の猫娘・セン"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/爪嵐の猫娘・セン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "サムライドリーマー・ミリン",
+    name: "サムライドリーマー・ミリン",
+    reading: ["さむらいどりーまーみりん", "サムライドリーマー・ミリン"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/サムライドリーマー・ミリン.zip"
+  },
+  {
+    id: "アックステイカー",
+    name: "アックステイカー",
+    reading: ["あっくすていかー", "アックステイカー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/アックステイカー.zip"
+  },
+  {
+    id: "アームドバトラー",
+    name: "アームドバトラー",
+    reading: ["あーむどばとらー", "アームドバトラー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/アームドバトラー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "楽隠居の元国王・フォリア",
+    name: "楽隠居の元国王・フォリア",
+    reading: ["らくいんきょのもとこくおうふぉりあ", "楽隠居の元国王・フォリア"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/楽隠居の元国王・フォリア.zip"
+  },
+  {
+    id: "アストラルダンサー",
+    name: "アストラルダンサー",
+    reading: ["あすとらるだんさー", "アストラルダンサー"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/アストラルダンサー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "テンペストドラゴン",
+    name: "テンペストドラゴン",
+    reading: ["てんぺすとどらごん", "テンペストドラゴン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/テンペストドラゴン.zip"
+  },
+  {
+    id: "天穿の銃槍騎・ラスティナ",
+    name: "天穿の銃槍騎・ラスティナ",
+    reading: ["てんせんのじゅうそうきらすてぃな", "天穿の銃槍騎・ラスティナ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/天穿の銃槍騎・ラスティナ.zip"
+  },
+  {
+    id: "ガルグイユ",
+    name: "ガルグイユ",
+    reading: ["がるぐいゆ", "ガルグイユ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/ガルグイユ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "トライヘッドハウンド",
+    name: "トライヘッドハウンド",
+    reading: ["とらいへっどはうんど", "トライヘッドハウンド"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/トライヘッドハウンド.zip"
+  },
+  {
+    id: "冥闇の剛刃・バザラガ",
+    name: "冥闇の剛刃・バザラガ",
+    reading: ["めいやみのごうじんばざらが", "冥闇の剛刃・バザラガ"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/冥闇の剛刃・バザラガ.zip"
+  },
+  {
+    id: "ボーンドミネーター",
+    name: "ボーンドミネーター",
+    reading: ["ぼーんどみねーたー", "ボーンドミネーター"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/ボーンドミネーター.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "煉獄のダークナイト",
+    name: "煉獄のダークナイト",
+    reading: ["れんごくのだーくないと", "煉獄のダークナイト"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/煉獄のダークナイト.zip"
+  },
+  {
+    id: "グラトニーデーモン",
+    name: "グラトニーデーモン",
+    reading: ["ぐらとにーでーもん", "グラトニーデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/グラトニーデーモン.zip"
+  },
+  {
+    id: "灰白の豹・アイル",
+    name: "灰白の豹・アイル",
+    reading: ["かいはくのひょうあいる", "灰白の豹・アイル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/灰白の豹・アイル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "日輪の巫女・ザルハメリナ",
+    name: "日輪の巫女・ザルハメリナ",
+    reading: ["にちりんのみこざるはめりな", "日輪の巫女・ザルハメリナ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/日輪の巫女・ザルハメリナ.zip"
+  },
+  {
+    id: "ルナールプリースト",
+    name: "ルナールプリースト",
+    reading: ["るなーるぷりーすと", "ルナールプリースト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/ルナールプリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "妄想少女A・ルナール",
+    name: "妄想少女A・ルナール",
+    reading: ["もうそうしょうじょあるなーる", "妄想少女A・ルナール"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/妄想少女A・ルナール.zip"
+  },
+  {
+    id: "ギガスファクトリー",
+    name: "ギガスファクトリー",
+    reading: ["ぎがすふぁくとりー", "ギガスファクトリー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/ギガスファクトリー.zip"
+  },
+  {
+    id: "マウンテンドール",
+    name: "マウンテンドール",
+    reading: ["まうんてんどーる", "マウンテンドール"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/マウンテンドール.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "追憶の大天使",
+    name: "追憶の大天使",
+    reading: ["ついおくのだいてんし", "追憶の大天使"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/追憶の大天使.zip"
+  },
+  {
+    id: "小さき赤き竜・ビィ",
+    name: "小さき赤き竜・ビィ",
+    reading: ["ちいさきあかきりゅうびぃ", "小さき赤き竜・ビィ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/小さき赤き竜・ビィ.zip"
+  },
+  {
+    id: "オーバーヒートデーモン",
+    name: "オーバーヒートデーモン",
+    reading: ["おーばーひーとでーもん", "オーバーヒートデーモン"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ETA",
+    zip: "voices/ETA/Gold/オーバーヒートデーモン.zip"
+  }
 ];

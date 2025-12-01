013c25f47e3f2529cdca6e715b8578e44fbdb242
panini4552 <panini4552@gmail.com>
2025-12-01 21:05:40 +0900

    Update Gold.js

--- data/RSC/Gold.js
+++ data/RSC/Gold.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const RSC_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "妖精の賢者",
+    name: "妖精の賢者",
+    reading: ["ようせいのけんじゃ", "ヨウセイノケンジャ", "妖精の賢者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/妖精の賢者.zip"
+  },
+  {
+    id: "幻惑のラクーン",
+    name: "幻惑のラクーン",
+    reading: ["げんわくのらくーん", "ゲンワクノラクーン", "幻惑のラクーン"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/幻惑のラクーン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "撲滅の兵団長",
+    name: "撲滅の兵団長",
+    reading: ["ぼくめつのへいだんちょう", "ボクメツノヘイダンチョウ", "撲滅の兵団長"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/撲滅の兵団長.zip"
+  },
+  {
+    id: "テンペストスレイヤー",
+    name: "テンペストスレイヤー",
+    reading: ["てんぺすとすれいやー", "テンペストスレイヤー", "テンペストスレイヤー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/テンペストスレイヤー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "キマイラマイスター",
+    name: "キマイラマイスター",
+    reading: ["きまいらまいすたー", "キマイラマイスター", "キマイラマイスター"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/キマイラマイスター.zip"
+  },
+  {
+    id: "アンブレラウィッチ",
+    name: "アンブレラウィッチ",
+    reading: ["あんぶれらうぃっち", "アンブレラウィッチ", "アンブレラウィッチ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/アンブレラウィッチ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "勇躍の獣人・ジーナ",
+    name: "勇躍の獣人・ジーナ",
+    reading: ["ゆうやくのじゅうじんじーな", "ユウヤクノジュウジンジーナ", "勇躍の獣人・ジーナ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/勇躍の獣人・ジーナ.zip"
+  },
+  {
+    id: "オラクルドラゴン",
+    name: "オラクルドラゴン",
+    reading: ["おらくるどらごん", "オラクルドラゴン", "オラクルドラゴン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/オラクルドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "マンマル2号",
+    name: "マンマル2号",
+    reading: ["まんまるにごう", "マンマルニゴウ", "マンマル2号"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/マンマル2号.zip"
+  },
+  {
+    id: "デッドバイオレーター",
+    name: "デッドバイオレーター",
+    reading: ["でっどばいおれーたー", "デッドバイオレーター", "デッドバイオレーター"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/デッドバイオレーター.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "デモンディーヴァ",
+    name: "デモンディーヴァ",
+    reading: ["でもんでぃーゔぁ", "デモンディーヴァ", "デモンディーヴァ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/デモンディーヴァ.zip"
+  },
+  {
+    id: "マリシャスウォール",
+    name: "マリシャスウォール",
+    reading: ["まりしゃすうぉーる", "マリシャスウォール", "マリシャスウォール"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/マリシャスウォール.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖晶の大神官",
+    name: "聖晶の大神官",
+    reading: ["せいしょうのおおしんかん", "セイショウノオオシンカン", "聖晶の大神官"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/聖晶の大神官.zip"
+  },
+  {
+    id: "神威のユニコーン",
+    name: "神威のユニコーン",
+    reading: ["かむいのゆにこーん", "カムイノユニコーン", "神威のユニコーン"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/神威のユニコーン.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "大型機械兵",
+    name: "大型機械兵",
+    reading: ["おおがたきかいへい", "オオガタキカイヘイ", "大型機械兵"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/大型機械兵.zip"
+  },
+  {
+    id: "オートマティックメイデン",
+    name: "オートマティックメイデン",
+    reading: ["おーとまてぃっくめいでん", "オートマティックメイデン", "オートマティックメイデン"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/オートマティックメイデン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "恒久の天使",
+    name: "恒久の天使",
+    reading: ["こうきゅうのてんし", "コウキュウノテンシ", "恒久の天使"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/恒久の天使.zip"
+  },
+  {
+    id: "マスターコック",
+    name: "マスターコック",
+    reading: ["ますたーこっく", "マスターコック", "マスターコック"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RSC",
+    zip: "voices/RSC/Gold/マスターコック.zip"
+  }
 ];

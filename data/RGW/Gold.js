3d42025d011b7835afe04e3793ef33cd4b0f204e
panini4552 <panini4552@gmail.com>
2025-12-04 21:51:50 +0900

    Update Gold.js

--- data/RGW/Gold.js
+++ data/RGW/Gold.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
+export const RGW_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "アクアフェアリー",
+    name: "アクアフェアリー",
+    reading: ["あくあふぇありー", "アクアフェアリー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/アクアフェアリー.zip"
+  },
+  {
+    id: "妖精の騎士・タムリン",
+    name: "妖精の騎士・タムリン",
+    reading: ["ようせいのきしたむりん", "妖精の騎士・タムリン"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/妖精の騎士・タムリン.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "神速のクイックブレーダー",
+    name: "神速のクイックブレーダー",
+    reading: ["しんそくのくいっくぶれーだー", "神速のクイックブレーダー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/神速のクイックブレーダー.zip"
+  },
+  {
+    id: "剣戟のルミナスナイト",
+    name: "剣戟のルミナスナイト",
+    reading: ["けんげきのるみなすないと", "剣戟のルミナスナイト"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/剣戟のルミナスナイト.zip"
+  },
+  {
+    id: "タクティクスコマンダー",
+    name: "タクティクスコマンダー",
+    reading: ["たくてぃくすこまんだー", "タクティクスコマンダー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/タクティクスコマンダー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "工房の錬金術師・ノノ",
+    name: "工房の錬金術師・ノノ",
+    reading: ["こうぼうのれんきんじゅつしのの", "工房の錬金術師・ノノ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/工房の錬金術師・ノノ.zip"
+  },
+  {
+    id: "マナリアの詠唱者・クレイグ",
+    name: "マナリアの詠唱者・クレイグ",
+    reading: ["まなりあのえいしょうしゃくれいぐ", "マナリアの詠唱者・クレイグ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/マナリアの詠唱者・クレイグ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "激昂のリヴァイアサン",
+    name: "激昂のリヴァイアサン",
+    reading: ["げきこうのりヴぁいあさん", "激昂のリヴァイアサン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/激昂のリヴァイアサン.zip"
+  },
+  {
+    id: "紅炎の竜爪・エチカ",
+    name: "紅炎の竜爪・エチカ",
+    reading: ["こうえんのりゅうそうえちか", "紅炎の竜爪・エチカ"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/紅炎の竜爪・エチカ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥道の烈牙・オルトロス",
+    name: "冥道の烈牙・オルトロス",
+    reading: ["めいどうのれつがおるとろす", "冥道の烈牙・オルトロス"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/冥道の烈牙・オルトロス.zip"
+  },
+  {
+    id: "冥河の流葬・カローン",
+    name: "冥河の流葬・カローン",
+    reading: ["めいがのりゅうそうかろーん", "冥河の流葬・カローン"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/冥河の流葬・カローン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "高貴なる闇夜・ヴァイト",
+    name: "高貴なる闇夜・ヴァイト",
+    reading: ["こうきなるやみよヴぁいと", "高貴なる闇夜・ヴァイト"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/高貴なる闇夜・ヴァイト.zip"
+  },
+  {
+    id: "魔刃の吸血鬼",
+    name: "魔刃の吸血鬼",
+    reading: ["まじんのきゅうけつき", "魔刃の吸血鬼"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/魔刃の吸血鬼.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "天昇のプリズムプリースト",
+    name: "天昇のプリズムプリースト",
+    reading: ["てんしょうのぷりずむぷりーすと", "天昇のプリズムプリースト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/天昇のプリズムプリースト.zip"
+  },
+  {
+    id: "道標の御使い",
+    name: "道標の御使い",
+    reading: ["みちしるべのみつかい", "道標の御使い"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/道標の御使い.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "次元の観測者・ティル",
+    name: "次元の観測者・ティル",
+    reading: ["じげんのかんそくしゃてぃる", "次元の観測者・ティル"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/次元の観測者・ティル.zip"
+  },
+  {
+    id: "外界の審判者・ノルン",
+    name: "外界の審判者・ノルン",
+    reading: ["がいかいのしんぱんしゃのるん", "外界の審判者・ノルン"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/外界の審判者・ノルン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ジャイアントハッピーピッグ",
+    name: "ジャイアントハッピーピッグ",
+    reading: ["じゃいあんとはっぴーぴっぐ", "ジャイアントハッピーピッグ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/ジャイアントハッピーピッグ.zip"
+  },
+  {
+    id: "メチャカワ傭兵・フィーナ",
+    name: "メチャカワ傭兵・フィーナ",
+    reading: ["めちゃかわようへいふぃーな", "メチャカワ傭兵・フィーナ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RGW",
+    zip: "voices/RGW/Gold/メチャカワ傭兵・フィーナ.zip"
+  }
 ];

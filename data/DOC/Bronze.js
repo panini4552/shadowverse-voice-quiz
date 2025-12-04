77d683c044e8c2e641b784adeda221bcb7dadeea
panini4552 <panini4552@gmail.com>
2025-12-04 21:13:10 +0900

    Update Bronze.js

--- data/DOC/Bronze.js
+++ data/DOC/Bronze.js
@@ -1,15 +1,251 @@
-export const Classic_Legendary = [
-  // === エルフ ===
-  {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
-  },
+// === エルフ ===
+{
+  id: "妖精の先導者",
+  name: "妖精の先導者",
+  reading: ["ようせいのせんどうしゃ", "妖精の先導者"],
+  class: "エルフ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/妖精の先導者.zip"
+},
+{
+  id: "旋風のランサー",
+  name: "旋風のランサー",
+  reading: ["せんぷうのらんさー", "旋風のランサー"],
+  class: "エルフ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/旋風のランサー.zip"
+},
+{
+  id: "レイジィスロース",
+  name: "レイジィスロース",
+  reading: ["れいじぃすろーす", "レイジィスロース"],
+  class: "エルフ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/レイジィスロース.zip"
+},
 
+// === ロイヤル ===
+{
+  id: "流水の槍使い",
+  name: "流水の槍使い",
+  reading: ["りゅうすいのやりつかい", "流水の槍使い"],
+  class: "ロイヤル",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/流水の槍使い.zip"
+},
+{
+  id: "結束の騎兵",
+  name: "結束の騎兵",
+  reading: ["けっそくのきへい", "結束の騎兵"],
+  class: "ロイヤル",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/結束の騎兵.zip"
+},
+{
+  id: "タクティカルドッグ",
+  name: "タクティカルドッグ",
+  reading: ["たくてぃかるどっぐ", "タクティカルドッグ"],
+  class: "ロイヤル",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/タクティカルドッグ.zip"
+},
 
+// === ウィッチ ===
+{
+  id: "クレイソーサラー",
+  name: "クレイソーサラー",
+  reading: ["くれいそーさらー", "クレイソーサラー"],
+  class: "ウィッチ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/クレイソーサラー.zip"
+},
+{
+  id: "禁書庫の番人",
+  name: "禁書庫の番人",
+  reading: ["きんしょこのばんにん", "禁書庫の番人"],
+  class: "ウィッチ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/禁書庫の番人.zip"
+},
+{
+  id: "マナリアシャンデリアン",
+  name: "マナリアシャンデリアン",
+  reading: ["まなりあしゃんでりあん", "マナリアシャンデリアン"],
+  class: "ウィッチ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/マナリアシャンデリアン.zip"
+},
 
-];
+// === ドラゴン ===
+{
+  id: "火炎の竜闘士",
+  name: "火炎の竜闘士",
+  reading: ["かえんのりゅうとうし", "火炎の竜闘士"],
+  class: "ドラゴン",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/火炎の竜闘士.zip"
+},
+{
+  id: "崇拝の竜騎士",
+  name: "崇拝の竜騎士",
+  reading: ["すうはいのりゅうきし", "崇拝の竜騎士"],
+  class: "ドラゴン",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/崇拝の竜騎士.zip"
+},
+{
+  id: "オーブキャンサー",
+  name: "オーブキャンサー",
+  reading: ["おーぶきゃんさー", "オーブキャンサー"],
+  class: "ドラゴン",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/オーブキャンサー.zip"
+},
+
+// === ネクロマンサー ===
+{
+  id: "白骨の賛美者",
+  name: "白骨の賛美者",
+  reading: ["はっこつのさんびしゃ", "白骨の賛美者"],
+  class: "ネクロマンサー",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/白骨の賛美者.zip"
+},
+{
+  id: "悪霊の剣士",
+  name: "悪霊の剣士",
+  reading: ["あくりょうのけんし", "悪霊の剣士"],
+  class: "ネクロマンサー",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/悪霊の剣士.zip"
+},
+{
+  id: "スカルバンデット",
+  name: "スカルバンデット",
+  reading: ["すかるばんでっと", "スカルバンデット"],
+  class: "ネクロマンサー",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/スカルバンデット.zip"
+},
+
+// === ヴァンパイア ===
+{
+  id: "強欲の悪魔使い",
+  name: "強欲の悪魔使い",
+  reading: ["ごうよくのあくまつかい", "強欲の悪魔使い"],
+  class: "ヴァンパイア",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/強欲の悪魔使い.zip"
+},
+{
+  id: "デモニックソルジャー",
+  name: "デモニックソルジャー",
+  reading: ["でもにっくそるじゃー", "デモニックソルジャー"],
+  class: "ヴァンパイア",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/デモニックソルジャー.zip"
+},
+{
+  id: "鉄球のオーガ",
+  name: "鉄球のオーガ",
+  reading: ["てっきゅうのおーが", "鉄球のオーガ"],
+  class: "ヴァンパイア",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/鉄球のオーガ.zip"
+},
+
+// === ビショップ ===
+{
+  id: "輝きのシスター",
+  name: "輝きのシスター",
+  reading: ["かがやきのしすたー", "輝きのシスター"],
+  class: "ビショップ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/輝きのシスター.zip"
+},
+{
+  id: "風を呼ぶ翼人",
+  name: "風を呼ぶ翼人",
+  reading: ["かぜをよぶよくじん", "風を呼ぶ翼人"],
+  class: "ビショップ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/風を呼ぶ翼人.zip"
+},
+{
+  id: "セイントフラミンゴ",
+  name: "セイントフラミンゴ",
+  reading: ["せいんとふらみんご", "セイントフラミンゴ"],
+  class: "ビショップ",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/セイントフラミンゴ.zip"
+},
+
+// === ネメシス ===
+{
+  id: "知恵のホムンクルス",
+  name: "知恵のホムンクルス",
+  reading: ["ちえのほむんくるす", "知恵のホムンクルス"],
+  class: "ネメシス",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/知恵のホムンクルス.zip"
+},
+{
+  id: "幼き糸使い",
+  name: "幼き糸使い",
+  reading: ["おさなきいとつかい", "幼き糸使い"],
+  class: "ネメシス",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/幼き糸使い.zip"
+},
+{
+  id: "魔銅の番人",
+  name: "魔銅の番人",
+  reading: ["まどうのばんにん", "魔銅の番人"],
+  class: "ネメシス",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/魔銅の番人.zip"
+},
+
+// === ニュートラル ===
+{
+  id: "浄火の天使",
+  name: "浄火の天使",
+  reading: ["じょうかのてんし", "浄火の天使"],
+  class: "ニュートラル",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/浄火の天使.zip"
+},
+{
+  id: "カエルの演奏家",
+  name: "カエルの演奏家",
+  reading: ["かえるのえんそうか", "カエルの演奏家"],
+  class: "ニュートラル",
+  rarity: "Bronze",
+  pack: "DOC",
+  zip: "voices/DOC/Bronze/カエルの演奏家.zip"
+}

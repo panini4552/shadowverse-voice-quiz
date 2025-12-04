9cdef0f66acc1a6e817889e03038e52af0104408
panini4552 <panini4552@gmail.com>
2025-12-04 21:25:56 +0900

    Update Silver.js

--- data/OOS/Silver.js
+++ data/OOS/Silver.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const OOS_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "翠玉の野狐",
+    name: "翠玉の野狐",
+    reading: ["すいぎょくのやこ", "スイギョクノヤコ", "翠玉の野狐"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/翠玉の野狐.zip"
+  },
+  {
+    id: "邪弓のエルフ",
+    name: "邪弓のエルフ",
+    reading: ["じゃきゅうのえるふ", "ジャキュウノエルフ", "邪弓のエルフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/邪弓のエルフ.zip"
+  },
+  {
+    id: "絶命の崇拝者",
+    name: "絶命の崇拝者",
+    reading: ["ぜつめいのすうはいしゃ", "ゼツメイノスウハイシャ", "絶命の崇拝者"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/絶命の崇拝者.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ソードジェネラル",
+    name: "ソードジェネラル",
+    reading: ["そーどじぇねらる", "ソードジェネラル", "ソードジェネラル"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/ソードジェネラル.zip"
+  },
+  {
+    id: "戦乱の勇士",
+    name: "戦乱の勇士",
+    reading: ["せんらんのゆうし", "センランノユウシ", "戦乱の勇士"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/戦乱の勇士.zip"
+  },
+  {
+    id: "空絶の崇拝者",
+    name: "空絶の崇拝者",
+    reading: ["くうぜつのすうはいしゃ", "クウゼツノスウハイシャ", "空絶の崇拝者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/空絶の崇拝者.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ブルームウィッチ",
+    name: "ブルームウィッチ",
+    reading: ["ぶるーむうぃっち", "ブルームウィッチ", "ブルームウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/ブルームウィッチ.zip"
+  },
+  {
+    id: "スクロールウィザード",
+    name: "スクロールウィザード",
+    reading: ["すくろーるうぃざーど", "スクロールウィザード", "スクロールウィザード"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/スクロールウィザード.zip"
+  },
+  {
+    id: "絶尽の崇拝者",
+    name: "絶尽の崇拝者",
+    reading: ["ぜつじんのすうはいしゃ", "ゼツジンノスウハイシャ", "絶尽の崇拝者"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/絶尽の崇拝者.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ウィンディドラゴニュート",
+    name: "ウィンディドラゴニュート",
+    reading: ["うぃんでぃどらごにゅーと", "ウィンディドラゴニュート", "ウィンディドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/ウィンディドラゴニュート.zip"
+  },
+  {
+    id: "ミニマムドラゴン",
+    name: "ミニマムドラゴン",
+    reading: ["みにまむどらごん", "ミニマムドラゴン", "ミニマムドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/ミニマムドラゴン.zip"
+  },
+  {
+    id: "烈絶の崇拝者",
+    name: "烈絶の崇拝者",
+    reading: ["れつぜつのすうはいしゃ", "レツゼツノスウハイシャ", "烈絶の崇拝者"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/烈絶の崇拝者.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ホロウシップ",
+    name: "ホロウシップ",
+    reading: ["ほろうしっぷ", "ホロウシップ", "ホロウシップ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/ホロウシップ.zip"
+  },
+  {
+    id: "スカルファナティック",
+    name: "スカルファナティック",
+    reading: ["すかるふぁなてぃっく", "スカルファナティック", "スカルファナティック"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/スカルファナティック.zip"
+  },
+  {
+    id: "絶叫の崇拝者",
+    name: "絶叫の崇拝者",
+    reading: ["ぜっきょうのすうはいしゃ", "ゼッキョウノスウハイシャ", "絶叫の崇拝者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/絶叫の崇拝者.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "棘の吸血鬼",
+    name: "棘の吸血鬼",
+    reading: ["とげのきゅうけつき", "トゲノキュウケツキ", "棘の吸血鬼"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/棘の吸血鬼.zip"
+  },
+  {
+    id: "イビルレオ",
+    name: "イビルレオ",
+    reading: ["いびるれお", "イビルレオ", "イビルレオ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/イビルレオ.zip"
+  },
+  {
+    id: "愛絶の崇拝者",
+    name: "愛絶の崇拝者",
+    reading: ["あいぜつのすうはいしゃ", "アイゼツノスウハイシャ", "愛絶の崇拝者"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/愛絶の崇拝者.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "熟達の翼戦士",
+    name: "熟達の翼戦士",
+    reading: ["じゅくたつのよくせんし", "ジュクタツノヨクセンシ", "熟達の翼戦士"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/熟達の翼戦士.zip"
+  },
+  {
+    id: "砕氷の聖獣",
+    name: "砕氷の聖獣",
+    reading: ["さいひょうのせいじゅう", "サイヒョウノセイジュウ", "砕氷の聖獣"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/砕氷の聖獣.zip"
+  },
+  {
+    id: "背理盾・ゼノン",
+    name: "背理盾・ゼノン",
+    reading: ["はいりたて・ぜのん", "ハイリタテゼノン", "背理盾・ゼノン"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/背理盾・ゼノン.zip"
+  },
+  {
+    id: "絶望の崇拝者",
+    name: "絶望の崇拝者",
+    reading: ["ぜつぼうのすうはいしゃ", "ゼツボウノスウハイシャ", "絶望の崇拝者"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/絶望の崇拝者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エンジニアドワーフ",
+    name: "エンジニアドワーフ",
+    reading: ["えんじにあどわーふ", "エンジニアドワーフ", "エンジニアドワーフ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/エンジニアドワーフ.zip"
+  },
+  {
+    id: "レーザーバード",
+    name: "レーザーバード",
+    reading: ["れーざーばーど", "レーザーバード", "レーザーバード"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/レーザーバード.zip"
+  },
+  {
+    id: "奏絶の崇拝者",
+    name: "奏絶の崇拝者",
+    reading: ["そうぜつのすうはいしゃ", "ソウゼツノスウハイシャ", "奏絶の崇拝者"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/奏絶の崇拝者.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "天空の遣い",
+    name: "天空の遣い",
+    reading: ["てんくうのつかい", "テンクウノツカイ", "天空の遣い"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "OOS",
+    zip: "voices/OOS/Silver/天空の遣い.zip"
+  }
 ];

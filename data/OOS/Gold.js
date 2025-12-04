615133c469dbfb569fd3e16e3fc3fbbdc2a19a77
panini4552 <panini4552@gmail.com>
2025-12-04 21:24:15 +0900

    Update Gold.js

--- data/OOS/Gold.js
+++ data/OOS/Gold.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const OOS_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "螺旋角の巨象",
+    name: "螺旋角の巨象",
+    reading: ["らせんかくのきょぞう", "ラセンカクノキョゾウ", "螺旋角の巨象"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/螺旋角の巨象.zip"
+  },
+  {
+    id: "ブロッサムルナール",
+    name: "ブロッサムルナール",
+    reading: ["ぶろっさむるなーる", "ブロッサムルナール", "ブロッサムルナール"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/ブロッサムルナール.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "アルセーヌ・ルパン",
+    name: "アルセーヌ・ルパン",
+    reading: ["あるせーぬ・るぱん", "アルセーヌルパン", "アルセーヌ・ルパン"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/アルセーヌ・ルパン.zip"
+  },
+  {
+    id: "クレイモアマスター",
+    name: "クレイモアマスター",
+    reading: ["くれいもあますたー", "クレイモアマスター", "クレイモアマスター"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/クレイモアマスター.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アシッドゴーレム",
+    name: "アシッドゴーレム",
+    reading: ["あしっどごーれむ", "アシッドゴーレム", "アシッドゴーレム"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/アシッドゴーレム.zip"
+  },
+  {
+    id: "強欲の魔女",
+    name: "強欲の魔女",
+    reading: ["ごうよくのまじょ", "ゴウヨクノマジョ", "強欲の魔女"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/強欲の魔女.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "パニッシュマーメイド",
+    name: "パニッシュマーメイド",
+    reading: ["ぱにっしゅまーめいど", "パニッシュマーメイド", "パニッシュマーメイド"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/パニッシュマーメイド.zip"
+  },
+  {
+    id: "ドラゴニックマーセナリー",
+    name: "ドラゴニックマーセナリー",
+    reading: ["どらごにっくまーせなりー", "ドラゴニックマーセナリー", "ドラゴニックマーセナリー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/ドラゴニックマーセナリー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "デスキャットリーパー",
+    name: "デスキャットリーパー",
+    reading: ["ですきゃっとりーぱー", "デスキャットリーパー", "デスキャットリーパー"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/デスキャットリーパー.zip"
+  },
+  {
+    id: "スピリットイーター",
+    name: "スピリットイーター",
+    reading: ["すぴりっといーたー", "スピリットイーター", "スピリットイーター"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/スピリットイーター.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "カーリー",
+    name: "カーリー",
+    reading: ["かーりー", "カーリー", "カーリー"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/カーリー.zip"
+  },
+  {
+    id: "酔狂の大悪魔",
+    name: "酔狂の大悪魔",
+    reading: ["すいきょうのだいあくま", "スイキョウノダイアクマ", "酔狂の大悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/酔狂の大悪魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "呪われた翼・シロ",
+    name: "呪われた翼・シロ",
+    reading: ["のろわれたつばさ・しろ", "ノロワレタツバサシロ", "呪われた翼・シロ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/呪われた翼・シロ.zip"
+  },
+  {
+    id: "ユニコーンウォーリアー",
+    name: "ユニコーンウォーリアー",
+    reading: ["ゆにこーんうぉーりあー", "ユニコーンウォーリアー", "ユニコーンウォーリアー"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/ユニコーンウォーリアー.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "不吉の人形師",
+    name: "不吉の人形師",
+    reading: ["ふきつのにんぎょうし", "フキツノニンギョウシ", "不吉の人形師"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/不吉の人形師.zip"
+  },
+  {
+    id: "ジェットパックガンナー",
+    name: "ジェットパックガンナー",
+    reading: ["じぇっとぱっくがんなー", "ジェットパックガンナー", "ジェットパックガンナー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/ジェットパックガンナー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "アラエル",
+    name: "アラエル",
+    reading: ["あらえる", "アラエル", "アラエル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/アラエル.zip"
+  },
+  {
+    id: "荒天の雷神",
+    name: "荒天の雷神",
+    reading: ["こうてんのらいじん", "コウテンノライジン", "荒天の雷神"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "OOS",
+    zip: "voices/OOS/Gold/荒天の雷神.zip"
+  }
 ];

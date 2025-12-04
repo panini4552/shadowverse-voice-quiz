8481cc7b01d82fd1c7b1fbc46a4eca21f1d0d7ac
panini4552 <panini4552@gmail.com>
2025-12-04 21:50:33 +0900

    Update Legendary.js

--- data/RGW/Legendary.js
+++ data/RGW/Legendary.js
@@ -1,15 +1,170 @@
-export const Classic_Legendary = [
+export const RGW_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "シャイニングヴァルキリー",
+    name: "シャイニングヴァルキリー",
+    reading: ["しゃいにんぐゔぁるきりー", "シャイニングヴァルキリー", "シャイニングヴァルキリー"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/シャイニングヴァルキリー.zip"
+  },
+  {
+    id: "神鉄の鍛冶師・レーネ",
+    name: "神鉄の鍛冶師・レーネ",
+    reading: ["しんてつのかじしれーね", "シンテツノカジシレーネ", "神鉄の鍛冶師・レーネ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/神鉄の鍛冶師・レーネ.zip"
+  },
+  {
+    id: "聖緑の輝き・カーバンクル",
+    name: "聖緑の輝き・カーバンクル",
+    reading: ["せいりょくのかがやきかーばんくる", "セイリョクノカガヤキカーバンクル", "聖緑の輝き・カーバンクル"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/聖緑の輝き・カーバンクル.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レジェンドソードコマンダー",
+    name: "レジェンドソードコマンダー",
+    reading: ["れじぇんどそーどこまんだー", "レジェンドソードコマンダー", "レジェンドソードコマンダー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/レジェンドソードコマンダー.zip"
+  },
+  {
+    id: "グロリアスセイバー・オーレリア",
+    name: "グロリアスセイバー・オーレリア",
+    reading: ["ぐろりあすせいばーおーれりあ", "グロリアスセイバーオーレリア", "グロリアスセイバー・オーレリア"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/グロリアスセイバー・オーレリア.zip"
+  },
+  {
+    id: "カイザーフォートレス",
+    name: "カイザーフォートレス",
+    reading: ["かいざーふぉーとれす", "カイザーフォートレス", "カイザーフォートレス"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/カイザーフォートレス.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "セブンズフォースソーサラー",
+    name: "セブンズフォースソーサラー",
+    reading: ["せぶんずふぉーすすぉーさらー", "セブンズフォースソーサラー", "セブンズフォースソーサラー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/セブンズフォースソーサラー.zip"
+  },
+  {
+    id: "始まりの魔術・マナリア",
+    name: "始まりの魔術・マナリア",
+    reading: ["はじまりのまじゅつまなりあ", "ハジマリノマジュツマナリア", "始まりの魔術・マナリア"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/始まりの魔術・マナリア.zip"
+  },
+  {
+    id: "マナリアの双姫・アン＆グレア",
+    name: "マナリアの双姫・アン＆グレア",
+    reading: ["まなりあのそうきあんあんどぐれあ", "マナリアノソウキアンアンドグレア", "マナリアの双姫・アン＆グレア"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/マナリアの双姫・アン＆グレア.zip"
+  },
 
-];
+  // === ドラゴン ===
+  {
+    id: "インフィニットフレイムドラゴン",
+    name: "インフィニットフレイムドラゴン",
+    reading: ["いんふぃにっとふれいむどらごん", "インフィニットフレイムドラゴン", "インフィニットフレイムドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/インフィニットフレイムドラゴン.zip"
+  },
+  {
+    id: "黒翼の覇者・フォルテ",
+    name: "黒翼の覇者・フォルテ",
+    reading: ["こくよくのはしゃふぉるて", "コクヨクノハシャフォルテ", "黒翼の覇者・フォルテ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/黒翼の覇者・フォルテ.zip"
+  },
+  {
+    id: "独眼竜・伊達政宗",
+    name: "独眼竜・伊達政宗",
+    reading: ["どくがんりゅうだてまさむね", "ドクガンリュウダテマサムネ", "独眼竜・伊達政宗"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/独眼竜・伊達政宗.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "デッドソウルテイカー",
+    name: "デッドソウルテイカー",
+    reading: ["でっどそうるていかー", "デッドソウルテイカー", "デッドソウルテイカー"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/デッドソウルテイカー.zip"
+  },
+  {
+    id: "冥焔の獄犬・ケルベロス",
+    name: "冥焔の獄犬・ケルベロス",
+    reading: ["めいえんのごくけんけるべろす", "メイエンノゴクケンケルベロス", "冥焔の獄犬・ケルベロス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/冥焔の獄犬・ケルベロス.zip"
+  },
+  {
+    id: "永劫の決闘・モルディカイ",
+    name: "永劫の決闘・モルディカイ",
+    reading: ["えいごうのけっとうもるでぃかい", "エイゴウノケットウモルディカイ", "永劫の決闘・モルディカイ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/永劫の決闘・モルディカイ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "アビスドゥームロード",
+    name: "アビスドゥームロード",
+    reading: ["あびすどぅーむろーど", "アビスドゥームロード", "アビスドゥームロード"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/アビスドゥームロード.zip"
+  },
+  {
+    id: "紅き血の女王・ヴァンピィ",
+    name: "紅き血の女王・ヴァンピィ",
+    reading: ["あかきちのじょおうゔぁんぴぃ", "アカキチノジョオウヴァンピィ", "紅き血の女王・ヴァンピィ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "RGW",
+    zip: "voices/RGW/Legendary/紅き血の女王・ヴァンピィ.zip"
+  },
+  {
+    id: "月下の狼王・ヴァルツ",
+    name: "月下の狼王・ヴァルツ",
+    reading: ["げっかのおうおうゔぁるつ", "ゲッカノオウオウヴァルツ", "月下の狼王・ヴァルツ"],
+    class: "ヴァンパイア",

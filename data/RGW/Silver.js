59549a14237014ae87722a7c4318fa3133c38cb3
panini4552 <panini4552@gmail.com>
2025-12-04 21:56:44 +0900

    Update Silver.js

--- data/RGW/Silver.js
+++ data/RGW/Silver.js
@@ -1,15 +1,208 @@
-export const Classic_Legendary = [
+export const RGW_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Baby_Felper_Encourage",
+    name: "奮起のベビーフェルパー",
+    reading: ["ふんきのべびーふぇるぱー", "奮起のベビーフェルパー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/奮起のベビーフェルパー.zip"
+  },
+  {
+    id: "Owlman_Vivid",
+    name: "オウルマンビビット",
+    reading: ["おうるまんびびっと", "オウルマンビビット"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/オウルマンビビット.zip"
+  },
+  {
+    id: "Dark_Elf_Alba",
+    name: "旋律のダークエルフ・アルバ",
+    reading: ["せんりつのだーくえるふ・あるば", "旋律のダークエルフ・アルバ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/旋律のダークエルフ・アルバ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "Brave_Vanguard",
+    name: "ブレイブヴァンガード",
+    reading: ["ぶれいぶゔぁんがーど", "ブレイブヴァンガード"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/ブレイブヴァンガード.zip"
+  },
+  {
+    id: "Luminous_Mage_Light",
+    name: "光灯すルミナスメイジ",
+    reading: ["ひかりともするみなすめいじ", "光灯すルミナスメイジ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/光灯すルミナスメイジ.zip"
+  },
+  {
+    id: "Luminous_Lancer",
+    name: "ルミナスランサー",
+    reading: ["るみなすらんさー", "ルミナスランサー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/ルミナスランサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "Levi_Wisdom",
+    name: "遥かなる叡智・レヴィ",
+    reading: ["はるかなるえいち・れゔぃ", "遥かなる叡智・レヴィ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/遥かなる叡智・レヴィ.zip"
+  },
+  {
+    id: "Hanna_Manaria_Maid",
+    name: "マナリアメイド・ハンナ",
+    reading: ["まなりあめいど・はんな", "マナリアメイド・ハンナ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/マナリアメイド・ハンナ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "Twin_Dragon_Guide",
+    name: "双竜の導き手",
+    reading: ["そうりゅうのみちびきて", "双竜の導き手"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/双竜の導き手.zip"
+  },
+  {
+    id: "Dragosnake_Shell",
+    name: "堅殻のドラゴスネーク",
+    reading: ["けんかくのどらごすねーく", "堅殻のドラゴスネーク"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/堅殻のドラゴスネーク.zip"
+  },
+  {
+    id: "Katakura_Kojuro",
+    name: "片倉小十郎",
+    reading: ["かたくらこじゅうろう", "片倉小十郎"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/片倉小十郎.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "Mino_Death_God",
+    name: "空想する死神・ミーノ",
+    reading: ["くうそうするしにがみ・みーの", "空想する死神・ミーノ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/空想する死神・ミーノ.zip"
+  },
+  {
+    id: "Patrolling_Undead",
+    name: "巡回する不死者",
+    reading: ["じゅんかいするふししゃ", "巡回する不死者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/巡回する不死者.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "Soul_Mini_Devil_Dance",
+    name: "舞踏のソウルミニデビル",
+    reading: ["ぶとうのそうるみにでびる", "舞踏のソウルミニデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/舞踏のソウルミニデビル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "Sacred_Sister_Tina",
+    name: "セイクリッドシスター・ティナ",
+    reading: ["せいくりっどしすたー・てぃな", "セイクリッドシスター・ティナ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/セイクリッドシスター・ティナ.zip"
+  },
+  {
+    id: "Holy_Knight_Lucius",
+    name: "ホーリーナイト・ルシウス",
+    reading: ["ほーりーないと・るしうす", "ホーリーナイト・ルシウス"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/ホーリーナイト・ルシウス.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "Rosa_Liberation",
+    name: "解放の少女・ローザ",
+    reading: ["かいほうのしょうじょ・ろーざ", "解放の少女・ローザ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/解放の少女・ローザ.zip"
+  },
+  {
+    id: "Kasim_Brave_Boy",
+    name: "勇気の少年・カシム",
+    reading: ["ゆうきのしょうねん・かしむ", "勇気の少年・カシム"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/勇気の少年・カシム.zip"
+  },
+  {
+    id: "Variable_Maker",
+    name: "ヴァリアブルメーカー",
+    reading: ["ゔぁりあぶるめーかー", "ヴァリアブルメーカー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/ヴァリアブルメーカー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "Mini_Goblin_Mage_Ambition",
+    name: "大望のミニゴブリンメイジ",
+    reading: ["たいもうのみにごぶりんめいじ", "大望のミニゴブリンメイジ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/大望のミニゴブリンメイジ.zip"
+  },
+  {
+    id: "Archangel_Message",
+    name: "御言葉の大天使",
+    reading: ["みことばのだいてんし", "御言葉の大天使"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "RGW",
+    zip: "voices/RGW/Silver/御言葉の大天使.zip"
+  }
 ];

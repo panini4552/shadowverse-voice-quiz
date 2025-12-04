706c6ed44639c7713ec1ef7f672fdab81032da46
panini4552 <panini4552@gmail.com>
2025-12-04 21:27:16 +0900

    Update Bronze.js

--- data/OOS/Bronze.js
+++ data/OOS/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const OOS_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "不殺の隠者",
+    name: "不殺の隠者",
+    reading: ["ふさつのいんじゃ", "フサツノインジャ", "不殺の隠者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/不殺の隠者.zip"
+  },
+  {
+    id: "花束の妖精",
+    name: "花束の妖精",
+    reading: ["はなたばのようせい", "ハナタバノヨウセイ", "花束の妖精"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/花束の妖精.zip"
+  },
+  {
+    id: "ホーンビースト",
+    name: "ホーンビースト",
+    reading: ["ほーんびーすと", "ホーンビースト", "ホーンビースト"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/ホーンビースト.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "簒奪の隠者",
+    name: "簒奪の隠者",
+    reading: ["さんだつのいんじゃ", "サンダツノインジャ", "簒奪の隠者"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/簒奪の隠者.zip"
+  },
+  {
+    id: "アサルトバンデッド",
+    name: "アサルトバンデッド",
+    reading: ["あさるとばんでっど", "アサルトバンデッド", "アサルトバンデッド"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/アサルトバンデッド.zip"
+  },
+  {
+    id: "ペンギンガーディアン",
+    name: "ペンギンガーディアン",
+    reading: ["ぺんぎんがーでぃあん", "ペンギンガーディアン", "ペンギンガーディアン"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/ペンギンガーディアン.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "真実の隠者",
+    name: "真実の隠者",
+    reading: ["しんじつのいんじゃ", "シンジツノインジャ", "真実の隠者"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/真実の隠者.zip"
+  },
+  {
+    id: "氷結の魔獣",
+    name: "氷結の魔獣",
+    reading: ["ひょうけつのまじゅう", "ヒョウケツノマジュウ", "氷結の魔獣"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/氷結の魔獣.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "侮蔑の隠者",
+    name: "侮蔑の隠者",
+    reading: ["ぶべつのいんじゃ", "ブベツノインジャ", "侮蔑の隠者"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/侮蔑の隠者.zip"
+  },
+  {
+    id: "ビギナードラグーン",
+    name: "ビギナードラグーン",
+    reading: ["びぎなーどらぐーん", "ビギナードラグーン", "ビギナードラグーン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/ビギナードラグーン.zip"
+  },
+  {
+    id: "シーテンタクル",
+    name: "シーテンタクル",
+    reading: ["しーてんたくる", "シーテンタクル", "シーテンタクル"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/シーテンタクル.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "沈黙の隠者",
+    name: "沈黙の隠者",
+    reading: ["ちんもくのいんじゃ", "チンモクノインジャ", "沈黙の隠者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/沈黙の隠者.zip"
+  },
+  {
+    id: "冥府の召喚者",
+    name: "冥府の召喚者",
+    reading: ["めいふのしょうかんしゃ", "メイフノショウカンシャ", "冥府の召喚者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/冥府の召喚者.zip"
+  },
+  {
+    id: "クランプス",
+    name: "クランプス",
+    reading: ["くらんぷす", "クランプス", "クランプス"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/クランプス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "姦淫の隠者",
+    name: "姦淫の隠者",
+    reading: ["かんいんのいんじゃ", "カンインノインジャ", "姦淫の隠者"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/姦淫の隠者.zip"
+  },
+  {
+    id: "怪腕の魔剣士",
+    name: "怪腕の魔剣士",
+    reading: ["かいわんのまけんし", "カイワンノマケンシ", "怪腕の魔剣士"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/怪腕の魔剣士.zip"
+  },
+  {
+    id: "パラノイアデビル",
+    name: "パラノイアデビル",
+    reading: ["ぱらのいあでびる", "パラノイアデビル", "パラノイアデビル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/パラノイアデビル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "安息の隠者",
+    name: "安息の隠者",
+    reading: ["あんそくのいんじゃ", "アンソクノインジャ", "安息の隠者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/安息の隠者.zip"
+  },
+  {
+    id: "神殿の癒し手",
+    name: "神殿の癒し手",
+    reading: ["しんでんのいやして", "シンデンノイヤシテ", "神殿の癒し手"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/神殿の癒し手.zip"
+  },
+  {
+    id: "カラドリウス",
+    name: "カラドリウス",
+    reading: ["からどりうす", "カラドリウス", "カラドリウス"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/カラドリウス.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "破壊の隠者",
+    name: "破壊の隠者",
+    reading: ["はかいのいんじゃ", "ハカイノインジャ", "破壊の隠者"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/破壊の隠者.zip"
+  },
+  {
+    id: "ドールレディ",
+    name: "ドールレディ",
+    reading: ["どーるれでぃ", "ドールレディ", "ドールレディ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/ドールレディ.zip"
+  },
+  {
+    id: "メカニカルアナライザー",
+    name: "メカニカルアナライザー",
+    reading: ["めかにかるあならいざー", "メカニカルアナライザー", "メカニカルアナライザー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/メカニカルアナライザー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "流浪の指揮者",
+    name: "流浪の指揮者",
+    reading: ["るろうのしきしゃ", "ルロウノシキシャ", "流浪の指揮者"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/流浪の指揮者.zip"
+  },
+  {
+    id: "マウンテンギガース",
+    name: "マウンテンギガース",
+    reading: ["まうんてんぎがーす", "マウンテンギガース", "マウンテンギガース"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "OOS",
+    zip: "voices/OOS/Bronze/マウンテンギガース.zip"
+  }
 ];

cf8394ae56dceab4e3c10d57fc6a1dbbb35987a9
panini4552 <panini4552@gmail.com>
2025-12-04 21:11:59 +0900

    Update Silver.js

--- data/DOC/Silver.js
+++ data/DOC/Silver.js
@@ -1,15 +1,206 @@
-export const Classic_Legendary = [
-  // === エルフ ===
-  {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
-    class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
-  },
+// === エルフ ===
+{
+  id: "領域のルナール",
+  name: "領域のルナール",
+  reading: ["りょういきのるなーる", "領域のルナール"],
+  class: "エルフ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/領域のルナール.zip"
+},
+{
+  id: "ブリーズエルフ",
+  name: "ブリーズエルフ",
+  reading: ["ぶりーずえるふ", "ブリーズエルフ"],
+  class: "エルフ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/ブリーズエルフ.zip"
+},
+{
+  id: "妖花の捕食者",
+  name: "妖花の捕食者",
+  reading: ["ようかのほしょくしゃ", "妖花の捕食者"],
+  class: "エルフ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/妖花の捕食者.zip"
+},
 
+// === ロイヤル ===
+{
+  id: "星降りの姫君",
+  name: "星降りの姫君",
+  reading: ["ほしふりのひめぎみ", "星降りの姫君"],
+  class: "ロイヤル",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/星降りの姫君.zip"
+},
+{
+  id: "レイピアダンディ",
+  name: "レイピアダンディ",
+  reading: ["れいぴあだんでぃ", "レイピアダンディ"],
+  class: "ロイヤル",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/レイピアダンディ.zip"
+},
+{
+  id: "二刀の騎士",
+  name: "二刀の騎士",
+  reading: ["にとうのきし", "二刀の騎士"],
+  class: "ロイヤル",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/二刀の騎士.zip"
+},
 
+// === ウィッチ ===
+{
+  id: "炸裂の魔女",
+  name: "炸裂の魔女",
+  reading: ["さくれつのまじょ", "炸裂の魔女"],
+  class: "ウィッチ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/炸裂の魔女.zip"
+},
+{
+  id: "氷塊のゴーレム",
+  name: "氷塊のゴーレム",
+  reading: ["ひょうかいのごーれむ", "氷塊のゴーレム"],
+  class: "ウィッチ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/氷塊のゴーレム.zip"
+},
 
-];
+// === ドラゴン ===
+{
+  id: "オーシャンスピリット",
+  name: "オーシャンスピリット",
+  reading: ["おーしゃんすぴりっと", "オーシャンスピリット"],
+  class: "ドラゴン",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/オーシャンスピリット.zip"
+},
+{
+  id: "氷翼の賢竜",
+  name: "氷翼の賢竜",
+  reading: ["ひょうよくのけんりゅう", "氷翼の賢竜"],
+  class: "ドラゴン",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/氷翼の賢竜.zip"
+},
+
+// === ネクロマンサー ===
+{
+  id: "怨恨の収集者",
+  name: "怨恨の収集者",
+  reading: ["えんこんのしゅうしゅうしゃ", "怨恨の収集者"],
+  class: "ネクロマンサー",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/怨恨の収集者.zip"
+},
+{
+  id: "静謐の墓守",
+  name: "静謐の墓守",
+  reading: ["せいひつのはかもり", "静謐の墓守"],
+  class: "ネクロマンサー",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/静謐の墓守.zip"
+},
+
+// === ヴァンパイア ===
+{
+  id: "悪魔の令嬢",
+  name: "悪魔の令嬢",
+  reading: ["あくまのれいじょう", "悪魔の令嬢"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/悪魔の令嬢.zip"
+},
+{
+  id: "まどわしの魔狼",
+  name: "まどわしの魔狼",
+  reading: ["まどわしのまろう", "まどわしの魔狼"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/まどわしの魔狼.zip"
+},
+{
+  id: "乱舞の獣人",
+  name: "乱舞の獣人",
+  reading: ["らんぶのじゅうじん", "乱舞の獣人"],
+  class: "ヴァンパイア",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/乱舞の獣人.zip"
+},
+
+// === ビショップ ===
+{
+  id: "ユルルング",
+  name: "ユルルング",
+  reading: ["ゆるるんぐ", "ユルルング"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/ユルルング.zip"
+},
+{
+  id: "密かなる代行者",
+  name: "密かなる代行者",
+  reading: ["ひそかなるだいこうしゃ", "密かなる代行者"],
+  class: "ビショップ",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/密かなる代行者.zip"
+},
+
+// === ネメシス ===
+{
+  id: "フェイクマリオネッター",
+  name: "フェイクマリオネッター",
+  reading: ["ふぇいくまりおねったー", "フェイクマリオネッター"],
+  class: "ネメシス",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/フェイクマリオネッター.zip"
+},
+{
+  id: "鉄翼のエクスプローラー",
+  name: "鉄翼のエクスプローラー",
+  reading: ["てつよくのえくすぷろーらー", "鉄翼のエクスプローラー"],
+  class: "ネメシス",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/鉄翼のエクスプローラー.zip"
+},
+{
+  id: "多腕のアーティファクト",
+  name: "多腕のアーティファクト",
+  reading: ["たうでのあーてぃふぁくと", "多腕のアーティファクト"],
+  class: "ネメシス",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/多腕のアーティファクト.zip"
+},
+
+// === ニュートラル ===
+{
+  id: "新鋭の花火師・マーヴェラ",
+  name: "新鋭の花火師・マーヴェラ",
+  reading: ["しんえいのはなびし・まーゔぇら", "新鋭の花火師・マーヴェラ"],
+  class: "ニュートラル",
+  rarity: "Silver",
+  pack: "DOC",
+  zip: "voices/DOC/Silver/新鋭の花火師・マーヴェラ.zip"
+}

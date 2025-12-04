8c3a203061c5baa57a00ceedac49c7e28d6c21e7
panini4552 <panini4552@gmail.com>
2025-12-04 21:41:08 +0900

    Update Bronze.js

--- data/EOP/Bronze.js
+++ data/EOP/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const EOP_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "提灯のフェアリー",
+    name: "提灯のフェアリー",
+    reading: ["ちょうちんのふぇありー", "チョウチンノフェアリー", "提灯のフェアリー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/提灯のフェアリー.zip"
+  },
+  {
+    id: "ガーデナー・カルクス",
+    name: "ガーデナー・カルクス",
+    reading: ["がーでなーかるくす", "ガーデナーカルクス", "ガーデナー・カルクス"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/ガーデナー・カルクス.zip"
+  },
+  {
+    id: "ウィンドランサー",
+    name: "ウィンドランサー",
+    reading: ["うぃんどらんさー", "ウィンドランサー", "ウィンドランサー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/ウィンドランサー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "帳場ガエル",
+    name: "帳場ガエル",
+    reading: ["ちょうばがえる", "チョウバガエル", "帳場ガエル"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/帳場ガエル.zip"
+  },
+  {
+    id: "バイオレンスソルジャー",
+    name: "バイオレンスソルジャー",
+    reading: ["ばいおれんすすおるじゃー", "バイオレンスソルジャー", "バイオレンスソルジャー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/バイオレンスソルジャー.zip"
+  },
+  {
+    id: "拙速の斧使い",
+    name: "拙速の斧使い",
+    reading: ["せっそくのおのつかい", "セッソクノオノツカイ", "拙速の斧使い"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/拙速の斧使い.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "宿帳のフクロウ",
+    name: "宿帳のフクロウ",
+    reading: ["やどちょうのふくろう", "ヤドチョウノフクロウ", "宿帳のフクロウ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/宿帳のフクロウ.zip"
+  },
+  {
+    id: "チャクラムウィザード",
+    name: "チャクラムウィザード",
+    reading: ["ちゃくらむうぃざーど", "チャクラムウィザード", "チャクラムウィザード"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/チャクラムウィザード.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "送迎の走竜",
+    name: "送迎の走竜",
+    reading: ["そうげいのそうりゅう", "ソウゲイノソウリュウ", "送迎の走竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/送迎の走竜.zip"
+  },
+  {
+    id: "献身の水竜",
+    name: "献身の水竜",
+    reading: ["けんしんのすいりゅう", "ケンシンノスイリュウ", "献身の水竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/献身の水竜.zip"
+  },
+  {
+    id: "ドラゴンブリーダー",
+    name: "ドラゴンブリーダー",
+    reading: ["どらごんぶりーだー", "ドラゴンブリーダー", "ドラゴンブリーダー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/ドラゴンブリーダー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "御宿の幽霊犬",
+    name: "御宿の幽霊犬",
+    reading: ["おんやどのゆうれいけん", "オンヤドノユウレイケン", "御宿の幽霊犬"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/御宿の幽霊犬.zip"
+  },
+  {
+    id: "ウィップネクロマンサー",
+    name: "ウィップネクロマンサー",
+    reading: ["うぃっぷねくろまんさー", "ウィップネクロマンサー", "ウィップネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/ウィップネクロマンサー.zip"
+  },
+  {
+    id: "首無しの戦鬼",
+    name: "首無しの戦鬼",
+    reading: ["くびなしのせんき", "クビナシノセンキ", "首無しの戦鬼"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/首無しの戦鬼.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ドアマンバット",
+    name: "ドアマンバット",
+    reading: ["どあまんばっと", "ドアマンバット", "ドアマンバット"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/ドアマンバット.zip"
+  },
+  {
+    id: "銀釘の射手",
+    name: "銀釘の射手",
+    reading: ["ぎんくぎのいして", "ギンクギノイシテ", "銀釘の射手"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/銀釘の射手.zip"
+  },
+  {
+    id: "パルクールウルフ",
+    name: "パルクールウルフ",
+    reading: ["ぱるくーるうるふ", "パルクールウルフ", "パルクールウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/パルクールウルフ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "福呼びの狐",
+    name: "福呼びの狐",
+    reading: ["ふくよびのきつね", "フクヨビノキツネ", "福呼びの狐"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/福呼びの狐.zip"
+  },
+  {
+    id: "アルミラージディフェンサー",
+    name: "アルミラージディフェンサー",
+    reading: ["あるみらーじでぃふぇんさー", "アルミラージディフェンサー", "アルミラージディフェンサー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/アルミラージディフェンサー.zip"
+  },
+  {
+    id: "双刃のフェザーフォルク",
+    name: "双刃のフェザーフォルク",
+    reading: ["そうじんのふぇざーふぉるく", "ソウジンノフェザーフォルク", "双刃のフェザーフォルク"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/双刃のフェザーフォルク.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "絡繰り給仕",
+    name: "絡繰り給仕",
+    reading: ["からくりきゅうじ", "カラクリキュウジ", "絡繰り給仕"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/絡繰り給仕.zip"
+  },
+  {
+    id: "モータージーニアス",
+    name: "モータージーニアス",
+    reading: ["もーたーじーにあす", "モータージーニアス", "モータージーニアス"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/モータージーニアス.zip"
+  },
+  {
+    id: "クラウンドール",
+    name: "クラウンドール",
+    reading: ["くらうんどーる", "クラウンドール", "クラウンドール"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/クラウンドール.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "照明の案内人",
+    name: "照明の案内人",
+    reading: ["しょうめいのあんないにん", "ショウメイノアンナイニン", "照明の案内人"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/照明の案内人.zip"
+  },
+  {
+    id: "劫火の巨人",
+    name: "劫火の巨人",
+    reading: ["ごうかのきょじん", "ゴウカノキョジン", "劫火の巨人"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "EOP",
+    zip: "voices/EOP/Bronze/劫火の巨人.zip"
+  }
 ];

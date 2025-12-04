ec52a5aac3f75966fe7ad740e7c99b8848a4fbeb
panini4552 <panini4552@gmail.com>
2025-12-04 21:37:45 +0900

    Update Silver.js

--- data/EOP/Silver.js
+++ data/EOP/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+export const EOP_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "森の遊撃者",
+    name: "森の遊撃者",
+    reading: ["もりのゆうげきしゃ", "モリノユウゲキシャ", "森の遊撃者"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/森の遊撃者.zip"
+  },
+  {
+    id: "仲居のエルフ",
+    name: "仲居のエルフ",
+    reading: ["なかいのえるふ", "ナカイノエルフ", "仲居のエルフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/仲居のエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "達人の芸者",
+    name: "達人の芸者",
+    reading: ["たつじんのげいしゃ", "タツジンノゲイシャ", "達人の芸者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/達人の芸者.zip"
+  },
+  {
+    id: "辺境の騎士見習い",
+    name: "辺境の騎士見習い",
+    reading: ["へんきょうのきしみならい", "ヘンキョウノキシミナライ", "辺境の騎士見習い"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/辺境の騎士見習い.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "管弦の魔術師",
+    name: "管弦の魔術師",
+    reading: ["かんげんのまじゅつし", "カンゲンノマジュツシ", "管弦の魔術師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/管弦の魔術師.zip"
+  },
+  {
+    id: "ルインソーサラー",
+    name: "ルインソーサラー",
+    reading: ["るいんそーさらー", "ルインソーサラー", "ルインソーサラー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/ルインソーサラー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "トロピカルマーメイド",
+    name: "トロピカルマーメイド",
+    reading: ["とろぴかるまーめいど", "トロピカルマーメイド", "トロピカルマーメイド"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/トロピカルマーメイド.zip"
+  },
+  {
+    id: "釜焚きの竜人",
+    name: "釜焚きの竜人",
+    reading: ["かまたきのりゅうじん", "カマタキノリュウジン", "釜焚きの竜人"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/釜焚きの竜人.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥果の収穫者",
+    name: "冥果の収穫者",
+    reading: ["めいかのしゅうかくしゃ", "メイカノシュウカクシャ", "冥果の収穫者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/冥果の収穫者.zip"
+  },
+  {
+    id: "スカルケンタウロス",
+    name: "スカルケンタウロス",
+    reading: ["すかるけんたうろす", "スカルケンタウロス", "スカルケンタウロス"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/スカルケンタウロス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ルームサービスデビル",
+    name: "ルームサービスデビル",
+    reading: ["るーむさーびすでびる", "ルームサービスデビル", "ルームサービスデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/ルームサービスデビル.zip"
+  },
+  {
+    id: "沼地の大蠍",
+    name: "沼地の大蠍",
+    reading: ["ぬまちのおおさそり", "ヌマチノオオサソリ", "沼地の大蠍"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/沼地の大蠍.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ペガサスナイト",
+    name: "ペガサスナイト",
+    reading: ["ぺがさすないと", "ペガサスナイト", "ペガサスナイト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/ペガサスナイト.zip"
+  },
+  {
+    id: "警策の翼人",
+    name: "警策の翼人",
+    reading: ["けいさくのよくじん", "ケイサクノヨクジン", "警策の翼人"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/警策の翼人.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ねじまき売り子",
+    name: "ねじまき売り子",
+    reading: ["ねじまきうりこ", "ネジマキウリコ", "ねじまき売り子"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/ねじまき売り子.zip"
+  },
+  {
+    id: "機構の撃ち手",
+    name: "機構の撃ち手",
+    reading: ["きこうのうちて", "キコウノウチテ", "機構の撃ち手"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/機構の撃ち手.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリンブレイバー",
+    name: "ゴブリンブレイバー",
+    reading: ["ごぶりんぶれいばー", "ゴブリンブレイバー", "ゴブリンブレイバー"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "EOP",
+    zip: "voices/EOP/Silver/ゴブリンブレイバー.zip"
+  }
 ];

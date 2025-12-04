942eabe3f6a6b1c80c95ccb79758670c267ec01f
panini4552 <panini4552@gmail.com>
2025-12-04 21:36:01 +0900

    Update Gold.js

--- data/EOP/Gold.js
+++ data/EOP/Gold.js
@@ -1,15 +1,181 @@
-export const Classic_Legendary = [
+export const EOP_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "清泉のスピリット",
+    name: "清泉のスピリット",
+    reading: ["せいせんのすぴりっと", "セイセンノスピリット", "清泉のスピリット"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/清泉のスピリット.zip"
+  },
+  {
+    id: "妖精の貴公子",
+    name: "妖精の貴公子",
+    reading: ["ようせいのきこうし", "ヨウセイノキコウシ", "妖精の貴公子"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/妖精の貴公子.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "レパードグラップラー",
+    name: "レパードグラップラー",
+    reading: ["れぱーどぐらっぷらー", "レパードグラップラー", "レパードグラップラー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/レパードグラップラー.zip"
+  },
+  {
+    id: "ノーブルガーディアン",
+    name: "ノーブルガーディアン",
+    reading: ["のーぶるがーでぃあん", "ノーブルガーディアン", "ノーブルガーディアン"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/ノーブルガーディアン.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "雷雲のアルケミスト",
+    name: "雷雲のアルケミスト",
+    reading: ["らいうんのあるけみすと", "ライウンノアルケミスト", "雷雲のアルケミスト"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/雷雲のアルケミスト.zip"
+  },
+  {
+    id: "リミットスペル・バーゼント",
+    name: "リミットスペル・バーゼント",
+    reading: ["りみっとすぺる・ばーぜんと", "リミットスペルバーゼント", "リミットスペル・バーゼント"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/リミットスペル・バーゼント.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "氷蝕のドラゴン",
+    name: "氷蝕のドラゴン",
+    reading: ["ひょうしょくのどらごん", "ヒョウショクノドラゴン", "氷蝕のドラゴン"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/氷蝕のドラゴン.zip"
+  },
+  {
+    id: "竜頭のスレイヤー",
+    name: "竜頭のスレイヤー",
+    reading: ["りゅうずのすれいやー", "リュウズノスレイヤー", "竜頭のスレイヤー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/竜頭のスレイヤー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "寒冷の死霊術師",
+    name: "寒冷の死霊術師",
+    reading: ["かんれいのしりょうじゅつし", "カンレイノシリョウジュツシ", "寒冷の死霊術師"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/寒冷の死霊術師.zip"
+  },
+  {
+    id: "亡き街の大幽霊",
+    name: "亡き街の大幽霊",
+    reading: ["なきまちのだいゆうれい", "ナキマチノダイユウレイ", "亡き街の大幽霊"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/亡き街の大幽霊.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "変幻のフェイカー",
+    name: "変幻のフェイカー",
+    reading: ["へんげんのふぇいかー", "ヘンゲンノフェイカー", "変幻のフェイカー"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/変幻のフェイカー.zip"
+  },
+  {
+    id: "デビルガーディアン",
+    name: "デビルガーディアン",
+    reading: ["でびるがーでぃあん", "デビルガーディアン", "デビルガーディアン"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/デビルガーディアン.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ブーメランシスター",
+    name: "ブーメランシスター",
+    reading: ["ぶーめらんしすたー", "ブーメランシスター", "ブーメランシスター"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/ブーメランシスター.zip"
+  },
+  {
+    id: "淀みの神父",
+    name: "淀みの神父",
+    reading: ["よどみのしんぷ", "ヨドミノシンプ", "淀みの神父"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/淀みの神父.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "トリガーマリオネッター",
+    name: "トリガーマリオネッター",
+    reading: ["とりがーまりおねったー", "トリガーマリオネッター", "トリガーマリオネッター"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/トリガーマリオネッター.zip"
+  },
+  {
+    id: "セレスティアルアーティファクト",
+    name: "セレスティアルアーティファクト",
+    reading: ["せれすてぃあるあーてぃふぁくと", "セレスティアルアーティファクト", "セレスティアルアーティファクト"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/セレスティアルアーティファクト.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ウェポンオーガ",
+    name: "ウェポンオーガ",
+    reading: ["うぇぽんおーが", "ウェポンオーガ", "ウェポンオーガ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/ウェポンオーガ.zip"
+  },
+  {
+    id: "グランスエンジェル",
+    name: "グランスエンジェル",
+    reading: ["ぐらんすえんじぇる", "グランスエンジェル", "グランスエンジェル"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "EOP",
+    zip: "voices/EOP/Gold/グランスエンジェル.zip"
+  }
 ];

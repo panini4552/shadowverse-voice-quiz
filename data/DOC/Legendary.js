ab233d5ffda03c59c7a2eb7fce5148fbad9f4dfe
panini4552 <panini4552@gmail.com>
2025-12-04 21:06:54 +0900

    Update Legendary.js

--- data/DOC/Legendary.js
+++ data/DOC/Legendary.js
@@ -1,15 +1,226 @@
-export const Classic_Legendary = [
+export const DOC_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ブリリアントフェアリー",
+    name: "ブリリアントフェアリー",
+    reading: ["ぶりりあんとふぇありー", "ブリリアントフェアリー", "ブリリアントフェアリー"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ブリリアントフェアリー.zip"
+  },
+  {
+    id: "デッドリーエルフ",
+    name: "デッドリーエルフ",
+    reading: ["でっどりーえるふ", "デッドリーエルフ", "デッドリーエルフ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/デッドリーエルフ.zip"
+  },
+  {
+    id: "アイヴィーキング",
+    name: "アイヴィーキング",
+    reading: ["あいゔぃーきんぐ", "アイヴィーキング", "アイヴィーキング"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/アイヴィーキング.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ビクトリーブレイダー",
+    name: "ビクトリーブレイダー",
+    reading: ["びくとりーぶれいだー", "ビクトリーブレイダー", "ビクトリーブレイダー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ビクトリーブレイダー.zip"
+  },
+  {
+    id: "ゴールデンウォーリアー",
+    name: "ゴールデンウォーリアー",
+    reading: ["ごーるでんうぉーりあー", "ゴールデンウォーリアー", "ゴールデンウォーリアー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ゴールデンウォーリアー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "クロノウィッチ",
+    name: "クロノウィッチ",
+    reading: ["くろのうぃっち", "クロノウィッチ", "クロノウィッチ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/クロノウィッチ.zip"
+  },
+  {
+    id: "プレデターゴーレム",
+    name: "プレデターゴーレム",
+    reading: ["ぷれでたーごーれむ", "プレデターゴーレム", "プレデターゴーレム"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/プレデターゴーレム.zip"
+  },
+  {
+    id: "スペリオルコントラクター",
+    name: "スペリオルコントラクター",
+    reading: ["すぺりおるこんとらくたー", "スペリオルコントラクター", "スペリオルコントラクター"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/スペリオルコントラクター.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "イグニスドラゴン",
+    name: "イグニスドラゴン",
+    reading: ["いぐにすどらごん", "イグニスドラゴン", "イグニスドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/イグニスドラゴン.zip"
+  },
+  {
+    id: "天威のドラグーン",
+    name: "天威のドラグーン",
+    reading: ["てんいのどらぐーん", "テンイノドラグーン", "天威のドラグーン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/天威のドラグーン.zip"
+  },
+  {
+    id: "ブルータルドラゴニュート",
+    name: "ブルータルドラゴニュート",
+    reading: ["ぶるーたるどらごにゅーと", "ブルータルドラゴニュート", "ブルータルドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ブルータルドラゴニュート.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "スケルトンレイダー",
+    name: "スケルトンレイダー",
+    reading: ["すけるとんれいだー", "スケルトンレイダー", "スケルトンレイダー"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/スケルトンレイダー.zip"
+  },
+  {
+    id: "マッドネスリーパー",
+    name: "マッドネスリーパー",
+    reading: ["まっどねすりーぱー", "マッドネスリーパー", "マッドネスリーパー"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/マッドネスリーパー.zip"
+  },
+  {
+    id: "ケルヌンノス",
+    name: "ケルヌンノス",
+    reading: ["けるぬんのす", "ケルヌンノス", "ケルヌンノス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ケルヌンノス.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ダークエンペラー",
+    name: "ダークエンペラー",
+    reading: ["だーくえんぺらー", "ダークエンペラー", "ダークエンペラー"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ダークエンペラー.zip"
+  },
+  {
+    id: "夜天の吸血鬼",
+    name: "夜天の吸血鬼",
+    reading: ["やてんのきゅうけつき", "ヤテンノキュウケツキ", "夜天の吸血鬼"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/夜天の吸血鬼.zip"
+  },
+  {
+    id: "ブラッディデーモン",
+    name: "ブラッディデーモン",
+    reading: ["ぶらっでぃでーもん", "ブラッディデーモン", "ブラッディデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ブラッディデーモン.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ゴッド・オブ・カース",
+    name: "ゴッド・オブ・カース",
+    reading: ["ごっどおぶかーす", "ゴッドオブカース", "ゴッド・オブ・カース"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ゴッド・オブ・カース.zip"
+  },
+  {
+    id: "ホーリーセイバー",
+    name: "ホーリーセイバー",
+    reading: ["ほーりーせいばー", "ホーリーセイバー", "ホーリーセイバー"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ホーリーセイバー.zip"
+  },
+  {
+    id: "セイクリッドレオ",
+    name: "セイクリッドレオ",
+    reading: ["せいくりっどれお", "セイクリッドレオ", "セイクリッドレオ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/セイクリッドレオ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ジェネシスアーティファクト",
+    name: "ジェネシスアーティファクト",
+    reading: ["じぇねしすあーてぃふぁくと", "ジェネシスアーティファクト", "ジェネシスアーティファクト"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ジェネシスアーティファクト.zip"
+  },
+  {
+    id: "モノリスクリエイター",
+    name: "モノリスクリエイター",
+    reading: ["ものりすくりえいたー", "モノリスクリエイター", "モノリスクリエイター"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/モノリスクリエイター.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ユピテル",
+    name: "ユピテル",
+    reading: ["ゆぴてる", "ユピテル", "ユピテル"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "DOC",
+    zip: "voices/DOC/Legendary/ユピテル.zip"
+  }
 ];

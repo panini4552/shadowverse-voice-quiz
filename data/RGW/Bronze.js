8dca881bd08db2d3cb20526c2f5e9390254acca8
panini4552 <panini4552@gmail.com>
2025-12-04 21:59:11 +0900

    Update Bronze.js

--- data/RGW/Bronze.js
+++ data/RGW/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const RGW_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "Leafman_Autumn",
+    name: "紅葉のリーフマン",
+    reading: ["もみじのりーふまん", "紅葉のリーフマン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/紅葉のリーフマン.zip"
+  },
+  {
+    id: "Machete_Elf",
+    name: "マチェットエルフ",
+    reading: ["まちぇっとえるふ", "マチェットエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/マチェットエルフ.zip"
+  },
+  {
+    id: "Hornet_User",
+    name: "ホーネットユーザー",
+    reading: ["ほーねっとゆーざー", "ホーネットユーザー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ホーネットユーザー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "Great_Thief",
+    name: "グレートシーフ",
+    reading: ["ぐれーとしーふ", "グレートシーフ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/グレートシーフ.zip"
+  },
+  {
+    id: "Luminous_Healer_Rilala",
+    name: "ルミナスヒーラー・リララ",
+    reading: ["るみなすひーらー・りらら", "ルミナスヒーラー・リララ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ルミナスヒーラー・リララ.zip"
+  },
+  {
+    id: "Sergeant_Cat",
+    name: "サージェントキャット",
+    reading: ["さーじぇんときゃっと", "サージェントキャット"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/サージェントキャット.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "Wisdom_Owl",
+    name: "ウィズダムオウル",
+    reading: ["うぃずだむおうる", "ウィズダムオウル"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ウィズダムオウル.zip"
+  },
+  {
+    id: "Elegant_Witch",
+    name: "エレガントウィッチ",
+    reading: ["えれがんとうぃっち", "エレガントウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/エレガントウィッチ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "Dragon_Rider_Slayer",
+    name: "撃墜の竜騎",
+    reading: ["げきついのりゅうき", "撃墜の竜騎"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/撃墜の竜騎.zip"
+  },
+  {
+    id: "Martial_Dragonewt",
+    name: "武闘の竜人",
+    reading: ["ぶとうのりゅうじん", "武闘の竜人"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/武闘の竜人.zip"
+  },
+  {
+    id: "Hail_Dragon",
+    name: "ヘイルドラゴン",
+    reading: ["へいるどらごん", "ヘイルドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ヘイルドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "Abyss_Attendant",
+    name: "深淵の従者",
+    reading: ["しんえんのじゅうしゃ", "深淵の従者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/深淵の従者.zip"
+  },
+  {
+    id: "Graceful_Ghost",
+    name: "麗墓の幽霊",
+    reading: ["れいぼのゆうれい", "麗墓の幽霊"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/麗墓の幽霊.zip"
+  },
+  {
+    id: "Dead_Buyer",
+    name: "デッドバイヤー",
+    reading: ["でっどばいやー", "デッドバイヤー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/デッドバイヤー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "Devious_Spider_Demon",
+    name: "狡蜘の悪魔",
+    reading: ["こうちのあくま", "狡蜘の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/狡蜘の悪魔.zip"
+  },
+  {
+    id: "Expert_Devil_Hunter",
+    name: "熟練のデビルハンター",
+    reading: ["じゅくれんのでびるはんたー", "熟練のデビルハンター"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/熟練のデビルハンター.zip"
+  },
+  {
+    id: "Rampage_Evil",
+    name: "狂騒の悪鬼",
+    reading: ["きょうそうのあっき", "狂騒の悪鬼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/狂騒の悪鬼.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "Priest_of_Judgement",
+    name: "天罰の司祭",
+    reading: ["てんばつのしさい", "天罰の司祭"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/天罰の司祭.zip"
+  },
+  {
+    id: "Masked_Nun",
+    name: "仮面の修道女",
+    reading: ["かめんのしゅうどうじょ", "仮面の修道女"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/仮面の修道女.zip"
+  },
+  {
+    id: "Zlatolok",
+    name: "ズラトロク",
+    reading: ["ずらとろく", "ズラトロク"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ズラトロク.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "Doll_in_Garden",
+    name: "花園に咲く人形",
+    reading: ["はなぞのにさくにんぎょう", "花園に咲く人形"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/花園に咲く人形.zip"
+  },
+  {
+    id: "Water_Gunner",
+    name: "ウォーターガンナー",
+    reading: ["うぉーたーがんなー", "ウォーターガンナー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ウォーターガンナー.zip"
+  },
+  {
+    id: "Mechanical_Dog",
+    name: "メカニカルドッグ",
+    reading: ["めかにかるどっぐ", "メカニカルドッグ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/メカニカルドッグ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "Journey_Goblin",
+    name: "ジャーニーゴブリン",
+    reading: ["じゃーにーごぶりん", "ジャーニーゴブリン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/ジャーニーゴブリン.zip"
+  },
+  {
+    id: "Angel_of_Gospel",
+    name: "福音の天使",
+    reading: ["ふくいんのてんし", "福音の天使"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RGW",
+    zip: "voices/RGW/Bronze/福音の天使.zip"
+  }
 ];

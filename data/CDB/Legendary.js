cd804a18eef2c02d2def15a37e520c846967e9a4
panini4552 <panini4552@gmail.com>
2025-12-06 13:06:10 +0900

    Update Legendary.js

--- data/CDB/Legendary.js
+++ data/CDB/Legendary.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const CDB_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "コスモスファング",
+    name: "コスモスファング",
+    reading: ["こすもすふぁんぐ", "コスモスファング", "コスモスファング"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/コスモスファング.zip"
+  },
+  {
+    id: "黒百合の女王・ノワリス",
+    name: "黒百合の女王・ノワリス",
+    reading: ["くろゆりのじょおうのわりす", "クロユリノジョオウノワリス", "黒百合の女王・ノワリス"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/黒百合の女王・ノワリス.zip"
+  },
+  {
+    id: "ベネディクションエルフ",
+    name: "ベネディクションエルフ",
+    reading: ["べねでぃくしょんえるふ", "ベネディクションエルフ", "ベネディクションエルフ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ベネディクションエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ヴァリアントフェンサー",
+    name: "ヴァリアントフェンサー",
+    reading: ["ゔぁりあんとふぇんさー", "ヴァリアントフェンサー", "ヴァリアントフェンサー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ヴァリアントフェンサー.zip"
+  },
+  {
+    id: "千金武装の大参謀",
+    name: "千金武装の大参謀",
+    reading: ["せんきんぶそうのだいさんぼう", "センキンブソウノダイサンボウ", "千金武装の大参謀"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/千金武装の大参謀.zip"
+  },
+  {
+    id: "マキシマムジェネラル",
+    name: "マキシマムジェネラル",
+    reading: ["まきしまむじぇねらる", "マキシマムジェネラル", "マキシマムジェネラル"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/マキシマムジェネラル.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ミスティックキング",
+    name: "ミスティックキング",
+    reading: ["みすてぃっくきんぐ", "ミスティックキング", "ミスティックキング"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ミスティックキング.zip"
+  },
+  {
+    id: "万能の奇跡・メルティーナ",
+    name: "万能の奇跡・メルティーナ",
+    reading: ["ばんのうのきせきめるてぃーな", "バンノウノキセキメルティーナ", "万能の奇跡・メルティーナ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/万能の奇跡・メルティーナ.zip"
+  },
+  {
+    id: "アストロジカルソーサラー",
+    name: "アストロジカルソーサラー",
+    reading: ["あすとろじかるそーさらー", "アストロジカルソーサラー", "アストロジカルソーサラー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/アストロジカルソーサラー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "レーヴァテインドラゴン",
+    name: "レーヴァテインドラゴン",
+    reading: ["れーゔぁていんどらごん", "レーヴァテインドラゴン", "レーヴァテインドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/レーヴァテインドラゴン.zip"
+  },
+  {
+    id: "キャプテン・ドラグウッド",
+    name: "キャプテン・ドラグウッド",
+    reading: ["きゃぷてんどらぐうっど", "キャプテンドラグウッド", "キャプテン・ドラグウッド"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/キャプテン・ドラグウッド.zip"
+  },
+  {
+    id: "金色の威信・リュミオール",
+    name: "金色の威信・リュミオール",
+    reading: ["きんいろのいしんりゅみおーる", "キンイロノイシンリュミオール", "金色の威信・リュミオール"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/金色の威信・リュミオール.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "マスカレードゴースト",
+    name: "マスカレードゴースト",
+    reading: ["ますかれーどごーすと", "マスカレードゴースト", "マスカレードゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/マスカレードゴースト.zip"
+  },
+  {
+    id: "高貴の霊使・トゥルシェラ",
+    name: "高貴の霊使・トゥルシェラ",
+    reading: ["こうきのれいつかいとぅるしぇら", "コウキノレイツカイトゥルシェラ", "高貴の霊使・トゥルシェラ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/高貴の霊使・トゥルシェラ.zip"
+  },
+  {
+    id: "デスサイズハウンド",
+    name: "デスサイズハウンド",
+    reading: ["ですさいずはうんど", "デスサイズハウンド", "デスサイズハウンド"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/デスサイズハウンド.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ハウリングデーモン",
+    name: "ハウリングデーモン",
+    reading: ["はうりんぐでーもん", "ハウリングデーモン", "ハウリングデーモン"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ハウリングデーモン.zip"
+  },
+  {
+    id: "デモニックキャット・トバリ",
+    name: "デモニックキャット・トバリ",
+    reading: ["でもにっくきゃっととばり", "デモニックキャットトバリ", "デモニックキャット・トバリ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/デモニックキャット・トバリ.zip"
+  },
+  {
+    id: "劇毒の魔風・パズズ",
+    name: "劇毒の魔風・パズズ",
+    reading: ["げきどくのまふうぱずず", "ゲキドクノマフウパズズ", "劇毒の魔風・パズズ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/劇毒の魔風・パズズ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ダイヤモンドマスター",
+    name: "ダイヤモンドマスター",
+    reading: ["だいやもんどますたー", "ダイヤモンドマスター", "ダイヤモンドマスター"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ダイヤモンドマスター.zip"
+  },
+  {
+    id: "雷導の神鳥",
+    name: "雷導の神鳥",
+    reading: ["らいどうのしんちょう", "ライドウノシンチョウ", "雷導の神鳥"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/雷導の神鳥.zip"
+  },
+  {
+    id: "ムーンパラディン・アストルフォ",
+    name: "ムーンパラディン・アストルフォ",
+    reading: ["むーんぱらでぃんあすとるふぉ", "ムーンパラディンアストルフォ", "ムーンパラディン・アストルフォ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ムーンパラディン・アストルフォ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "アブソリュート・チェイスト",
+    name: "アブソリュート・チェイスト",
+    reading: ["あぶそりゅーとちぇいすと", "アブソリュートチェイスト", "アブソリュート・チェイスト"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/アブソリュート・チェイスト.zip"
+  },
+  {
+    id: "人造の代弁者・ヴァーミディア",
+    name: "人造の代弁者・ヴァーミディア",
+    reading: ["じんぞうのだいべんしゃゔぁーみでぃあ", "ジンゾウノダイベンシャヴァーミディア", "人造の代弁者・ヴァーミディア"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/人造の代弁者・ヴァーミディア.zip"
+  },
+  {
+    id: "万識を超えたもの",
+    name: "万識を超えたもの",
+    reading: ["ばんしきをこえたもの", "バンシキヲコエタモノ", "万識を超えたもの"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/万識を超えたもの.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ウーラノス",
+    name: "ウーラノス",
+    reading: ["うーらのす", "ウーラノス", "ウーラノス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/ウーラノス.zip"
+  },
+  {
+    id: "箱庭の追憶・オリヴィエ＆シルヴィア",
+    name: "箱庭の追憶・オリヴィエ＆シルヴィア",
+    reading: ["はこにわのついおくおりゔぃえあんどしるゔぃあ", "ハコニワノツイオクオリヴィエアンドシルヴィア", "箱庭の追憶・オリヴィエ＆シルヴィア"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "CDB",
+    zip: "voices/CDB/Legendary/箱庭の追憶・オリヴィエ＆シルヴィア.zip"
+  }
 ];

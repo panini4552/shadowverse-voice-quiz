2cb89eb8e0f061b1da42c70fb2a33cb75da5d7db
panini4552 <panini4552@gmail.com>
2025-12-06 13:11:00 +0900

    Update Silver.js

--- data/CDB/Silver.js
+++ data/CDB/Silver.js
@@ -1,15 +1,217 @@
-export const Classic_Legendary = [
+export const CDB_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ローズディアー",
+    name: "ローズディアー",
+    reading: ["ろーずでぃあー", "ローズディアー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ローズディアー.zip"
+  },
+  {
+    id: "迷林の大梟",
+    name: "迷林の大梟",
+    reading: ["めいりんのおおふくろう", "迷林の大梟"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/迷林の大梟.zip"
+  },
+  {
+    id: "フォレストマーチャント",
+    name: "フォレストマーチャント",
+    reading: ["ふぉれすとまーちゃんと", "フォレストマーチャント"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/フォレストマーチャント.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "マッハナイト",
+    name: "マッハナイト",
+    reading: ["まっはないと", "マッハナイト"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/マッハナイト.zip"
+  },
+  {
+    id: "エレガントバンデッド",
+    name: "エレガントバンデッド",
+    reading: ["えれがんとばんでっど", "エレガントバンデッド"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/エレガントバンデッド.zip"
+  },
+  {
+    id: "マギウィッパー",
+    name: "マギウィッパー",
+    reading: ["まぎうぃっぱー", "マギウィッパー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/マギウィッパー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マジカルナイト",
+    name: "マジカルナイト",
+    reading: ["まじかるないと", "マジカルナイト"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/マジカルナイト.zip"
+  },
+  {
+    id: "カードキャスター・マリ",
+    name: "カードキャスター・マリ",
+    reading: ["かーどきゃすたーまり", "カードキャスター・マリ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/カードキャスター・マリ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ランスリザード",
+    name: "ランスリザード",
+    reading: ["らんすりざーど", "ランスリザード"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ランスリザード.zip"
+  },
+  {
+    id: "マーメイドシンガー",
+    name: "マーメイドシンガー",
+    reading: ["まーめいどしんがー", "マーメイドシンガー"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/マーメイドシンガー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ミスチーフゾンビ",
+    name: "ミスチーフゾンビ",
+    reading: ["みすちーふぞんび", "ミスチーフゾンビ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ミスチーフゾンビ.zip"
+  },
+  {
+    id: "フギン＆ムニン",
+    name: "フギン＆ムニン",
+    reading: ["ふぎんあんどむにん", "フギン＆ムニン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/フギン＆ムニン.zip"
+  },
+  {
+    id: "屍肉のハイドラ",
+    name: "屍肉のハイドラ",
+    reading: ["しにくのはいどら", "屍肉のハイドラ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/屍肉のハイドラ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "トロンボーンデビル",
+    name: "トロンボーンデビル",
+    reading: ["とろんぼーんでびる", "トロンボーンデビル"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/トロンボーンデビル.zip"
+  },
+  {
+    id: "マリシャスブレイダー",
+    name: "マリシャスブレイダー",
+    reading: ["まりしゃすぶれいだー", "マリシャスブレイダー"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/マリシャスブレイダー.zip"
+  },
+  {
+    id: "フュリオスウルフ",
+    name: "フュリオスウルフ",
+    reading: ["ふゅりおすうるふ", "フュリオスウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/フュリオスウルフ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "サファイアプリースト",
+    name: "サファイアプリースト",
+    reading: ["さふぁいあぷりーすと", "サファイアプリースト"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/サファイアプリースト.zip"
+  },
+  {
+    id: "ミュースシェフ",
+    name: "ミュースシェフ",
+    reading: ["みゅーすしぇふ", "ミュースシェフ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ミュースシェフ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "クリーニングドール",
+    name: "クリーニングドール",
+    reading: ["くりーにんぐどーる", "クリーニングドール"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/クリーニングドール.zip"
+  },
+  {
+    id: "異界の生命",
+    name: "異界の生命",
+    reading: ["いかいのせいめい", "異界の生命"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/異界の生命.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ナチュラルペインター",
+    name: "ナチュラルペインター",
+    reading: ["なちゅらるぺいんたー", "ナチュラルペインター"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ナチュラルペインター.zip"
+  },
+  {
+    id: "ダンジョンサーチャー",
+    name: "ダンジョンサーチャー",
+    reading: ["だんじょんさーちゃー", "ダンジョンサーチャー"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "CDB",
+    zip: "voices/CDB/Silver/ダンジョンサーチャー.zip"
+  }
 ];

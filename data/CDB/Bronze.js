4a6142470f959453fa13eb2028fb6ebc522f3959
panini4552 <panini4552@gmail.com>
2025-12-06 13:14:03 +0900

    Update Bronze.js

--- data/CDB/Bronze.js
+++ data/CDB/Bronze.js
@@ -1,15 +1,262 @@
-export const Classic_Legendary = [
+export const CDB_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "夢追いのエルフ",
+    name: "夢追いのエルフ",
+    reading: ["ゆめおいのえるふ", "夢追いのエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/夢追いのエルフ.zip"
+  },
+  {
+    id: "ワイルドエレメンタラー",
+    name: "ワイルドエレメンタラー",
+    reading: ["わいるどえれめんたらー", "ワイルドエレメンタラー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/ワイルドエレメンタラー.zip"
+  },
+  {
+    id: "宿り木のモモンガ",
+    name: "宿り木のモモンガ",
+    reading: ["やどりぎのももんが", "宿り木のモモンガ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/宿り木のモモンガ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "フレイムソルジャー",
+    name: "フレイムソルジャー",
+    reading: ["ふれいむそるじゃー", "フレイムソルジャー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/フレイムソルジャー.zip"
+  },
+  {
+    id: "清白の騎士",
+    name: "清白の騎士",
+    reading: ["せいはくのきし", "清白の騎士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/清白の騎士.zip"
+  },
+  {
+    id: "バトルメイド",
+    name: "バトルメイド",
+    reading: ["ばとるめいど", "バトルメイド"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/バトルメイド.zip"
+  },
+  {
+    id: "望遠の船長",
+    name: "望遠の船長",
+    reading: ["ぼうえんのせんちょう", "望遠の船長"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/望遠の船長.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マジカルルーク",
+    name: "マジカルルーク",
+    reading: ["まじかるるーく", "マジカルルーク"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/マジカルルーク.zip"
+  },
+  {
+    id: "予見の魔術師",
+    name: "予見の魔術師",
+    reading: ["よけんのまじゅつし", "予見の魔術師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/予見の魔術師.zip"
+  },
+  {
+    id: "トランプマジシャン",
+    name: "トランプマジシャン",
+    reading: ["とらんぷまじしゃん", "トランプマジシャン"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/トランプマジシャン.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "トカゲの騎兵",
+    name: "トカゲの騎兵",
+    reading: ["とかげのきへい", "トカゲの騎兵"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/トカゲの騎兵.zip"
+  },
+  {
+    id: "竜壊の剣士",
+    name: "竜壊の剣士",
+    reading: ["りゅうかいのけんし", "竜壊の剣士"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/竜壊の剣士.zip"
+  },
+  {
+    id: "砕石竜",
+    name: "砕石竜",
+    reading: ["さいせきりゅう", "砕石竜"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/砕石竜.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥火のピッツァヨーラ",
+    name: "冥火のピッツァヨーラ",
+    reading: ["めいかのぴっつぁよーら", "冥火のピッツァヨーラ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/冥火のピッツァヨーラ.zip"
+  },
+  {
+    id: "スケルトンブレイカー",
+    name: "スケルトンブレイカー",
+    reading: ["すけるとんぶれいかー", "スケルトンブレイカー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/スケルトンブレイカー.zip"
+  },
+  {
+    id: "弔魂の幽霊船",
+    name: "弔魂の幽霊船",
+    reading: ["ちょうこんのゆうれいせん", "弔魂の幽霊船"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/弔魂の幽霊船.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "美装の悪魔",
+    name: "美装の悪魔",
+    reading: ["びそうのあくま", "美装の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/美装の悪魔.zip"
+  },
+  {
+    id: "猛悪のハーピー",
+    name: "猛悪のハーピー",
+    reading: ["もうあくのはーぴー", "猛悪のハーピー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/猛悪のハーピー.zip"
+  },
+  {
+    id: "魔剣の鬼人",
+    name: "魔剣の鬼人",
+    reading: ["まけんのおにん", "魔剣の鬼人"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/魔剣の鬼人.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "エメラルドメイデン",
+    name: "エメラルドメイデン",
+    reading: ["えめらるどめいでん", "エメラルドメイデン"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/エメラルドメイデン.zip"
+  },
+  {
+    id: "ミュースクラウン",
+    name: "ミュースクラウン",
+    reading: ["みゅーすくらうん", "ミュースクラウン"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/ミュースクラウン.zip"
+  },
+  {
+    id: "聖鎧のチーター",
+    name: "聖鎧のチーター",
+    reading: ["せいがいのちーたー", "聖鎧のチーター"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/聖鎧のチーター.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "機構翼の侍女",
+    name: "機構翼の侍女",
+    reading: ["きこうよくのじじょ", "機構翼の侍女"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/機構翼の侍女.zip"
+  },
+  {
+    id: "マリオネットダーリン",
+    name: "マリオネットダーリン",
+    reading: ["まりおねっとだーりん", "マリオネットダーリン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/マリオネットダーリン.zip"
+  },
+  {
+    id: "朽ちた巨兵",
+    name: "朽ちた巨兵",
+    reading: ["くちたきょへい", "朽ちた巨兵"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/朽ちた巨兵.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "宣告の天使",
+    name: "宣告の天使",
+    reading: ["せんこくのてんし", "宣告の天使"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/宣告の天使.zip"
+  },
+  {
+    id: "ライノマン",
+    name: "ライノマン",
+    reading: ["らいのまん", "ライノマン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "CDB",
+    zip: "voices/CDB/Bronze/ライノマン.zip"
+  }
 ];

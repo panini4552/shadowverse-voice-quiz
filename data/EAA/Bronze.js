1652706f59fdcb59ff9378ec225f0b6ef2ca005d
panini4552 <panini4552@gmail.com>
2025-12-07 20:04:28 +0900

    Update Bronze.js

--- data/EAA/Bronze.js
+++ data/EAA/Bronze.js
@@ -1,15 +1,235 @@
-export const Classic_Legendary = [
+export const EAA_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "若芽の組員",
+    name: "若芽の組員",
+    reading: ["わかめのくみいん", "若芽の組員"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/若芽の組員.zip"
+  },
+  {
+    id: "ビーストランサー",
+    name: "ビーストランサー",
+    reading: ["びーすとらんさー", "ビーストランサー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/ビーストランサー.zip"
+  },
+  {
+    id: "森越えの商人",
+    name: "森越えの商人",
+    reading: ["もりごえのしょうにん", "森越えの商人"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/森越えの商人.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "海原の斥候",
+    name: "海原の斥候",
+    reading: ["うなばらのせっこう", "海原の斥候"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/海原の斥候.zip"
+  },
+  {
+    id: "放浪の騎士",
+    name: "放浪の騎士",
+    reading: ["ほうろうのきし", "放浪の騎士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/放浪の騎士.zip"
+  },
+  {
+    id: "ヘヴィーウォーリアー",
+    name: "ヘヴィーウォーリアー",
+    reading: ["へヴぃーうぉーりあー", "ヘヴィーウォーリアー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/ヘヴィーウォーリアー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "没頭の実験体",
+    name: "没頭の実験体",
+    reading: ["ぼっとうのじっけんたい", "没頭の実験体"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/没頭の実験体.zip"
+  },
+  {
+    id: "群青の魔女",
+    name: "群青の魔女",
+    reading: ["ぐんじょうのまじょ", "群青の魔女"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/群青の魔女.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "亀裂の盗人",
+    name: "亀裂の盗人",
+    reading: ["きれつのぬすっと", "亀裂の盗人"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/亀裂の盗人.zip"
+  },
+  {
+    id: "ダークマーメイド",
+    name: "ダークマーメイド",
+    reading: ["だーくまーめいど", "ダークマーメイド"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/ダークマーメイド.zip"
+  },
+  {
+    id: "ダンスクラブ",
+    name: "ダンスクラブ",
+    reading: ["だんすくらぶ", "ダンスクラブ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/ダンスクラブ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "奈落の軍曹",
+    name: "奈落の軍曹",
+    reading: ["ならくのぐんそう", "奈落の軍曹"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/奈落の軍曹.zip"
+  },
+  {
+    id: "甘味の死霊術師",
+    name: "甘味の死霊術師",
+    reading: ["かんみのしりょうじゅつし", "甘味の死霊術師"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/甘味の死霊術師.zip"
+  },
+  {
+    id: "忠犬のゴースト",
+    name: "忠犬のゴースト",
+    reading: ["ちゅうけんのごーすと", "忠犬のゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/忠犬のゴースト.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "威圧の防衛隊長",
+    name: "威圧の防衛隊長",
+    reading: ["いあつのぼうえいたいちょう", "威圧の防衛隊長"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/威圧の防衛隊長.zip"
+  },
+  {
+    id: "スピニングウルフ",
+    name: "スピニングウルフ",
+    reading: ["すぴにんぐうるふ", "スピニングウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/スピニングウルフ.zip"
+  },
+  {
+    id: "フロストオーガ",
+    name: "フロストオーガ",
+    reading: ["ふろすとおーが", "フロストオーガ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/フロストオーガ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "宗規の構成員",
+    name: "宗規の構成員",
+    reading: ["しゅうきのこうせいいん", "宗規の構成員"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/宗規の構成員.zip"
+  },
+  {
+    id: "アメシストレオ",
+    name: "アメシストレオ",
+    reading: ["あめしすとれお", "アメシストレオ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/アメシストレオ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "黒錆の下っ端",
+    name: "黒錆の下っ端",
+    reading: ["くろさびのしたっぱ", "黒錆の下っ端"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/黒錆の下っ端.zip"
+  },
+  {
+    id: "はぐれの人形使い",
+    name: "はぐれの人形使い",
+    reading: ["はぐれのにんぎょうつかい", "はぐれの人形使い"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/はぐれの人形使い.zip"
+  },
+  {
+    id: "遺跡の擬態兵",
+    name: "遺跡の擬態兵",
+    reading: ["いせきのぎたいへい", "遺跡の擬態兵"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/遺跡の擬態兵.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "天球の天使",
+    name: "天球の天使",
+    reading: ["てんきゅうのてんし", "天球の天使"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/天球の天使.zip"
+  },
+  {
+    id: "神秘の探索者",
+    name: "神秘の探索者",
+    reading: ["しんぴのたんさくしゃ", "神秘の探索者"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "EAA",
+    zip: "voices/EAA/Bronze/神秘の探索者.zip"
+  }
 ];

7853e55ae4b8e34567ff21e10a73e51acfb95390
panini4552 <panini4552@gmail.com>
2025-12-07 20:23:20 +0900

    Update Bronze.js

--- data/AOA/Bronze.js
+++ data/AOA/Bronze.js
@@ -1,15 +1,235 @@
-export const Classic_Legendary = [
+export const AOA_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "サルビアパンサー",
+    name: "サルビアパンサー",
+    reading: ["さるびあぱんさー", "サルビアパンサー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/サルビアパンサー.zip"
+  },
+  {
+    id: "アネモネタイガー",
+    name: "アネモネタイガー",
+    reading: ["あねもねたいがー", "アネモネタイガー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/アネモネタイガー.zip"
+  },
+  {
+    id: "ソードラビット",
+    name: "ソードラビット",
+    reading: ["そーどらびっと", "ソードラビット"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ソードラビット.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "奮闘する格闘家",
+    name: "奮闘する格闘家",
+    reading: ["ふんとうするかくとうか", "奮闘する格闘家"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/奮闘する格闘家.zip"
+  },
+  {
+    id: "愚直なる研磨・トニー",
+    name: "愚直なる研磨・トニー",
+    reading: ["ぐちょくなるけんま・とにー", "愚直なる研磨・トニー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/愚直なる研磨・トニー.zip"
+  },
+  {
+    id: "ペンギンモンク",
+    name: "ペンギンモンク",
+    reading: ["ぺんぎんもんく", "ペンギンモンク"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ペンギンモンク.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "リトルセージ",
+    name: "リトルセージ",
+    reading: ["りとるせーじ", "リトルセージ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/リトルセージ.zip"
+  },
+  {
+    id: "群れの召喚士",
+    name: "群れの召喚士",
+    reading: ["むれのしょうかんし", "群れの召喚士"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/群れの召喚士.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "竜人のストライカー",
+    name: "竜人のストライカー",
+    reading: ["りゅうじんのすとらいかー", "竜人のストライカー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/竜人のストライカー.zip"
+  },
+  {
+    id: "イプピアーラ",
+    name: "イプピアーラ",
+    reading: ["いぷぴあーら", "イプピアーラ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/イプピアーラ.zip"
+  },
+  {
+    id: "荒天のドラゴン",
+    name: "荒天のドラゴン",
+    reading: ["こうてんのどらごん", "荒天のドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/荒天のドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ゴーストハントガール",
+    name: "ゴーストハントガール",
+    reading: ["ごーすとはんとがーる", "ゴーストハントガール"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ゴーストハントガール.zip"
+  },
+  {
+    id: "グリードセクストン",
+    name: "グリードセクストン",
+    reading: ["ぐりーどせくすとん", "グリードセクストン"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/グリードセクストン.zip"
+  },
+  {
+    id: "ファイトコープスドッグ",
+    name: "ファイトコープスドッグ",
+    reading: ["ふぁいとこーぷすどっぐ", "ファイトコープスドッグ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ファイトコープスドッグ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "極徒会表番・デナン",
+    name: "極徒会表番・デナン",
+    reading: ["ごくとかいおもてばん・でなん", "極徒会表番・デナン"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/極徒会表番・デナン.zip"
+  },
+  {
+    id: "ハーモニックウルフ",
+    name: "ハーモニックウルフ",
+    reading: ["はーもにっくうるふ", "ハーモニックウルフ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ハーモニックウルフ.zip"
+  },
+  {
+    id: "誘いの夢魔",
+    name: "誘いの夢魔",
+    reading: ["いざないのむま", "誘いの夢魔"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/誘いの夢魔.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "純真の花少女",
+    name: "純真の花少女",
+    reading: ["じゅんしんのはなしょうじょ", "純真の花少女"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/純真の花少女.zip"
+  },
+  {
+    id: "アクアプリースト",
+    name: "アクアプリースト",
+    reading: ["あくあぷりーすと", "アクアプリースト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/アクアプリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "スモーメカニック",
+    name: "スモーメカニック",
+    reading: ["すもーめかにっく", "スモーメカニック"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/スモーメカニック.zip"
+  },
+  {
+    id: "バレットテイカー",
+    name: "バレットテイカー",
+    reading: ["ばれっとていかー", "バレットテイカー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/バレットテイカー.zip"
+  },
+  {
+    id: "エンシェントレリック",
+    name: "エンシェントレリック",
+    reading: ["えんしぇんとれりっく", "エンシェントレリック"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/エンシェントレリック.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "スマートゴブリン",
+    name: "スマートゴブリン",
+    reading: ["すまーとごぶりん", "スマートゴブリン"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/スマートゴブリン.zip"
+  },
+  {
+    id: "ハイキングギガース",
+    name: "ハイキングギガース",
+    reading: ["はいきんぐぎがーす", "ハイキングギガース"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "AOA",
+    zip: "voices/AOA/Bronze/ハイキングギガース.zip"
+  }
 ];

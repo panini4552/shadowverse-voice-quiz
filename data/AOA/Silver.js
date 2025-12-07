e906b6b0d0d8338ba67c8afb2f2ee11852d1f4f2
panini4552 <panini4552@gmail.com>
2025-12-07 20:20:49 +0900

    Update Silver.js

--- data/AOA/Silver.js
+++ data/AOA/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+[
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ナチュラルテイマー",
+    name: "ナチュラルテイマー",
+    reading: ["なちゅらるていまー", "ナチュラルテイマー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/ナチュラルテイマー.zip"
+  },
+  {
+    id: "農作のエルフ",
+    name: "農作のエルフ",
+    reading: ["のうさくのえるふ", "農作のエルフ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/農作のエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "必中の訓練生",
+    name: "必中の訓練生",
+    reading: ["ひっちゅうのくんれんせい", "必中の訓練生"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/必中の訓練生.zip"
+  },
+  {
+    id: "才気の戦略家",
+    name: "才気の戦略家",
+    reading: ["さいきのせんりゃくか", "才気の戦略家"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/才気の戦略家.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "鐘の魔術師",
+    name: "鐘の魔術師",
+    reading: ["かねのまじゅつし", "鐘の魔術師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/鐘の魔術師.zip"
+  },
+  {
+    id: "ルナルの教会長・グリュネ",
+    name: "ルナルの教会長・グリュネ",
+    reading: ["るなるのきょうかいちょう ぐりゅね", "ルナルの教会長・グリュネ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/ルナルの教会長・グリュネ.zip"
+  },
 
-];
+  // === ドラゴン ===
+  {
+    id: "オルカサーファー",
+    name: "オルカサーファー",
+    reading: ["おるかさーふぁー", "オルカサーファー"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/オルカサーファー.zip"
+  },
+  {
+    id: "貫禄のドラゴニュート・ガンベイン",
+    name: "貫禄のドラゴニュート・ガンベイン",
+    reading: ["かんろくのどらごにゅーと がんべいん", "貫禄のドラゴニュート・ガンベイン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/貫禄のドラゴニュート・ガンベイン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "カースドソーサラー・リーズ",
+    name: "カースドソーサラー・リーズ",
+    reading: ["かーすどそーさらー りーず", "カースドソーサラー・リーズ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/カースドソーサラー・リーズ.zip"
+  },
+  {
+    id: "ミラーゴースト",
+    name: "ミラーゴースト",
+    reading: ["みらーごーすと", "ミラーゴースト"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/ミラーゴースト.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "韋駄天のパシリ・マロン",
+    name: "韋駄天のパシリ・マロン",
+    reading: ["いだてんのぱしり まろん", "韋駄天のパシリ・マロン"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/韋駄天のパシリ・マロン.zip"
+  },
+  {
+    id: "魔眼のスケバン",
+    name: "魔眼のスケバン",
+    reading: ["まがんのすけばん", "魔眼のスケバン"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/魔眼のスケバン.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "不退転の乙女・キーラ",
+    name: "不退転の乙女・キーラ",
+    reading: ["ふたいてんのおとめ きーら", "不退転の乙女・キーラ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/不退転の乙女・キーラ.zip"
+  },
+  {
+    id: "浄火の令嬢",
+    name: "浄火の令嬢",
+    reading: ["じょうかのれいじょう", "浄火の令嬢"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/浄火の令嬢.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ホバーバイカー",
+    name: "ホバーバイカー",
+    reading: ["ほばーばいかー", "ホバーバイカー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/ホバーバイカー.zip"
+  },
+  {
+    id: "勉学の職人・ホフレ",
+    name: "勉学の職人・ホフレ",
+    reading: ["べんがくのしょくにん ほふれ", "勉学の職人・ホフレ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/勉学の職人・ホフレ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "青春物語・ファイター＆スケルトン",
+    name: "青春物語・ファイター＆スケルトン",
+    reading: ["せいしゅんものがたり ふぁいたー あんど すけるとん", "青春物語・ファイター＆スケルトン"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "AOA",
+    zip: "voices/AOA/Silver/青春物語・ファイター＆スケルトン.zip"
+  }
+]

5f3161c03b9ad1e3bf3499134dfb6b9e855de301
panini4552 <panini4552@gmail.com>
2025-12-07 20:03:48 +0900

    Update Silver.js

--- data/EAA/Silver.js
+++ data/EAA/Silver.js
@@ -1,15 +1,199 @@
-export const Classic_Legendary = [
+[
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "根際の刺客",
+    name: "根際の刺客",
+    reading: ["ねぎわのしかく", "根際の刺客"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/根際の刺客.zip"
+  },
+  {
+    id: "ガイアフェアリー",
+    name: "ガイアフェアリー",
+    reading: ["がいあふぇありー", "ガイアフェアリー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/ガイアフェアリー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "潮流の砲手",
+    name: "潮流の砲手",
+    reading: ["ちょうりゅうのほうしゅ", "潮流の砲手"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/潮流の砲手.zip"
+  },
+  {
+    id: "鉄壁のガードナー",
+    name: "鉄壁のガードナー",
+    reading: ["てっぺきのがーどなー", "鉄壁のガードナー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/鉄壁のガードナー.zip"
+  },
+  {
+    id: "ウィンドスラッシャー",
+    name: "ウィンドスラッシャー",
+    reading: ["うぃんどすらっしゃー", "ウィンドスラッシャー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/ウィンドスラッシャー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "熱中の研究者",
+    name: "熱中の研究者",
+    reading: ["ねっちゅうのけんきゅうしゃ", "熱中の研究者"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/熱中の研究者.zip"
+  },
+  {
+    id: "クリスタルウィッチ",
+    name: "クリスタルウィッチ",
+    reading: ["くりすたるうぃっち", "クリスタルウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/クリスタルウィッチ.zip"
+  },
 
-];
+  // === ドラゴン ===
+  {
+    id: "倒壊の襲撃者",
+    name: "倒壊の襲撃者",
+    reading: ["とうかいのしゅうげきしゃ", "倒壊の襲撃者"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/倒壊の襲撃者.zip"
+  },
+  {
+    id: "リバイバルドラグーン",
+    name: "リバイバルドラグーン",
+    reading: ["りばいばるどらぐーん", "リバイバルドラグーン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/リバイバルドラグーン.zip"
+  },
+  {
+    id: "瞬刃のドラゴニュート",
+    name: "瞬刃のドラゴニュート",
+    reading: ["しゅんじんのどらごにゅーと", "瞬刃のドラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/瞬刃のドラゴニュート.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥府の中尉",
+    name: "冥府の中尉",
+    reading: ["めいふのちゅうい", "冥府の中尉"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/冥府の中尉.zip"
+  },
+  {
+    id: "クロースケルトン",
+    name: "クロースケルトン",
+    reading: ["くろーすけるとん", "クロースケルトン"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/クロースケルトン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "猛襲の特攻隊長",
+    name: "猛襲の特攻隊長",
+    reading: ["もうしゅうのとっこうたいちょう", "猛襲の特攻隊長"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/猛襲の特攻隊長.zip"
+  },
+  {
+    id: "デモンビーター",
+    name: "デモンビーター",
+    reading: ["でもんびーたー", "デモンビーター"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/デモンビーター.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "戒律の諜報員",
+    name: "戒律の諜報員",
+    reading: ["かいりつのちょうほういん", "戒律の諜報員"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/戒律の諜報員.zip"
+  },
+  {
+    id: "ルビーファルコン",
+    name: "ルビーファルコン",
+    reading: ["るびーふぁるこん", "ルビーファルコン"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/ルビーファルコン.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "精錬の用心棒",
+    name: "精錬の用心棒",
+    reading: ["せいれんのようじんぼう", "精錬の用心棒"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/精錬の用心棒.zip"
+  },
+  {
+    id: "サンドボーダー",
+    name: "サンドボーダー",
+    reading: ["さんどぼーだー", "サンドボーダー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/サンドボーダー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "エンジェルパニッシャー",
+    name: "エンジェルパニッシャー",
+    reading: ["えんじぇるぱにっしゃー", "エンジェルパニッシャー"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/エンジェルパニッシャー.zip"
+  },
+  {
+    id: "オウルキャットガール",
+    name: "オウルキャットガール",
+    reading: ["おうるきゃっとがーる", "オウルキャットガール"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "EAA",
+    zip: "voices/EAA/Silver/オウルキャットガール.zip"
+  }
+]

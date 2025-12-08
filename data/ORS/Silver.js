5d201ddb59a157b2f7f1e7ef34014f54a4afef05
panini4552 <panini4552@gmail.com>
2025-12-08 13:21:50 +0900

    Update Silver.js

--- data/ORS/Silver.js
+++ data/ORS/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+export const ORS_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ゴールデンインセクト",
+    name: "ゴールデンインセクト",
+    reading: ["ごーるでんいんせくと", "ゴールデンインセクト", "ゴールデンインセクト"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/ゴールデンインセクト.zip"
+  },
+  {
+    id: "森緑のエレメンタラー",
+    name: "森緑のエレメンタラー",
+    reading: ["しんりょくのえれめんたらー", "シンリョクノエレメンタラー", "森緑のエレメンタラー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/森緑のエレメンタラー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "盤上の圧政者",
+    name: "盤上の圧政者",
+    reading: ["ばんじょうのあっせいしゃ", "バンジョウノアッセイシャ", "盤上の圧政者"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/盤上の圧政者.zip"
+  },
+  {
+    id: "チーフランサー",
+    name: "チーフランサー",
+    reading: ["ちーふらんさー", "チーフランサー", "チーフランサー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/チーフランサー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "切除の魔術師",
+    name: "切除の魔術師",
+    reading: ["せつじょのまじゅつし", "セツジョノマジュツシ", "切除の魔術師"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/切除の魔術師.zip"
+  },
+  {
+    id: "マーキュリーウィッチ",
+    name: "マーキュリーウィッチ",
+    reading: ["まーきゅりーうぃっち", "マーキュリーウィッチ", "マーキュリーウィッチ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/マーキュリーウィッチ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "追憶の白竜",
+    name: "追憶の白竜",
+    reading: ["ついおくのはくりゅう", "ツイオクノハクリュウ", "追憶の白竜"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/追憶の白竜.zip"
+  },
+  {
+    id: "リザードハンター",
+    name: "リザードハンター",
+    reading: ["りざーどはんたー", "リザードハンター", "リザードハンター"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/リザードハンター.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "蘇りし邪悪",
+    name: "蘇りし邪悪",
+    reading: ["よみがえりしじゃあく", "ヨミガエリシジャアク", "蘇りし邪悪"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/蘇りし邪悪.zip"
+  },
+  {
+    id: "カースドキング",
+    name: "カースドキング",
+    reading: ["かーすどきんぐ", "カースドキング", "カースドキング"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/カースドキング.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "アウナス",
+    name: "アウナス",
+    reading: ["あうなす", "アウナス", "アウナス"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/アウナス.zip"
+  },
+  {
+    id: "杯の誘惑者",
+    name: "杯の誘惑者",
+    reading: ["さかずきのゆうわくしゃ", "サカズキノユウワクシャ", "杯の誘惑者"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/杯の誘惑者.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "シトリンラビット",
+    name: "シトリンラビット",
+    reading: ["しとりんらびっと", "シトリンラビット", "シトリンラビット"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/シトリンラビット.zip"
+  },
+  {
+    id: "ウェポンアルミラージ",
+    name: "ウェポンアルミラージ",
+    reading: ["うぇぽんあるみらーじ", "ウェポンアルミラージ", "ウェポンアルミラージ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/ウェポンアルミラージ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "権謀のフィクサー",
+    name: "権謀のフィクサー",
+    reading: ["けんぼうのふぃくさー", "ケンボウノフィクサー", "権謀のフィクサー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/権謀のフィクサー.zip"
+  },
+  {
+    id: "ミスリルメッセンジャー",
+    name: "ミスリルメッセンジャー",
+    reading: ["みすりるめっせんじゃー", "ミスリルメッセンジャー", "ミスリルメッセンジャー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/ミスリルメッセンジャー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "漆黒の使徒",
+    name: "漆黒の使徒",
+    reading: ["しっこくのしと", "シッコクノシト", "漆黒の使徒"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "ORS",
+    zip: "voices/ORS/Silver/漆黒の使徒.zip"
+  }
 ];

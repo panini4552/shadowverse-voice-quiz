a03de3bdecc720a7cde5d5e54095557eacd4dc80
panini4552 <panini4552@gmail.com>
2025-12-08 11:54:54 +0900

    Replace Classic_Legendary with HOR_Gold data

--- data/HOR/Gold.js
+++ data/HOR/Gold.js
@@ -1,15 +1,190 @@
-export const Classic_Legendary = [
+export const HOR_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "想望の少女・カノン",
+    name: "想望の少女・カノン",
+    reading: ["そうぼうのしょうじょ・かのん", "想望の少女・カノン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/想望の少女・カノン.zip"
+  },
+  {
+    id: "レイジングアックス・アビー",
+    name: "レイジングアックス・アビー",
+    reading: ["れいじんぐあっくす・あびー", "レイジングアックス・アビー"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/レイジングアックス・アビー.zip"
+  },
+  {
+    id: "新芽の風・メイ",
+    name: "新芽の風・メイ",
+    reading: ["しんめのかぜ・めい", "新芽の風・メイ"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/新芽の風・メイ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "光剣の使い手",
+    name: "光剣の使い手",
+    reading: ["こうけんのつかいて", "光剣の使い手"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/光剣の使い手.zip"
+  },
+  {
+    id: "ラブロマンスレディ",
+    name: "ラブロマンスレディ",
+    reading: ["らぶろまんすれでぃ", "ラブロマンスレディ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/ラブロマンスレディ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "オウルハンドラー",
+    name: "オウルハンドラー",
+    reading: ["おうるはんどらー", "オウルハンドラー"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/オウルハンドラー.zip"
+  },
+  {
+    id: "エクスプロードメイジ",
+    name: "エクスプロードメイジ",
+    reading: ["えくすぷろーどめいじ", "エクスプロードメイジ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/エクスプロードメイジ.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "白金の竜剣士",
+    name: "白金の竜剣士",
+    reading: ["はっきんのりゅうけんし", "白金の竜剣士"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/白金の竜剣士.zip"
+  },
+  {
+    id: "珊瑚礁の精霊",
+    name: "珊瑚礁の精霊",
+    reading: ["さんごしょうのせいれい", "珊瑚礁の精霊"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/珊瑚礁の精霊.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "魂の貰い手",
+    name: "魂の貰い手",
+    reading: ["たましいのもらいて", "魂の貰い手"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/魂の貰い手.zip"
+  },
+  {
+    id: "コープスエリミネーター",
+    name: "コープスエリミネーター",
+    reading: ["こーぷすえりみねーたー", "コープスエリミネーター"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/コープスエリミネーター.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ラヴェナスキング・ガデル",
+    name: "ラヴェナスキング・ガデル",
+    reading: ["らヴぇなすきんぐ・がでる", "ラヴェナスキング・ガデル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/ラヴェナスキング・ガデル.zip"
+  },
+  {
+    id: "魔性の扇動者",
+    name: "魔性の扇動者",
+    reading: ["ましょうのせんどうしゃ", "魔性の扇動者"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/魔性の扇動者.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "神域の狼",
+    name: "神域の狼",
+    reading: ["しんいきのおおかみ", "神域の狼"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/神域の狼.zip"
+  },
+  {
+    id: "玉兎の治療師",
+    name: "玉兎の治療師",
+    reading: ["ぎょくとのちりょうし", "玉兎の治療師"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/玉兎の治療師.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ゲートリベレーター・ラズリ",
+    name: "ゲートリベレーター・ラズリ",
+    reading: ["げーとりべれーたー・らずり", "ゲートリベレーター・ラズリ"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/ゲートリベレーター・ラズリ.zip"
+  },
+  {
+    id: "ロストメモリー・ジャスパー",
+    name: "ロストメモリー・ジャスパー",
+    reading: ["ろすとめもりー・じゃすぱー", "ロストメモリー・ジャスパー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/ロストメモリー・ジャスパー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "希望の断片・キリエ",
+    name: "希望の断片・キリエ",
+    reading: ["きぼうのだんぺん・きりえ", "希望の断片・キリエ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/希望の断片・キリエ.zip"
+  },
+  {
+    id: "ツインシスター・アスカ＆シオリ",
+    name: "ツインシスター・アスカ＆シオリ",
+    reading: ["ついんしすたー・あすかあんどしおり", "ツインシスター・アスカ＆シオリ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "HOR",
+    zip: "voices/HOR/Gold/ツインシスター・アスカ＆シオリ.zip"
+  }
 ];

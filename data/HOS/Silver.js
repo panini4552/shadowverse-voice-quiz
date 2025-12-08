a22162a82bd23d65db6f4c1029939f00717951ed
panini4552 <panini4552@gmail.com>
2025-12-08 16:47:13 +0900

    Update Silver.js

--- data/HOS/Silver.js
+++ data/HOS/Silver.js
@@ -1,15 +1,163 @@
-export const Classic_Legendary = [
+export const HOS_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "果実の獣人",
+    name: "果実の獣人",
+    reading: ["果実の獣人", "かじつのじゅうじん"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/果実の獣人.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "疾駆する獣戦士",
+    name: "疾駆する獣戦士",
+    reading: ["疾駆する獣戦士", "しっくするじゅうせんし"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/疾駆する獣戦士.zip"
+  },
+  {
+    id: "ブレイドライツの精鋭",
+    name: "ブレイドライツの精鋭",
+    reading: ["ブレイドライツの精鋭", "ぶれいどらいつのせいえい"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/ブレイドライツの精鋭.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "データソーサラー",
+    name: "データソーサラー",
+    reading: ["データソーサラー", "でーたそーさらー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/データソーサラー.zip"
+  },
+  {
+    id: "地平の魔女",
+    name: "地平の魔女",
+    reading: ["地平の魔女", "ちへいのまじょ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/地平の魔女.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "繁栄のステゴサウルス",
+    name: "繁栄のステゴサウルス",
+    reading: ["繁栄のステゴサウルス", "はんえいのすてごさうるす"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/繁栄のステゴサウルス.zip"
+  },
+  {
+    id: "ナテラの戦闘員",
+    name: "ナテラの戦闘員",
+    reading: ["ナテラの戦闘員", "なてらのせんとういん"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/ナテラの戦闘員.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "フルメタルゾンビ",
+    name: "フルメタルゾンビ",
+    reading: ["フルメタルゾンビ", "ふるめたるぞんび"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/フルメタルゾンビ.zip"
+  },
+  {
+    id: "金鉱の死霊術師",
+    name: "金鉱の死霊術師",
+    reading: ["金鉱の死霊術師", "きんこうのしりょうじゅつし"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/金鉱の死霊術師.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ナイトメーカー",
+    name: "ナイトメーカー",
+    reading: ["ナイトメーカー", "ないとめーかー"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/ナイトメーカー.zip"
+  },
+  {
+    id: "ダイスデビル",
+    name: "ダイスデビル",
+    reading: ["ダイスデビル", "だいすでびる"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/ダイスデビル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ブレッシングミュース",
+    name: "ブレッシングミュース",
+    reading: ["ブレッシングミュース", "ぶれっしんぐみゅーす"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/ブレッシングミュース.zip"
+  },
+  {
+    id: "エスカマリの調停者",
+    name: "エスカマリの調停者",
+    reading: ["エスカマリの調停者", "えすかまりのちょうていしゃ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/エスカマリの調停者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "補修のコヨーテ",
+    name: "補修のコヨーテ",
+    reading: ["補修のコヨーテ", "ほしゅうのこよーて"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/補修のコヨーテ.zip"
+  },
+  {
+    id: "量産型機械兵",
+    name: "量産型機械兵",
+    reading: ["量産型機械兵", "りょうさんがたきかいへい"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/量産型機械兵.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "アナーキーリザード",
+    name: "アナーキーリザード",
+    reading: ["アナーキーリザード", "あなーきーりざーど"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "HOS",
+    zip: "voices/HOS/Silver/アナーキーリザード.zip"
+  }
 ];

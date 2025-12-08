2e13c771b9ba85a7f4e996e19d8608509d4a3768
panini4552 <panini4552@gmail.com>
2025-12-08 14:22:38 +0900

    Update Gold.js

--- data/RSL/Gold.js
+++ data/RSL/Gold.js
@@ -1,15 +1,208 @@
-export const Classic_Legendary = [
+export const RSL_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "スノーソルジャー",
+    name: "スノーソルジャー",
+    reading: ["すのーそるじゃー", "スノーソルジャー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/スノーソルジャー.zip"
+  },
+  {
+    id: "蟲毒の姫・リコリス",
+    name: "蟲毒の姫・リコリス",
+    reading: ["こどくのひめりこりす", "蟲毒の姫・リコリス"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/蟲毒の姫・リコリス.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "善導の騎士",
+    name: "善導の騎士",
+    reading: ["ぜんどうのきし", "善導の騎士"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/善導の騎士.zip"
+  },
+  {
+    id: "闇夜の斥候・トロイア",
+    name: "闇夜の斥候・トロイア",
+    reading: ["やみよのせっこうとろいあ", "闇夜の斥候・トロイア"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/闇夜の斥候・トロイア.zip"
+  },
+  {
+    id: "マスターエージェント・デイ",
+    name: "マスターエージェント・デイ",
+    reading: ["ますたーえーじぇんとでい", "マスターエージェント・デイ"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/マスターエージェント・デイ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "封魔の術師",
+    name: "封魔の術師",
+    reading: ["ふうまのじゅつし", "封魔の術師"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/封魔の術師.zip"
+  },
+  {
+    id: "規範の生徒会長・ハンナ",
+    name: "規範の生徒会長・ハンナ",
+    reading: ["きはんのせいとかいちょうはんな", "規範の生徒会長・ハンナ"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/規範の生徒会長・ハンナ.zip"
+  },
+  {
+    id: "本能の求道者・ファウスト",
+    name: "本能の求道者・ファウスト",
+    reading: ["ほんのうのきゅうどうしゃふぁうすと", "本能の求道者・ファウスト"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/本能の求道者・ファウスト.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "天駆の聖竜",
+    name: "天駆の聖竜",
+    reading: ["てんくのせいりゅう", "天駆の聖竜"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/天駆の聖竜.zip"
+  },
+  {
+    id: "マーメイドコマンダー",
+    name: "マーメイドコマンダー",
+    reading: ["まーめいどこまんだー", "マーメイドコマンダー"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/マーメイドコマンダー.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "デスサイズゴブリン",
+    name: "デスサイズゴブリン",
+    reading: ["ですさいずごぶりん", "デスサイズゴブリン"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/デスサイズゴブリン.zip"
+  },
+  {
+    id: "黒き舞踏・オディール",
+    name: "黒き舞踏・オディール",
+    reading: ["くろきぶとうおでぃーる", "黒き舞踏・オディール"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/黒き舞踏・オディール.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "コントラクトキーパー",
+    name: "コントラクトキーパー",
+    reading: ["こんとらくときーぱー", "コントラクトキーパー"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/コントラクトキーパー.zip"
+  },
+  {
+    id: "失墜の誘惑・マステマ",
+    name: "失墜の誘惑・マステマ",
+    reading: ["しっついのゆうわくますてま", "失墜の誘惑・マステマ"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/失墜の誘惑・マステマ.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ペンデュラムアルミラージ",
+    name: "ペンデュラムアルミラージ",
+    reading: ["ぺんでゅらむあるみらーじ", "ペンデュラムアルミラージ"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/ペンデュラムアルミラージ.zip"
+  },
+  {
+    id: "病難の女神・セクメト",
+    name: "病難の女神・セクメト",
+    reading: ["びょうなんのめがみせくめと", "病難の女神・セクメト"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/病難の女神・セクメト.zip"
+  },
+  {
+    id: "白き舞踏・オデット",
+    name: "白き舞踏・オデット",
+    reading: ["しろきぶとうおでっと", "白き舞踏・オデット"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/白き舞踏・オデット.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エクセキュートガンナー",
+    name: "エクセキュートガンナー",
+    reading: ["えくせきゅーとがんなー", "エクセキュートガンナー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/エクセキュートガンナー.zip"
+  },
+  {
+    id: "境界の内包者",
+    name: "境界の内包者",
+    reading: ["きょうかいのないほうしゃ", "境界の内包者"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/境界の内包者.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "純心の賢者・スノウ",
+    name: "純心の賢者・スノウ",
+    reading: ["じゅんしんのけんじゃすのう", "純心の賢者・スノウ"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/純心の賢者・スノウ.zip"
+  },
+  {
+    id: "不定の輝き・オケアノス",
+    name: "不定の輝き・オケアノス",
+    reading: ["ふていのかがやきおけあのす", "不定の輝き・オケアノス"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "RSL",
+    zip: "voices/RSL/Gold/不定の輝き・オケアノス.zip"
+  }
 ];

ad6f72bbb28fd43635192d83bd601afea4ae6416
panini4552 <panini4552@gmail.com>
2025-12-08 12:58:49 +0900

    Update Legendary.js

--- data/ORS/Legendary.js
+++ data/ORS/Legendary.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const ORS_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "天空妖精・アイリス",
+    name: "天空妖精・アイリス",
+    reading: ["てんくうようせい・あいりす", "天空妖精・アイリス"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/天空妖精・アイリス.zip"
+  },
+  {
+    id: "フロストエイジプリンセス",
+    name: "フロストエイジプリンセス",
+    reading: ["ふろすとえいじぷりんせす", "フロストエイジプリンセス"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/フロストエイジプリンセス.zip"
+  },
+  {
+    id: "ドゥルガー",
+    name: "ドゥルガー",
+    reading: ["どぅるがー", "ドゥルガー"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/ドゥルガー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "漆黒の残光・ロサ",
+    name: "漆黒の残光・ロサ",
+    reading: ["しっこくのざんこう・ろさ", "漆黒の残光・ロサ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/漆黒の残光・ロサ.zip"
+  },
+  {
+    id: "極彩の美剣士",
+    name: "極彩の美剣士",
+    reading: ["ごくさいのびけんし", "極彩の美剣士"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/極彩の美剣士.zip"
+  },
+  {
+    id: "誅戮の武者",
+    name: "誅戮の武者",
+    reading: ["ちゅうりくのむしゃ", "誅戮の武者"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/誅戮の武者.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "摂理の隠者・ミデロ",
+    name: "摂理の隠者・ミデロ",
+    reading: ["せつりのいんじゃ・みでろ", "摂理の隠者・ミデロ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/摂理の隠者・ミデロ.zip"
+  },
+  {
+    id: "永生の大魔女",
+    name: "永生の大魔女",
+    reading: ["えいせいのだいまじょ", "永生の大魔女"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/永生の大魔女.zip"
+  },
+  {
+    id: "エイル",
+    name: "エイル",
+    reading: ["えいる", "エイル"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/エイル.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "竜国の王女・イルマ",
+    name: "竜国の王女・イルマ",
+    reading: ["りゅうこくのおうじょ・いるま", "竜国の王女・イルマ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/竜国の王女・イルマ.zip"
+  },
+  {
+    id: "苦悶の飢餓竜",
+    name: "苦悶の飢餓竜",
+    reading: ["くもんのきがりゅう", "苦悶の飢餓竜"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/苦悶の飢餓竜.zip"
+  },
+  {
+    id: "ヴァニティードラゴン",
+    name: "ヴァニティードラゴン",
+    reading: ["ゔぁにてぃーどらごん", "ヴァニティードラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/ヴァニティードラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "万死の否定者",
+    name: "万死の否定者",
+    reading: ["ばんしのひていしゃ", "万死の否定者"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/万死の否定者.zip"
+  },
+  {
+    id: "狂恋のネクロマンサー",
+    name: "狂恋のネクロマンサー",
+    reading: ["きょうれんのねくろまんさー", "狂恋のネクロマンサー"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/狂恋のネクロマンサー.zip"
+  },
+  {
+    id: "アビスアベンジャー・ヴィスカリア",
+    name: "アビスアベンジャー・ヴィスカリア",
+    reading: ["あびすあべんじゃー・ゔぃすかりあ", "アビスアベンジャー・ヴィスカリア"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/アビスアベンジャー・ヴィスカリア.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "宵闇の狩人・リアン＆アルフィ",
+    name: "宵闇の狩人・リアン＆アルフィ",
+    reading: ["よいやみのかりゅうど・りあん&あるふぃ", "宵闇の狩人・リアン＆アルフィ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/宵闇の狩人・リアン＆アルフィ.zip"
+  },
+  {
+    id: "ディアボロス・ヘドネ",
+    name: "ディアボロス・ヘドネ",
+    reading: ["でぃあぼろす・へどね", "ディアボロス・ヘドネ"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/ディアボロス・ヘドネ.zip"
+  },
+  {
+    id: "夕闇の人狼・ルージュ＆ヴェール",
+    name: "夕闇の人狼・ルージュ＆ヴェール",
+    reading: ["ゆうやみのじんろう・るーじゅ&ゔぇーる", "夕闇の人狼・ルージュ＆ヴェール"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/夕闇の人狼・ルージュ＆ヴェール.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "契約の双天使",
+    name: "契約の双天使",
+    reading: ["けいやくのそうてんし", "契約の双天使"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/契約の双天使.zip"
+  },
+  {
+    id: "タンザナイトコンヴィクター",
+    name: "タンザナイトコンヴィクター",
+    reading: ["たんざないとこんゔぃくたー", "タンザナイトコンヴィクター"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/タンザナイトコンヴィクター.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "至純の原型・アギル",
+    name: "至純の原型・アギル",
+    reading: ["しじゅんのげんけい・あぎる", "至純の原型・アギル"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/至純の原型・アギル.zip"
+  },
+  {
+    id: "アーティフィシャルエデン",
+    name: "アーティフィシャルエデン",
+    reading: ["あーてぃふぃしゃるえでん", "アーティフィシャルエデン"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/アーティフィシャルエデン.zip"
+  },
+  {
+    id: "ミリオンガンズ・アンタニタ",
+    name: "ミリオンガンズ・アンタニタ",
+    reading: ["みりおんがんず・あんたにた", "ミリオンガンズ・アンタニタ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/ミリオンガンズ・アンタニタ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "コンパッションドミニオン",
+    name: "コンパッションドミニオン",
+    reading: ["こんぱっしょんどみにおん", "コンパッションドミニオン"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/コンパッションドミニオン.zip"
+  },
+  {
+    id: "デミゴッド・ギルガメッシュ",
+    name: "デミゴッド・ギルガメッシュ",
+    reading: ["でみごっど・ぎるがめっしゅ", "デミゴッド・ギルガメッシュ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "ORS",
+    zip: "voices/ORS/Legendary/デミゴッド・ギルガメッシュ.zip"
+  }
 ];

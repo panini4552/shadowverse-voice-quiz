4c56cfd868fa8ef2f2d99900b43268083861f756
panini4552 <panini4552@gmail.com>
2025-12-08 13:02:01 +0900

    Update Gold.js

--- data/ORS/Gold.js
+++ data/ORS/Gold.js
@@ -1,15 +1,163 @@
-export const Classic_Legendary = [
+export const ORS_Gold = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "繁栄の撒き手",
+    name: "繁栄の撒き手",
+    reading: ["はんえいのまきて", "ハンエイノマキテ", "繁栄の撒き手"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/繁栄の撒き手.zip"
+  },
+  {
+    id: "エレガンスシルフィード",
+    name: "エレガンスシルフィード",
+    reading: ["えれがんすすいるふぃーど", "エレガンスシルフィード", "エレガンスシルフィード"],
+    class: "エルフ",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/エレガンスシルフィード.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "氷結の騎士",
+    name: "氷結の騎士",
+    reading: ["ひょうけつのきし", "ヒョウケツノキシ", "氷結の騎士"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/氷結の騎士.zip"
+  },
+  {
+    id: "イベイジョンマーセナリー",
+    name: "イベイジョンマーセナリー",
+    reading: ["いべいじょんまーせなりー", "イベイジョンマーセナリー", "イベイジョンマーセナリー"],
+    class: "ロイヤル",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/イベイジョンマーセナリー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "支配の魔女",
+    name: "支配の魔女",
+    reading: ["しはいのまじょ", "シハイノマジョ", "支配の魔女"],
+    class: "ウィッチ",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/支配の魔女.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "流麗なる竜人",
+    name: "流麗なる竜人",
+    reading: ["りゅうれいなるりゅうじん", "リュウレイナルリュウジン", "流麗なる竜人"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/流麗なる竜人.zip"
+  },
+  {
+    id: "ラグジュアリーマーメイド",
+    name: "ラグジュアリーマーメイド",
+    reading: ["らぐじゅありーまーめいど", "ラグジュアリーマーメイド", "ラグジュアリーマーメイド"],
+    class: "ドラゴン",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/ラグジュアリーマーメイド.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "悪辣の残滓",
+    name: "悪辣の残滓",
+    reading: ["あくらつのざんし", "アクラツノザンシ", "悪辣の残滓"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/悪辣の残滓.zip"
+  },
+  {
+    id: "アンデッドスレイヤー",
+    name: "アンデッドスレイヤー",
+    reading: ["あんでっどすれいやー", "アンデッドスレイヤー", "アンデッドスレイヤー"],
+    class: "ネクロマンサー",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/アンデッドスレイヤー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ノートリアスジェネラル",
+    name: "ノートリアスジェネラル",
+    reading: ["のーとりあすじぇねらる", "ノートリアスジェネラル", "ノートリアスジェネラル"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/ノートリアスジェネラル.zip"
+  },
+  {
+    id: "ヘルヴォール",
+    name: "ヘルヴォール",
+    reading: ["へるゔぉーる", "ヘルヴォール", "ヘルヴォール"],
+    class: "ヴァンパイア",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/ヘルヴォール.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ミュースの料理人",
+    name: "ミュースの料理人",
+    reading: ["みゅーすのりょうりにん", "ミュースノリョウリニン", "ミュースの料理人"],
+    class: "ビショップ",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/ミュースの料理人.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "インテレクトサプライヤー",
+    name: "インテレクトサプライヤー",
+    reading: ["いんてれくとさぷらいやー", "インテレクトサプライヤー", "インテレクトサプライヤー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/インテレクトサプライヤー.zip"
+  },
+  {
+    id: "ディテスタブルスタチュー",
+    name: "ディテスタブルスタチュー",
+    reading: ["でぃてすたぶるすたちゅー", "ディテスタブルスタチュー", "ディテスタブルスタチュー"],
+    class: "ネメシス",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/ディテスタブルスタチュー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "暗黒の御使い",
+    name: "暗黒の御使い",
+    reading: ["あんこくのみつかい", "アンコクノミツカイ", "暗黒の御使い"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/暗黒の御使い.zip"
+  },
+  {
+    id: "穢れし天兵",
+    name: "穢れし天兵",
+    reading: ["けがれしてんぺい", "ケガレシテンペイ", "穢れし天兵"],
+    class: "ニュートラル",
+    rarity: "Gold",
+    pack: "ORS",
+    zip: "voices/ORS/Gold/穢れし天兵.zip"
+  }
 ];

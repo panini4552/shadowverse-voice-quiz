db693cd5de64175eebab41376cfcc60e48bdf6b5
panini4552 <panini4552@gmail.com>
2025-12-08 13:29:04 +0900

    Update Bronze.js

--- data/ORS/Bronze.js
+++ data/ORS/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+// ORS 全レアリティ（Legendary / Gold / Silver / Bronze）統合データ
+// すべて既存のフォーマットに準拠
+
+export const ORS_All = [
+  // ---- ORS Bronze のデータ ----
+export const ORS_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ワイズベア",
+    name: "ワイズベア",
+    reading: ["わいずべあ", "ワイズベア"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/ワイズベア.zip"
+  },
+  {
+    id: "眺望のエルフ",
+    name: "眺望のエルフ",
+    reading: ["ちょうぼうのえるふ", "眺望のエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/眺望のエルフ.zip"
+  },
+  {
+    id: "キューティーキャット",
+    name: "キューティーキャット",
+    reading: ["きゅーてぃーきゃっと", "キューティーキャット"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/キューティーキャット.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ツインフェンサー",
+    name: "ツインフェンサー",
+    reading: ["ついんふぇんさー", "ツインフェンサー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/ツインフェンサー.zip"
+  },
+  {
+    id: "粉砕の勇士",
+    name: "粉砕の勇士",
+    reading: ["ふんさいのゆうし", "粉砕の勇士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/粉砕の勇士.zip"
+  },
+  {
+    id: "パレスナイト",
+    name: "パレスナイト",
+    reading: ["ぱれすないと", "パレスナイト"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/パレスナイト.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アーマードスノーマン",
+    name: "アーマードスノーマン",
+    reading: ["あーまーどすのーまん", "アーマードスノーマン"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/アーマードスノーマン.zip"
+  },
+  {
+    id: "トキシックウィッチ",
+    name: "トキシックウィッチ",
+    reading: ["ときしっくうぃっち", "トキシックウィッチ"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/トキシックウィッチ.zip"
+  },
+
+  // === ドラゴン ===
+  {
+    id: "リジッドフィッシュ",
+    name: "リジッドフィッシュ",
+    reading: ["りじっどふぃっしゅ", "リジッドフィッシュ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/リジッドフィッシュ.zip"
+  },
+  {
+    id: "サイレンスドラゴン",
+    name: "サイレンスドラゴン",
+    reading: ["さいれんすどらごん", "サイレンスドラゴン"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/サイレンスドラゴン.zip"
+  },
+  {
+    id: "人魚の守護者",
+    name: "人魚の守護者",
+    reading: ["にんぎょのしゅごしゃ", "人魚の守護者"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/人魚の守護者.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ネメアの獅子",
+    name: "ネメアの獅子",
+    reading: ["ねめあのしし", "ネメアの獅子"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/ネメアの獅子.zip"
+  },
+  {
+    id: "ペインフルソウル",
+    name: "ペインフルソウル",
+    reading: ["ぺいんふるそうる", "ペインフルソウル"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/ペインフルソウル.zip"
+  },
+  {
+    id: "屍の討滅者",
+    name: "屍の討滅者",
+    reading: ["しかばねのとうめつしゃ", "屍の討滅者"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/屍の討滅者.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "鍵盤の悪鬼",
+    name: "鍵盤の悪鬼",
+    reading: ["けんばんのあっき", "鍵盤の悪鬼"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/鍵盤の悪鬼.zip"
+  },
+  {
+    id: "開眼の魔人",
+    name: "開眼の魔人",
+    reading: ["かいがんのまじん", "開眼の魔人"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/開眼の魔人.zip"
+  },
+  {
+    id: "イビルコレクター",
+    name: "イビルコレクター",
+    reading: ["いびるこれくたー", "イビルコレクター"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/イビルコレクター.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "グルメアルミラージ",
+    name: "グルメアルミラージ",
+    reading: ["ぐるめあるみらーじ", "グルメアルミラージ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/グルメアルミラージ.zip"
+  },
+  {
+    id: "配達の翼人",
+    name: "配達の翼人",
+    reading: ["はいたつのよくじん", "配達の翼人"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/配達の翼人.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "エイミングガンナー",
+    name: "エイミングガンナー",
+    reading: ["えいみんぐがんなー", "エイミングガンナー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/エイミングガンナー.zip"
+  },
+  {
+    id: "夢想の人形",
+    name: "夢想の人形",
+    reading: ["むそうのにんぎょう", "夢想の人形"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/夢想の人形.zip"
+  },
+  {
+    id: "愛玩の操者",
+    name: "愛玩の操者",
+    reading: ["あいがんのそうしゃ", "愛玩の操者"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/愛玩の操者.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "グラスソルジャー",
+    name: "グラスソルジャー",
+    reading: ["ぐらすそるじゃー", "グラスソルジャー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/グラスソルジャー.zip"
+  },
+  {
+    id: "メドリングエンジェル",
+    name: "メドリングエンジェル",
+    reading: ["めどりんぐえんじぇる", "メドリングエンジェル"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "ORS",
+    zip: "voices/ORS/Bronze/メドリングエンジェル.zip"
+  }
+];
+
+// ---- ここまで ORS Bronze ----
 
 ];

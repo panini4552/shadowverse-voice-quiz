d5042fbe791e735bb239d8a62c7acf395f7c8a6d
panini4552 <panini4552@gmail.com>
2025-12-08 15:31:45 +0900

    Update Bronze.js

--- data/RSL/Bronze.js
+++ data/RSL/Bronze.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const RSL_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ツリーフェアリー",
+    name: "ツリーフェアリー",
+    reading: ["ツリーフェアリー", "つりーふぇありー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ツリーフェアリー.zip"
+  },
+  {
+    id: "プリズムバタフライ",
+    name: "プリズムバタフライ",
+    reading: ["プリズムバタフライ", "ぷりずむばたふらい"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/プリズムバタフライ.zip"
+  },
+  {
+    id: "勇猛なる妖精騎士",
+    name: "勇猛なる妖精騎士",
+    reading: ["勇猛なる妖精騎士", "ゆうもうなるようせいきし"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/勇猛なる妖精騎士.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "渾身の大剣使い",
+    name: "渾身の大剣使い",
+    reading: ["渾身の大剣使い", "こんしんのだいけんつかい"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/渾身の大剣使い.zip"
+  },
+  {
+    id: "還元の姫君",
+    name: "還元の姫君",
+    reading: ["還元の姫君", "かんげんのひめぎみ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/還元の姫君.zip"
+  },
+  {
+    id: "奏功の騎士・トリスタン",
+    name: "奏功の騎士・トリスタン",
+    reading: ["奏功の騎士・トリスタン", "そうこうのきしとりすたん"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/奏功の騎士・トリスタン.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "奮闘の召喚士・ベリル",
+    name: "奮闘の召喚士・ベリル",
+    reading: ["奮闘の召喚士・ベリル", "ふんとうのしょうかんしべりる"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/奮闘の召喚士・ベリル.zip"
+  },
+  {
+    id: "熟練の教師・ジル",
+    name: "熟練の教師・ジル",
+    reading: ["熟練の教師・ジル", "じゅくれんのきょうしじる"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/熟練の教師・ジル.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "スタティックドラゴン",
+    name: "スタティックドラゴン",
+    reading: ["スタティックドラゴン", "すたてぃっくどらごん"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/スタティックドラゴン.zip"
+  },
+  {
+    id: "有翼の聖魚",
+    name: "有翼の聖魚",
+    reading: ["有翼の聖魚", "ゆうよくのせいぎょ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/有翼の聖魚.zip"
+  },
+  {
+    id: "フォーチュンマーメイド",
+    name: "フォーチュンマーメイド",
+    reading: ["フォーチュンマーメイド", "ふぉーちゅんまーめいど"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/フォーチュンマーメイド.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ゴーストユーザー",
+    name: "ゴーストユーザー",
+    reading: ["ゴーストユーザー", "ごーすとゆーざー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ゴーストユーザー.zip"
+  },
+  {
+    id: "鏡面の魔獣",
+    name: "鏡面の魔獣",
+    reading: ["鏡面の魔獣", "きょうめんのまじゅう"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/鏡面の魔獣.zip"
+  },
+  {
+    id: "ノーブルマミー・メリエン",
+    name: "ノーブルマミー・メリエン",
+    reading: ["ノーブルマミー・メリエン", "のーぶるまみーめりえん"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ノーブルマミー・メリエン.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ハートブレイクデビル",
+    name: "ハートブレイクデビル",
+    reading: ["ハートブレイクデビル", "はーとぶれいくでびる"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ハートブレイクデビル.zip"
+  },
+  {
+    id: "ボルトデーモン",
+    name: "ボルトデーモン",
+    reading: ["ボルトデーモン", "ぼるとでーもん"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ボルトデーモン.zip"
+  },
+  {
+    id: "悪夢の使徒",
+    name: "悪夢の使徒",
+    reading: ["悪夢の使徒", "あくむのしと"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/悪夢の使徒.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "フォースヒーラー",
+    name: "フォースヒーラー",
+    reading: ["フォースヒーラー", "ふぉーすひーらー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/フォースヒーラー.zip"
+  },
+  {
+    id: "オーアタイガー",
+    name: "オーアタイガー",
+    reading: ["オーアタイガー", "おーあたいがー"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/オーアタイガー.zip"
+  },
+  {
+    id: "濃霧の祈祷師",
+    name: "濃霧の祈祷師",
+    reading: ["濃霧の祈祷師", "のうむのきとうし"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/濃霧の祈祷師.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "クレイジードール",
+    name: "クレイジードール",
+    reading: ["クレイジードール", "くれいじーどーる"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/クレイジードール.zip"
+  },
+  {
+    id: "有翼の人形・ミーア",
+    name: "有翼の人形・ミーア",
+    reading: ["有翼の人形・ミーア", "ゆうよくのにんぎょうみーあ"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/有翼の人形・ミーア.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "戦火の天使",
+    name: "戦火の天使",
+    reading: ["戦火の天使", "せんかのてんし"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/戦火の天使.zip"
+  },
+  {
+    id: "セイントギャル・アルディス",
+    name: "セイントギャル・アルディス",
+    reading: ["セイントギャル・アルディス", "せいんとぎゃるあるでぃす"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/セイントギャル・アルディス.zip"
+  },
+  {
+    id: "ヴァルキュリアランス",
+    name: "ヴァルキュリアランス",
+    reading: ["ヴァルキュリアランス", "ゔぁるきゅりあらんす"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "RSL",
+    zip: "voices/RSL/Bronze/ヴァルキュリアランス.zip"
+  }
 ];

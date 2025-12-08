487034655ca4a0bdaa09a0d0706756c50d6723ec
panini4552 <panini4552@gmail.com>
2025-12-08 11:57:59 +0900

    Update Silver.js

--- data/HOR/Silver.js
+++ data/HOR/Silver.js
@@ -1,15 +1,172 @@
-export const Classic_Legendary = [
+export const HOR_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "恵みの祈願・レイン",
+    name: "恵みの祈願・レイン",
+    reading: ["めぐみのきがん・れいん", "恵みの祈願・レイン"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/恵みの祈願・レイン.zip"
+  },
+  {
+    id: "オタカラトンボ",
+    name: "オタカラトンボ",
+    reading: ["おたからとんぼ", "オタカラトンボ"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/オタカラトンボ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "トワイライトエスコート",
+    name: "トワイライトエスコート",
+    reading: ["とわいらいとえすこーと", "トワイライトエスコート"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/トワイライトエスコート.zip"
+  },
+  {
+    id: "豪放の戦士・マルク",
+    name: "豪放の戦士・マルク",
+    reading: ["ごうほうのせんし・まるく", "豪放の戦士・マルク"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/豪放の戦士・マルク.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "マルチプルウィザード",
+    name: "マルチプルウィザード",
+    reading: ["まるちぷるうぃざーど", "マルチプルウィザード"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/マルチプルウィザード.zip"
+  },
+  {
+    id: "ラットマジシャン",
+    name: "ラットマジシャン",
+    reading: ["らっとまじしゃん", "ラットマジシャン"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/ラットマジシャン.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "フェザーズリザード",
+    name: "フェザーズリザード",
+    reading: ["ふぇざーずりざーど", "フェザーズリザード"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/フェザーズリザード.zip"
+  },
+  {
+    id: "クロードラゴニュート",
+    name: "クロードラゴニュート",
+    reading: ["くろーどらごにゅーと", "クロードラゴニュート"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/クロードラゴニュート.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "亡骸の復活者",
+    name: "亡骸の復活者",
+    reading: ["なきがらのふっかつしゃ", "亡骸の復活者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/亡骸の復活者.zip"
+  },
+  {
+    id: "反魂のヴァイオリニスト",
+    name: "反魂のヴァイオリニスト",
+    reading: ["はんごんのゔぁいおりにすと", "反魂のヴァイオリニスト"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/反魂のヴァイオリニスト.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "デビルナース",
+    name: "デビルナース",
+    reading: ["でびるなーす", "デビルナース"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/デビルナース.zip"
+  },
+  {
+    id: "闊歩する大蠍",
+    name: "闊歩する大蠍",
+    reading: ["かっぽするおおさそり", "闊歩する大蠍"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/闊歩する大蠍.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "ホーリーキャット",
+    name: "ホーリーキャット",
+    reading: ["ほーりーきゃっと", "ホーリーキャット"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/ホーリーキャット.zip"
+  },
+  {
+    id: "ターコイズシスター",
+    name: "ターコイズシスター",
+    reading: ["たーこいずしすたー", "ターコイズシスター"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/ターコイズシスター.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "紡ぎ繋ぐ糸・エヴァミア",
+    name: "紡ぎ繋ぐ糸・エヴァミア",
+    reading: ["つむぎつなぐいと・えゔぁみあ", "紡ぎ繋ぐ糸・エヴァミア"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/紡ぎ繋ぐ糸・エヴァミア.zip"
+  },
+  {
+    id: "フェイクウィング・エリーナ",
+    name: "フェイクウィング・エリーナ",
+    reading: ["ふぇいくうぃんぐ・えりーな", "フェイクウィング・エリーナ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/フェイクウィング・エリーナ.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ヴァルキリー・ブラウ",
+    name: "ヴァルキリー・ブラウ",
+    reading: ["ゔぁるきりー・ぶらう", "ヴァルキリー・ブラウ"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "HOR",
+    zip: "voices/HOR/Silver/ヴァルキリー・ブラウ.zip"
+  }
 ];

199e0a99e1339e5fdaf2262c741ba3bc86f88f67
panini4552 <panini4552@gmail.com>
2025-12-08 16:42:16 +0900

    Update Legendary.js

--- data/HOS/Legendary.js
+++ data/HOS/Legendary.js
@@ -1,15 +1,334 @@
-export const Classic_Legendary = [
+export const HOS_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "荊棘の乙女",
+    name: "荊棘の乙女",
+    reading: ["荊棘の乙女", "いばらのおとめ"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/荊棘の乙女.zip"
+  },
+  {
+    id: "穿孔の鉄腕・ダミアン",
+    name: "穿孔の鉄腕・ダミアン",
+    reading: ["穿孔の鉄腕・ダミアン", "せんこうのてつわんだみあん"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/穿孔の鉄腕・ダミアン.zip"
+  },
+  {
+    id: "大自然の抱擁・ラティカ",
+    name: "大自然の抱擁・ラティカ",
+    reading: ["大自然の抱擁・ラティカ", "だいしぜんのほうようらてぃか"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/大自然の抱擁・ラティカ.zip"
+  },
+  {
+    id: "光耀の狐火・セッカ",
+    name: "光耀の狐火・セッカ",
+    reading: ["光耀の狐火・セッカ", "こうようのきつねびせっか"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/光耀の狐火・セッカ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "ゲイレルル",
+    name: "ゲイレルル",
+    reading: ["ゲイレルル", "げいれるる"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/ゲイレルル.zip"
+  },
+  {
+    id: "王者の曙光・ベイリオン",
+    name: "王者の曙光・ベイリオン",
+    reading: ["王者の曙光・ベイリオン", "おうじゃのしょこうべいりおん"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/王者の曙光・ベイリオン.zip"
+  },
+  {
+    id: "リベリオンデュオ・バニー＆バロン",
+    name: "リベリオンデュオ・バニー＆バロン",
+    reading: ["リベリオンデュオ・バニー＆バロン", "りべりおんでゅおばにーあんどばろん"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/リベリオンデュオ・バニー＆バロン.zip"
+  },
+  {
+    id: "ドミネイトクイーン・ナハト・ナハト",
+    name: "ドミネイトクイーン・ナハト・ナハト",
+    reading: ["ドミネイトクイーン・ナハト・ナハト", "どみにいとくいーんなはとなはと"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/ドミネイトクイーン・ナハト・ナハト.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ローズパレスメイジ",
+    name: "ローズパレスメイジ",
+    reading: ["ローズパレスメイジ", "ろーずぱれすめいじ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/ローズパレスメイジ.zip"
+  },
+  {
+    id: "アクセルキーパー・マイザー",
+    name: "アクセルキーパー・マイザー",
+    reading: ["アクセルキーパー・マイザー", "あくせるきーぱーまいざー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/アクセルキーパー・マイザー.zip"
+  },
+  {
+    id: "蒼の先導者・テトラ",
+    name: "蒼の先導者・テトラ",
+    reading: ["蒼の先導者・テトラ", "あおのせんどうしゃてとら"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/蒼の先導者・テトラ.zip"
+  },
+  {
+    id: "メナスバランサー・ヴィンセント",
+    name: "メナスバランサー・ヴィンセント",
+    reading: ["メナスバランサー・ヴィンセント", "めなすばらんさーゔぃんせんと"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/メナスバランサー・ヴィンセント.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "クリスタルドラゴニュート",
+    name: "クリスタルドラゴニュート",
+    reading: ["クリスタルドラゴニュート", "くりすたるどらごにゅーと"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/クリスタルドラゴニュート.zip"
+  },
+  {
+    id: "呪縛の閃影・ヴァイディ",
+    name: "呪縛の閃影・ヴァイディ",
+    reading: ["呪縛の閃影・ヴァイディ", "じゅばくのせんえいヴぁいでぃ"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/呪縛の閃影・ヴァイディ.zip"
+  },
+  {
+    id: "フェイタルドラグーン・ローラ",
+    name: "フェイタルドラグーン・ローラ",
+    reading: ["フェイタルドラグーン・ローラ", "ふぇいたるどらぐーんろーら"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/フェイタルドラグーン・ローラ.zip"
+  },
+  {
+    id: "翔破の竜炎・ドラーク",
+    name: "翔破の竜炎・ドラーク",
+    reading: ["翔破の竜炎・ドラーク", "しょうはのりゅうえんどらーく"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/翔破の竜炎・ドラーク.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "アテム",
+    name: "アテム",
+    reading: ["アテム", "あてむ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/アテム.zip"
+  },
+  {
+    id: "紫紺の創造者・エンネア",
+    name: "紫紺の創造者・エンネア",
+    reading: ["紫紺の創造者・エンネア", "しこんのそうぞうしゃえんねあ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/紫紺の創造者・エンネア.zip"
+  },
+  {
+    id: "再生の親愛・ルベル",
+    name: "再生の親愛・ルベル",
+    reading: ["再生の親愛・ルベル", "さいせいのしんあいるべる"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/再生の親愛・ルベル.zip"
+  },
+  {
+    id: "現世の幽鬼・カゲロウ",
+    name: "現世の幽鬼・カゲロウ",
+    reading: ["現世の幽鬼・カゲロウ", "うつしよのゆうきかげろう"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/現世の幽鬼・カゲロウ.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "混濁の魔獅子",
+    name: "混濁の魔獅子",
+    reading: ["混濁の魔獅子", "こんだくのまじし"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/混濁の魔獅子.zip"
+  },
+  {
+    id: "真紅の挑戦者・モノ",
+    name: "真紅の挑戦者・モノ",
+    reading: ["真紅の挑戦者・モノ", "しんくのちょうせんしゃもの"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/真紅の挑戦者・モノ.zip"
+  },
+  {
+    id: "グラスピングスパイダー",
+    name: "グラスピングスパイダー",
+    reading: ["グラスピングスパイダー", "ぐらすぴんぐすぱいだー"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/グラスピングスパイダー.zip"
+  },
+  {
+    id: "連環の吸血鬼・アルザード",
+    name: "連環の吸血鬼・アルザード",
+    reading: ["連環の吸血鬼・アルザード", "れんかんのきゅうけつきあるざーど"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/連環の吸血鬼・アルザード.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "コンコルディア",
+    name: "コンコルディア",
+    reading: ["コンコルディア", "こんこるでぃあ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/コンコルディア.zip"
+  },
+  {
+    id: "ジャッジメントシスター・セリーナ",
+    name: "ジャッジメントシスター・セリーナ",
+    reading: ["ジャッジメントシスター・セリーナ", "じゃっじめんとしすたーせりーな"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/ジャッジメントシスター・セリーナ.zip"
+  },
+  {
+    id: "実直の参謀・ムニャール",
+    name: "実直の参謀・ムニャール",
+    reading: ["実直の参謀・ムニャール", "じっちょくのさんぼうむにゃーる"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/実直の参謀・ムニャール.zip"
+  },
+  {
+    id: "崩壊の聖女・リモニウム",
+    name: "崩壊の聖女・リモニウム",
+    reading: ["崩壊の聖女・リモニウム", "ほうかいのせいじょりもにうむ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/崩壊の聖女・リモニウム.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ターミナルガールズ",
+    name: "ターミナルガールズ",
+    reading: ["ターミナルガールズ", "たーみなるがーるず"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/ターミナルガールズ.zip"
+  },
+  {
+    id: "既知の掌握者・ベルフォメット",
+    name: "既知の掌握者・ベルフォメット",
+    reading: ["既知の掌握者・ベルフォメット", "きちのしょうあくしゃべるふぉめっと"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/既知の掌握者・ベルフォメット.zip"
+  },
+  {
+    id: "アドバンスホロウ・イルガンノ",
+    name: "アドバンスホロウ・イルガンノ",
+    reading: ["アドバンスホロウ・イルガンノ", "あどばんすほろういるがんの"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/アドバンスホロウ・イルガンノ.zip"
+  },
+  {
+    id: "イルエタニティ・アイシィレンドリング",
+    name: "イルエタニティ・アイシィレンドリング",
+    reading: ["イルエタニティ・アイシィレンドリング", "いるえたにてぃあいしいれんどりんぐ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/イルエタニティ・アイシィレンドリング.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "因果に抗う者・エレナ",
+    name: "因果に抗う者・エレナ",
+    reading: ["因果に抗う者・エレナ", "いんがにあらがうものえれな"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/因果に抗う者・エレナ.zip"
+  },
+  {
+    id: "寵愛の君",
+    name: "寵愛の君",
+    reading: ["寵愛の君", "ちょうあいのきみ"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/寵愛の君.zip"
+  },
+  {
+    id: "始原の機械神",
+    name: "始原の機械神",
+    reading: ["始原の機械神", "しげんのきかいしん"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOS",
+    zip: "voices/HOS/Legendary/始原の機械神.zip"
+  }
 ];

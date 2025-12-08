f1566ccf3e5ce68d4688ee0fd52e8181804fb7e7
panini4552 <panini4552@gmail.com>
2025-12-08 14:21:29 +0900

    Update Legendary.js

--- data/RSL/Legendary.js
+++ data/RSL/Legendary.js
@@ -1,15 +1,244 @@
-export const Classic_Legendary = [
+export const RSL_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ラフレシアプリンセス",
+    name: "ラフレシアプリンセス",
+    reading: ["らふれしあぷりんせす", "ラフレシアプリンセス"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/ラフレシアプリンセス.zip"
+  },
+  {
+    id: "統率の女王・バビー",
+    name: "統率の女王・バビー",
+    reading: ["とうそつのじょおう・ばびー", "統率の女王・バビー"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/統率の女王・バビー.zip"
+  },
+  {
+    id: "神秘の妖精竜",
+    name: "神秘の妖精竜",
+    reading: ["しんぴのようせいりゅう", "神秘の妖精竜"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/神秘の妖精竜.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "鎮定の王・シャルルマーニュ",
+    name: "鎮定の王・シャルルマーニュ",
+    reading: ["ちんていのおう・しゃるるまーにゅ", "鎮定の王・シャルルマーニュ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/鎮定の王・シャルルマーニュ.zip"
+  },
+  {
+    id: "パイレーツクイーン・アルビダ",
+    name: "パイレーツクイーン・アルビダ",
+    reading: ["ぱいれーつくいーん・あるびだ", "パイレーツクイーン・アルビダ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/パイレーツクイーン・アルビダ.zip"
+  },
+  {
+    id: "無欠の英雄・ローラン",
+    name: "無欠の英雄・ローラン",
+    reading: ["むけつのえいゆう・ろーらん", "無欠の英雄・ローラン"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/無欠の英雄・ローラン.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "アニマステイマー",
+    name: "アニマステイマー",
+    reading: ["あにますていまー", "アニマステイマー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/アニマステイマー.zip"
+  },
+  {
+    id: "ランペイジワード・ジンジャー",
+    name: "ランペイジワード・ジンジャー",
+    reading: ["らんぺいじわーど・じんじゃー", "ランペイジワード・ジンジャー"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/ランペイジワード・ジンジャー.zip"
+  },
+  {
+    id: "悲恋の魔女・メディア",
+    name: "悲恋の魔女・メディア",
+    reading: ["ひれんのまじょ・めでぃあ", "悲恋の魔女・メディア"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/悲恋の魔女・メディア.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "アブソーブドラゴン",
+    name: "アブソーブドラゴン",
+    reading: ["あぶそーぶどらごん", "アブソーブドラゴン"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/アブソーブドラゴン.zip"
+  },
+  {
+    id: "最果ての竜神",
+    name: "最果ての竜神",
+    reading: ["さいはてのりゅうじん", "最果ての竜神"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/最果ての竜神.zip"
+  },
+  {
+    id: "終幕の竜騎士",
+    name: "終幕の竜騎士",
+    reading: ["しゅうまくのりゅうきし", "終幕の竜騎士"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/終幕の竜騎士.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "グランドリッチ・ヘリオ",
+    name: "グランドリッチ・ヘリオ",
+    reading: ["ぐらんどりっち・へりお", "グランドリッチ・ヘリオ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/グランドリッチ・ヘリオ.zip"
+  },
+  {
+    id: "闇色の童話・ダークアリス",
+    name: "闇色の童話・ダークアリス",
+    reading: ["やみいろのどうわ・だーくありす", "闇色の童話・ダークアリス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/闇色の童話・ダークアリス.zip"
+  },
+  {
+    id: "蔓延る君主・ゼベット",
+    name: "蔓延る君主・ゼベット",
+    reading: ["はびこるくんしゅ・ぜべっと", "蔓延る君主・ゼベット"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/蔓延る君主・ゼベット.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "リベレイトヴァンパイア",
+    name: "リベレイトヴァンパイア",
+    reading: ["りべれいとゔぁんぱいあ", "リベレイトヴァンパイア"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/リベレイトヴァンパイア.zip"
+  },
+  {
+    id: "酷虐の悪魔・ミルナード",
+    name: "酷虐の悪魔・ミルナード",
+    reading: ["こくぎゃくのあくま・みるなーど", "酷虐の悪魔・ミルナード"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/酷虐の悪魔・ミルナード.zip"
+  },
+  {
+    id: "血の饗宴・ヴァルプルギスナハト",
+    name: "血の饗宴・ヴァルプルギスナハト",
+    reading: ["ちのきょうえん・ヴぁるぷるぎすなはと", "血の饗宴・ヴァルプルギスナハト"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/血の饗宴・ヴァルプルギスナハト.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "天命の執行者",
+    name: "天命の執行者",
+    reading: ["てんめいのしっこうしゃ", "天命の執行者"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/天命の執行者.zip"
+  },
+  {
+    id: "神託の決定者・ライナ",
+    name: "神託の決定者・ライナ",
+    reading: ["しんたくのけっていしゃ・らいな", "神託の決定者・ライナ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/神託の決定者・ライナ.zip"
+  },
+  {
+    id: "不動の神盾・ブローディア",
+    name: "不動の神盾・ブローディア",
+    reading: ["ふどうのしんじゅん・ぶろーでぃあ", "不動の神盾・ブローディア"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/不動の神盾・ブローディア.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "デスティニーセイント・イオ",
+    name: "デスティニーセイント・イオ",
+    reading: ["ですてぃにーせいんと・いお", "デスティニーセイント・イオ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/デスティニーセイント・イオ.zip"
+  },
+  {
+    id: "エレクトリックライノ",
+    name: "エレクトリックライノ",
+    reading: ["えれくとりっくらいの", "エレクトリックライノ"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/エレクトリックライノ.zip"
+  },
+  {
+    id: "天咆剣・ダイン",
+    name: "天咆剣・ダイン",
+    reading: ["てんほうけん・だいん", "天咆剣・ダイン"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/天咆剣・ダイン.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "刻限の調律・クロノス",
+    name: "刻限の調律・クロノス",
+    reading: ["こくげんのちょうりつ・くろのす", "刻限の調律・クロノス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "RSL",
+    zip: "voices/RSL/Legendary/刻限の調律・クロノス.zip"
+  }
 ];

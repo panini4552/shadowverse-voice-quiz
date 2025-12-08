bc5af82404161dd4a37c4b6b5f7642521f5c636b
panini4552 <panini4552@gmail.com>
2025-12-08 16:48:08 +0900

    Update Bronze.js

--- data/HOS/Bronze.js
+++ data/HOS/Bronze.js
@@ -1,15 +1,253 @@
-export const Classic_Legendary = [
+export const HOS_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "フィアフルトレント",
+    name: "フィアフルトレント",
+    reading: ["フィアフルトレント", "ふぃあふるとれんと"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/フィアフルトレント.zip"
+  },
+  {
+    id: "青葉の精霊",
+    name: "青葉の精霊",
+    reading: ["青葉の精霊", "あおばのせいれい"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/青葉の精霊.zip"
+  },
+  {
+    id: "スコルの守り人",
+    name: "スコルの守り人",
+    reading: ["スコルの守り人", "すこるのまもりびと"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/スコルの守り人.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "クラッシュウルフ",
+    name: "クラッシュウルフ",
+    reading: ["クラッシュウルフ", "くらっしゅうるふ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/クラッシュウルフ.zip"
+  },
+  {
+    id: "ペネトレイトフォックス",
+    name: "ペネトレイトフォックス",
+    reading: ["ペネトレイトフォックス", "ぺねとれいとふぉっくす"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/ペネトレイトフォックス.zip"
+  },
+  {
+    id: "無暁の戦士",
+    name: "無暁の戦士",
+    reading: ["無暁の戦士", "むぎょうのせんし"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/無暁の戦士.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ブーストウィッチ",
+    name: "ブーストウィッチ",
+    reading: ["ブーストウィッチ", "ぶーすとうぃっち"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/ブーストウィッチ.zip"
+  },
+  {
+    id: "緻密の占術師",
+    name: "緻密の占術師",
+    reading: ["緻密の占術師", "ちみつのせんじゅつし"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/緻密の占術師.zip"
+  },
+  {
+    id: "レヴィールの捜査官",
+    name: "レヴィールの捜査官",
+    reading: ["レヴィールの捜査官", "れゔぃーるのそうさかん"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/レヴィールの捜査官.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "轟雷のヴェロキラプトル",
+    name: "轟雷のヴェロキラプトル",
+    reading: ["轟雷のヴェロキラプトル", "ごうらいのヴぇろきらぷとる"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/轟雷のヴェロキラプトル.zip"
+  },
+  {
+    id: "腐乱のディロフォサウルス",
+    name: "腐乱のディロフォサウルス",
+    reading: ["腐乱のディロフォサウルス", "ふらんのでぃろふぉさうるす"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/腐乱のディロフォサウルス.zip"
+  },
+  {
+    id: "マーナガルムの偵察者",
+    name: "マーナガルムの偵察者",
+    reading: ["マーナガルムの偵察者", "まーながるむのていさつしゃ"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/マーナガルムの偵察者.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "スカルドローン",
+    name: "スカルドローン",
+    reading: ["スカルドローン", "すかるどろーん"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/スカルドローン.zip"
+  },
+  {
+    id: "機械鋸の魂伐者",
+    name: "機械鋸の魂伐者",
+    reading: ["機械鋸の魂伐者", "きかいのこぎりのたましいばさみ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/機械鋸の魂伐者.zip"
+  },
+  {
+    id: "夢想の骸骨",
+    name: "夢想の骸骨",
+    reading: ["夢想の骸骨", "むそうのがいこつ"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/夢想の骸骨.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "メタリックバット",
+    name: "メタリックバット",
+    reading: ["メタリックバット", "めたりっくばっと"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/メタリックバット.zip"
+  },
+  {
+    id: "ウェポンテイマー",
+    name: "ウェポンテイマー",
+    reading: ["ウェポンテイマー", "うぇぽんていまー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/ウェポンテイマー.zip"
+  },
+  {
+    id: "ローリングヴァンパイア",
+    name: "ローリングヴァンパイア",
+    reading: ["ローリングヴァンパイア", "ろーりんぐヴぁんぱいあ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/ローリングヴァンパイア.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "推進の翼人",
+    name: "推進の翼人",
+    reading: ["推進の翼人", "すいしんのよくじん"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/推進の翼人.zip"
+  },
+  {
+    id: "大空の旗手",
+    name: "大空の旗手",
+    reading: ["大空の旗手", "おおぞらのきしゅ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/大空の旗手.zip"
+  },
+  {
+    id: "エスカマリの援助者",
+    name: "エスカマリの援助者",
+    reading: ["エスカマリの援助者", "えすかまりのえんじょしゃ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/エスカマリの援助者.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "旧式機械兵",
+    name: "旧式機械兵",
+    reading: ["旧式機械兵", "きゅうしききかいへい"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/旧式機械兵.zip"
+  },
+  {
+    id: "メタルマンモス",
+    name: "メタルマンモス",
+    reading: ["メタルマンモス", "めたるまんもす"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/メタルマンモス.zip"
+  },
+  {
+    id: "蔓延る異形",
+    name: "蔓延る異形",
+    reading: ["蔓延る異形", "はびこるいぎょう"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/蔓延る異形.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "フロートライダー",
+    name: "フロートライダー",
+    reading: ["フロートライダー", "ふろーとらいだー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/フロートライダー.zip"
+  },
+  {
+    id: "レヴィールの悪党",
+    name: "レヴィールの悪党",
+    reading: ["レヴィールの悪党", "れゔぃーるのあくとう"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "HOS",
+    zip: "voices/HOS/Bronze/レヴィールの悪党.zip"
+  }
 ];

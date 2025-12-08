0ba8e575363711ed401c410d0533f7aec35fb103
panini4552 <panini4552@gmail.com>
2025-12-08 17:07:05 +0900

    Update Silver.js

--- data/Basic/Silver.js
+++ data/Basic/Silver.js
@@ -1,15 +1,188 @@
-export const Classic_Legendary = [
+export const Basic_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "メタルエルフメイジ",
+    name: "メタルエルフメイジ",
+    reading: ["メタルエルフメイジ", "めたるえるふめいじ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/メタルエルフメイジ.zip"
+  },
+  {
+    id: "ローズガーデンキーパー",
+    name: "ローズガーデンキーパー",
+    reading: ["ローズガーデンキーパー", "ろーずがーでんきーぱー"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/ローズガーデンキーパー.zip"
+  },
+  {
+    id: "マグナボタニスト",
+    name: "マグナボタニスト",
+    reading: ["マグナボタニスト", "まぐなぼたにすと"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/マグナボタニスト.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "フローラルフェンサー",
+    name: "フローラルフェンサー",
+    reading: ["フローラルフェンサー", "ふろーらるふぇんさー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/フローラルフェンサー.zip"
+  },
+  {
+    id: "セージコマンダー",
+    name: "セージコマンダー",
+    reading: ["セージコマンダー", "せーじこまんだー"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/セージコマンダー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "デモンフレイムメイジ",
+    name: "デモンフレイムメイジ",
+    reading: ["デモンフレイムメイジ", "でもんふれいむめいじ"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/デモンフレイムメイジ.zip"
+  },
+  {
+    id: "ライトニングシューター",
+    name: "ライトニングシューター",
+    reading: ["ライトニングシューター", "らいとにんぐしゅーたー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/ライトニングシューター.zip"
+  },
+  {
+    id: "フレイムデストロイヤー",
+    name: "フレイムデストロイヤー",
+    reading: ["フレイムデストロイヤー", "ふれいむですとろいやー"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/フレイムデストロイヤー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ドラゴンウォーリア",
+    name: "ドラゴンウォーリア",
+    reading: ["ドラゴンウォーリア", "どらごんうぉーりあ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/ドラゴンウォーリア.zip"
+  },
+  {
+    id: "ドレッドドラゴン",
+    name: "ドレッドドラゴン",
+    reading: ["ドレッドドラゴン", "どれっどどらごん"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/ドレッドドラゴン.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "悪戯なネクロマンサー",
+    name: "悪戯なネクロマンサー",
+    reading: ["悪戯なネクロマンサー", "いたずらなねくろまんさー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/悪戯なネクロマンサー.zip"
+  },
+  {
+    id: "地獄の解放者",
+    name: "地獄の解放者",
+    reading: ["地獄の解放者", "じごくのかいほうしゃ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/地獄の解放者.zip"
+  },
+  {
+    id: "アンデッドキング",
+    name: "アンデッドキング",
+    reading: ["アンデッドキング", "あんでっどきんぐ"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/アンデッドキング.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ワードローブレイダー",
+    name: "ワードローブレイダー",
+    reading: ["ワードローブレイダー", "わーどろーぶれいだー"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/ワードローブレイダー.zip"
+  },
+  {
+    id: "アビスビースト",
+    name: "アビスビースト",
+    reading: ["アビスビースト", "あびすびーすと"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/アビスビースト.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "鉄槌の僧侶",
+    name: "鉄槌の僧侶",
+    reading: ["鉄槌の僧侶", "てっついのそうりょ"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/鉄槌の僧侶.zip"
+  },
+  {
+    id: "高位プリースト",
+    name: "高位プリースト",
+    reading: ["高位プリースト", "こういぷりーすと"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/高位プリースト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "魔鋼の傀儡",
+    name: "魔鋼の傀儡",
+    reading: ["魔鋼の傀儡", "まこうのくぐつ"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/魔鋼の傀儡.zip"
+  },
+  {
+    id: "白黒翼・ネクス",
+    name: "白黒翼・ネクス",
+    reading: ["白黒翼・ネクス", "はっこくよく・ねくす"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "Basic",
+    zip: "voices/Basic/Silver/白黒翼・ネクス.zip"
+  }
 ];

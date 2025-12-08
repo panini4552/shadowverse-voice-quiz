cae21834344e3ae30c6dfb80fc6fd0cf3c33235f
panini4552 <panini4552@gmail.com>
2025-12-08 11:54:27 +0900

    Update Legendary.js

--- data/HOR/Legendary.js
+++ data/HOR/Legendary.js
@@ -1,15 +1,262 @@
-export const Classic_Legendary = [
+export const HOR_Legendary = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "使命の妖精姫・アリア",
+    name: "使命の妖精姫・アリア",
+    reading: ["しめいのようせいひめ・ありあ", "使命の妖精姫・アリア"],
     class: "エルフ",
     rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/使命の妖精姫・アリア.zip"
+  },
+  {
+    id: "オベロン",
+    name: "オベロン",
+    reading: ["おべろん", "オベロン"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/オベロン.zip"
+  },
+  {
+    id: "シャインクリスタリア・リリィ",
+    name: "シャインクリスタリア・リリィ",
+    reading: ["しゃいんくりすたりあ・りりぃ", "シャインクリスタリア・リリィ"],
+    class: "エルフ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/シャインクリスタリア・リリィ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "時空の提督・モニカ",
+    name: "時空の提督・モニカ",
+    reading: ["じくうのていとく・もにか", "時空の提督・モニカ"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/時空の提督・モニカ.zip"
+  },
+  {
+    id: "天下無双の武人",
+    name: "天下無双の武人",
+    reading: ["てんかむそうのぶじん", "天下無双の武人"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/天下無双の武人.zip"
+  },
+  {
+    id: "大海の征服者・ロジャー",
+    name: "大海の征服者・ロジャー",
+    reading: ["たいかいのせいふくしゃ・ろじゃー", "大海の征服者・ロジャー"],
+    class: "ロイヤル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/大海の征服者・ロジャー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "燦爛の巫女・パメラ",
+    name: "燦爛の巫女・パメラ",
+    reading: ["さんらんのみこ・ぱめら", "燦爛の巫女・パメラ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/燦爛の巫女・パメラ.zip"
+  },
+  {
+    id: "巡りの大魔術師・レヴィ",
+    name: "巡りの大魔術師・レヴィ",
+    reading: ["めぐりのだいまじゅつし・れゔぃ", "巡りの大魔術師・レヴィ"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/巡りの大魔術師・レヴィ.zip"
+  },
+  {
+    id: "セレブレイトゴーレム",
+    name: "セレブレイトゴーレム",
+    reading: ["せれぶれいとごーれむ", "セレブレイトゴーレム"],
+    class: "ウィッチ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/セレブレイトゴーレム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "黒蒼の絆・フォルテ＆ミリアム",
+    name: "黒蒼の絆・フォルテ＆ミリアム",
+    reading: ["こくそうのきずな・ふぉるてあんどみりあむ", "黒蒼の絆・フォルテ＆ミリアム"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/黒蒼の絆・フォルテ＆ミリアム.zip"
+  },
+  {
+    id: "邪智の竜・ファフニール",
+    name: "邪智の竜・ファフニール",
+    reading: ["じゃちのりゅう・ふぁふにーる", "邪智の竜・ファフニール"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/邪智の竜・ファフニール.zip"
+  },
+  {
+    id: "ワールドエンドシャーク",
+    name: "ワールドエンドシャーク",
+    reading: ["わーるどえんどしゃーく", "ワールドエンドシャーク"],
+    class: "ドラゴン",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/ワールドエンドシャーク.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "冥境の咆犬・ケルベロス",
+    name: "冥境の咆犬・ケルベロス",
+    reading: ["めいきょうのほうけん・けるべろす", "冥境の咆犬・ケルベロス"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/冥境の咆犬・ケルベロス.zip"
+  },
+  {
+    id: "ネバーデッドパイレーツ",
+    name: "ネバーデッドパイレーツ",
+    reading: ["ねばーでっどぱいれーつ", "ネバーデッドパイレーツ"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/ネバーデッドパイレーツ.zip"
+  },
+  {
+    id: "死の揺籠・ミント",
+    name: "死の揺籠・ミント",
+    reading: ["しのゆりかご・みんと", "死の揺籠・ミント"],
+    class: "ネクロマンサー",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/死の揺籠・ミント.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "バロン・グレートフィール",
+    name: "バロン・グレートフィール",
+    reading: ["ばろん・ぐれーとふぃーる", "バロン・グレートフィール"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/バロン・グレートフィール.zip"
+  },
+  {
+    id: "終焉の魔狼・フェンリル",
+    name: "終焉の魔狼・フェンリル",
+    reading: ["しゅうえんのまろう・ふぇんりる", "終焉の魔狼・フェンリル"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/終焉の魔狼・フェンリル.zip"
+  },
+  {
+    id: "転輪の魔神",
+    name: "転輪の魔神",
+    reading: ["てんりんのまじん", "転輪の魔神"],
+    class: "ヴァンパイア",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/転輪の魔神.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "白翼の慈愛・アイテール",
+    name: "白翼の慈愛・アイテール",
+    reading: ["はくよくのじあい・あいてーる", "白翼の慈愛・アイテール"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/白翼の慈愛・アイテール.zip"
+  },
+  {
+    id: "神託の旅立ち・ジャンヌ",
+    name: "神託の旅立ち・ジャンヌ",
+    reading: ["しんたくのたびだち・じゃんぬ", "神託の旅立ち・ジャンヌ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/神託の旅立ち・ジャンヌ.zip"
+  },
+  {
+    id: "アポストルアームズ",
+    name: "アポストルアームズ",
+    reading: ["あぽすとるあーむず", "アポストルアームズ"],
+    class: "ビショップ",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/アポストルアームズ.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "天来の救光・レトラフィア",
+    name: "天来の救光・レトラフィア",
+    reading: ["てんらいのきゅうこう・れとらふぃあ", "天来の救光・レトラフィア"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/天来の救光・レトラフィア.zip"
+  },
+  {
+    id: "混沌の終幕・シン",
+    name: "混沌の終幕・シン",
+    reading: ["こんとんのしゅうまく・しん", "混沌の終幕・シン"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/混沌の終幕・シン.zip"
+  },
+  {
+    id: "サテライトインベーダー",
+    name: "サテライトインベーダー",
+    reading: ["さてらいといんべーだー", "サテライトインベーダー"],
+    class: "ネメシス",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/サテライトインベーダー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ラグナロクの開幕・オーディン",
+    name: "ラグナロクの開幕・オーディン",
+    reading: ["らぐなろくのかいまく・おーでぃん", "ラグナロクの開幕・オーディン"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/ラグナロクの開幕・オーディン.zip"
+  },
+  {
+    id: "颶風の天槍・グリームニル",
+    name: "颶風の天槍・グリームニル",
+    reading: ["ぐふうのてんそう・ぐりーむにる", "颶風の天槍・グリームニル"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/颶風の天槍・グリームニル.zip"
+  },
+  {
+    id: "渾然の双極・フラム＝グラス",
+    name: "渾然の双極・フラム＝グラス",
+    reading: ["こんぜんのそうきょく・ふらむ＝ぐらす", "渾然の双極・フラム＝グラス"],
+    class: "ニュートラル",
+    rarity: "Legendary",
+    pack: "HOR",
+    zip: "voices/HOR/Legendary/渾然の双極・フラム＝グラス.zip"
+  }
 ];

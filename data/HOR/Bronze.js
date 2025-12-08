43e52612ef20adafbfc5038f2e296deb514d0c40
panini4552 <panini4552@gmail.com>
2025-12-08 11:58:16 +0900

    Replace Classic_Legendary with HOR_Bronze data

--- data/HOR/Bronze.js
+++ data/HOR/Bronze.js
@@ -1,15 +1,226 @@
-export const Classic_Legendary = [
+export const HOR_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "救援のエルフ",
+    name: "救援のエルフ",
+    reading: ["きゅうえんのえるふ", "救援のエルフ"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/救援のエルフ.zip"
+  },
+  {
+    id: "タイガーガール",
+    name: "タイガーガール",
+    reading: ["たいがーがーる", "タイガーガール"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/タイガーガール.zip"
+  },
+  {
+    id: "冷徹のダークエルフ",
+    name: "冷徹のダークエルフ",
+    reading: ["れいてつのだーくえるふ", "冷徹のダークエルフ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/冷徹のダークエルフ.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "刃の盗賊",
+    name: "刃の盗賊",
+    reading: ["やいばのとうぞく", "刃の盗賊"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/刃の盗賊.zip"
+  },
+  {
+    id: "灼熱の剛剣士",
+    name: "灼熱の剛剣士",
+    reading: ["しゃくねつのごうけんし", "灼熱の剛剣士"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/灼熱の剛剣士.zip"
+  },
+  {
+    id: "スイープフェルパー",
+    name: "スイープフェルパー",
+    reading: ["すいーぷふぇるぱー", "スイープフェルパー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/スイープフェルパー.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "インスタントテイマー",
+    name: "インスタントテイマー",
+    reading: ["いんすたんとていまー", "インスタントテイマー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/インスタントテイマー.zip"
+  },
+  {
+    id: "景観の魔導師",
+    name: "景観の魔導師",
+    reading: ["けいかんのまどうし", "景観の魔導師"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/景観の魔導師.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "凪の祈祷師",
+    name: "凪の祈祷師",
+    reading: ["なぎのきとうし", "凪の祈祷師"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/凪の祈祷師.zip"
+  },
+  {
+    id: "雷龍",
+    name: "雷龍",
+    reading: ["らいりゅう", "雷龍"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/雷龍.zip"
+  },
+  {
+    id: "海藻のスピリット",
+    name: "海藻のスピリット",
+    reading: ["かいそうのすぴりっと", "海藻のスピリット"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/海藻のスピリット.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "ブロッサムソウル",
+    name: "ブロッサムソウル",
+    reading: ["ぶろっさむそうる", "ブロッサムソウル"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ブロッサムソウル.zip"
+  },
+  {
+    id: "ゴーストフレンド",
+    name: "ゴーストフレンド",
+    reading: ["ごーすとふれんど", "ゴーストフレンド"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ゴーストフレンド.zip"
+  },
+  {
+    id: "ボーンマローダー",
+    name: "ボーンマローダー",
+    reading: ["ぼーんまろーだー", "ボーンマローダー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ボーンマローダー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ブラッドスミス",
+    name: "ブラッドスミス",
+    reading: ["ぶらっどすみす", "ブラッドスミス"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ブラッドスミス.zip"
+  },
+  {
+    id: "悪魔の小隊長",
+    name: "悪魔の小隊長",
+    reading: ["あくまのしょうたいちょう", "悪魔の小隊長"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/悪魔の小隊長.zip"
+  },
+  {
+    id: "マスクドジェントル",
+    name: "マスクドジェントル",
+    reading: ["ますくどじぇんとる", "マスクドジェントル"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/マスクドジェントル.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "仙狐の巡礼者",
+    name: "仙狐の巡礼者",
+    reading: ["せんこのじゅんれいしゃ", "仙狐の巡礼者"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/仙狐の巡礼者.zip"
+  },
+  {
+    id: "ラビットハーバリスト",
+    name: "ラビットハーバリスト",
+    reading: ["らびっとはーばりすと", "ラビットハーバリスト"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ラビットハーバリスト.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "フェアリーマリオネット",
+    name: "フェアリーマリオネット",
+    reading: ["ふぇありーまりおねっと", "フェアリーマリオネット"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/フェアリーマリオネット.zip"
+  },
+  {
+    id: "ギミックレオン",
+    name: "ギミックレオン",
+    reading: ["ぎみっくれおん", "ギミックレオン"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ギミックレオン.zip"
+  },
+  {
+    id: "ジェットウィールダー",
+    name: "ジェットウィールダー",
+    reading: ["じぇっとうぃーるだー", "ジェットウィールダー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/ジェットウィールダー.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "正義のブイ・フィーナ",
+    name: "正義のブイ・フィーナ",
+    reading: ["せいぎのぶい・ふぃーな", "正義のブイ・フィーナ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "HOR",
+    zip: "voices/HOR/Bronze/正義のブイ・フィーナ.zip"
+  }
 ];

408bc979ba81e0ef55bca1f0293a013227f32293
panini4552 <panini4552@gmail.com>
2025-12-08 14:27:49 +0900

    Update Silver.js

--- data/RSL/Silver.js
+++ data/RSL/Silver.js
@@ -1,15 +1,199 @@
-export const Classic_Legendary = [
+export const RSL_Silver = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "フォレストイーター",
+    name: "フォレストイーター",
+    reading: ["ふぉれすといーたー", "フォレストイーター"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/フォレストイーター.zip"
+  },
+  {
+    id: "ジャイアントホーネット",
+    name: "ジャイアントホーネット",
+    reading: ["じゃいあんとほーねっと", "ジャイアントホーネット"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/ジャイアントホーネット.zip"
+  },
+  {
+    id: "孤独の共振・ミツェル",
+    name: "孤独の共振・ミツェル",
+    reading: ["こどくのきょうしんみつぇる", "孤独の共振・ミツェル"],
+    class: "エルフ",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/孤独の共振・ミツェル.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "透過の剣士",
+    name: "透過の剣士",
+    reading: ["とうかのけんし", "透過の剣士"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/透過の剣士.zip"
+  },
+  {
+    id: "バイオレントパイレーツ",
+    name: "バイオレントパイレーツ",
+    reading: ["ばいおれんとぱいれーつ", "バイオレントパイレーツ"],
+    class: "ロイヤル",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/バイオレントパイレーツ.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "学園の竜術士",
+    name: "学園の竜術士",
+    reading: ["がくえんのりゅうじゅつし", "学園の竜術士"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/学園の竜術士.zip"
+  },
+  {
+    id: "マナリアウィズダム",
+    name: "マナリアウィズダム",
+    reading: ["まなりあうぃずだむ", "マナリアウィズダム"],
+    class: "ウィッチ",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/マナリアウィズダム.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "バイルドラゴン",
+    name: "バイルドラゴン",
+    reading: ["ばいるどらごん", "バイルドラゴン"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/バイルドラゴン.zip"
+  },
+  {
+    id: "狂気の呪剣・ベオウルフ",
+    name: "狂気の呪剣・ベオウルフ",
+    reading: ["きょうきのじゅけんべおうるふ", "狂気の呪剣・ベオウルフ"],
+    class: "ドラゴン",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/狂気の呪剣・ベオウルフ.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "イビルエクスポーザー",
+    name: "イビルエクスポーザー",
+    reading: ["いびるえくすぽーざー", "イビルエクスポーザー"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/イビルエクスポーザー.zip"
+  },
+  {
+    id: "死都の支配者",
+    name: "死都の支配者",
+    reading: ["しとのしはいしゃ", "死都の支配者"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/死都の支配者.zip"
+  },
+  {
+    id: "怨念のネクロマンサー・リュート",
+    name: "怨念のネクロマンサー・リュート",
+    reading: ["おんねんのねくろまんさーりゅーと", "怨念のネクロマンサー・リュート"],
+    class: "ネクロマンサー",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/怨念のネクロマンサー・リュート.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "水翼の悪魔",
+    name: "水翼の悪魔",
+    reading: ["すいよくのあくま", "水翼の悪魔"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/水翼の悪魔.zip"
+  },
+  {
+    id: "罪業の乙女・ブラー",
+    name: "罪業の乙女・ブラー",
+    reading: ["ざいごうのおとめぶらー", "罪業の乙女・ブラー"],
+    class: "ヴァンパイア",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/罪業の乙女・ブラー.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "聖域の大司教・カトレア",
+    name: "聖域の大司教・カトレア",
+    reading: ["せいいきのだいしきょうかとれあ", "聖域の大司教・カトレア"],
+    class: "ビショップ",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/聖域の大司教・カトレア.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "秘薬の創出者",
+    name: "秘薬の創出者",
+    reading: ["ひやくのそうしゅつしゃ", "秘薬の創出者"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/秘薬の創出者.zip"
+  },
+  {
+    id: "ヘヴンライダー",
+    name: "ヘヴンライダー",
+    reading: ["へう゛んらいだー", "ヘヴンライダー"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/ヘヴンライダー.zip"
+  },
+  {
+    id: "暗影の巫女・エネス",
+    name: "暗影の巫女・エネス",
+    reading: ["あんえいのみこえねす", "暗影の巫女・エネス"],
+    class: "ネメシス",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/暗影の巫女・エネス.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "レリックハンター",
+    name: "レリックハンター",
+    reading: ["れりっくはんたー", "レリックハンター"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/レリックハンター.zip"
+  },
+  {
+    id: "選別の天使・アルテア",
+    name: "選別の天使・アルテア",
+    reading: ["せんべつのてんしあるてあ", "選別の天使・アルテア"],
+    class: "ニュートラル",
+    rarity: "Silver",
+    pack: "RSL",
+    zip: "voices/RSL/Silver/選別の天使・アルテア.zip"
+  }
 ];

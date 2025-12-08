b8a8caa212f8880de96a862cc8b1544088f5b212
panini4552 <panini4552@gmail.com>
2025-12-08 17:20:10 +0900

    Update Bronze.js

--- data/Basic/Bronze.js
+++ data/Basic/Bronze.js
@@ -1,15 +1,415 @@
-export const Classic_Legendary = [
+export const Basic_Bronze = [
   // === エルフ ===
   {
-    id: "Ancient_Elf",
-    name: "エンシェントエルフ",
-    reading: ["えんしぇんとえるふ", "エンシェントエルフ"],
+    id: "ウォーターフェアリー",
+    name: "ウォーターフェアリー",
+    reading: ["ウォーターフェアリー", "うぉーたーふぇありー"],
     class: "エルフ",
-    rarity: "Legendary",
-    pack: "Classic",
-    zip: "voices/Classic/Legendary/Ancient_Elf.zip"
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ウォーターフェアリー.zip"
+  },
+  {
+    id: "フェアリーウィスパラー",
+    name: "フェアリーウィスパラー",
+    reading: ["フェアリーウィスパラー", "ふぇありーうぃすぱらー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/フェアリーウィスパラー.zip"
+  },
+  {
+    id: "エルフガード",
+    name: "エルフガード",
+    reading: ["エルフガード", "えるふがーど"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/エルフガード.zip"
+  },
+  {
+    id: "ダークエルフ・フォーレ",
+    name: "ダークエルフ・フォーレ",
+    reading: ["ダークエルフ・フォーレ", "だーくえるふ・ふぉーれ"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ダークエルフ・フォーレ.zip"
+  },
+  {
+    id: "大狼",
+    name: "大狼",
+    reading: ["大狼", "たいろう"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/大狼.zip"
+  },
+  {
+    id: "トレント",
+    name: "トレント",
+    reading: ["トレント", "とれんと"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/トレント.zip"
+  },
+  {
+    id: "エルフトラッカー",
+    name: "エルフトラッカー",
+    reading: ["エルフトラッカー", "えるふとらっかー"],
+    class: "エルフ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/エルフトラッカー.zip"
   },
 
+  // === ロイヤル ===
+  {
+    id: "クイックブレーダー",
+    name: "クイックブレーダー",
+    reading: ["クイックブレーダー", "くいっくぶれーだー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/クイックブレーダー.zip"
+  },
+  {
+    id: "オースレスナイト",
+    name: "オースレスナイト",
+    reading: ["オースレスナイト", "おーすれすないと"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/オースレスナイト.zip"
+  },
+  {
+    id: "クノイチエッグ",
+    name: "クノイチエッグ",
+    reading: ["クノイチエッグ", "くのいちえっぐ"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/クノイチエッグ.zip"
+  },
+  {
+    id: "アセンティックナイト",
+    name: "アセンティックナイト",
+    reading: ["アセンティックナイト", "あせんてぃっくないと"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/アセンティックナイト.zip"
+  },
+  {
+    id: "ホワイトジェネラル",
+    name: "ホワイトジェネラル",
+    reading: ["ホワイトジェネラル", "ほわいとじぇねらる"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ホワイトジェネラル.zip"
+  },
+  {
+    id: "ニンジャマスター",
+    name: "ニンジャマスター",
+    reading: ["ニンジャマスター", "にんじゃますたー"],
+    class: "ロイヤル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ニンジャマスター.zip"
+  },
 
+  // === ウィッチ ===
+  {
+    id: "ルーキーマジシャン・サミー",
+    name: "ルーキーマジシャン・サミー",
+    reading: ["ルーキーマジシャン・サミー", "るーきーまじしゃん・さみー"],
+    class: "ウィッチ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ルーキーマジシャン・サミー.zip"
+  },
 
+  // === ドラゴン ===
+  {
+    id: "ドラゴンライダー",
+    name: "ドラゴンライダー",
+    reading: ["ドラゴンライダー", "どらごんらいだー"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ドラゴンライダー.zip"
+  },
+  {
+    id: "古の飛竜",
+    name: "古の飛竜",
+    reading: ["古の飛竜", "いにしえのひりゅう"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/古の飛竜.zip"
+  },
+  {
+    id: "デスドラゴン",
+    name: "デスドラゴン",
+    reading: ["デスドラゴン", "ですどらごん"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/デスドラゴン.zip"
+  },
+  {
+    id: "ディザスタードラゴン",
+    name: "ディザスタードラゴン",
+    reading: ["ディザスタードラゴン", "でぃざすたーどらごん"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ディザスタードラゴン.zip"
+  },
+  {
+    id: "ドラゴンガード",
+    name: "ドラゴンガード",
+    reading: ["ドラゴンガード", "どらごんがーど"],
+    class: "ドラゴン",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ドラゴンガード.zip"
+  },
+
+  // === ネクロマンサー ===
+  {
+    id: "スパルトイサージェント",
+    name: "スパルトイサージェント",
+    reading: ["スパルトイサージェント", "すぱるといさーじぇんと"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/スパルトイサージェント.zip"
+  },
+  {
+    id: "スペクター",
+    name: "スペクター",
+    reading: ["スペクター", "すぺくたー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/スペクター.zip"
+  },
+  {
+    id: "ルーキーネクロマンサー",
+    name: "ルーキーネクロマンサー",
+    reading: ["ルーキーネクロマンサー", "るーきーねくろまんさー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ルーキーネクロマンサー.zip"
+  },
+  {
+    id: "エルダースパルトイソルジャー",
+    name: "エルダースパルトイソルジャー",
+    reading: ["エルダースパルトイソルジャー", "えるだーすぱるといそるじゃー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/エルダースパルトイソルジャー.zip"
+  },
+  {
+    id: "グレイブウェイカー",
+    name: "グレイブウェイカー",
+    reading: ["グレイブウェイカー", "ぐれいぶうぇいかー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/グレイブウェイカー.zip"
+  },
+  {
+    id: "ゴーストライダー",
+    name: "ゴーストライダー",
+    reading: ["ゴーストライダー", "ごーすとらいだー"],
+    class: "ネクロマンサー",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ゴーストライダー.zip"
+  },
+
+  // === ヴァンパイア ===
+  {
+    id: "ナイトメア",
+    name: "ナイトメア",
+    reading: ["ナイトメア", "ないとめあ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ナイトメア.zip"
+  },
+  {
+    id: "スウィートヴァンパイア",
+    name: "スウィートヴァンパイア",
+    reading: ["スウィートヴァンパイア", "すうぃーとヴぁんぱいあ"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/スウィートヴァンパイア.zip"
+  },
+  {
+    id: "狂気の処刑人",
+    name: "狂気の処刑人",
+    reading: ["狂気の処刑人", "きょうきのしょけいにん"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/狂気の処刑人.zip"
+  },
+  {
+    id: "ダークジェネラル",
+    name: "ダークジェネラル",
+    reading: ["ダークジェネラル", "だーくじぇねらる"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ダークジェネラル.zip"
+  },
+  {
+    id: "インプランサー",
+    name: "インプランサー",
+    reading: ["インプランサー", "いんぷらんさー"],
+    class: "ヴァンパイア",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/インプランサー.zip"
+  },
+
+  // === ビショップ ===
+  {
+    id: "スネークプリースト",
+    name: "スネークプリースト",
+    reading: ["スネークプリースト", "すねーくぷりーすと"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/スネークプリースト.zip"
+  },
+  {
+    id: "大修道女",
+    name: "大修道女",
+    reading: ["大修道女", "だいしゅうどうにょ"],
+    class: "ビショップ",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/大修道女.zip"
+  },
+
+  // === ネメシス ===
+  {
+    id: "ドールユーザー",
+    name: "ドールユーザー",
+    reading: ["ドールユーザー", "どーるゆーざー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ドールユーザー.zip"
+  },
+  {
+    id: "マキナサーヴァント",
+    name: "マキナサーヴァント",
+    reading: ["マキナサーヴァント", "まきなさーゔぁんと"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/マキナサーヴァント.zip"
+  },
+  {
+    id: "魔鉄の獅子",
+    name: "魔鉄の獅子",
+    reading: ["魔鉄の獅子", "まてつのしし"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/魔鉄の獅子.zip"
+  },
+  {
+    id: "オートマタナイト",
+    name: "オートマタナイト",
+    reading: ["オートマタナイト", "おーとまたないと"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/オートマタナイト.zip"
+  },
+  {
+    id: "トイソルジャー",
+    name: "トイソルジャー",
+    reading: ["トイソルジャー", "といそるじゃー"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/トイソルジャー.zip"
+  },
+  {
+    id: "鉄鋼の拳闘士",
+    name: "鉄鋼の拳闘士",
+    reading: ["鉄鋼の拳闘士", "てっこうのけんとうし"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/鉄鋼の拳闘士.zip"
+  },
+  {
+    id: "黒鉄の兵士",
+    name: "黒鉄の兵士",
+    reading: ["黒鉄の兵士", "こくてつのへいし"],
+    class: "ネメシス",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/黒鉄の兵士.zip"
+  },
+
+  // === ニュートラル ===
+  {
+    id: "ゴブリン",
+    name: "ゴブリン",
+    reading: ["ゴブリン", "ごぶりん"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ゴブリン.zip"
+  },
+  {
+    id: "ファイター",
+    name: "ファイター",
+    reading: ["ファイター", "ふぁいたー"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ファイター.zip"
+  },
+  {
+    id: "流浪の傭兵",
+    name: "流浪の傭兵",
+    reading: ["流浪の傭兵", "るろうのようへい"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/流浪の傭兵.zip"
+  },
+  {
+    id: "ゴリアテ",
+    name: "ゴリアテ",
+    reading: ["ゴリアテ", "ごりあて"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/ゴリアテ.zip"
+  },
+  {
+    id: "天剣の乙女",
+    name: "天剣の乙女",
+    reading: ["天剣の乙女", "てんけんのおとめ"],
+    class: "ニュートラル",
+    rarity: "Bronze",
+    pack: "Basic",
+    zip: "voices/Basic/Bronze/天剣の乙女.zip"
+  }
 ];
